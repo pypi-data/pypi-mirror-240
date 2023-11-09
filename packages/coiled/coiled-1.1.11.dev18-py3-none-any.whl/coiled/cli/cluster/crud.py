9cf313e91858edec099eb23c916224c811b3e2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:24 +0000

    Add package coiled/1.1.11.dev18/coiled-1.1.11.dev18-py3-none-any.whl

---
+++ packages/coiled/coiled-1.1.11.dev18-py3-none-any.whl/coiled/cli/cluster/crud.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import click
+
+import coiled
+
+from ..utils import CONTEXT_SETTINGS
+from .utils import find_cluster
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument("cluster", default="", required=False)
+@click.option(
+    "--account",
+    default=None,
+    help="Coiled account (uses default account if not specified)",
+)
+def stop(
+    cluster: str,
+    account: str | None,
+):
+    with coiled.Cloud(account=account) as cloud:
+        cluster_info = find_cluster(cloud, cluster)
+        cluster_id = cluster_info["id"]
+        cloud.delete_cluster(cluster_id, account)
