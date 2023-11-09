e6d00882b63c456baa49f3012cdb840c5dadcda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:06:01 +0000

    Add package C4dynamics/0.0.15/C4dynamics-0.0.15-py3-none-any.whl

---
+++ packages/C4dynamics/C4dynamics-0.0.15-py3-none-any.whl/C4dynamics/importc4d.py
@@ -0,0 +1,22 @@
+
+import sys, os, importlib
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+# 
+# load C4dynamics
+## 
+import C4dynamics as c4d
+
+prefix = 'C4dynamics'
+matching_modules = [module_name for module_name in sys.modules if module_name.startswith(prefix)]
+for module_name in matching_modules:
+    # print(module_name)
+    del sys.modules[module_name]
+
+# importlib.reload(c4d)
+import C4dynamics as c4d
+
+from C4dynamics.utils.params import * 
+from C4dynamics.utils.tictoc import * 
+
+
