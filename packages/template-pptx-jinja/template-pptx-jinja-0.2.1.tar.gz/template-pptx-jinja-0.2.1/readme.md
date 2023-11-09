975c5ed270063afcce7dd1b950612d1801292679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:33 +0000

    Add package template-pptx-jinja/0.2.1/template-pptx-jinja-0.2.1.tar.gz

---
+++ packages/template-pptx-jinja/template-pptx-jinja-0.2.1.tar.gz/template-pptx-jinja-0.2.1/readme.md
@@ -0,0 +1,23 @@
+# Template pptx Jinja2
+
+This is a Python3 package that can generate `.pptx` file from template using Jinja2 scheme.
+
+You can define your own Jinja2 filters.
+
+It can also replace picture.
+
+See `example.py` for example.
+
+See the PyPI package at https://pypi.org/project/template-pptx-jinja/0.2.1
+
+## Install
+
+Run
+
+    pip install template-pptx-jinja
+
+## Dev
+
+    python3 -m venv env
+    source env/bin/activate
+    pip install -r requirements.txt
---
+++ packages/template-pptx-jinja/template-pptx-jinja-0.2.1.tar.gz/template-pptx-jinja-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(filename):
+    current_working_directory = os.getcwd()
+    print(current_working_directory)
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        return myfile.read()
+
+
+setup(name='template-pptx-jinja',
+    version="0.2.1",
+    description='PowerPoint presentation builder from template using Jinja2',
+    long_description=read('readme.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Thykof/template-pptx-jinja',
+    author='Thykof',
+    author_email='thykof@protonmail.ch',
+    install_requires=read('requirements.txt').split(),
+    license='',
+    packages=find_packages(),
+    keywords=['powerpoint', 'ppt', 'pptx', 'template'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ]
+)
