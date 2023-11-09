b53c2028e34b39e39dc686e5b68ae540d0562f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:14 +0000

    Add package clickable-ut/8.0.1/clickable-ut-8.0.1.tar.gz

---
+++ packages/clickable-ut/clickable-ut-8.0.1.tar.gz/clickable-ut-8.0.1/clickable/commands/no_lock.py
@@ -0,0 +1,16 @@
+from clickable.logger import logger
+
+from .base import Command
+
+
+class NoLockCommand(Command):
+    def __init__(self):
+        super().__init__()
+        self.cli_conf.name = 'no-lock'
+        self.cli_conf.help_msg = "Turns off the device's display timeout"
+        self.command_conf.device_command = True
+
+    def run(self):
+        logger.info('Turning off device activity timeout')
+        command = 'gsettings set com.ubuntu.touch.system activity-timeout 0'
+        self.device.run_command(command, cwd=self.config.cwd)
