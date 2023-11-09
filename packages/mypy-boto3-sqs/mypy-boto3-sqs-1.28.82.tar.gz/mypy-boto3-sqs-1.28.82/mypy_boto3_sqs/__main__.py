8f4aaca9379fb7f2c33c59e820324070f6cbdd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 15:05:07 +0000

    Add package mypy-boto3-sqs/1.28.82/mypy-boto3-sqs-1.28.82.tar.gz

---
+++ packages/mypy-boto3-sqs/mypy-boto3-sqs-1.28.82.tar.gz/mypy-boto3-sqs-1.28.82/mypy_boto3_sqs/__main__.py
@@ -0,0 +1,39 @@
+"""
+Main CLI entrypoint.
+"""
+
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3.SQS 1.28.82\nVersion:         1.28.82\nBuilder version:"
+        " 7.19.1\nDocs:           "
+        " https://youtype.github.io/boto3_stubs_docs/mypy_boto3_sqs//\nBoto3 docs:     "
+        " https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#SQS\nOther"
+        " services:  https://pypi.org/project/boto3-stubs/\nChangelog:      "
+        " https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("1.28.82")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
