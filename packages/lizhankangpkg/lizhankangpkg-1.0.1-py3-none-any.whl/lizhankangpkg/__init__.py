acfdd652d560500e15c7f2d16c614b012861dc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:46 +0000

    Add package lizhankangpkg/1.0.1/lizhankangpkg-1.0.1-py3-none-any.whl

---
+++ packages/lizhankangpkg/lizhankangpkg-1.0.1-py3-none-any.whl/lizhankangpkg/__init__.py
@@ -0,0 +1,9 @@
+# coding:utf-8 
+# @Author : lizhankang
+# @Subject : 
+# @Time : 2023 - 11 - 09
+from .rsa import *
+
+def welcome():
+    for i in range(10):
+        print("welcome to lizhankangpkg 滴滴滴 ...")
