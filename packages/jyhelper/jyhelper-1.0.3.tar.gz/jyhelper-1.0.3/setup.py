9ccb56eb414441300780f0477c91e5ccd2b79b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:04:03 +0000

    Add package jyhelper/1.0.3/jyhelper-1.0.3.tar.gz

---
+++ packages/jyhelper/jyhelper-1.0.3.tar.gz/jyhelper-1.0.3/setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Time : 2023/11/08 18:38 
+# @Author : JY
+
+from setuptools import setup
+
+setup(
+    name='jyhelper',
+    version='1.0.3',
+    description='各种实用、常用的小函数、类',
+    author='JY',
+    author_email='your-email@example.com',
+    packages=['jyhelper'],
+    install_requires=[
+        'pymysql',
+    ],
+)
