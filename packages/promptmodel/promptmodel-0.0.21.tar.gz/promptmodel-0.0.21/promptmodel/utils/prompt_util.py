03dcf6d62eafe992e565a3b65954f1bb66e118ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:33 +0000

    Add package promptmodel/0.0.21/promptmodel-0.0.21.tar.gz

---
+++ packages/promptmodel/promptmodel-0.0.21.tar.gz/promptmodel-0.0.21/promptmodel/utils/prompt_util.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import yaml
+import asyncio
+from datetime import datetime
+from threading import Thread
+from typing import Any, Dict, Tuple, List, Union, Optional, Coroutine
+from litellm import token_counter
+
+from promptmodel.database.crud import (
+    get_latest_version_prompts,
+    get_deployed_prompts,
+)
+from promptmodel.utils.config_utils import read_config, upsert_config
+from promptmodel.utils import logger
+from promptmodel.promptmodel_init import CacheManager, update_deployed_db
+
+
+async def fetch_prompts(name) -> Tuple[List[Dict[str, str]], Dict[str, Any]]:
+    """fetch prompts.
+
+    Args:
+        name (str): name of promtpmodel
+
+    Returns:
+        Tuple[List[Dict[str, str]], Optional[Dict[str, Any]]]: (prompts, version_detail)
+    """
+    # Check dev_branch activate
+    config = read_config()
+    if "dev_branch" in config and config["dev_branch"]["initializing"] == True:
+        return [], {}
+    elif "dev_branch" in config and config["dev_branch"]["online"] == True:
+        # get prompt from local DB
+        prompt_rows, version_detail = get_latest_version_prompts(name)
+        if prompt_rows is None:
+            return [], {}
+        return [
+            {"role": prompt.role, "content": prompt.content} for prompt in prompt_rows
+        ], version_detail
+    else:
+        if (
+            "project" in config
+            and "use_cache" in config["project"]
+            and config["project"]["use_cache"] == True
+        ):
+            cache_manager = CacheManager()
+            # call update_local API in background task
+            cache_update_thread = Thread(
+                target=cache_manager.cache_update_background_task, args=(config,)
+            )
+            cache_update_thread.daemon = True
+            cache_update_thread.start()
+
+            # get prompt from local DB by ratio
+            prompt_rows, version_detail = get_deployed_prompts(name)
+            if prompt_rows is None:
+                return [], {}
+        else:
+            await update_deployed_db(config)  # wait for update local DB cache
+            prompt_rows, version_detail = get_deployed_prompts(name)
+            if prompt_rows is None:
+                return [], {}
+
+        return [
+            {"role": prompt.role, "content": prompt.content} for prompt in prompt_rows
+        ], version_detail
+
+
+def set_inputs_to_prompts(inputs: Dict[str, Any], prompts: List[Dict[str, str]]):
+    messages = [
+        {"content": prompt["content"].format(**inputs), "role": prompt["role"]}
+        for prompt in prompts
+    ]
+    return messages
+
+
+def num_tokens_for_messages(
+    messages: List[Dict[str, str]], model: str = "gpt-3.5-turbo-0613"
+) -> int:
+    tokens_per_message = 0
+    tokens_per_name = 0
+    if model.startswith("gpt-3.5-turbo"):
+        tokens_per_message = 4
+        tokens_per_name = -1
+
+    if model.startswith("gpt-4"):
+        tokens_per_message = 3
+        tokens_per_name = 1
+
+    if model.endswith("-0613") or model == "gpt-3.5-turbo-16k":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    sum = 0
+    sum = token_counter(model=model, messages=messages)
+    for message in messages:
+        sum += tokens_per_message
+        if "name" in message:
+            sum += tokens_per_name
+    return sum
+
+
+def num_tokens_from_functions_input(
+    functions: List[Any], model="gpt-3.5-turbo-0613"
+) -> int:
+    """Return the number of tokens used by a list of functions."""
+
+    num_tokens = 0
+    for function in functions:
+        function_tokens = token_counter(model=model, text=function["name"])
+        function_tokens += token_counter(model=model, text=function["description"])
+
+        if "parameters" in function:
+            parameters = function["parameters"]
+            if "properties" in parameters:
+                for properties_key in parameters["properties"]:
+                    function_tokens += token_counter(model=model, text=properties_key)
+                    v = parameters["properties"][properties_key]
+                    for field in v:
+                        if field == "type":
+                            function_tokens += 2
+                            function_tokens += token_counter(
+                                model=model, text=v["type"]
+                            )
+                        elif field == "description":
+                            function_tokens += 2
+                            function_tokens += token_counter(
+                                model=model, text=v["description"]
+                            )
+                        elif field == "enum":
+                            function_tokens -= 3
+                            for o in v["enum"]:
+                                function_tokens += 3
+                                function_tokens += token_counter(model=model, text=o)
+                        else:
+                            print(f"Warning: not supported field {field}")
+                function_tokens += 11
+
+        num_tokens += function_tokens
+
+    num_tokens += 12
+    return num_tokens
+
+
+def num_tokens_from_function_call_output(
+    function_call_output: Dict[str, str] = {}, model="gpt-3.5-turbo-0613"
+) -> int:
+    num_tokens = 1
+    num_tokens += token_counter(model=model, text=function_call_output["name"])
+    if "arguments" in function_call_output:
+        num_tokens += token_counter(model=model, text=function_call_output["arguments"])
+    return num_tokens
+
+
+import asyncio
+
+
+def run_async_in_sync(coro: Coroutine):
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:  # No running loop
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        result = loop.run_until_complete(coro)
+        # loop.close()
+        return result
+
+    if loop.is_running():
+        # nest_asyncio.apply already done
+        return loop.run_until_complete(coro)
+    else:
+        return loop.run_until_complete(coro)
