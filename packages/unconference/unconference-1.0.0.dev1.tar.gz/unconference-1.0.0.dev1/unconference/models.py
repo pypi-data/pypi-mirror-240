6edfd8420a5e07bde46fd63f518d845412c80933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:26 +0000

    Add package unconference/1.0.0.dev1/unconference-1.0.0.dev1.tar.gz

---
+++ packages/unconference/unconference-1.0.0.dev1.tar.gz/unconference-1.0.0.dev1/unconference/models.py
@@ -0,0 +1,80 @@
+from django.conf import settings
+from django.db import models
+
+from .settings import get_talk_choices
+
+
+class UnConferenceEvent(models.Model):
+    """
+    Model to store unconference events.
+    """
+
+    title = models.TextField(null=False, blank=False)
+    start = models.DateTimeField(null=False)
+    end = models.DateTimeField(null=False)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "UnConference event"
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class ScheduleTime(models.Model):
+    """
+    Timeslots for the event's schedule; allow_sessions should be set to True for any
+    timeslots where sessions can be schedule, and False for timeslots like lunch break.
+    """
+
+    unconference_event = models.ForeignKey(
+        UnConferenceEvent,
+        on_delete=models.PROTECT,
+    )
+    title = models.TextField(null=False, blank=False)
+    start = models.DateTimeField(null=False)
+    end = models.DateTimeField(null=False)
+    allow_sessions = models.BooleanField(default=False)
+    max_sessions = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} ({self.start} - {self.end})"
+
+
+class Room(models.Model):
+    """
+    Rooms where sessions can take place.
+    """
+
+    unconference_event = models.ForeignKey(
+        UnConferenceEvent,
+        on_delete=models.PROTECT,
+    )
+    title = models.TextField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Session(models.Model):
+    leaders = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=False)
+    schedule_time = models.ForeignKey(
+        ScheduleTime,
+        on_delete=models.PROTECT,
+        limit_choices_to={"allow_sessions": True},
+    )
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.PROTECT,
+    )
+    title = models.TextField(null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    session_type = models.PositiveSmallIntegerField(
+        choices=get_talk_choices(),
+        default=1,
+        help_text="The type of session.",
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.schedule_time})"
