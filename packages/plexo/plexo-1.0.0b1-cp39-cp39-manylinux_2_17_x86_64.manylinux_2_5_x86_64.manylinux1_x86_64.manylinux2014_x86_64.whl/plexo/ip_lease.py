0249fbe29bc1f9fd77476cabd0e3f4a348eba612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:05:44 +0000

    Add package plexo/1.0.0b1/plexo-1.0.0b1-cp39-cp39-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl

---
+++ packages/plexo/plexo-1.0.0b1-cp39-cp39-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl/plexo/ip_lease.py
@@ -0,0 +1,64 @@
+#  pyplexo
+#  Copyright © 2018-2023  Alecks Gates
+#
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyplexo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
+from pyrsistent import pmap
+
+from plexo.exceptions import IpLeaseExists, IpNotFound, IpNotLeased, IpsExhausted
+from plexo.typing import IPAddress, IPNetwork
+
+
+class IpLeaseManager:
+    def __init__(self, ip_cidr: IPNetwork):
+        self._ip_lease_map = pmap({ip_address: False for ip_address in ip_cidr})
+        self._available_ips = set(self._ip_lease_map.keys())
+
+    def lease_address(self, ip_address: IPAddress):
+        if ip_address not in self._ip_lease_map:
+            raise IpNotFound(f"ip_address {ip_address} not found")
+
+        if self._ip_lease_map[ip_address]:
+            raise IpLeaseExists(f"ip_address {ip_address} is already leased")
+
+        self._ip_lease_map = self._ip_lease_map.set(ip_address, True)
+        self._available_ips.discard(ip_address)
+
+        return ip_address
+
+    def release_address(self, ip_address: IPAddress):
+        if ip_address not in self._ip_lease_map:
+            raise IpNotFound(f"ip_address {ip_address} not found")
+
+        if not self._ip_lease_map[ip_address]:
+            raise IpNotLeased(f"ip_address {ip_address} is not leased")
+
+        self._ip_lease_map = self._ip_lease_map.set(ip_address, False)
+        self._available_ips.add(ip_address)
+
+        return ip_address
+
+    def address_is_leased(self, ip_address: IPAddress):
+        if ip_address not in self._ip_lease_map:
+            raise IpNotFound(f"ip_address {ip_address} not found")
+
+        return self._ip_lease_map[ip_address]
+
+    def get_address(self) -> IPAddress:
+        try:
+            ip_address = self._available_ips.pop()
+        except KeyError:
+            raise IpsExhausted("No more ip addresses are available")
+
+        return self.lease_address(ip_address)
---
+++ packages/plexo/plexo-1.0.0b1-cp39-cp39-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl/plexo/neuron/plexo_multicast_neuron.py
@@ -0,0 +1,40 @@
+#  pyplexo
+#  Copyright © 2018-2023  Alecks Gates
+#
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyplexo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
+from plexo.codec.plexo_multicast_codec import (
+    plexo_approval_codec,
+    plexo_heartbeat_codec,
+    plexo_preparation_codec,
+    plexo_promise_codec,
+    plexo_proposal_codec,
+    plexo_rejection_codec,
+)
+from plexo.namespace import plexo_namespace
+from plexo.neuron.neuron import Neuron
+from plexo.schema.plexo_multicast.plexo_approval import PlexoApproval
+from plexo.schema.plexo_multicast.plexo_heartbeat import PlexoHeartbeat
+from plexo.schema.plexo_multicast.plexo_preparation import PlexoPreparation
+from plexo.schema.plexo_multicast.plexo_promise import PlexoPromise
+from plexo.schema.plexo_multicast.plexo_proposal import PlexoProposal
+from plexo.schema.plexo_multicast.plexo_rejection import PlexoRejection
+
+
+approval_neuron = Neuron(PlexoApproval, plexo_namespace, plexo_approval_codec)
+heartbeat_neuron = Neuron(PlexoHeartbeat, plexo_namespace, plexo_heartbeat_codec)
+preparation_neuron = Neuron(PlexoPreparation, plexo_namespace, plexo_preparation_codec)
+promise_neuron = Neuron(PlexoPromise, plexo_namespace, plexo_promise_codec)
+proposal_neuron = Neuron(PlexoProposal, plexo_namespace, plexo_proposal_codec)
+rejection_neuron = Neuron(PlexoRejection, plexo_namespace, plexo_rejection_codec)
