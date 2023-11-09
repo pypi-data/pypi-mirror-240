4bb1adf83fcd4b30b4d6832e6f077a1f4bcac7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:27 +0000

    Add package firesoft/0.0.2/firesoft-0.0.2.tar.gz

---
+++ packages/firesoft/firesoft-0.0.2.tar.gz/firesoft-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='firesoft',
+    version='0.0.2',
+    packages=['firesoft', 'firesoft.utils'],
+    install_requires=[
+    ],
+    url='',
+    license='',
+    author='Salah Anwer',
+    author_email='salahanwer.dev@gmail.com',
+    description='',
+    python_requires=">=3",
+)
