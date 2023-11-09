bf216cfe84f6821affcea0d77f250f97911ed41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:33 +0000

    Add package pejecoins/0.0.2/pejecoins-0.0.2.tar.gz

---
+++ packages/pejecoins/pejecoins-0.0.2.tar.gz/pejecoins-0.0.2/src/pejecoins/example.py
@@ -0,0 +1,14 @@
+import __init__ as test
+
+resultado = test.suma(2,3)
+
+print(resultado)
+
+a = [   [-1, 0, 1],
+        [7, (3/2), -(13/2)],
+        [3, (1/2), -(5/2)]]
+b = [[11],
+        [-4],
+        [10]]
+
+resultado= test.multmat(a, b)
