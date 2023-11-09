31055e148002310d97a296ed83984ba5cd11693d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:01 +0000

    Add package qaspen/0.0.6a0/qaspen-0.0.6a0-py3-none-any.whl

---
+++ packages/qaspen/qaspen-0.0.6a0-py3-none-any.whl/qaspen/qaspen_types.py
@@ -0,0 +1,28 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from qaspen.table.base_table import BaseTable
+
+
+FromTable = typing.TypeVar(
+    "FromTable",
+    bound="BaseTable",
+)
+
+FieldType = typing.TypeVar(
+    "FieldType",
+)
+
+FieldDefaultType = typing.Union[
+    FieldType,
+    typing.Callable[
+        [],
+        typing.Union[FieldType, typing.Any],
+    ],
+    None,
+]
+
+CallableDefaultType = typing.Callable[
+    [],
+    FieldType,
+]
