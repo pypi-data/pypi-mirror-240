0b86d914e0022c17e38890ed6d62600c69642846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:27 +0000

    Add package phalcon/1.0.4/phalcon-1.0.4.tar.gz

---
+++ packages/phalcon/phalcon-1.0.4.tar.gz/phalcon-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read()
+
+setup(
+    name="phalcon",
+    version="1.0.4",
+    author_email="mreyeswilson@gmail.com",
+    author="Wilson Mendoza",
+    packages=find_packages(),
+    install_requires=[requirements],
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        phalcon=app.main:cli
+    """,
+    description="Command Line Interface for phalcon dev env"
+)
