8e014a1f3234cfeebacb0bb173f504be911072a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:02 +0000

    Add package labelu/0.8.1/labelu-0.8.1-py3-none-any.whl

---
+++ packages/labelu/labelu-0.8.1-py3-none-any.whl/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,15 @@
+(function () {
+  window.__frontend = {
+    version: '3.2.0-alpha.5',
+    deps: {
+      '@labelu/annotation': '1.0.1',
+      '@labelu/audio-annotator-react': '1.2.1',
+      '@labelu/components': '1.0.1',
+      '@labelu/components-react': '1.2.1',
+      '@labelu/formatter': '1.0.2',
+      '@labelu/interface': '1.2.1',
+      '@labelu/video-annotator-react': '1.2.1',
+      '@labelu/video-react': '1.2.1',
+    },
+  };
+})();
