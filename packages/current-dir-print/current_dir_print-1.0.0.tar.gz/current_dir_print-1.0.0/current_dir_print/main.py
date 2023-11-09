06bfcf04d0ad03d799a28cb861bb5ee47cf8d451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 15:05:04 +0000

    Add package current-dir-print/1.0.0/current_dir_print-1.0.0.tar.gz

---
+++ packages/current-dir-print/current_dir_print-1.0.0.tar.gz/current_dir_print-1.0.0/current_dir_print/main.py
@@ -0,0 +1,5 @@
+import os
+
+
+def get_current_dir():
+    print(os.getcwd())
---
+++ packages/current-dir-print/current_dir_print-1.0.0.tar.gz/current_dir_print-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="current_dir_print",
+    version="1.0.0",
+    description="Your package description",
+    author="safafdaf Name",
+    author_email="asfdsafasfa@email.com",
+    packages=find_packages(),
+    install_requires=[],
+)
