a9b72b4c8a1615767ce6338a21cf59373136f535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:41 +0000

    Add package Aishapackage/2.0.0/Aishapackage-2.0.0.tar.gz

---
+++ packages/Aishapackage/Aishapackage-2.0.0.tar.gz/Aishapackage-2.0.0/Paystackpy/__init__.py
@@ -0,0 +1,2 @@
+from .api import PaystackAPI
+from .api import Transaction
---
+++ packages/Aishapackage/Aishapackage-2.0.0.tar.gz/Aishapackage-2.0.0/Paystackpy/api/paystack.py
@@ -0,0 +1,5 @@
+from errors import APIError
+class PaystackAPI:
+    
+    def __init__(self, api_key: str) -> None:
+        self.api_key = api_key
