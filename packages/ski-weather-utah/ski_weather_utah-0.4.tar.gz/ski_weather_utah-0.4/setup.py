41cdabbc3b45b7a4748d703bf06b52c8a2f47aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 15:05:21 +0000

    Add package ski-weather-utah/0.4/ski_weather_utah-0.4.tar.gz

---
+++ packages/ski-weather-utah/ski_weather_utah-0.4.tar.gz/ski_weather_utah-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ski_weather_utah",
+    version="0.4",
+    description="A server-side app that sends daily updates for weather at select Utah ski resorts.",
+    author="Alex Calder",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "webdriver-manager",
+        "configparser",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ski-utah-config = ski_weather_utah.setup_script.py:run_setup_script',
+        ]
+    }
+)
