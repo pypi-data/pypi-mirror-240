35fedb26fe6e53913dcfd0fae94f3b327ebf0dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:08:14 +0000

    Add package mknodes/0.48.5/mknodes-0.48.5.tar.gz

---
+++ packages/mknodes/mknodes-0.48.5.tar.gz/mknodes-0.48.5/mknodes/mdlib/mdconverter.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Any, Literal
+
+import markdown
+
+from mknodes.utils import log
+
+
+logger = log.get_logger(__name__)
+
+
+class MdConverter(markdown.Markdown):
+    def __init__(
+        self,
+        extensions: Sequence[str | markdown.Extension] | None = None,
+        extension_configs: dict[str, dict[str, Any]] | None = None,
+        output_format: Literal["xhtml", "html"] = "html",
+        tab_length: int = 4,
+    ):
+        exts: Sequence[str | markdown.Extension] = [
+            "toc",
+            "tables",
+            # "fenced_code",
+            "pymdownx.emoji",
+            "md_in_html",
+            "attr_list",
+            "admonition",
+        ]
+        if extensions:
+            exts = list({*exts, *extensions})
+        super().__init__(
+            extensions=exts,
+            extension_configs=extension_configs or {},
+            output_format=output_format,
+            tab_length=tab_length,
+        )
+
+    def register_extensions(self, extensions: dict):
+        ext_names = list(extensions.keys())
+        self.registerExtensions(ext_names, extensions)
---
+++ packages/mknodes/mknodes-0.48.5.tar.gz/mknodes-0.48.5/mknodes/templatenodes/mkclasstable/__init__.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from mknodes.basenodes import mktable
+from mknodes.utils import layouts, log, reprhelpers
+
+
+logger = log.get_logger(__name__)
+
+
+class MkClassTable(mktable.MkTable):
+    """Node for a table showing info for a list of classes."""
+
+    def __init__(
+        self,
+        klasses: list[type] | set[type],
+        *,
+        layout: Literal["compact", "extended"] | layouts.Layout = "extended",
+        **kwargs,
+    ):
+        self.klasses = klasses
+        self.layout = layout
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return reprhelpers.get_nondefault_repr(self)
+
+    @property
+    def layouter(self):
+        match self.layout:
+            case "compact":
+                return layouts.CompactClassLayout(link_provider=self.ctx.links)
+            case "extended":
+                return layouts.ExtendedClassLayout(link_provider=self.ctx.links)
+            case layouts.Layout():
+                return self.layout
+            case _:
+                raise ValueError(self.layout)
+
+    @property
+    def data(self):
+        if not self.klasses:
+            return {}
+        layouter = self.layouter
+        data = [layouter.get_row_for(kls) for kls in self.klasses]
+        return {
+            k: [self.to_child_node(dic[k]) for dic in data]  # type: ignore[index]
+            for k in data[0]
+        }
+
+    @classmethod
+    def create_example_page(cls, page):
+        import mknodes as mk
+
+        klasses = [mk.MkTable, MkClassTable, mk.MkNav]
+        node_1 = MkClassTable(klasses=klasses, layout="compact")
+        node_2 = MkClassTable(klasses=klasses, layout="extended")
+        page += mk.MkReprRawRendered(node_1, header="### Compact layout")
+        page += mk.MkReprRawRendered(node_2, header="### Extended layout")
+
+
+if __name__ == "__main__":
+    table = MkClassTable(klasses=[mktable.MkTable], layout="extended")
+    print(table)
