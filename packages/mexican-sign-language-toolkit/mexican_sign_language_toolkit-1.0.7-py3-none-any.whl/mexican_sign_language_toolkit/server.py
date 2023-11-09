2e3713632e1b44dbfb4fd32d99fc2c6b541d0f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 15:05:05 +0000

    Add package mexican-sign-language-toolkit/1.0.7/mexican_sign_language_toolkit-1.0.7-py3-none-any.whl

---
+++ packages/mexican-sign-language-toolkit/mexican_sign_language_toolkit-1.0.7-py3-none-any.whl/mexican_sign_language_toolkit/server.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import numpy as np
+from mexican_sign_language_toolkit.pipeline import pipeline,VideoPipeline
+import glob 
+
+def predict(path):
+  predict_with_path = pipeline(VideoPipeline())
+  return predict_with_path(path)
+
+def launch_server():
+  demo = gr.Interface(
+    predict,
+    [gr.Video(sources="webcam", mirror_webcam=False)],
+    ["text"],
+    examples=[*glob.glob('**/*.mp4', recursive=True),*glob.glob('**/*.mkv', recursive=True)],
+    title="Mexican sign language recognizer demo",
+    description="Mexican sign language recognizer by Ricardo Morfin, Ernesto Lozano, and Carlos Sanchez",
+  )
+  demo.launch(share=True)
