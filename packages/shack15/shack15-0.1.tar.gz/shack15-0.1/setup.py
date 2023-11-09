1cc64a03b86b476a25bd991c14bd9ee971781bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:05:55 +0000

    Add package shack15/0.1/shack15-0.1.tar.gz

---
+++ packages/shack15/shack15-0.1.tar.gz/shack15-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shack15',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'embeddings-cli=embeddings.cli:main',
+        ],
+    },
+)
