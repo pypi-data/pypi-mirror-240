83af1acfdff991d57744435b412de5b4ff165276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:44 +0000

    Add package magpylib/4.4.1rc0/magpylib-4.4.1rc0.tar.gz

---
+++ packages/magpylib/magpylib-4.4.1rc0.tar.gz/magpylib-4.4.1rc0/magpylib/current/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `magpylib.current` subpackage contains all electric current classes.
+"""
+
+__all__ = ["Loop", "Line"]
+
+from magpylib._src.obj_classes.class_current_Loop import Loop
+from magpylib._src.obj_classes.class_current_Line import Line
