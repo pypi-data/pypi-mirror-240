749d1901c425af934938b67670d492eef0ad402a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:21 +0000

    Add package djangorestframework-jwt-v2/1.11.2/djangorestframework_jwt_v2-1.11.2-py2.py3-none-any.whl

---
+++ packages/djangorestframework-jwt-v2/djangorestframework_jwt_v2-1.11.2-py2.py3-none-any.whl/rest_framework_jwt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangorestframework-jwt-v2'
+__version__ = '1.11.2'
+__author__ = 'José Padilla'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Blimp LLC'
+
+# Version synonym
+VERSION = __version__
