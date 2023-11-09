7aab3e8097b268bd1dcdbe7b5282e805189b6129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:10 +0000

    Add package fastapi-sqlalchemy-toolkit/0.5.2/fastapi_sqlalchemy_toolkit-0.5.2.tar.gz

---
+++ packages/fastapi-sqlalchemy-toolkit/fastapi_sqlalchemy_toolkit-0.5.2.tar.gz/fastapi_sqlalchemy_toolkit-0.5.2/examples/app/models.py
@@ -0,0 +1,21 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from fastapi_sqlalchemy_toolkit import Base
+
+
+class Parent(Base):
+    title: Mapped[str]
+    slug: Mapped[str] = mapped_column(unique=True)
+
+    children: Mapped[list["Child"]] = relationship(back_populates="parent")
+
+
+class Child(Base):
+    title: Mapped[str]
+    slug: Mapped[str] = mapped_column(unique=True)
+
+    parent_id: Mapped[UUID] = mapped_column(ForeignKey("parent.id", ondelete="CASCADE"))
+    parent: Mapped[Parent] = relationship(back_populates="children")
---
+++ packages/fastapi-sqlalchemy-toolkit/fastapi_sqlalchemy_toolkit-0.5.2.tar.gz/fastapi_sqlalchemy_toolkit-0.5.2/fastapi_sqlalchemy_toolkit/__init__.py
@@ -0,0 +1,4 @@
+from .base_model import Base
+from .filters import FieldFilter, NullableQuery
+from .model_manager import ModelManager
+from .ordering import ordering_dep
