4e6e823204e9d94a10b899ebae3908976638797c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:45 +0000

    Add package aios3/0.2.1/aios3-0.2.1.tar.gz

---
+++ packages/aios3/aios3-0.2.1.tar.gz/aios3-0.2.1/scripts/compile_requirements.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Pin current dependencies versions.
+#
+
+rm -f requirements.txt
+rm -f requirements.dev.txt
+
+pip-compile requirements.dev.in
+pip-compile requirements.in
+
+python scripts/include_pyproject_requirements.py
