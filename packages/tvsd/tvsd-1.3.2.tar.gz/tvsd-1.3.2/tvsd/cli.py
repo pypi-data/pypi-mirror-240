6c3bd89c810aa320050d076f68e760518873453e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:21 +0000

    Add package tvsd/1.3.2/tvsd-1.3.2.tar.gz

---
+++ packages/tvsd/tvsd-1.3.2.tar.gz/tvsd-1.3.2/tvsd/cli.py
@@ -0,0 +1,267 @@
+"""TVSD CLI entry point, main app module."""
+import logging
+import os
+import shutil
+from typing import Optional
+
+import typer
+from rich import print as rprint
+
+from tvsd import __app_name__, __version__, app, state
+from tvsd._variables import state_base_path, state_series_dir, state_temp_base_path
+from tvsd.actions import list_shows_as_table, search_media_and_download
+from tvsd.config import apply_config, validate_config_file
+from tvsd.utils import video_in_dir
+
+
+def _version_callback(value: bool) -> None:
+    """
+    Print the current version of the application and exit.
+
+    Args:
+        value (bool): A boolean value indicating whether to print the version or not.
+
+    Returns:
+        None
+    """
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    _: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    ),
+    verbose: Optional[bool] = False,
+    series_dir: Optional[str] = typer.Option(
+        None,
+        "--series-dir",
+        "-sd",
+        help="Specify the series directory, overrides config file",
+    ),
+    base_path: Optional[str] = typer.Option(
+        None,
+        "--base-path",
+        "-bp",
+        help="Specify the base path, overrides config file",
+    ),
+) -> None:
+    """
+    Entry point for the TVSD CLI application.
+
+    This function initializes the config file before setting instance level.
+    It applies the config and sets the state of the application based on the provided options.
+
+    Args:
+        _: Optional[bool]: Show the application's version and exit.
+        verbose (Optional[bool], optional): Show verbose output. Defaults to False.
+        series_dir (Optional[str], optional): Specify the series directory, overrides config file. Defaults to None.
+        base_path (Optional[str], optional): Specify the base path, overrides config file. Defaults to None.
+    """
+    # initialize the config file before setting instance level
+    validate_config_file()
+    apply_config()
+
+    if verbose:
+        typer.echo("Will write verbose output")
+        state["verbose"] = True
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if series_dir:
+        state["series_dir"] = series_dir
+        logging.info("Series directory set to %s", series_dir)
+
+    if base_path:
+        state["base_path"] = base_path
+        logging.info("Base path set to %s", base_path)
+
+
+@app.command()
+def search(
+    query: str,
+    specials_only: bool = typer.Option(
+        False,
+        "--specials",
+        "-s",
+        help="Download Specials Only",
+    ),
+):
+    """Search for media and download
+
+    Args:
+        specials_only (Optional[bool], optional): Download only specials episode
+        query (str): query string
+    """
+    validate_config_file()
+    search_media_and_download(query, specials_only)
+
+
+@app.command()
+def clean_temp():
+    """
+    Cleans the temp directory.
+
+    This function validates the config file and then prompts the user to confirm
+    whether they want to delete all files in the temp directory. If the user confirms,
+    all files in the temp directory are deleted and a new empty directory is created.
+
+    Raises:
+        FileNotFoundError: If temp directory does not exist
+    """
+    validate_config_file()
+
+    try:
+        dir_content = os.listdir(state_temp_base_path())
+        if len(dir_content) == 0:
+            raise FileNotFoundError
+
+        rprint(f"{state_temp_base_path()} contents: ")
+        for item in dir_content:
+            rprint(f"  {item}")
+
+        confirm = typer.prompt(
+            text="Do you want to delete all files in temp directory?",
+            type=str,
+            default="n",
+        )
+        if confirm.capitalize() == "Y":
+            shutil.rmtree(state_temp_base_path(), ignore_errors=True)
+            os.mkdir(state_temp_base_path())
+            logging.info("All files deleted")
+
+    except FileNotFoundError:
+        logging.info("Temp directory %s does not exist", state_temp_base_path())
+
+
+@app.command()
+def list_shows():
+    """
+    List all shows in the database.
+
+    This function retrieves a list of all shows in the database and displays them in a table format.
+    """
+    list_shows_as_table(show_index=False)
+
+
+@app.command()
+def remove_show():
+    """List shows and remove selected show.
+
+    This function lists all the shows and their indices, prompts the user to select a show index to remove,
+    and then removes the selected show. If the user selects an invalid index or cancels the prompt, the function
+    aborts and raises a typer.Abort() exception.
+
+    Returns:
+        None
+    """
+    shows, num_rows = list_shows_as_table(show_index=True)
+
+    while True:
+        choice = typer.prompt(
+            "Select show index to remove", type=int, default=-1, show_default=False
+        )
+        if choice == -1:
+            typer.echo("No input received, exiting...")
+            raise typer.Abort()
+        if choice < num_rows:
+            if typer.confirm(f"Will remove {shows[choice]}. Are you sure?", abort=True):
+                typer.echo("Removing show: " + shows[choice])
+                shutil.rmtree(
+                    os.path.join(state_base_path(), state_series_dir(), shows[choice])
+                )
+                typer.echo("Show removed")
+            break
+
+        typer.echo("Option out of range, please try again")
+
+
+@app.command()
+def print_state():
+    """
+    Print the current state of the application.
+
+    This function prints the current state of the application, including all key-value pairs in the `state` dictionary.
+
+    Raises:
+        ConfigFileError: If the configuration file is invalid or missing.
+    """
+    validate_config_file()
+
+    for key, value in state.items():
+        typer.echo(f"{key}: {value}")
+
+
+@app.command()
+def clean_base(
+    interactive: bool = typer.Option(
+        False,
+        "--interactive",
+        "-i",
+        help="Interactive mode",
+    ),
+    greedy: bool = typer.Option(
+        False,
+        "--greedy",
+        "-g",
+        help="Remove directories without videos",
+    ),
+    target: str = typer.Option(
+        os.path.join(state_base_path(), state_series_dir()),
+        help="Target directory",
+    ),
+    _no_confirm: bool = typer.Option(
+        False,
+        "--no-confirm",
+    ),
+):
+    """
+    Remove empty directories in the base path
+
+    Args:
+        interactive (bool, optional): Whether to run in interactive mode. Defaults to False
+        greedy (bool, optional): Remove all directories without videos, even it they are not empty. Defaults to False.
+        target (str, optional): Target directory. Defaults to os.path.join(state_base_path(), state_series_dir()).
+        _no_confirm (bool, optional): Don't show prompt to confirm actions. Defaults to False.
+    """
+    validate_config_file()
+    if greedy and not _no_confirm:
+        typer.confirm(
+            "Greedy mode will remove directories without videos, even if they contain other content",
+            abort=True,
+        )
+
+    for root, dirs, _ in os.walk(target, topdown=False):
+        for name in dirs:
+            path = os.path.join(root, name)
+            if not os.listdir(path) and (
+                not interactive
+                or typer.confirm(f"Found Empty Directory, Remove {path}?")
+            ):
+                # empty dir
+                logging.info("Empty Directory, Removing %s", path)
+                shutil.rmtree(path)
+            elif (
+                greedy
+                and not video_in_dir(path)
+                and (
+                    not interactive
+                    or typer.confirm(
+                        f"Found Directory w/o videos and sub-dir, Remove {path}?"
+                    )
+                )
+            ):
+                # clean_base(interactive, greedy, target, _no_confirm=True)
+                # # not empty dir and no video, remove
+                logging.info(
+                    "Directory without video and and sub-dir, Removing %s", {path}
+                )
+                shutil.rmtree(path)
---
+++ packages/tvsd/tvsd-1.3.2.tar.gz/tvsd-1.3.2/tvsd/utils.py
@@ -0,0 +1,196 @@
+"""
+TVSD Utilities, contains useful functions 
+"""
+
+import logging
+import mimetypes
+import os
+import re
+from typing import List
+
+import cloudscraper
+import typer
+
+from tvsd._variables import state_base_path, state_series_dir, state_specials_dir
+
+# from tvsd.config import BASE_PATH, SPECIALS_DIR
+
+
+SCRAPER = cloudscraper.create_scraper(
+    delay=10,
+    # browser={
+    #     "custom": "ScraperBot/1.0",
+    # },
+    browser={
+        "browser": "chrome",
+        "platform": "windows",
+        "desktop": True,
+        "mobile": False,
+    },
+)
+
+
+def mkdir_if_no(check_dir: str, recursive: bool = True):
+    """Creates a directory if it does not exist
+
+    Args:
+        check_dir (str): Directory to check
+    """
+    if not os.path.isdir(check_dir):
+        if recursive:
+            os.makedirs(check_dir, exist_ok=True)
+        else:
+            try:
+                os.mkdir(check_dir)
+            except FileNotFoundError:
+                logging.error(
+                    "Parent directory does not exist, cannot create directory %s",
+                    {check_dir},
+                )
+
+
+def mkdir_from_base(check_dir: str):
+    """
+    Creates a directory from the base path.
+
+    Args:
+        check_dir (str): Directory to check.
+    """
+    mkdir_if_no(os.path.join(state_base_path(), check_dir))
+
+
+def relative_to_absolute_path(path: str) -> str:
+    """Converts a relative path to an absolute path
+
+    Args:
+        path (str): Relative path
+
+    Returns:
+        str: Absolute path
+    """
+    return os.path.join(state_base_path(), path)
+
+
+def file_exists(file_path: str) -> bool:
+    """Checks if a file exists
+
+    Args:
+        file_path (str): Path to file
+
+    Returns:
+        bool: True if file exists
+    """
+    return os.path.isfile(file_path)
+
+
+def file_exists_in_base(file_path: str) -> bool:
+    """Checks if a file exists in the base path
+
+    Args:
+        file_path (str): Path to file
+
+    Returns:
+        bool: True if file exists
+    """
+    return file_exists(os.path.join(state_base_path(), file_path))
+
+
+def get_next_specials_index(show_dir: str) -> int:
+    """
+    Gets the next specials index.
+
+    Args:
+        show_dir (str): Path to show directory.
+
+    Returns:
+        int: Next specials index.
+    """
+    existing_episode_indexes: List[int] = []
+    specials_dir = os.path.join(show_dir, state_specials_dir())
+    if os.path.exists(specials_dir):
+        for existing_special in os.listdir(specials_dir):
+            match = re.search(r"S00E(\d{2})", existing_special)
+            if match:
+                existing_episode_indexes.append(int(match.group(1)))
+        existing_episode_indexes.sort()
+        if existing_episode_indexes:
+            return existing_episode_indexes[-1]
+    return 0
+
+
+def check_dir_mounted(path: str) -> bool:
+    """
+    Check if a directory is mounted and exists.
+
+    Args:
+        path (str): The path to check.
+
+    Returns:
+        bool: True if the directory exists and is mounted, False otherwise.
+    """
+    if not os.path.ismount(path) and not os.path.isdir(path):
+        print(path, "has not been mounted yet. Exiting...")
+        return False
+    if not os.path.isdir(os.path.join(path, state_series_dir())):
+        print(f"{path} does not contain a {state_series_dir()} directory.")
+        if (
+            typer.prompt(
+                "Would you like to create it? [y/n]", default="y", show_default=True
+            ).capitalize()
+            == "Y"
+        ):
+            mkdir_if_no(os.path.join(path, state_series_dir()))
+        else:
+            return False
+    return True
+
+
+def _detect_video_mimetype(video_path: str) -> str:
+    """
+    Detects the mimetype of a video file.
+
+    Args:
+        video_path (str): The path to the video file.
+
+    Returns:
+        str: The mimetype of the video file.
+    """
+    mimetypes.init()
+    mimestart = mimetypes.guess_type(video_path)[0]
+
+    if mimestart != None:
+        mimestart = mimestart.split("/")[0]
+        return mimestart
+    return ""
+
+
+def is_video(video_path: str) -> bool:
+    """
+    Checks if a file is a video.
+
+    Args:
+        video_path (str): Path to video file.
+
+    Returns:
+        bool: True if the file is a video, False otherwise.
+    """
+    return _detect_video_mimetype(video_path) == "video"
+
+
+def video_in_dir(dir_path: str, recursive: bool = True) -> bool:
+    """Checks if a directory contains a video
+
+    Args:
+        dir_path (str): Path to directory
+        recursive (bool, optional): Whether to search recursively. Defaults to True.
+
+    Returns:
+        bool: True if directory contains a video
+    """
+    for file in os.listdir(dir_path):
+        if is_video(os.path.join(dir_path, file)):
+            return True
+        if os.path.isdir(os.path.join(dir_path, file)) and recursive:
+            if video_in_dir(os.path.join(dir_path, file)):
+                return True
+    return False
