8442ecb963c19ceaf91145c898666d74270e9a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:02:58 +0000

    Add package psij-python/0.9.1/psij-python-0.9.1.tar.gz

---
+++ packages/psij-python/psij-python-0.9.1.tar.gz/psij-python-0.9.1/src/psij/version.py
@@ -0,0 +1,5 @@
+"""This module stores the current version of this library."""
+
+# Use PEP 440 for this
+#: This is a global constant with the PSI/J Python version number
+VERSION = '0.9.1'
---
+++ packages/psij-python/psij-python-0.9.1.tar.gz/psij-python-0.9.1/tests/run_job.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# This is meant as a simple test file to check if psij was installed successfully
+
+import sys
+
+from psij import Job, JobExecutor, JobSpec
+
+
+if __name__ == '__main__':
+    name = 'local'
+    url = None
+
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        url = sys.argv[2]
+
+    jobs = list()
+    job = Job(JobSpec(executable='/bin/date'))
+    ex = JobExecutor.get_instance(name=name, url=url)
+    ex.submit(job)
+    jobs.append(job)
+    print('Job submitted')
+    status = job.wait()
+    print('Job done: {}'.format(status))
