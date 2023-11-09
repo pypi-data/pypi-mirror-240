e5e480cb230e0e9ae79e120146f8bdbff9ddfa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:23 +0000

    Add package hfirstdemo/0.1.0/hfirstdemo-0.1.0.tar.gz

---
+++ packages/hfirstdemo/hfirstdemo-0.1.0.tar.gz/hfirstdemo-0.1.0/hfirstdemo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def hello():
+    print("Hello from mypackage!")
---
+++ packages/hfirstdemo/hfirstdemo-0.1.0.tar.gz/hfirstdemo-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hfirstdemo',
+    version='0.1.0',
+    py_modules=['hfirstdemo'],
+    description='My first Python package',
+    author='xgsj-hyd',
+    author_email='455059546@qq.com',
+    url='https://github.com/hydong/hfirstdemo',
+    install_requires=[]
+)
