db9110d396f7ed10096b8e60bbfaa940855d4be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:35 +0000

    Add package astro-iam-api-client/1b1/astro_iam_api_client-1b1-py3-none-any.whl

---
+++ packages/astro-iam-api-client/astro_iam_api_client-1b1-py3-none-any.whl/astro_identity_and_access_management_iam_api_client/models/workspace_role_role.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class WorkspaceRoleRole(str, Enum):
+    WORKSPACE_AUTHOR = "WORKSPACE_AUTHOR"
+    WORKSPACE_MEMBER = "WORKSPACE_MEMBER"
+    WORKSPACE_OPERATOR = "WORKSPACE_OPERATOR"
+    WORKSPACE_OWNER = "WORKSPACE_OWNER"
+
+    def __str__(self) -> str:
+        return str(self.value)
