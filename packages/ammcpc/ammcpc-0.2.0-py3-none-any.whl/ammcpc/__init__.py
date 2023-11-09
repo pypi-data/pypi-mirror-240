519c3fe0aa54e558b0b8e77d650aac5f7f88a582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:30 +0000

    Add package ammcpc/0.2.0/ammcpc-0.2.0-py3-none-any.whl

---
+++ packages/ammcpc/ammcpc-0.2.0-py3-none-any.whl/ammcpc/__init__.py
@@ -0,0 +1,1 @@
+from .ammcpc import MediaConchPolicyCheckerCommand  # noqa: F401
---
+++ packages/ammcpc/ammcpc-0.2.0-py3-none-any.whl/ammcpc/__main__.py
@@ -0,0 +1,4 @@
+"""ammcpc.__main__: executed when ammcpc directory is called as a script."""
+from .ammcpc import main
+
+main()
