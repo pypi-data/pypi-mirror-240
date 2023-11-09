c7c3a6cdd2c29a09f19079c3afed88f074d68a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:30 +0000

    Add package autodistill/0.1.18/autodistill-0.1.18-py3-none-any.whl

---
+++ packages/autodistill/autodistill-0.1.18-py3-none-any.whl/autodistill/core/base_model.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+import supervision as sv
+
+from .ontology import Ontology
+
+
+@dataclass
+class BaseModel(ABC):
+    ontology: Ontology
+
+    def set_ontology(self, ontology: Ontology):
+        self.ontology = ontology
+
+    @abstractmethod
+    def predict(self, input: Any) -> Any:
+        pass
+
+    @abstractmethod
+    def label(
+        self, input_folder: str, extension: str = ".jpg", output_folder: str = None
+    ) -> sv.BaseDataset:
+        pass
