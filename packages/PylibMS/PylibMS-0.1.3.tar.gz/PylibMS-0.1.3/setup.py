3647e616ffae775600f7f8d2227a68003f930247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:06:07 +0000

    Add package PylibMS/0.1.3/PylibMS-0.1.3.tar.gz

---
+++ packages/PylibMS/PylibMS-0.1.3.tar.gz/PylibMS-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PylibMS",
+    version="0.1.3",
+    author="AbdyyEee",
+    packages=find_packages(),
+    license="mit",
+    description="Library for Nintendo's LMS file formats (MSBT, MSBP) for Nintendo 3DS and Wii U",
+    keywords=["nintendo", "msbf", "msbt", "msbp", "modding", "lms", "libmessagestudio"],
+    python_requires=">=3.10"
+)
