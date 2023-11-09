f88e29a5a453ad10ec7fba283376075d069d2005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:05:52 +0000

    Add package swarms/2.0.9/swarms-2.0.9.tar.gz

---
+++ packages/swarms/swarms-2.0.9.tar.gz/swarms-2.0.9/swarms/chunkers/__init__.py
@@ -0,0 +1,13 @@
+# from swarms.chunkers.base import BaseChunker
+# from swarms.chunkers.markdown import MarkdownChunker
+# from swarms.chunkers.text import TextChunker
+# from swarms.chunkers.pdf import PdfChunker
+
+
+# __all__ = [
+#     "BaseChunker",
+#     "ChunkSeparator",
+#     "MarkdownChunker",
+#     "TextChunker",
+#     "PdfChunker",
+# ]
