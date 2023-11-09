21b4656685215a3a1c47631d122992d1af787933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:06:22 +0000

    Add package pyearth/0.1.22/pyearth-0.1.22-py2.py3-none-any.whl

---
+++ packages/pyearth/pyearth-0.1.22-py2.py3-none-any.whl/pyearth/visual/formatter.py
@@ -0,0 +1,28 @@
+import matplotlib as mpl
+def log_formatter(x):
+    a, b = '{:.1e}'.format(x).split('e')
+    b = int(b)
+    return r'${} \times 10^{{{}}}$'.format(a, b)
+
+def float_formatter(x):
+    a = '{:.1f}'.format(x)
+    return a
+
+class MathTextSciFormatter(mpl.ticker.Formatter):
+    def __init__(self, fmt="%1.2e"):
+        self.fmt = fmt
+    def __call__(self, x, pos=None):
+        s = self.fmt % x
+        decimal_point = '.'
+        positive_sign = '+'
+        tup = s.split('e')
+        significand = tup[0].rstrip(decimal_point)
+        sign = tup[1][0].replace(positive_sign, '')
+        exponent = tup[1][1:].lstrip('0')
+        if exponent:
+            exponent = '10^{%s%s}' % (sign, exponent)
+        if significand and exponent:
+            s =  r'%s{\times}%s' % (significand, exponent)
+        else:
+            s =  r'%s%s' % (significand, exponent)
+        return "${}$".format(s)
