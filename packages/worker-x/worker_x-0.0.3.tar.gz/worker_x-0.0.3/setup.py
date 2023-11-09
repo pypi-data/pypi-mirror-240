9942e234718f482e5f4bebd82de37a633e27ede1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:26 +0000

    Add package worker-x/0.0.3/worker_x-0.0.3.tar.gz

---
+++ packages/worker-x/worker_x-0.0.3.tar.gz/worker_x-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="worker_x",
+    version="0.0.3",
+    author="Minhdz117",
+    description="Multi threading worker",
+    packages=find_packages(),
+    keywords=["worker","multi task","multi thread","threading"]
+)
