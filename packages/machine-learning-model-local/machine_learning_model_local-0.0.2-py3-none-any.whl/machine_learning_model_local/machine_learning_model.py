a77cccea1e18bd326fc0518e8c6dfab24691534f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:21 +0000

    Add package machine-learning-model-local/0.0.2/machine_learning_model_local-0.0.2-py3-none-any.whl

---
+++ packages/machine-learning-model-local/machine_learning_model_local-0.0.2-py3-none-any.whl/machine_learning_model_local/machine_learning_model.py
@@ -0,0 +1,10 @@
+# TODO: This is an example file which you should delete after implementing
+from dotenv import load_dotenv
+from circles_local_database_python.generic_crud import GenericCRUD
+load_dotenv()
+
+class MachineLearningModel(GenericCRUD):
+    def __init__(self) -> None:
+        pass
+   
+    
