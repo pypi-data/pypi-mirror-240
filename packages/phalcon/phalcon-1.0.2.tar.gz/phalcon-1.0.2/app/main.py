73f2af42f2d263bce00c8276fe735ec755544de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:12 +0000

    Add package phalcon/1.0.2/phalcon-1.0.2.tar.gz

---
+++ packages/phalcon/phalcon-1.0.2.tar.gz/phalcon-1.0.2/app/main.py
@@ -0,0 +1,35 @@
+import click
+from .funcs import is_docker_available
+from .services.phalcon_service import PhalconService
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def serve():
+    """Start a complete php server with phalcon installed."""
+    if not is_docker_available():
+        return
+    
+    svc = PhalconService()
+    svc.run_server()
+
+@cli.command()
+def add():
+    """Add new project to the server (sudo required)."""
+    svc = PhalconService()
+    svc.add_project_from_github()
+
+
+@cli.command()
+def remove():
+    """Remove an existing project from the server (sudo required)."""
+    svc = PhalconService()
+    svc.remove_project()
+
+@cli.command()
+def new():
+    """Creates a new phalcon project"""
+    svc = PhalconService()
+    svc.create_phalcon_project()
