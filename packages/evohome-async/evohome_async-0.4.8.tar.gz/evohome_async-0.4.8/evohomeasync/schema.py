da7f1b576caaf4340ee5ffb4030da12178db2158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:08:07 +0000

    Add package evohome-async/0.4.8/evohome_async-0.4.8.tar.gz

---
+++ packages/evohome-async/evohome_async-0.4.8.tar.gz/evohome_async-0.4.8/evohomeasync/schema.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""evohomeasync provides an async client for the *original* Evohome API."""
+
+from typing import Any
+
+# TCC config, status dicts
+_EvoLeafT = bool | float | int | str | list[str]  # Any
+_EvoDictT = dict[str, Any]  # '_EvoDictT' | _EvoLeafT]
+_EvoListT = list[_EvoDictT]
+_EvoSchemaT = _EvoDictT | _EvoListT
+
+# TCC identifiers
+_DhwIdT = str
+_GatewayIdT = str
+_LocationIdT = str
+_SystemIdT = str
+_ZoneIdT = str
+
+_DeviceIdT = _DhwIdT | _ZoneIdT
+
+# TCC other
+_ModeT = str
+_SystemModeT = str
+
+_TaskIdT = str
