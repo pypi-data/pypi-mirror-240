4e6924f1d10adb237e966268f823af9af65a628d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:08:26 +0000

    Add package magic-commit/0.4.0/magic-commit-0.4.0.tar.gz

---
+++ packages/magic-commit/magic-commit-0.4.0.tar.gz/magic-commit-0.4.0/README.md
@@ -0,0 +1,65 @@
+# Magic Commit! ✨
+
+`magic-commit` is a command-line tool for writing your commit messages. It pings OpenAI's GPT-3 API to generate commit messages based on your commit history.
+
+## Installation
+
+On Mac:
+```bash
+brew install magic-commit
+```
+
+## Setup
+
+You'll need to set up an OpenAI account and get an API key. You can do that on [OpenAI's website](https://platform.openai.com/account/api-keys).
+
+Once you have a key, add it to `magic-commit` like so:
+```bash
+magic-commit -k <your-key-here>
+```
+
+## Usage
+
+Running `magic-commit` is straightforward:
+```bash
+magic-commit # will run in your current directory
+>>> [your commit message] # automatically copied to your clipboard
+```
+
+To specify a directory:
+```bash
+magic-commit -d <path-to-git-repo>
+```
+
+For help:
+```bash
+magic-commit -h # or --help
+```
+
+## Specifying a model
+
+To change the model for this run:
+```bash
+magic-commit -m <model-name> 
+```
+
+To change the model globally:
+```bash
+magic-commit --set_model <model-name>
+```
+
+For models, note that:
+- You need to specify an [OpenAI GPT model](https://platform.openai.com/docs/models).
+    - e.g. `gpt-3.5-turbo-0301`, or `gpt-4`
+- Your account needs to have access to the model you specify.
+
+## Contributing
+
+Feel free to open an issue or submit a pull request.
+
+To publish a new version to PyPI:
+```bash
+pip install twine # if you don't have it already
+python setup.py sdist bdist_wheel # build the package
+twine upload dist/* # upload to PyPI
+```
---
+++ packages/magic-commit/magic-commit-0.4.0.tar.gz/magic-commit-0.4.0/magic_commit/magic_commit.py
@@ -0,0 +1,235 @@
+import os
+import subprocess
+from logging import Logger  # Just so the linter doesn't complain
+import threading
+import itertools
+import time
+import sys
+
+import openai
+from jinja2 import Environment, PackageLoader
+
+from .config import get_api_key, get_model, set_api_key, set_model
+
+
+class GitRepositoryError(Exception):
+    """Custom exception for Git repository errors."""
+
+    pass
+
+
+class OpenAIKeyError(Exception):
+    """Custom exception for OpenAI API key errors."""
+
+    pass
+
+
+def is_git_repository(directory: str) -> bool:
+    """
+    Check if a directory is a Git repository.
+
+    Parameters
+    ----------
+    directory : str
+        The path to check.
+
+    Returns
+    -------
+    bool
+        True if the directory is a Git repository, False otherwise.
+    """
+    print(directory)
+    return os.path.isdir(os.path.join(directory, ".git"))
+
+
+def run_git_diff(directory: str) -> str:
+    """
+    Run the `git diff --cached` command and return the result.
+
+    Parameters
+    ----------
+    directory : str
+        The directory to run the command in.
+
+    Returns
+    -------
+    str
+        The output of the command.
+    """
+    # TODO: Write unit tests for this function
+    return subprocess.run(
+        ["git", "diff", "--cached"], cwd=directory, capture_output=True, text=True
+    ).stdout
+
+
+def run_git_log(directory: str) -> subprocess.CompletedProcess:
+    """
+    Run 'git log' command in a specified directory.
+
+    Parameters
+    ----------
+    directory : str
+        The directory to run the command in.
+
+    Returns
+    -------
+    subprocess.CompletedProcess
+        The result of the subprocess run command.
+    """
+    return subprocess.run(
+        ["git", "log", "--pretty=%B", "-n", "10"],
+        cwd=directory,
+        capture_output=True,
+        text=True,
+    )
+
+
+def get_commit_messages(directory: str) -> str:
+    """
+    Fetch the last 10 commit messages from the Git repository.
+
+    Parameters
+    ----------
+    directory : str
+        The path to the Git repository.
+
+    Returns
+    -------
+    str
+        The commit messages.
+
+    Raises
+    ------
+    GitRepositoryError
+        If the specified directory is not a Git repository.
+    """
+    if not is_git_repository(directory):
+        raise GitRepositoryError("The specified directory is not a Git repository.")
+
+    result = run_git_log(directory)
+
+    if result.returncode != 0:
+        raise GitRepositoryError("Unable to get commit messages.")
+
+    return result.stdout
+
+
+def generate_commit_message(diff: str, api_key: str, model: str) -> str:
+    """
+    Generate a commit message.
+
+    Parameters
+    ----------
+    diff : str
+        The git diff to use.
+    api_key : str
+        The OpenAI API key.
+    model : str
+        The OpenAI GPT model to use.
+
+    Returns
+    -------
+    str
+        The generated commit message.
+
+    Raises
+    ------
+    OpenAIKeyError
+        If the OpenAI API key is not set.
+    """
+    # Perform guard check for the OpenAI API key
+    if not api_key:
+        raise OpenAIKeyError("OpenAI API key not set.")
+
+    # Generate the prompt
+    system_msg = render_template("", "system")
+    user_msg = render_template(diff, "user")
+
+    # Generate the commit message
+    messages = [
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": user_msg},
+    ]
+    openai.api_key = api_key
+    response = openai.ChatCompletion.create(model=model, messages=messages)
+
+    return response.choices[0].message.content.strip()
+
+
+def render_template(message: str, template_name: str) -> str:
+    """
+    Render the commit message template.
+
+    Parameters
+    ----------
+    message : str
+        The commit message to render.
+    template_name : str
+        The name of the template to use.
+
+    Returns
+    -------
+    str
+        The rendered commit message.
+    """
+    # TODO: Write unit tests for this function
+
+    # Render any escape sequences in the message
+    message.encode().decode("unicode_escape")
+
+    # Render and return the template
+    jinja = Environment(loader=PackageLoader("magic_commit", "templates")).get_template(
+        f"{template_name}.jinja"
+    )
+    return jinja.render(diff=message)
+
+def animate_loading(stop_event: threading.Event) -> None:
+    """
+    Display a simple loading animation with dots until a stop_event is set.
+
+    Parameters
+    ----------
+    stop_event : threading.Event
+        An event to signal when the animation should stop.
+    """
+    for c in itertools.cycle(['.  ', '.. ', '...']):
+        if stop_event.is_set():
+            sys.stdout.write('\r       \r')  # Clear the line
+            sys.stdout.flush()
+            break
+        sys.stdout.write('\rLoading' + c)
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+def run_magic_commit(directory: str, api_key: str, model: str) -> str:
+    """
+    Generate a commit message and return it.
+
+    Parameters
+    ----------
+    directory : str
+        The path to the Git repository.
+    api_key : str
+        The OpenAI API key.
+    model : str
+        The OpenAI GPT model to use.
+
+    Returns
+    -------
+    str
+        The generated commit message.
+    """
+    # Create a threading event to signal when to stop the loading animation
+    stop_loading = threading.Event()
+    loading_thread = threading.Thread(target=animate_loading, args=(stop_loading,))
+    loading_thread.start()
+
+    try:
+        diff = run_git_diff(directory)
+        commit_message = generate_commit_message(diff, api_key, model)
+    finally:
+        # Ensure the loading animation stops
+        stop_loading.set()
+        loading_thread.join()
+
+    return commit_message
