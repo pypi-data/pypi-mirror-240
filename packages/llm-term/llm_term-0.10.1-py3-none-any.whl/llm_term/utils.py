ba35b0b9fb5b7d62b784ad09785b07a4e2281c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:52 +0000

    Add package llm-term/0.10.1/llm_term-0.10.1-py3-none-any.whl

---
+++ packages/llm-term/llm_term-0.10.1-py3-none-any.whl/llm_term/utils.py
@@ -0,0 +1,178 @@
+"""
+Helper functions for the CLI
+"""
+
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from pathlib import Path
+from textwrap import dedent
+
+import click
+from openai import OpenAI, Stream
+from openai.types.chat import ChatCompletionChunk
+from prompt_toolkit import PromptSession
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.history import FileHistory
+from rich.columns import Columns
+from rich.console import Console
+from rich.live import Live
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.spinner import Spinner
+
+from llm_term.__about__ import __application__, __version__
+from llm_term.base import Message, NoPadding
+
+
+def print_header(console: Console, model: str) -> None:
+    """
+    Print the header
+    """
+    console.print(
+        Panel(
+            f"[bold red]{__application__} "
+            f"is a command line interface for OpenAI's Chat API[/bold red]",
+            title=f"[blue bold]{__application__} v{__version__}[/blue bold]",
+            subtitle=f"[yellow bold]{model}[/yellow bold]",
+            style="green bold",
+            expand=False,
+        ),
+    )
+    console.print("")
+
+
+def check_credentials(api_key: str) -> OpenAI:
+    """
+    Check the credentials
+    """
+    if api_key is not None:
+        client = OpenAI(api_key=api_key)
+        return client
+    else:
+        msg = (
+            "You must set the OPENAI_API_KEY environment variable "
+            "or set the `--api-key` / `-k` option"
+        )
+        raise click.ClickException(msg)
+
+
+def setup_system_message(model: str, message: str | None = None) -> Message:
+    """
+    Set up the system message
+    """
+    setup = f"""
+        You are a helpful AI assistant named {__application__}, built by OpenAI and based on
+        the model {model}. Help the user by responding to their request, the output should
+        be concise and always written in markdown. Ensure that all code blocks have the correct
+        language tag.
+
+        The current UTC date and time at the start of this conversation is
+        {datetime.now(tz=timezone.utc).isoformat()}
+        """
+    system_message = message or dedent(setup).strip()
+    return Message(role="system", content=system_message)
+
+
+def chat_session(
+    client: OpenAI,
+    console: Console,
+    system_message: Message,
+    model: str,
+    stream: bool,
+    panel: bool,
+    chat_message: str,
+    avatar: str,
+) -> None:
+    """
+    Chat session with ChatGPT
+    """
+    history_file = Path().home() / ".llm-term-history.txt"
+    history = FileHistory(str(history_file))
+    session: PromptSession = PromptSession(history=history, erase_when_done=True)
+    messages: list[Message] = [system_message]
+    if chat_message.strip() != "":
+        messages.append(Message(role="user", content=chat_message))
+    message_counter = 0
+    while True:
+        if message_counter == 0 and len(messages) == 2:  # noqa: PLR2004
+            console.print(f"{avatar}: {chat_message}")
+            history.append_string(chat_message)
+            if panel is False:
+                console.print("")
+                console.rule()
+        else:
+            text = session.prompt(f"{avatar}: ", auto_suggest=AutoSuggestFromHistory())
+            if not text:
+                continue
+            console.print(f"{avatar}: {text}")
+            if panel is False:
+                console.print("")
+                console.rule()
+            messages.append(Message(role="user", content=text))
+        console.print("")
+        streamed_message = print_response(
+            client=client,
+            console=console,
+            messages=messages,
+            model=model,
+            stream=stream,
+            panel=panel,
+        )
+        if panel is False:
+            console.print("")
+            console.rule()
+        messages.append(streamed_message)
+        console.print("")
+        message_counter += 1
+
+
+def print_response(
+    client: OpenAI, console: Console, messages: list[Message], model: str, stream: bool, panel: bool
+) -> Message:
+    """
+    Stream the response
+    """
+    panel_class = Panel if panel is True else NoPadding
+    if stream is False:
+        with Live(Spinner("aesthetic"), refresh_per_second=15, console=console, transient=True):
+            response = client.chat.completions.create(model=model, messages=messages, stream=False)
+            complete_response = response["choices"][0]["message"]["content"]
+            console.print(panel_class(Markdown(complete_response), title="🤖", title_align="left"))
+        message = Message(role="assistant", content=complete_response)
+    else:
+        response = client.chat.completions.create(model=model, messages=messages, stream=True)
+        message = render_streamed_response(response=response, console=console, panel=panel)
+    return message
+
+
+def render_streamed_response(response: Stream, console: Console, panel: bool) -> Message:
+    """
+    Render the streamed response and a spinner
+    """
+    panel_class = Panel if panel is True else NoPadding
+    complete_message = ""
+    rich_response = Columns(
+        [
+            panel_class(Markdown(complete_message), title="🤖", title_align="left"),
+            Spinner("aesthetic"),
+        ]
+    )
+    with Live(
+        rich_response,
+        refresh_per_second=15,
+        console=console,
+    ) as live:
+        chunk: ChatCompletionChunk
+        for chunk in response:
+            chunk_text = chunk.choices[0].delta.content or ""
+            complete_message += chunk_text
+            updated_response = Columns(
+                [
+                    panel_class(Markdown(complete_message), title="🤖", title_align="left"),
+                    Spinner("aesthetic"),
+                ]
+            )
+            live.update(updated_response)
+        live.update(panel_class(Markdown(complete_message), title="🤖", title_align="left"))
+    return Message(role="assistant", content=complete_message)
