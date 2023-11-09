51f7883a68b98fae73f7f0f70d8b6b5e3d193030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:29 +0000

    Add package MacTools/1.0.0rc4/MacTools-1.0.0.RC4.tar.gz

---
+++ packages/MacTools/MacTools-1.0.0.RC4.tar.gz/MacTools-1.0.0.RC4/setup.py
@@ -0,0 +1,22 @@
+# MacTools OUI Cache Setup
+
+from setuptools import setup, find_packages
+from mactools.version import __version__
+
+DESCRIPTION = 'MacTools',
+LONG_DESCRIPTION = 'MAC Address-focused library similar to `ipaddress`'
+
+setup(
+    name='MacTools',
+    author='Michael Buckley',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'textfsm',
+        'appdirs',
+        'httpx'
+    ],
+    keywords=['python','networking','network','mac','oui','ieee']
+)
