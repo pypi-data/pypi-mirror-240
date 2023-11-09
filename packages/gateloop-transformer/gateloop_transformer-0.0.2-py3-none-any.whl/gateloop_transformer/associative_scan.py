ae4f09aed20b86f3843a05b06668eef0794f9b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:43 +0000

    Add package gateloop-transformer/0.0.2/gateloop_transformer-0.0.2-py3-none-any.whl

---
+++ packages/gateloop-transformer/gateloop_transformer-0.0.2-py3-none-any.whl/gateloop_transformer/associative_scan.py
@@ -0,0 +1,106 @@
+# take from S5-pytorch repository
+# https://github.com/i404788/s5-pytorch/blob/74e2fdae00b915a62c914bf3615c0b8a4279eb84/s5/jax_compat.py#L51-L134
+
+# will be adapted to test out GateLoop on a small scale https://arxiv.org/abs/2311.01927
+
+import torch
+import torch.nn.functional as F
+from functools import partial
+from optree import tree_flatten, tree_unflatten
+
+def safe_map(f, *args):
+    args = list(map(list, args))
+    n = len(args[0])
+    for arg in args[1:]:
+        assert len(arg) == n, f'length mismatch: {list(map(len, args))}'
+    return list(map(f, *args))
+
+def slice_along_axis(start, end, stride=None, axis=0):
+    return (slice(None),) * axis + (slice(start, end, stride),)
+
+# Pytorch impl. of jax.lax.associative_scan
+
+def associative_scan(
+    operator,
+    elems,
+    axis = 0
+):
+    if not callable(operator):
+        raise TypeError("lax.associative_scan: fn argument should be callable.")
+
+    elems_flat, tree = tree_flatten(elems)
+
+    def combine(a_flat, b_flat):
+        # Lower `fn` to operate on flattened sequences of elems.
+        a = tree_unflatten(tree, a_flat)
+        b = tree_unflatten(tree, b_flat)
+        c = operator(a, b)
+        c_flat, _ = tree_flatten(c)
+        return c_flat
+
+    assert axis >= 0 or axis < elems_flat[0].ndim, "Axis should be within bounds of input"
+
+    num_elems = int(elems_flat[0].shape[axis])
+
+    if not all(int(elem.shape[axis]) == num_elems for elem in elems_flat[1:]):
+        raise ValueError('Array inputs to associative_scan must have the same '
+                         'first dimension. (saw: {})'
+                         .format([elem.shape for elem in elems_flat]))
+
+    def _scan(elems):
+        """Perform scan on `elems`."""
+        num_elems = elems[0].shape[axis]
+
+        if num_elems < 2:
+            return elems
+
+        # Combine adjacent pairs of elements.
+
+        reduced_elems = combine(
+          [elem[slice_along_axis(0, -1, stride=2, axis=axis)] for elem in elems],
+          [elem[slice_along_axis(1, None, stride=2, axis=axis)] for elem in elems])
+
+        # Recursively compute scan for partially reduced tensors.
+
+        odd_elems = _scan(reduced_elems)
+
+        if num_elems % 2 == 0:
+            even_elems = combine(
+                [e[slice_along_axis(0, -1, axis=axis)] for e in odd_elems],
+                [e[slice_along_axis(2, None, stride=2, axis=axis)] for e in elems])
+        else:
+            even_elems = combine(
+                odd_elems,
+                [e[slice_along_axis(2, None, stride=2, axis=axis)] for e in elems])
+
+        # The first element of a scan is the same as the first element
+        # of the original `elems`.
+
+        even_elems = [
+          torch.cat([elem[slice_along_axis(0, 1, axis=axis)], result], dim=axis)
+          if result.shape.numel() > 0 and elem.shape[axis] > 0 else
+          result if result.shape.numel() > 0 else
+          elem[slice_along_axis(0, 1, axis=axis)]  # Jax allows/ignores concat with 0-dim, Pytorch does not
+          for (elem, result) in zip(elems, even_elems)]
+
+        return list(safe_map(partial(_interleave, axis=axis), even_elems, odd_elems))
+
+    scans = _scan(elems_flat)
+
+    return tree_unflatten(tree, scans)
+
+def _interleave(a, b, axis):
+    # https://stackoverflow.com/questions/60869537/how-can-i-interleave-5-pytorch-tensors
+    if b_trunc := (a.shape[axis] == b.shape[axis] + 1):
+        pad = [0, 0] * b.ndim
+        pad[(b.ndim-axis-1)*2+1] = 1 # +1=always end of dim, pad-order is reversed so start is at end
+        b = F.pad(b, pad)
+
+    stacked = torch.stack([a, b], dim=axis+1)
+    interleaved = torch.flatten(stacked, start_dim=axis, end_dim=axis+1)
+
+    if not b_trunc:
+        return interleaved
+
+    # TODO: find torch alternative for slice_along axis for torch.jit.script to work
+    return interleaved[slice_along_axis(0, b.shape[axis]+a.shape[axis]-1, axis=axis)]
