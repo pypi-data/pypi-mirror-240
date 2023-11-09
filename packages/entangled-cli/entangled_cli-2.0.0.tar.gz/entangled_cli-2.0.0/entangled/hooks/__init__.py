49dc5f97e26b1db107835dcacd4d766425e2ca6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:06 +0000

    Add package entangled-cli/2.0.0/entangled_cli-2.0.0.tar.gz

---
+++ packages/entangled-cli/entangled_cli-2.0.0.tar.gz/entangled_cli-2.0.0/entangled/hooks/__init__.py
@@ -0,0 +1,30 @@
+import logging
+from .base import HookBase, PrerequisitesFailed
+from . import build, task, quarto_attributes
+from ..config import config
+from ..construct import construct
+
+
+hooks: dict[str, type[HookBase]] = {
+    "build": build.Hook,
+    "brei": task.Hook,
+    "quarto_attributes": quarto_attributes.Hook }
+
+
+def get_hooks() -> list[HookBase]:
+    active_hooks = []
+    for h in config.hooks:
+        if h in hooks:
+            try:
+                hook_cfg = construct(hooks[h].Config, config.hook.get(h, {}))
+                hook_instance = hooks[h](hook_cfg)
+                hook_instance.check_prerequisites()
+                active_hooks.append(hook_instance)
+            except PrerequisitesFailed as e:
+                logging.error("hook `%s`: %s", h, str(e))
+        else:
+            logging.error("no such hook available: `%s`", h)
+    return active_hooks
+
+
+__all__ = ["hooks", "PrerequisitesFailed"]
