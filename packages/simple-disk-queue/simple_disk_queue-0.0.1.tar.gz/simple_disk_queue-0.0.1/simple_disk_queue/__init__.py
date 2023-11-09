2ed13774847f468d52e6b8a0f74a4a88c6891c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 14:03:31 +0000

    Add package simple-disk-queue/0.0.1/simple_disk_queue-0.0.1.tar.gz

---
+++ packages/simple-disk-queue/simple_disk_queue-0.0.1.tar.gz/simple_disk_queue-0.0.1/simple_disk_queue/__init__.py
@@ -0,0 +1,15 @@
+"""Main functions for persist_to_disk.
+"""
+from .disk_queue import DiskQueue
+# Global config so user could set the root directory for persist ops
+
+
+
+
+__all__ = [
+    'DiskQueue',
+]
+
+__version__ = "0.0.1"
+__author__ = 'Zhen Lin'
+__credits__ = ''
