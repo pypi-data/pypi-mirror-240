748749f898a447acfb0bc9a60a679cb9c2de1feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:30 +0000

    Add package edfio/0.1.0/edfio-0.1.0-py3-none-any.whl

---
+++ packages/edfio/edfio-0.1.0-py3-none-any.whl/edfio/__init__.py
@@ -0,0 +1,22 @@
+from edfio.edf import (
+    AnonymizedDateError,
+    Edf,
+    EdfAnnotation,
+    EdfSignal,
+    Patient,
+    Recording,
+    read_edf,
+)
+
+__version__ = "0.1.0"
+
+
+__all__ = [
+    "AnonymizedDateError",
+    "Edf",
+    "EdfAnnotation",
+    "EdfSignal",
+    "Patient",
+    "Recording",
+    "read_edf",
+]
