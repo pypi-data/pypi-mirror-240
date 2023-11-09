b57114717004987022b57847f021e7986bfdc43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:27 +0000

    Add package dotdrop/1.14.0/dotdrop-1.14.0-py3-none-any.whl

---
+++ packages/dotdrop/dotdrop-1.14.0-py3-none-any.whl/dotdrop/comparator.py
@@ -0,0 +1,189 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2017, deadc0de6
+
+handle the comparison of two dotfiles
+"""
+
+import os
+import filecmp
+
+# local imports
+from dotdrop.logger import Logger
+from dotdrop.utils import must_ignore, uniq_list, diff, \
+    get_file_perm
+
+
+class Comparator:
+    """compare dotfiles helper"""
+
+    def __init__(self, diff_cmd='', debug=False,
+                 ignore_missing_in_dotdrop=False):
+        """constructor
+        @diff_cmd: diff command to use
+        @debug: enable debug
+        """
+        self.diff_cmd = diff_cmd
+        self.debug = debug
+        self.log = Logger(debug=self.debug)
+        self.ignore_missing_in_dotdrop = ignore_missing_in_dotdrop
+
+    def compare(self, local_path, deployed_path, ignore=None, mode=None):
+        """
+        diff local_path (dotdrop dotfile) and
+        deployed_path (destination file)
+        If mode is None, rights will be read from local_path
+        """
+        if not ignore:
+            ignore = []
+        local_path = os.path.expanduser(local_path)
+        deployed_path = os.path.expanduser(deployed_path)
+        self.log.dbg(f'comparing \"{local_path}\" and \"{deployed_path}\"')
+        self.log.dbg(f'ignore pattern(s): {ignore}')
+
+        # test type of file
+        if os.path.isdir(local_path) and not os.path.isdir(deployed_path):
+            ret = f'\"{local_path}\" is a dir'
+            ret += f' while \"{deployed_path}\" is a file\n'
+            return ret
+        if not os.path.isdir(local_path) and os.path.isdir(deployed_path):
+            ret = f'\"{local_path}\" is a file'
+            ret += f' while \"{deployed_path}\" is a dir\n'
+            return ret
+
+        # test content
+        if not os.path.isdir(local_path):
+            self.log.dbg(f'{local_path} is a file')
+            ret = self._comp_file(local_path, deployed_path, ignore)
+            if not ret:
+                ret = self._comp_mode(local_path, deployed_path, mode=mode)
+            return ret
+
+        self.log.dbg(f'\"{local_path}\" is a directory')
+
+        ret = self._comp_dir(local_path, deployed_path, ignore)
+        if not ret:
+            ret = self._comp_mode(local_path, deployed_path, mode=mode)
+        return ret
+
+    def _comp_mode(self, local_path, deployed_path, mode=None):
+        """
+        compare mode
+        If mode is None, rights will be read on local_path
+        """
+        local_mode = mode
+        if not local_mode:
+            local_mode = get_file_perm(local_path)
+        deployed_mode = get_file_perm(deployed_path)
+        if local_mode == deployed_mode:
+            return ''
+        msg = f'mode differ {local_path} ({local_mode:o}) '
+        msg += f'and {deployed_path} ({deployed_mode:o})'
+        self.log.dbg(msg)
+        ret = f'modes differ for {deployed_path} '
+        ret += f'({deployed_mode:o}) vs {local_mode:o}\n'
+        return ret
+
+    def _comp_file(self, local_path, deployed_path, ignore):
+        """compare a file"""
+        self.log.dbg(f'compare file {local_path} with {deployed_path}')
+        if (self.ignore_missing_in_dotdrop and not
+                os.path.exists(local_path)) \
+                or must_ignore([local_path, deployed_path], ignore,
+                               debug=self.debug):
+            self.log.dbg(f'ignoring diff {local_path} and {deployed_path}')
+            return ''
+        return self._diff(local_path, deployed_path)
+
+    def _comp_dir(self, local_path, deployed_path, ignore):
+        """compare a directory"""
+        self.log.dbg(f'compare directory {local_path} with {deployed_path}')
+        if not os.path.exists(deployed_path):
+            return ''
+        ign_missing = self.ignore_missing_in_dotdrop and not \
+            os.path.exists(local_path)
+        paths = [local_path, deployed_path]
+        must_ign = must_ignore(paths,
+                               ignore,
+                               debug=self.debug)
+        if ign_missing or must_ign:
+            self.log.dbg(f'ignoring diff {local_path} and {deployed_path}')
+            return ''
+        if not os.path.isdir(deployed_path):
+            return f'\"{deployed_path}\" is a file\n'
+
+        return self._compare_dirs(local_path, deployed_path, ignore)
+
+    def _compare_dirs(self, local_path, deployed_path, ignore):
+        """compare directories"""
+        self.log.dbg(f'compare {local_path} and {deployed_path}')
+        ret = []
+        comp = filecmp.dircmp(local_path, deployed_path)
+
+        # handle files only in deployed dir
+        self.log.dbg(f'files only in deployed dir: {comp.left_only}')
+        for i in comp.left_only:
+            if self.ignore_missing_in_dotdrop or \
+               must_ignore([os.path.join(local_path, i)],
+                           ignore, debug=self.debug):
+                continue
+            ret.append(f'=> \"{i}\" does not exist on destination\n')
+
+        # handle files only in dotpath dir
+        self.log.dbg(f'files only in dotpath dir: {comp.right_only}')
+        for i in comp.right_only:
+            if must_ignore([os.path.join(deployed_path, i)],
+                           ignore, debug=self.debug):
+                continue
+
+            if not self.ignore_missing_in_dotdrop:
+                ret.append(f'=> \"{i}\" does not exist in dotdrop\n')
+
+        # same local_path and deployed_path but different type
+        funny = comp.common_funny
+        self.log.dbg(f'files with different types: {funny}')
+        for i in funny:
+            source_file = os.path.join(local_path, i)
+            deployed_file = os.path.join(deployed_path, i)
+            if self.ignore_missing_in_dotdrop and \
+                    not os.path.exists(source_file):
+                continue
+            if must_ignore([source_file, deployed_file],
+                           ignore, debug=self.debug):
+                continue
+            short = os.path.basename(source_file)
+            # file vs dir
+            ret.append(f'=> different type: \"{short}\"\n')
+
+        # content is different
+        funny = comp.diff_files
+        funny.extend(comp.funny_files)
+        funny = uniq_list(funny)
+        self.log.dbg(f'files with different content: {funny}')
+        for i in funny:
+            source_file = os.path.join(local_path, i)
+            deployed_file = os.path.join(deployed_path, i)
+            if self.ignore_missing_in_dotdrop and \
+                    not os.path.exists(source_file):
+                continue
+            if must_ignore([source_file, deployed_file],
+                           ignore, debug=self.debug):
+                continue
+            ret.append(self._diff(source_file, deployed_file, header=True))
+
+        # recursively compare subdirs
+        for i in comp.common_dirs:
+            sublocal_path = os.path.join(local_path, i)
+            subdeployed_path = os.path.join(deployed_path, i)
+            ret.extend(self._comp_dir(sublocal_path, subdeployed_path, ignore))
+
+        return ''.join(ret)
+
+    def _diff(self, local_path, deployed_path, header=False):
+        """diff two files"""
+        out = diff(modified=local_path, original=deployed_path,
+                   diff_cmd=self.diff_cmd, debug=self.debug)
+        if header:
+            lshort = os.path.basename(local_path)
+            out = f'=> diff \"{lshort}\":\n{out}'
+        return out
---
+++ packages/dotdrop/dotdrop-1.14.0-py3-none-any.whl/dotdrop/importer.py
@@ -0,0 +1,314 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2020, deadc0de6
+
+handle import of dotfiles
+"""
+
+import os
+import shutil
+
+# local imports
+from dotdrop.logger import Logger
+from dotdrop.utils import strip_home, get_default_file_perms, \
+    get_file_perm, get_umask, must_ignore, \
+    get_unique_tmp_name, removepath, copytree_with_ign, \
+    copyfile
+from dotdrop.linktypes import LinkTypes
+from dotdrop.comparator import Comparator
+from dotdrop.templategen import Templategen
+from dotdrop.exceptions import UndefinedException
+
+
+class Importer:
+    """dotfile importer"""
+
+    def __init__(self, profile, conf, dotpath, diff_cmd,
+                 variables, dry=False, safe=True, debug=False,
+                 keepdot=True, ignore=None):
+        """constructor
+        @profile: the selected profile
+        @conf: configuration manager
+        @dotpath: dotfiles dotpath
+        @diff_cmd: diff command to use
+        @variables: dictionary of variables for the templates
+        @dry: simulate
+        @safe: ask for overwrite if True
+        @debug: enable debug
+        @keepdot: keep dot prefix
+        @ignore: patterns to ignore when importing
+        This may raise UndefinedException
+        """
+        self.profile = profile
+        if not self.profile:
+            raise UndefinedException("profile is undefined")
+        self.conf = conf
+        self.dotpath = dotpath
+        self.diff_cmd = diff_cmd
+        self.variables = variables
+        self.dry = dry
+        self.safe = safe
+        self.debug = debug
+        self.keepdot = keepdot
+        self.ignore = []
+        self.log = Logger(debug=self.debug)
+
+        # patch ignore patterns
+        if ignore:
+            for ign in ignore:
+                if ign.startswith('!'):
+                    self.ignore.append(ign)
+                    continue
+                if ign.startswith('*/'):
+                    self.ignore.append(ign)
+                    continue
+                newign = f'*/{ign}'
+                self.log.dbg(f'patching ignore {ign} to {newign}')
+                self.ignore.append(newign)
+
+        self.templater = Templategen(variables=self.variables,
+                                     base=self.dotpath,
+                                     debug=self.debug)
+
+        self.umask = get_umask()
+
+    def import_path(self, path, import_as=None,
+                    import_link=LinkTypes.NOLINK,
+                    import_mode=False,
+                    trans_install="",
+                    trans_update=""):
+        """
+        import a dotfile pointed by path
+        returns:
+            1: 1 dotfile imported
+            0: ignored
+            -1: error
+        """
+        path = os.path.abspath(path)
+        self.log.dbg(f'import {path}')
+        if not os.path.exists(path):
+            self.log.err(f'\"{path}\" does not exist, ignored!')
+            return -1
+
+        # check trans_update if any
+        tinstall = None
+        tupdate = None
+        if trans_install:
+            tinstall = self.conf.get_trans_install(trans_install)
+        if trans_update:
+            tupdate = self.conf.get_trans_update(trans_update)
+
+        return self._import(path, import_as=import_as,
+                            import_link=import_link,
+                            import_mode=import_mode,
+                            trans_update=tupdate,
+                            trans_install=tinstall)
+
+    def _import(self, path, import_as=None,
+                import_link=LinkTypes.NOLINK,
+                import_mode=False,
+                trans_install=None,
+                trans_update=None):
+        """
+        import path
+        returns:
+            1: 1 dotfile imported
+            0: ignored
+            -1: error
+        """
+
+        # normalize path
+        dst = path.rstrip(os.sep)
+        dst = os.path.abspath(dst)
+
+        # test if must be ignored
+        if self._ignore(dst):
+            return 0
+
+        # ask confirmation for symlinks
+        if self.safe:
+            realdst = os.path.realpath(dst)
+            if dst != realdst:
+                msg = f'\"{dst}\" is a symlink, dereference it and continue?'
+                if not self.log.ask(msg):
+                    return 0
+
+        # create src path
+        src = strip_home(dst)
+        if import_as:
+            # handle import as
+            src = os.path.expanduser(import_as)
+            src = src.rstrip(os.sep)
+            src = os.path.abspath(src)
+            src = strip_home(src)
+            self.log.dbg(f'import src for {dst} as {src}')
+        # with or without dot prefix
+        strip = '.' + os.sep
+        if self.keepdot:
+            strip = os.sep
+        src = src.lstrip(strip)
+
+        # get the permission
+        perm = get_file_perm(dst)
+
+        # get the link attribute
+        linktype = import_link
+        if linktype == LinkTypes.LINK_CHILDREN and \
+                not os.path.isdir(path):
+            self.log.err(f'importing \"{path}\" failed!')
+            return -1
+
+        if self._already_exists(src, dst):
+            return -1
+
+        self.log.dbg(f'import dotfile: src:{src} dst:{dst}')
+
+        if not self._import_to_dotpath(src, dst, trans_update=trans_update):
+            return -1
+
+        return self._import_in_config(path, src, dst, perm, linktype,
+                                      import_mode,
+                                      trans_update=trans_update,
+                                      trans_install=trans_install)
+
+    def _import_in_config(self, path, src, dst, perm,
+                          linktype, import_mode,
+                          trans_install=None,
+                          trans_update=None):
+        """
+        import path
+        returns:
+            1: 1 dotfile imported
+            0: ignored
+        """
+        # handle file mode
+        chmod = None
+        dflperm = get_default_file_perms(dst, self.umask)
+        self.log.dbg(f'import chmod: {import_mode}')
+        if import_mode or perm != dflperm:
+            msg = f'adopt mode {perm:o} (umask {dflperm:o})'
+            self.log.dbg(msg)
+            chmod = perm
+
+        # add file to config file
+        retconf = self.conf.new_dotfile(src, dst, linktype, chmod=chmod,
+                                        trans_install=trans_install,
+                                        trans_update=trans_update)
+        if not retconf:
+            self.log.warn(f'\"{path}\" ignored during import')
+            return 0
+
+        self.log.sub(f'\"{path}\" imported')
+        return 1
+
+    def _check_existing_dotfile(self, src, dst):
+        """
+        check if a dotfile in the dotpath
+        already exists for this src
+        """
+        if not os.path.exists(src):
+            return True
+        if not self.safe:
+            return True
+        cmp = Comparator(debug=self.debug,
+                         diff_cmd=self.diff_cmd)
+        diff = cmp.compare(src, dst)
+        if diff != '':
+            # files are different, dunno what to do
+            self.log.log(f'diff \"{dst}\" VS \"{src}\"')
+            self.log.emph(diff)
+            # ask user
+            msg = f'Dotfile \"{src}\" already exists, overwrite?'
+            if not self.log.ask(msg):
+                return False
+            self.log.dbg('will overwrite existing file')
+        return True
+
+    def _import_to_dotpath(self, in_dotpath, in_fs, trans_update=None):
+        """
+        prepare hierarchy for dotfile in dotpath and copy file
+        """
+        srcf = os.path.join(self.dotpath, in_dotpath)
+
+        # check we are not overwritting
+        if not self._check_existing_dotfile(srcf, in_fs):
+            return False
+
+        # import the file
+        if self.dry:
+            self.log.dry(f'would copy {in_fs} to {srcf}')
+            return True
+
+        # apply trans_update
+        in_fs = self._apply_trans_update(in_fs, trans_update)
+        if not in_fs:
+            # transformation failed
+            return False
+        # copy the file to the dotpath
+        try:
+            if not os.path.isdir(in_fs):
+                # is a file
+                self.log.dbg(f'{in_fs} is file')
+                copyfile(in_fs, srcf, debug=self.debug)
+            else:
+                # is a dir
+                if os.path.exists(srcf):
+                    shutil.rmtree(srcf)
+                self.log.dbg(f'{in_fs} is dir')
+                copytree_with_ign(in_fs, srcf,
+                                  ignore_func=self._ignore,
+                                  debug=self.debug)
+        except shutil.Error as exc:
+            in_dotpath = exc.args[0][0][0]
+            why = exc.args[0][0][2]
+            self.log.err(f'importing \"{in_fs}\" failed: {why}')
+
+        return os.path.exists(srcf)
+
+    def _already_exists(self, src, dst):
+        """
+        test no other dotfile exists with same
+        dst for this profile but different src
+        """
+        dfs = self.conf.get_dotfile_by_dst(dst, profile_key=self.profile)
+        if not dfs:
+            return False
+        for dotfile in dfs:
+            profiles = self.conf.get_profiles_by_dotfile_key(dotfile.key)
+            profiles = [x.key for x in profiles]
+            if self.profile in profiles and \
+                    not self.conf.get_dotfile_by_src_dst(src, dst):
+                # same profile
+                # different src
+                msg = f'duplicate dotfile: {dotfile.key}'
+                self.log.err(msg)
+                return True
+        return False
+
+    def _ignore(self, path):
+        if must_ignore([path], self.ignore, debug=self.debug):
+            self.log.dbg(f'ignoring import of {path}')
+            self.log.warn(f'{path} ignored')
+            return True
+        return False
+
+    def _apply_trans_update(self, path, trans):
+        """
+        apply transformation to path on filesystem)
+        returns
+        - the new path (tmp file) if trans
+        - original path if no trans
+        - None/empty string if error
+        """
+        if not trans:
+            return path
+        self.log.dbg(f'executing write transformation {trans}')
+        tmp = get_unique_tmp_name()
+        if not trans.transform(path, tmp, debug=self.debug,
+                               templater=self.templater):
+            msg = f'transformation \"{trans.key}\" failed for {path}'
+            self.log.err(msg)
+            if os.path.exists(tmp):
+                removepath(tmp, logger=self.log)
+            return None
+        return tmp
