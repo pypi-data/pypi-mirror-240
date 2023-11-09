e54f7cb6e8fed13718de0f1db54e75013e7d6e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:22 +0000

    Add package phalcon/1.0.10.19/phalcon-1.0.10.19-py3-none-any.whl

---
+++ packages/phalcon/phalcon-1.0.10.19-py3-none-any.whl/app/main.py
@@ -0,0 +1,37 @@
+import click
+from .funcs import is_docker_available
+from .services.phalcon_service import PhalconService
+
+@click.group()
+@click.version_option("1.0.10.19")
+def cli():
+    pass
+
+@cli.command()
+def serve():
+    """Start a complete php server with phalcon installed."""
+    if not is_docker_available():
+        return
+    
+    svc = PhalconService()
+    svc.run_server()
+
+@cli.command()
+@click.option("-o", "--override", "override", is_flag=True)
+def add(override):
+    """Add new project to the server (sudo required)."""
+    svc = PhalconService()
+    svc.add_project_from_github(override)
+
+
+@cli.command()
+def remove():
+    """Remove an existing project from the server (sudo required)."""
+    svc = PhalconService()
+    svc.remove_project()
+
+@cli.command()
+def new():
+    """Creates a new phalcon project"""
+    svc = PhalconService()
+    svc.create_phalcon_project()
