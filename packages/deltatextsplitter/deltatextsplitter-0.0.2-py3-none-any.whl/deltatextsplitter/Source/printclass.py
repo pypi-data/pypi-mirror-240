1b37b4a969f172b3bb791b49fad08b1bc7f1df55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 17:03:47 +0000

    Add package deltatextsplitter/0.0.2/deltatextsplitter-0.0.2-py3-none-any.whl

---
+++ packages/deltatextsplitter/deltatextsplitter-0.0.2-py3-none-any.whl/deltatextsplitter/Source/printclass.py
@@ -0,0 +1,17 @@
+"""
+Module for the printclass-fucntion, a member function of the deltatextsplitter class.
+"""
+
+
+def printclass_deltatextsplitter(self):
+    """
+    Member function of the deltatextsplitter-class to
+    print an overview of the content of the class.
+
+    # Parameters: None (taken from the class)
+    # Return: None (printed in the terminal)
+    """
+
+    print(" ==> NOTE: " + str(self.label))
+
+    # Done.
