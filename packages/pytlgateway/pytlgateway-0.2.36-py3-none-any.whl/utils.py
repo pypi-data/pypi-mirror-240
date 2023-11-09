0ef183a92e616c09f0235ff65439bc5b4d625082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:38 +0000

    Add package pytlgateway/0.2.36/pytlgateway-0.2.36-py3-none-any.whl

---
+++ packages/pytlgateway/pytlgateway-0.2.36-py3-none-any.whl/utils.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+def decode_exchange_id(suffix):
+    exchange = 0
+    if suffix == 'SH':
+        exchange = 101        #EXCHANGE_SSE 
+    elif suffix == 'SZ':
+        exchange = 102        #EXCHANGE_SZE 
+    
+    return exchange
+
+def get_log_default_path():
+    # python2: linux2, python3: linux
+    if sys.platform.startswith("linux") or sys.platform == "darwin":
+        dirs = "/shared/log"
+    elif sys.platform == "win32":
+        dirs = os.path.join(get_windows_first_disk() + "/tmp/linker/log")
+    else:
+        dirs = '.'
+
+    return dirs
+
+
+def get_digit_from_env(env_name, default_num):
+    num = str(os.environ.get(env_name))
+    return int(num) if num.isdigit() else default_num
+
+def get_log_given_path(path):
+    dirs = os.path.join(path)
+    return path
+
+def get_exchange_from_ticker(ticker:str):
+    stock_code = f'{ticker:06d}'
+    if stock_code.startswith('00'):
+        stock_code += '.SZ'
+    elif stock_code.startswith('6'):
+        stock_code += '.SH'
+    elif stock_code.startswith('5'):
+        stock_code += '.SH'
+    else:
+        stock_code += '.SZ'
+    return stock_code
