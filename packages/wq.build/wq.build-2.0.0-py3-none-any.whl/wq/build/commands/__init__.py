2a92011c228e397c1c78ca4266b2466537f0d658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:08:22 +0000

    Add package wq.build/2.0.0/wq.build-2.0.0-py3-none-any.whl

---
+++ packages/wq.build/wq.build-2.0.0-py3-none-any.whl/wq/build/commands/__init__.py
@@ -0,0 +1,15 @@
+from .collect import collectjson
+from .icons import icons
+from .info import _make_docs
+from .movefiles import movefiles
+from .setversion import setversion
+from .serviceworker import serviceworker
+
+__all__ = (
+    "collectjson",
+    "icons",
+    "_make_docs",
+    "movefiles",
+    "setversion",
+    "serviceworker",
+)
