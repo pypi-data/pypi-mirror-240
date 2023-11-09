2550b8ba1a41fae19b01873d5a3359938e965978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 16:03:41 +0000

    Add package atk-satish-helloworld/0.2/atk-satish-helloworld-0.2.tar.gz

---
+++ packages/atk-satish-helloworld/atk-satish-helloworld-0.2.tar.gz/atk-satish-helloworld-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="atk-satish-helloworld",
+    version="0.2",
+    packages=["atk-satish-helloworld"],
+    author="Satish Kumar",
+    author_email="satish@aganitha.com",
+    description="A 'Hello, World!' package",
+)
+
