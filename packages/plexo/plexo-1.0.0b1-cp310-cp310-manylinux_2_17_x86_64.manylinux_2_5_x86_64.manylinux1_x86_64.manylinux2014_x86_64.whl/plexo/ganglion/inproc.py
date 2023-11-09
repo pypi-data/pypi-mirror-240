8fc824ca0cfa411011d90af6155938b9825bdabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:05:43 +0000

    Add package plexo/1.0.0b1/plexo-1.0.0b1-cp310-cp310-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl

---
+++ packages/plexo/plexo-1.0.0b1-cp310-cp310-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl/plexo/ganglion/inproc.py
@@ -0,0 +1,41 @@
+#  pyplexo
+#  Copyright © 2018-2023  Alecks Gates
+#
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyplexo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+
+from plexo.exceptions import SynapseExists
+from plexo.ganglion.internal import GanglionInternalBase
+from plexo.neuron.neuron import Neuron
+from plexo.synapse.inproc import SynapseInproc
+from plexo.typing import UnencodedType
+
+
+class GanglionInproc(GanglionInternalBase):
+    async def _create_synapse_by_name(self, neuron: Neuron[UnencodedType], name: str):
+        if name in self._synapses:
+            raise SynapseExists(f"Synapse for {name} already exists.")
+
+        logging.debug(f"GanglionInproc:Creating synapse for type {name}")
+
+        synapse: SynapseInproc = SynapseInproc(neuron)
+
+        async with self._synapses_lock:
+            self._synapses = self._synapses.set(name, synapse)
+
+        return synapse
+
+    async def _create_synapse(self, neuron: Neuron):
+        return await self._create_synapse_by_name(neuron, neuron.name)
