d05f61fb8401ed141057d5f95ca813f36bced917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:06:09 +0000

    Add package commonroad-crime/0.3.1/commonroad-crime-0.3.1.tar.gz

---
+++ packages/commonroad-crime/commonroad-crime-0.3.1.tar.gz/commonroad-crime-0.3.1/tests/test_base.py
@@ -0,0 +1,31 @@
+import unittest
+
+from commonroad_crime.data_structure.scene import Scene
+from commonroad_crime.data_structure.base import CriMeBase
+from commonroad_crime.data_structure.configuration import CriMeConfiguration
+import commonroad_crime.utility.logger as util_logger
+
+
+class TestBase(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        scenario_id = "DEU_Test-1_1_T-1"
+        self.config = CriMeConfiguration.load(
+            f"../config_files/{scenario_id}.yaml", scenario_id
+        )
+        util_logger.initialize_logger(self.config)
+        self.config.print_configuration_summary()
+
+    def test_construction(self):
+        """
+        Test the construction of base classes.
+        """
+        self.config.update()
+        base_1 = CriMeBase(self.config)
+
+        scene = Scene(0, self.config.scenario)
+        self.config.update(sce=scene)
+        base_2 = CriMeBase(self.config)
+
+        self.assertIsInstance(base_1, CriMeBase)
+        self.assertIsInstance(base_2, CriMeBase)
