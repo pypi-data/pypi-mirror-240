4d9c90d0c9cf586f113b463349d20660122876bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 15:05:17 +0000

    Add package lizhankangpkg/0.1.0/lizhankangpkg-0.1.0.tar.gz

---
+++ packages/lizhankangpkg/lizhankangpkg-0.1.0.tar.gz/lizhankangpkg-0.1.0/src/exam_pkg_01/__init__.py
@@ -0,0 +1,5 @@
+# coding:utf-8 
+# @Author : lizhankang
+# @Subject : 
+# @Time : 2023 - 11 - 08
+from .exa import *
---
+++ packages/lizhankangpkg/lizhankangpkg-0.1.0.tar.gz/lizhankangpkg-0.1.0/src/exam_pkg_01/exa.py
@@ -0,0 +1,11 @@
+# coding:utf-8 
+# @Author : lizhankang
+# @Subject : 
+# @Time : 2023 - 11 - 08
+
+
+def add_one(number):
+    return number + 1
+
+def welcome():
+    print("Welcome to Liplus World 0.1.0 ... ")
