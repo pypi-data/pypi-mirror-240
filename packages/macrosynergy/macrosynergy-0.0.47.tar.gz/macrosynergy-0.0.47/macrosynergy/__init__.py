6eef03053072c28b0b3b2baa3da3d7f1413a582a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:09:36 +0000

    Add package macrosynergy/0.0.47/macrosynergy-0.0.47.tar.gz

---
+++ packages/macrosynergy/macrosynergy-0.0.47.tar.gz/macrosynergy-0.0.47/macrosynergy/__init__.py
@@ -0,0 +1,20 @@
+"""Macrosynergy Quant Research Package"""
+
+try:
+    from .version import git_revision as __git_revision__
+    from .version import version as __version__
+except ModuleNotFoundError:
+    from setup import get_version_info
+
+    FULLVERSION, GIT_REVISION = get_version_info()
+    __version__ = FULLVERSION
+    __git_revision__ = GIT_REVISION
+
+from . import visuals, dataquery, download, panel, pnl, management, signal
+
+__all__ = ["visuals", "dataquery", "download", "panel", "pnl", "management", "signal"]
+
+__name__ = ["__version__"]
+
+
+# allows the package version information to be accessed from the package
---
+++ packages/macrosynergy/macrosynergy-0.0.47.tar.gz/macrosynergy-0.0.47/macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.47'
+version: str = '0.0.47'
+full_version: str = '0.0.47'
+git_revision: str = '183d3ca934ce14fd0b95396cbacd2251003eea9a'
+release: bool = True
+if not release:
+    version = full_version
