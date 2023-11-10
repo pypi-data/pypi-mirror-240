cd096af1703f1eef155748bd10a4c69fe015cf41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:06:59 +0000

    Add package datajoint-link/0.10/datajoint_link-0.10-py3-none-any.whl

---
+++ packages/datajoint-link/datajoint_link-0.10-py3-none-any.whl/link/infrastructure/link.py
@@ -0,0 +1,91 @@
+"""Contains the link decorator that is used by the user to establish a link."""
+from __future__ import annotations
+
+import logging
+from collections.abc import Callable
+from functools import partial
+from typing import Any, Mapping, Optional
+
+from link.adapters.controller import DJController
+from link.adapters.custom_types import PrimaryKey
+from link.adapters.gateway import DJLinkGateway
+from link.adapters.identification import IdentificationTranslator
+from link.adapters.present import create_idle_entities_updater, create_state_change_logger
+from link.adapters.progress import DJProgressDisplayAdapter
+from link.domain import commands, events
+from link.service.handlers import (
+    delete,
+    delete_entity,
+    inform_batch_processing_finished,
+    inform_batch_processing_started,
+    inform_current_process_finished,
+    inform_next_process_started,
+    list_idle_entities,
+    log_state_change,
+    pull,
+    pull_entity,
+)
+from link.service.messagebus import CommandHandlers, EventHandlers, MessageBus
+from link.service.uow import UnitOfWork
+
+from . import DJConfiguration, create_tables
+from .facade import DJLinkFacade
+from .mixin import create_local_endpoint
+from .progress import TQDMProgressView
+from .sequence import IterationCallbackList, create_content_replacer
+
+
+def create_link(  # noqa: PLR0913
+    source_host: str,
+    source_schema: str,
+    outbound_schema: str,
+    outbound_table: str,
+    local_schema: str,
+    *,
+    stores: Optional[Mapping[str, str]] = None,
+) -> Callable[[type], Any]:
+    """Create a link."""
+    if stores is None:
+        stores = {}
+
+    def inner(obj: type) -> Any:
+        translator = IdentificationTranslator()
+        tables = create_tables(
+            DJConfiguration(
+                source_host, source_schema, outbound_schema, outbound_table, local_schema, obj.__name__, stores
+            )
+        )
+        facade = DJLinkFacade(tables.source, tables.outbound, tables.local)
+        gateway = DJLinkGateway(facade, translator)
+        uow = UnitOfWork(gateway)
+        source_restriction: IterationCallbackList[PrimaryKey] = IterationCallbackList()
+        idle_entities_updater = create_idle_entities_updater(translator, create_content_replacer(source_restriction))
+        logger = logging.getLogger(obj.__name__)
+
+        command_handlers: CommandHandlers = {}
+        event_handlers: EventHandlers = {}
+        bus = MessageBus(uow, command_handlers, event_handlers)
+        command_handlers[commands.PullEntity] = partial(pull_entity, uow=uow, message_bus=bus)
+        command_handlers[commands.DeleteEntity] = partial(delete_entity, uow=uow, message_bus=bus)
+        command_handlers[commands.PullEntities] = partial(pull, message_bus=bus)
+        command_handlers[commands.DeleteEntities] = partial(delete, message_bus=bus)
+        command_handlers[commands.ListIdleEntities] = partial(
+            list_idle_entities, uow=uow, output_port=idle_entities_updater
+        )
+        progress_view = TQDMProgressView()
+        display = DJProgressDisplayAdapter(translator, progress_view)
+        event_handlers[events.ProcessStarted] = [partial(inform_next_process_started, display=display)]
+        event_handlers[events.ProcessFinished] = [partial(inform_current_process_finished, display=display)]
+        event_handlers[events.BatchProcessingStarted] = [partial(inform_batch_processing_started, display=display)]
+        event_handlers[events.BatchProcessingFinished] = [partial(inform_batch_processing_finished, display=display)]
+        event_handlers[events.StateChanged] = [
+            partial(log_state_change, log=create_state_change_logger(translator, logger.info))
+        ]
+        event_handlers[events.InvalidOperationRequested] = [lambda event: None]
+
+        controller = DJController(bus, translator)
+        source_restriction.callback = controller.list_idle_entities
+
+        return create_local_endpoint(controller, tables, source_restriction, progress_view)
+
+    return inner
---
+++ packages/datajoint-link/datajoint_link-0.10-py3-none-any.whl/link/service/uow.py
@@ -0,0 +1,108 @@
+"""Contains the unit of work for links."""
+from __future__ import annotations
+
+from abc import ABC
+from collections import deque
+from types import TracebackType
+from typing import Callable, Iterable, Iterator, Protocol
+
+from link.domain import events
+from link.domain.custom_types import Identifier
+from link.domain.link import Link
+from link.domain.state import TRANSITION_MAP, Entity, Operations, Transition
+
+from .gateway import LinkGateway
+
+
+class SupportsLinkApply(Protocol):
+    """Protocol for an object that supports applying operations to links."""
+
+    def __call__(self, operation: Operations, *, requested: Iterable[Identifier]) -> Link:
+        """Apply the operation to the link."""
+
+
+class UnitOfWork(ABC):
+    """Controls if and when updates to entities of a link are persisted."""
+
+    def __init__(self, gateway: LinkGateway) -> None:
+        """Initialize the unit of work."""
+        self._gateway = gateway
+        self._link: Link | None = None
+        self._updates: deque[events.StateChanged] = deque()
+        self._events: deque[events.Event] = deque()
+        self._seen: list[Entity] = []
+
+    def __enter__(self) -> UnitOfWork:
+        """Enter the context in which updates to entities can be made."""
+
+        def augment_entity(entity: Entity) -> None:
+            original = getattr(entity, "apply")
+            augmented = augment_entity_apply(entity, original)
+            setattr(entity, "apply", augmented)
+            setattr(entity, "_is_expired", False)
+
+        def augment_entity_apply(
+            entity: Entity, original: Callable[[Operations], None]
+        ) -> Callable[[Operations], None]:
+            def augmented(operation: Operations) -> None:
+                assert hasattr(entity, "_is_expired")
+                if entity._is_expired:
+                    raise RuntimeError("Can not apply operation to expired entity.")
+                if entity not in self._seen:
+                    self._seen.append(entity)
+                current_state = entity.state
+                original(operation)
+                new_state = entity.state
+                if current_state is new_state:
+                    return
+                transition = Transition(current_state, new_state)
+                command = TRANSITION_MAP[transition]
+                self._updates.append(events.StateChanged(operation, entity.identifier, transition, command))
+
+            return augmented
+
+        self._link = self._gateway.create_link()
+        for entity in self._link:
+            augment_entity(entity)
+        return self
+
+    def __exit__(
+        self, exc_type: type[BaseException] | None, exc: BaseException | None, traceback: TracebackType | None
+    ) -> None:
+        """Exit the context rolling back any not yet persisted updates."""
+        self.rollback()
+        self._link = None
+
+    @property
+    def link(self) -> Link:
+        """Return the link object that is governed by this unit of work."""
+        if self._link is None:
+            raise RuntimeError("Not available outside of context")
+        return self._link
+
+    def commit(self) -> None:
+        """Persist updates made to the link."""
+        if self._link is None:
+            raise RuntimeError("Not available outside of context")
+        while self._updates:
+            self._gateway.apply([self._updates.popleft()])
+        for entity in self._seen:
+            while entity.events:
+                self._events.append(entity.events.popleft())
+        self.rollback()
+
+    def rollback(self) -> None:
+        """Throw away any not yet persisted updates."""
+        if self._link is None:
+            raise RuntimeError("Not available outside of context")
+        for entity in self._link:
+            setattr(entity, "_is_expired", True)
+        self._updates.clear()
+        self._seen.clear()
+
+    def collect_new_events(self) -> Iterator[events.Event]:
+        """Collect new events from entities."""
+        if self._link is not None:
+            raise RuntimeError("New events can not be collected when inside context")
+        while self._events:
+            yield self._events.popleft()
