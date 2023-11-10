1cdf87b9eb70c9e4f0a47ab30f806d9f892b0919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:20 +0000

    Add package nnetsauce/0.15.0/nnetsauce-0.15.0.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.15.0.tar.gz/nnetsauce-0.15.0/nnetsauce/sampling/rowsubsampling.py
@@ -0,0 +1,25 @@
+from .helpers import dosubsample
+
+class SubSampler:
+    """Subsampling class.
+
+    Attributes:
+
+       y: array-like, shape = [n_samples]
+           Target values.
+
+       row_sample: double
+           subsampling fraction
+
+       seed: int
+           reproductibility seed
+
+    """
+
+    def __init__(self, y, row_sample=0.8, seed=123):
+        self.y = y
+        self.row_sample = row_sample
+        self.seed = seed
+
+    def subsample(self):
+        return dosubsample(self.y, self.row_sample, self.seed)
