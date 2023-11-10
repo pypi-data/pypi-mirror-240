b4b254bf7a0d807255b16e2009518a4f8813798c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:14:10 +0000

    Add package ctat/0.0.4.dev1/ctat-0.0.4.dev1-py3-none-any.whl

---
+++ packages/ctat/ctat-0.0.4.dev1-py3-none-any.whl/ctat/tdt_export.py
@@ -0,0 +1,14 @@
+import requests
+
+nanobot_default_tables = ["table", "column", "datatype"]
+
+
+def export_cas_data(output_folder: str):
+    response = requests.get('http://localhost:3000/table.json?limit=20&shape=value_rows')
+    print(response.status_code)
+    response.raise_for_status()
+
+    table_records = response.json()
+    for table_rec in table_records:
+        if table_rec["table"] not in table_rec["table"]:
+            print(table_rec["table"])
