531d6d5c16010291fc57355f21fe219407f08d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 07:04:57 +0000

    Add package napari-spatialdata/0.3.0/napari-spatialdata-0.3.0.tar.gz

---
+++ packages/napari-spatialdata/napari-spatialdata-0.3.0.tar.gz/napari-spatialdata-0.3.0/src/napari_spatialdata/_model.py
@@ -0,0 +1,313 @@
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Any, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from anndata import AnnData
+from napari.layers import Layer
+from napari.utils.events import EmitterGroup, Event
+
+from napari_spatialdata._constants._constants import Symbol
+from napari_spatialdata._constants._pkg_constants import Key
+from napari_spatialdata.utils._utils import NDArrayA, _ensure_dense_vector
+
+__all__ = ["ImageModel"]
+
+
+@dataclass
+class ImageModel:
+    """Model which holds the data for interactive visualization."""
+
+    events: EmitterGroup = field(init=False, default=None, repr=True)
+    _layer: Layer = field(init=False, default=None, repr=True)
+    _adata: Optional[AnnData] = field(init=False, default=None, repr=True)
+    _spatial_key: str = field(default=Key.obsm.spatial, repr=False)
+    _adata_layer: Optional[str] = field(init=False, default=None, repr=False)
+    _label_key: Optional[str] = field(default=None, repr=True)
+    _coordinates: Optional[NDArrayA] = field(init=False, default=None, repr=True)
+    _points_coordinates: Optional[NDArrayA] = field(init=False, default=None, repr=True)
+    _points_var: Optional[pd.Series] = field(init=False, default=None, repr=True)
+    _scale: Optional[float] = field(init=False, default=None)
+    _system_name: Optional[str] = field(default=None, repr=True)
+
+    _spot_diameter: Union[NDArrayA, float] = field(init=False, default=1)
+    _point_diameter: Union[NDArrayA, float] = field(init=False, default=1)
+    _scale_key: Optional[str] = field(init=False, default="tissue_hires_scalef")  # TODO(giovp): use constants for these
+
+    _palette: Optional[str] = field(init=False, default=None, repr=False)
+    _cmap: str = field(init=False, default="viridis", repr=False)
+    _symbol: str = field(init=False, default=Symbol.DISC, repr=False)
+
+    VALID_ATTRIBUTES = ("obs", "var", "obsm", "points")
+
+    def __post_init__(self) -> None:
+        self.events = EmitterGroup(
+            source=self,
+            layer=Event,
+            adata=Event,
+        )
+
+    def get_items(self, attr: str) -> Tuple[str, ...]:
+        """
+        Return valid keys for an attribute.
+
+        Parameters
+        ----------
+        attr
+            Attribute of :mod:`anndata.AnnData` to access.
+
+        Returns
+        -------
+        The available items.
+        """
+        if attr in ("obs", "obsm"):
+            return tuple(map(str, getattr(self.adata, attr).keys()))
+        if attr == "points":
+            if self.points_var is not None:
+                return tuple(map(str, self.points_var.unique()))
+            return tuple(["No points found."])  # noqa: C409
+        return tuple(map(str, getattr(self.adata, attr).index))
+
+    @_ensure_dense_vector
+    def get_obs(
+        self, name: str, **_: Any
+    ) -> Tuple[Optional[Union[pd.Series, NDArrayA]], str]:  # TODO(giovp): fix docstring
+        """
+        Return an observation.
+
+        Parameters
+        ----------
+        name
+            Key in :attr:`anndata.AnnData.obs` to access.
+
+        Returns
+        -------
+        The values and the formatted ``name``.
+        """
+        if name not in self.adata.obs.columns:
+            raise KeyError(f"Key `{name}` not found in `adata.obs`.")
+        return self.adata.obs[name], self._format_key(name)
+
+    @_ensure_dense_vector
+    def get_var(self, name: Union[str, int], **_: Any) -> Tuple[Optional[NDArrayA], str]:  # TODO(giovp): fix docstring
+        """
+        Return a gene.
+
+        Parameters
+        ----------
+        name
+            Gene name in :attr:`anndata.AnnData.var_names` or :attr:`anndata.AnnData.raw.var_names`,
+            based on :paramref:`raw`.
+
+        Returns
+        -------
+        The values and the formatted ``name``.
+        """
+        try:
+            ix = self.adata._normalize_indices((slice(None), name))
+        except KeyError:
+            raise KeyError(f"Key `{name}` not found in `adata.var_names`.") from None
+
+        return self.adata._get_X(layer=self.adata_layer)[ix], self._format_key(name, adata_layer=True)
+
+    @_ensure_dense_vector
+    def get_obsm(self, name: str, index: Union[int, str] = 0) -> Tuple[Optional[NDArrayA], str]:
+        """
+        Return a vector from :attr:`anndata.AnnData.obsm`.
+
+        Parameters
+        ----------
+        name
+            Key in :attr:`anndata.AnnData.obsm`.
+        index
+            Index of the vector.
+
+        Returns
+        -------
+        The values and the formatted ``name``.
+        """
+        if name not in self.adata.obsm:
+            raise KeyError(f"Unable to find key `{name!r}` in `adata.obsm`.")
+        res = self.adata.obsm[name]
+        pretty_name = self._format_key(name, index=index)
+
+        if isinstance(res, pd.DataFrame):
+            try:
+                if isinstance(index, str):
+                    return res[index], pretty_name
+                if isinstance(index, int):
+                    return res.iloc[:, index], self._format_key(name, index=res.columns[index])
+            except KeyError:
+                raise KeyError(f"Key `{index}` not found in `adata.obsm[{name!r}].`") from None
+
+        if not isinstance(index, int):
+            try:
+                index = int(index, base=10)
+            except ValueError:
+                raise ValueError(
+                    f"Unable to convert `{index}` to an integer when accessing `adata.obsm[{name!r}]`."
+                ) from None
+        res = np.asarray(res)
+
+        return (res if res.ndim == 1 else res[:, index]), pretty_name
+
+    def get_points(self, name: str, **_: Any) -> Tuple[Optional[NDArrayA], str]:  # TODO(giovp): fix docstring
+        """
+        Return a gene in spatial coordinates.
+
+        Parameters
+        ----------
+        name
+            Gene name.
+
+        Returns
+        -------
+        The values and the formatted ``name``.
+        """
+        if name not in self.points_var.unique():
+            raise KeyError(f"Key `{name}` not found in `adata.uns['points']['gene']`.")
+        coords = self.points_coordinates
+        coords = coords[self.points_var == name]
+        return np.insert(coords[:, ::-1][:, :2] * self.scale, 0, values=0, axis=1), self._format_key(name)
+
+    def _format_key(
+        self, key: Union[str, int], index: Optional[Union[int, str]] = None, adata_layer: bool = False
+    ) -> str:
+        if index is not None:
+            return str(key) + f":{index}:{self.layer}"
+        if adata_layer:
+            return str(key) + (f":{self.adata_layer}" if self.adata_layer is not None else ":X") + f":{self.layer}"
+
+        return str(key) + (f":{self.layer}" if self.layer is not None else ":X")
+
+    @property
+    def layer(self) -> Optional[Layer]:  # noqa: D102
+        return self._layer
+
+    @layer.setter
+    def layer(self, layer: Optional[Layer]) -> None:
+        self._layer = layer
+        self.events.layer()
+
+    @property
+    def adata(self) -> AnnData:  # noqa: D102
+        return self._adata
+
+    @adata.setter
+    def adata(self, adata: AnnData) -> None:
+        self._adata = adata
+        self.events.adata()
+
+    @property
+    def spatial_key(self) -> str:  # noqa: D102
+        return self._spatial_key
+
+    @spatial_key.setter
+    def spatial_key(self, key: str) -> None:
+        self._spatial_key = key
+
+    @property
+    def adata_layer(self) -> Optional[str]:  # noqa: D102
+        return self._adata_layer
+
+    @adata_layer.setter
+    def adata_layer(self, adata_layer: str) -> None:
+        self._adata_layer = adata_layer
+
+    @property
+    def coordinates(self) -> NDArrayA:  # noqa: D102
+        return self._coordinates  # type: ignore[return-value]
+
+    @coordinates.setter
+    def coordinates(self, coordinates: NDArrayA) -> None:
+        self._coordinates = coordinates
+
+    @property
+    def points_coordinates(self) -> NDArrayA:  # noqa: D102
+        if TYPE_CHECKING:
+            assert self._points_coordinates is not None
+        return self._points_coordinates
+
+    @points_coordinates.setter
+    def points_coordinates(self, points_coordinates: NDArrayA) -> None:
+        self._points_coordinates = points_coordinates
+
+    @property
+    def points_var(self) -> pd.Series:  # noqa: D102
+        return self._points_var
+
+    @points_var.setter
+    def points_var(self, points_var: pd.Series) -> None:
+        self._points_var = points_var
+
+    @property
+    def scale(self) -> Optional[float]:  # noqa: D102
+        return self._scale
+
+    @scale.setter
+    def scale(self, scale: float) -> None:
+        self._scale = scale
+
+    @property
+    def spot_diameter(self) -> Union[NDArrayA, float]:  # noqa: D102
+        return self._spot_diameter
+
+    @spot_diameter.setter
+    def spot_diameter(self, spot_diameter: Union[NDArrayA, float]) -> None:
+        self._spot_diameter = spot_diameter
+
+    @property
+    def point_diameter(self) -> Union[NDArrayA, float]:  # noqa: D102
+        return self._point_diameter
+
+    @point_diameter.setter
+    def point_diameter(self, point_diameter: Union[NDArrayA, float]) -> None:
+        self._point_diameter = point_diameter
+
+    @property
+    def labels_key(self) -> Optional[str]:  # noqa: D102
+        return self._labels_key
+
+    @labels_key.setter
+    def labels_key(self, region_key: str) -> None:
+        self._labels_key = region_key
+
+    @property
+    def palette(self) -> Optional[str]:  # noqa: D102
+        return self._palette
+
+    @palette.setter
+    def palette(self, palette: str) -> None:
+        self._palette = palette
+
+    @property
+    def cmap(self) -> str:  # noqa: D102
+        return self._cmap
+
+    @cmap.setter
+    def cmap(self, cmap: str) -> None:
+        self._cmap = cmap
+
+    @property
+    def symbol(self) -> str:  # noqa: D102
+        return self._symbol
+
+    @symbol.setter
+    def symbol(self, symbol: str) -> None:
+        self._symbol = symbol
+
+    @property
+    def scale_key(self) -> Optional[str]:  # noqa: D102
+        return self._scale_key
+
+    @scale_key.setter
+    def scale_key(self, scale_key: str) -> None:
+        self._scale_key = scale_key
+
+    @property
+    def system_name(self) -> Optional[str]:  # noqa: D102
+        return self._system_name
+
+    @system_name.setter
+    def system_name(self, system_name: str) -> None:
+        self._system_name = system_name
---
+++ packages/napari-spatialdata/napari-spatialdata-0.3.0.tar.gz/napari-spatialdata-0.3.0/src/napari_spatialdata/_view.py
@@ -0,0 +1,278 @@
+from typing import Any, FrozenSet, Optional, Sequence
+
+import napari
+import numpy as np
+from anndata import AnnData
+from loguru import logger
+from napari._qt.qt_resources import get_stylesheet
+from napari._qt.utils import QImg2array
+from napari.layers import Labels
+from napari.viewer import Viewer
+from qtpy.QtCore import QSize, Qt
+from qtpy.QtWidgets import (
+    QComboBox,
+    QGridLayout,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+from napari_spatialdata._constants._pkg_constants import Key
+from napari_spatialdata._model import ImageModel
+from napari_spatialdata._scatterwidgets import AxisWidgets, MatplotlibWidget
+from napari_spatialdata._widgets import (
+    AListWidget,
+    CBarWidget,
+    ComponentWidget,
+    RangeSliderWidget,
+)
+
+__all__ = ["QtAdataViewWidget", "QtAdataScatterWidget"]
+
+
+class QtAdataScatterWidget(QWidget):
+    """Adata viewer widget."""
+
+    def __init__(self, input: Viewer):
+        super().__init__()
+
+        self._model = ImageModel()
+
+        self.setLayout(QGridLayout())
+
+        if isinstance(input, Viewer):
+            self._viewer = input
+            self._select_layer()
+            self._viewer.layers.selection.events.changed.connect(self._select_layer)
+            self._viewer.layers.selection.events.changed.connect(self._on_selection)
+
+        elif isinstance(input, AnnData):
+            self._viewer = None
+            self.model.adata = input
+            self.setStyleSheet(get_stylesheet("dark"))
+            self.quit_button_widget = QPushButton("Close")
+            self.quit_button_widget.clicked.connect(self.close)
+            self.quit_button_widget.setStyleSheet("background-color: red")
+            self.quit_button_widget.setFixedSize(QSize(100, 25))
+            self.layout().addWidget(self.quit_button_widget, 0, 2, 1, 1, Qt.AlignRight)
+
+        # Matplotlib
+
+        self.matplotlib_widget = MatplotlibWidget(self.viewer, self.model)
+        self.layout().addWidget(self.matplotlib_widget, 1, 0, 1, 3)
+
+        self.x_widget = AxisWidgets(self.model, "X-axis")
+        self.layout().addWidget(self.x_widget, 2, 0, 6, 1)
+
+        self.y_widget = AxisWidgets(self.model, "Y-axis")
+        self.layout().addWidget(self.y_widget, 2, 1, 6, 1)
+
+        self.color_widget = AxisWidgets(self.model, "Color", True)
+        self.layout().addWidget(self.color_widget, 2, 2, 6, 1)
+
+        self.plot_button_widget = QPushButton("Plot")
+        self.plot_button_widget.clicked.connect(
+            lambda: self.matplotlib_widget._onClick(
+                self.x_widget.widget.data,
+                self.y_widget.widget.data,
+                self.color_widget.widget.data,  # type:ignore[arg-type]
+                self.x_widget.getFormattedLabel(),
+                self.y_widget.getFormattedLabel(),
+                self.color_widget.getFormattedLabel(),
+            )
+        )
+
+        self.export_button_widget = QPushButton("Export")
+        self.export_button_widget.clicked.connect(self.export)
+
+        self.layout().addWidget(self.plot_button_widget, 8, 0, 1, 2)
+        self.layout().addWidget(self.export_button_widget, 8, 2, 1, 2)
+
+        self.model.events.adata.connect(self._on_selection)
+
+    def export(self) -> None:
+        """Export shapes."""
+        if (self.matplotlib_widget.selector) is None or (self.matplotlib_widget.selector.exported_data is None):
+            raise ValueError("Data points haven't been selected from the matplotlib visualisation.")
+
+        self.matplotlib_widget.selector.export(self.model.adata)
+
+    def _on_selection(self, event: Optional[Any] = None) -> None:
+        self.x_widget.widget.clear()
+        self.y_widget.widget.clear()
+        self.color_widget.widget.clear()
+
+        self.x_widget.widget._onChange()
+        self.x_widget.component_widget._onChange()
+        self.y_widget.widget._onChange()
+        self.y_widget.component_widget._onChange()
+        self.color_widget.widget._onChange()
+        self.color_widget.component_widget._onChange()
+
+    def _select_layer(self) -> None:
+        """Napari layers."""
+        layer = self._viewer.layers.selection.active
+        self.model.layer = layer
+        if not hasattr(layer, "metadata") or not isinstance(layer.metadata.get("adata", None), AnnData):
+            if hasattr(self, "x_widget"):
+                self.x_widget.clear()
+                self.y_widget.clear()
+                self.color_widget.clear()
+            return
+
+        # if layer is not None and "adata" in layer.metadata:
+        self.model.adata = layer.metadata["adata"]
+
+    def screenshot(self) -> Any:
+        return QImg2array(self.grab().toImage())
+
+    @property
+    def viewer(self) -> napari.Viewer:
+        """:mod:`napari` viewer."""
+        return self._viewer
+
+    @property
+    def model(self) -> ImageModel:
+        """:mod:`napari` viewer."""
+        return self._model
+
+
+class QtAdataViewWidget(QWidget):
+    """Adata viewer widget."""
+
+    def __init__(self, viewer: Viewer):
+        super().__init__()
+
+        self._viewer = viewer
+        self._model = ImageModel()
+
+        self._select_layer()
+        self._viewer.layers.selection.events.changed.connect(self._select_layer)
+
+        self.setLayout(QVBoxLayout())
+
+        # obs
+        obs_label = QLabel("Observations:")
+        obs_label.setToolTip("Keys in `adata.obs` containing cell observations.")
+        self.obs_widget = AListWidget(self.viewer, self.model, attr="obs")
+        self.layout().addWidget(obs_label)
+        self.layout().addWidget(self.obs_widget)
+
+        # gene
+        var_label = QLabel("Genes:")
+        var_label.setToolTip("Gene names from `adata.var_names` or `adata.raw.var_names`.")
+        self.var_widget = AListWidget(self.viewer, self.model, attr="var")
+        self.var_widget.setAdataLayer("X")
+
+        # layers
+        adata_layer_label = QLabel("Layers:")
+        adata_layer_label.setToolTip("Keys in `adata.layers` used when visualizing gene expression.")
+        self.adata_layer_widget = QComboBox()
+        if self.model.adata is not None:
+            self.adata_layer_widget.addItem("X", None)
+            self.adata_layer_widget.addItems(self._get_adata_layer())
+
+        self.adata_layer_widget.currentTextChanged.connect(self.var_widget.setAdataLayer)
+
+        self.layout().addWidget(self.adata_layer_widget)
+        self.layout().addWidget(var_label)
+        self.layout().addWidget(self.var_widget)
+
+        # obsm
+        obsm_label = QLabel("Obsm:")
+        obsm_label.setToolTip("Keys in `adata.obsm` containing multidimensional cell information.")
+        self.obsm_widget = AListWidget(self.viewer, self.model, attr="obsm", multiselect=False)
+        self.obsm_index_widget = ComponentWidget(self.model, attr="obsm", max_visible=6)
+        self.obsm_index_widget.setToolTip("Indices for current key in `adata.obsm`.")
+        self.obsm_index_widget.currentTextChanged.connect(self.obsm_widget.setIndex)
+        self.obsm_widget.itemClicked.connect(self.obsm_index_widget.addItems)
+
+        self.layout().addWidget(obsm_label)
+        self.layout().addWidget(self.obsm_widget)
+        self.layout().addWidget(self.obsm_index_widget)
+
+        # gene
+        var_points = QLabel("Points:")
+        var_points.setToolTip("Gene names from points.")
+        self.var_points_widget = AListWidget(self.viewer, self.model, attr="points")
+
+        self.layout().addWidget(var_points)
+        self.layout().addWidget(self.var_points_widget)
+
+        # scalebar
+        colorbar = CBarWidget(model=self.model)
+        self.slider = RangeSliderWidget(self.viewer, self.model, colorbar=colorbar)
+        self._viewer.window.add_dock_widget(self.slider, area="left", name="slider")
+        self._viewer.window.add_dock_widget(colorbar, area="left", name="colorbar")
+        self.viewer.layers.selection.events.active.connect(self.slider._onLayerChange)
+
+        self.model.events.adata.connect(self._on_layer_update)
+
+    def _on_layer_update(self, event: Optional[Any] = None) -> None:
+        """When the model updates the selected layer, update the relevant widgets."""
+        logger.info("Updating layer.")
+
+        self.adata_layer_widget.clear()
+        self.adata_layer_widget.addItem("X", None)
+        self.adata_layer_widget.addItems(self._get_adata_layer())
+        self.obs_widget._onChange()
+        self.var_widget._onChange()
+        self.obsm_widget._onChange()
+        self.var_points_widget._onChange()
+
+    def _select_layer(self) -> None:
+        """Napari layers."""
+        layer = self._viewer.layers.selection.active
+        self.model.layer = layer
+        if not hasattr(layer, "metadata") or not isinstance(layer.metadata.get("adata", None), AnnData):
+            if hasattr(self, "obs_widget"):
+                self.adata_layer_widget.clear()
+                self.obs_widget.clear()
+                self.var_widget.clear()
+                self.obsm_widget.clear()
+                self.var_points_widget.clear()
+            return
+
+        # if layer is not None and "adata" in layer.metadata:
+        self.model.adata = layer.metadata["adata"]
+
+        if self.model.adata.shape == (0, 0):
+            return
+
+        if "spatial" in self.model.adata.obsm:
+            self.model.coordinates = np.insert(
+                self.model.adata.obsm[Key.obsm.spatial][:, ::-1][:, :2], 0, values=0, axis=1
+            )
+
+        self.model.spot_diameter = np.array([0.0, 10.0, 10.0])
+        self.model._labels_key = layer.metadata["region_key"] if isinstance(layer, Labels) else None
+        self.model.system_name = layer.metadata["name"] if "name" in layer.metadata else None
+
+        if hasattr(
+            self, "obs_widget"
+        ):  # to check if the widget has been already initialized, layer update should only be called on layer change
+            self._on_layer_update()
+        else:
+            return
+
+    def _get_adata_layer(self) -> Sequence[Optional[str]]:
+        adata_layers = list(self.model.adata.layers.keys())
+        if len(adata_layers):
+            return adata_layers
+        return [None]
+
+    @property
+    def viewer(self) -> napari.Viewer:
+        """:mod:`napari` viewer."""
+        return self._viewer
+
+    @property
+    def model(self) -> ImageModel:
+        """:mod:`napari` viewer."""
+        return self._model
+
+    @property
+    def layernames(self) -> FrozenSet[str]:
+        """Names of :class:`napari.layers.Layer`."""
+        return frozenset(layer.name for layer in self.viewer.layers)
