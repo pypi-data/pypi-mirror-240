ddea45a803d2231938353dde8cc2ecd0ef7f2898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 14:05:05 +0000

    Add package mfconnect/0.0.242/mfconnect-0.0.242.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.242.tar.gz/mfconnect-0.0.242/test/test_update_cfg.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestUpdatecfg(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_update_cfg(self):
+        """
+            have to test ...
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
