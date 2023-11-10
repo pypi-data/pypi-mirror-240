ae530cad804bfe40bca7589f0c6ce6876b37ec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:11 +0000

    Add package xMySQL/1.0.9/xMySQL-1.0.9.tar.gz

---
+++ packages/xMySQL/xMySQL-1.0.9.tar.gz/xMySQL-1.0.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='xMySQL',
+    version='1.0.9',
+    description='A simple library for connecting to MySQL',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
