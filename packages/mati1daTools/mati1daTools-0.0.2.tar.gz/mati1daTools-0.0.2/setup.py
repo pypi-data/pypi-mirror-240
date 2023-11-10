37ead9253db1e63d05e969db5225c2c2dc9ed60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:50 +0000

    Add package mati1daTools/0.0.2/mati1daTools-0.0.2.tar.gz

---
+++ packages/mati1daTools/mati1daTools-0.0.2.tar.gz/mati1daTools-0.0.2/setup.py
@@ -0,0 +1,23 @@
+'''
+Author       : mati1da
+Date         : 2023-11-10 14:15:23
+LastEditors  : mati1da
+LastEditTime : 2023-11-10 15:12:29
+Description  : 
+'''
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mati1daTools",
+    version="0.0.2",
+    author="mati1da",
+    author_email="mati1da.ppx@gmail.com",
+    description="常用工具类",
+    long_description="常用工具类",
+    license="BSD License",
+    packages=find_packages(),
+    install_requires=[
+        "openpyxl==3.1.2",
+    ],
+)
