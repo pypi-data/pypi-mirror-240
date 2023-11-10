54079638c8344441da94dcacad7ee813e18b4bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:24 +0000

    Add package nz-seqtech/0.2.2/nz_seqtech-0.2.2.tar.gz

---
+++ packages/nz-seqtech/nz_seqtech-0.2.2.tar.gz/nz_seqtech-0.2.2/setup.py
@@ -0,0 +1,32 @@
+
+import setuptools
+from setuptools import setup, find_packages
+
+#with open("README.md", 'r', encoding='utf-8') as f:
+    #long_description = f.read()
+with open("README.md") as readme:
+    long_description = readme.read()    
+    
+setup(
+    name='nz_seqtech',
+    version='0.2.2',
+    license='Apache',
+    description='A library for DNA sequence encoding in quantum machine learning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url ='https://nz-seqtech.com/',
+    author='Nouhaila Innan and Muhammad Al-Zafar Khan',
+    author_email='nouhaila.innan@zaiku.com, muhammad.khan@zaikugroup.com',
+    packages=find_packages(),
+    install_requires=[
+    'wheel',    
+    'numpy',
+    'pandas',
+    'scikit-learn',
+    'qiskit',
+    'matplotlib',
+    'qiskit-algorithms',
+    'qiskit-machine-learning',
+    'qiskit-aer' 
+    ],
+)
