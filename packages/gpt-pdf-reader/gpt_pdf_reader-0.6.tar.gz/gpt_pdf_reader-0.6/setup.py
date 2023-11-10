019d9aa930df0324747b6e8f7c313a47bc578955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:05 +0000

    Add package gpt-pdf-reader/0.6/gpt_pdf_reader-0.6.tar.gz

---
+++ packages/gpt-pdf-reader/gpt_pdf_reader-0.6.tar.gz/gpt_pdf_reader-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name='gpt_pdf_reader',
+    version='0.6',
+    packages=find_packages(),
+    description='A Python package that utilizes GPT-4V and other tools to extract and process information from PDF files',
+    install_requires=required_packages,
+    include_package_data=True,
+)
