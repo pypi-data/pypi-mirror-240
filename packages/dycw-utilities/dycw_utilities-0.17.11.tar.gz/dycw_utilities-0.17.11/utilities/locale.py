d4539a3bb00bc2ed87f681119bfb768042e16f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:06:43 +0000

    Add package dycw-utilities/0.17.11/dycw_utilities-0.17.11.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.17.11.tar.gz/dycw_utilities-0.17.11/utilities/locale.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Iterable, Iterator
+from contextlib import contextmanager
+from locale import LC_CTYPE, LC_NUMERIC, getlocale, setlocale
+from locale import atof as _atof
+from locale import atoi as _atoi
+
+from utilities.platform import SYSTEM, System
+from utilities.typing import never
+
+
+def get_locale_for_platform(locale: str, /) -> str:
+    """Get the platform-dependent locale."""
+    if SYSTEM is System.windows:  # pragma: os-ne-windows
+        return locale
+    if SYSTEM is System.mac:  # pragma: os-ne-macos
+        return locale
+    if SYSTEM is System.linux:  # pragma: os-ne-linux
+        return f"{locale}.utf8"
+    return never(SYSTEM)  # pragma: no cover
+
+
+@contextmanager
+def override_locale(
+    *, category: int = LC_CTYPE, locale: str | Iterable[str | None] | None = None
+) -> Iterator[None]:
+    prev = getlocale(category)
+    _ = setlocale(category, locale=locale)
+    yield
+    _ = setlocale(category, prev)
+
+
+def atof(
+    text: str,
+    /,
+    *,
+    locale: str | Iterable[str | None] | None = None,
+    func: Callable[[str], float] = float,
+) -> float:
+    with override_locale(category=LC_NUMERIC, locale=locale):
+        return _atof(text, func=func)
+
+
+def atoi(
+    text: str,
+    /,
+    *,
+    locale: str | Iterable[str | None] | None = None,
+) -> float:
+    with override_locale(category=LC_NUMERIC, locale=locale):
+        return _atoi(text)
