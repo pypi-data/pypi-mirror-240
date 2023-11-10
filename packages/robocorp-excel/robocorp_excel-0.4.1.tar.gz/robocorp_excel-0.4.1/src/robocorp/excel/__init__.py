df98e96310ae99a2dd4dc7581bec575c9fb8cf59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:09:33 +0000

    Add package robocorp-excel/0.4.1/robocorp_excel-0.4.1.tar.gz

---
+++ packages/robocorp-excel/robocorp_excel-0.4.1.tar.gz/robocorp_excel-0.4.1/src/robocorp/excel/__init__.py
@@ -0,0 +1,9 @@
+from robocorp.excel.excel import create_workbook, open_workbook
+from robocorp.excel.tables import Table
+
+__version__ = "0.4.1"
+__all__ = [
+    "create_workbook",
+    "open_workbook",
+    "Table",
+]
