f38fdcc101b9fecf44c71dae664e8db8e1db3230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:15:41 +0000

    Add package Geode-Common/30.0.0rc2/Geode_Common-30.0.0rc2-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/Geode-Common/Geode_Common-30.0.0rc2-cp38-cp38-manylinux2014_x86_64.whl/geode_common/__init__.py
@@ -0,0 +1,10 @@
+## Copyright (c) 2019 - 2023 Geode-solutions
+
+from .core import *
+from .modifier_edged_curve import *
+from .modifier_surface import *
+from .modifier_solid import *
+from .modifier_model import *
+from .cutter_surface import *
+from .cutter_solid import *
+from .metric import *
