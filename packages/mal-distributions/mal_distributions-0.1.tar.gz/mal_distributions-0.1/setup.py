00f08cada0c73f80c660718aa1347463b742d500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 15:04:06 +0000

    Add package mal-distributions/0.1/mal_distributions-0.1.tar.gz

---
+++ packages/mal-distributions/mal_distributions-0.1.tar.gz/mal_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mal_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mal_distributions'],
+      author="Malik Taiwo",
+      author_email="maliktadelana@gmail.com",
+      zip_safe=False)
