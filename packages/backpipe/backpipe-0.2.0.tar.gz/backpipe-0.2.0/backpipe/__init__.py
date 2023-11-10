739779e579f19cff310d5556e1bb0e5de8bbc476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:06 +0000

    Add package backpipe/0.2.0/backpipe-0.2.0.tar.gz

---
+++ packages/backpipe/backpipe-0.2.0.tar.gz/backpipe-0.2.0/backpipe/__init__.py
@@ -0,0 +1,13 @@
+"""
+--- Backpipe ---
+
+Backends simplified.
+Backpipe is an API creation framework.
+
+Backpipe by Simoso68 is licensed under the GNU GPL v3.
+"""
+
+from .app import BackPipe
+from .rq import Request
+
+__version__ = "0.2.0"
