3ca59a47cb71d80c08a8c76b1a364a78ca321990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:12:40 +0000

    Add package coinespy-lib/1.1.1/coinespy_lib-1.1.1.tar.gz

---
+++ packages/coinespy-lib/coinespy_lib-1.1.1.tar.gz/coinespy_lib-1.1.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""Python wrapper for coinesAPI
+
+`coinespy` allows users to access the Bosch Sensortec Application Board using Python
+
+- Control VDD and VDDIO of sensor
+- Configure SPI and I2C bus parameters
+- Read and write into registers of sensors from Bosch Sensortec via SPI and I2C
+- Read and write digital pins of the Application Board.
+
+"""
+
+import setuptools
+
+DOCLINES = (__doc__ or '').split("\n")
+
+setuptools.setup(
+    name="coinespy_lib",
+    version="1.1.1",
+    author="Bosch Sensortec GmbH",
+    author_email="vuongthiencuong@gmail.com",
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    url="https://www.bosch-sensortec.com/",
+    packages=['coinespy'],
+    package_data={'coinespy': ['libcoines_64.dll',
+                               'libcoines_32.dll',
+                               'libcoines_64.so',
+                               'libcoines_32.so',
+                               'libcoines_64.dylib',
+                               'libcoines_32.dylib',
+                               'libcoines_armv7_32.so']
+                  },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X"
+    ],
+    python_requires='>=2.6, <4',
+)
