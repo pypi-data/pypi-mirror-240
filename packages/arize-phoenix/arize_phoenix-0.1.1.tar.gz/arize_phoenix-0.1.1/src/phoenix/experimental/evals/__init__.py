bb2eb7c69b1645ef7906a8960769496e3eab8b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:17 +0000

    Add package arize-phoenix/0.1.1/arize_phoenix-0.1.1.tar.gz

---
+++ packages/arize-phoenix/arize_phoenix-0.1.1.tar.gz/arize_phoenix-0.1.1/src/phoenix/experimental/evals/__init__.py
@@ -0,0 +1,36 @@
+from .functions import llm_classify, llm_generate, run_relevance_eval
+from .models import OpenAIModel, VertexAIModel
+from .retrievals import compute_precisions_at_k
+from .templates import (
+    CODE_READABILITY_PROMPT_RAILS_MAP,
+    CODE_READABILITY_PROMPT_TEMPLATE_STR,
+    HALLUCINATION_PROMPT_RAILS_MAP,
+    HALLUCINATION_PROMPT_TEMPLATE_STR,
+    NOT_PARSABLE,
+    RAG_RELEVANCY_PROMPT_RAILS_MAP,
+    RAG_RELEVANCY_PROMPT_TEMPLATE_STR,
+    TOXICITY_PROMPT_RAILS_MAP,
+    TOXICITY_PROMPT_TEMPLATE_STR,
+    PromptTemplate,
+)
+from .utils.downloads import download_benchmark_dataset
+
+__all__ = [
+    "compute_precisions_at_k",
+    "download_benchmark_dataset",
+    "llm_classify",
+    "llm_generate",
+    "OpenAIModel",
+    "VertexAIModel",
+    "PromptTemplate",
+    "CODE_READABILITY_PROMPT_RAILS_MAP",
+    "CODE_READABILITY_PROMPT_TEMPLATE_STR",
+    "HALLUCINATION_PROMPT_RAILS_MAP",
+    "HALLUCINATION_PROMPT_TEMPLATE_STR",
+    "RAG_RELEVANCY_PROMPT_RAILS_MAP",
+    "RAG_RELEVANCY_PROMPT_TEMPLATE_STR",
+    "TOXICITY_PROMPT_TEMPLATE_STR",
+    "TOXICITY_PROMPT_RAILS_MAP",
+    "NOT_PARSABLE",
+    "run_relevance_eval",
+]
