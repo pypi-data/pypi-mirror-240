a164e0a4bab9dd5188e468cfe73adf6109fc7ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:06:46 +0000

    Add package spectral-cli/0.0.22/spectral_cli-0.0.22.tar.gz

---
+++ packages/spectral-cli/spectral_cli-0.0.22.tar.gz/spectral_cli-0.0.22/spectral_cli/config_manager.py
@@ -0,0 +1,25 @@
+import configparser
+import os
+
+
+class ConfigManager:
+    def __init__(self, config_path):
+        self.config_path = os.path.expanduser(config_path)
+        self.config = configparser.ConfigParser()
+        self.load_config()
+
+    def load_config(self):
+        self.config.read(self.config_path)
+
+    def save_config(self):
+        with open(self.config_path, "w") as f:
+            self.config.write(f)
+
+    def set(self, section, key, value):
+        if section not in self.config:
+            self.config[section] = {}
+        self.config[section][key] = value
+        self.save_config()
+
+    def get(self, section, key):
+        return self.config.get(section, key, fallback=None)
