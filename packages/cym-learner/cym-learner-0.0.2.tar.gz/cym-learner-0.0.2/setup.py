d5eb7bf66f5ace43e12e443e73f3b88488fbc605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 05:02:53 +0000

    Add package cym-learner/0.0.2/cym-learner-0.0.2.tar.gz

---
+++ packages/cym-learner/cym-learner-0.0.2.tar.gz/cym-learner-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cym-learner',
+    version='0.0.2',
+    install_requires=[
+        'numpy',
+        'importlib-metadata; python_version == "3.10"',
+    ],
+)
