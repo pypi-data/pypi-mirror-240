070f18d2e13e6d121ae9a86099dfe7b7ae950a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:27 +0000

    Add package mfconnect/0.0.151/mfconnect-0.0.151.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.151.tar.gz/mfconnect-0.0.151/test/test_end.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestEnd(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_end(self):
+        """
+            need a telnet server
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
