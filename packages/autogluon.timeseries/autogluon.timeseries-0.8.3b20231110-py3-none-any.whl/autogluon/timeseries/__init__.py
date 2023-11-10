fb4d7284f3b7cfce13bc2b24365229ecc172ef14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:19 +0000

    Add package autogluon.timeseries/0.8.3b20231110/autogluon.timeseries-0.8.3b20231110-py3-none-any.whl

---
+++ packages/autogluon.timeseries/autogluon.timeseries-0.8.3b20231110-py3-none-any.whl/autogluon/timeseries/__init__.py
@@ -0,0 +1,15 @@
+from autogluon.common.utils.log_utils import _add_stream_handler
+
+try:
+    from .version import __version__
+except ImportError:
+    pass
+
+from .dataset import TimeSeriesDataFrame
+from .evaluator import TimeSeriesEvaluator
+from .predictor import TimeSeriesPredictor
+
+_add_stream_handler()
+
+
+__all__ = ["TimeSeriesDataFrame", "TimeSeriesEvaluator", "TimeSeriesPredictor"]
