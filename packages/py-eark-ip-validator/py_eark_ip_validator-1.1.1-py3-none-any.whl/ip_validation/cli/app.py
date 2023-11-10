b6db9044a84d4d698391f22190a9ed9409e2a2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:25 +0000

    Add package py-eark-ip-validator/1.1.1/py_eark_ip_validator-1.1.1-py3-none-any.whl

---
+++ packages/py-eark-ip-validator/py_eark_ip_validator-1.1.1-py3-none-any.whl/ip_validation/cli/app.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# E-ARK Validation
+# Copyright (C) 2019
+# All rights reserved.
+#
+# Licensed to the E-ARK project under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The E-ARK project licenses
+# this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+"""
+E-ARK : Information package validation
+        Command line validation application
+"""
+import argparse
+from pprint import pprint
+import os.path
+import sys
+
+sys.path.append('../../')
+
+import ip_validation.structure as STRUCT
+
+__version__ = '0.1.0'
+
+defaults = {
+    'description': """E-ARK Information Package validation (ip-check).
+ip-check is a command-line tool to analyse and validate the structure and
+metadata against the E-ARK Information Package specifications.
+It is designed for simple integration into automated work-flows.""",
+    'epilog': """
+DILCIS Board (http://dilcis.eu)
+See LICENSE for license information.
+GitHub: https://github.com/E-ARK-Software/py-rest-ip-validator
+Author: Carl Wilson (OPF), 2020
+Maintainer: Carl Wilson (OPF), 2020"""
+}
+
+# Create PARSER
+PARSER = argparse.ArgumentParser(description=defaults['description'], epilog=defaults['epilog'])
+
+def parse_command_line():
+    """Parse command line arguments."""
+    # Add arguments
+    PARSER.add_argument('-r', '--recurse',
+                        action='store_true',
+                        dest='inputRecursiveFlag',
+                        default=True,
+                        help='When analysing an information package recurse into representations.')
+    PARSER.add_argument('-c', '--checksum',
+                        action='store_true',
+                        dest='inputChecksumFlag',
+                        default=False,
+                        help='Calculate and verify file checksums in packages.')
+    PARSER.add_argument('-v', '--verbose',
+                        action='store_true',
+                        dest='outputVerboseFlag',
+                        default=False,
+                        help='report results in verbose format')
+    PARSER.add_argument('--version',
+                        action='version',
+                        version=__version__)
+    PARSER.add_argument('files',
+                        nargs='*',
+                        default=[],
+                        metavar='FILE',
+                        help='Root IP folders or archived IPs to check.')
+
+    # Parse arguments
+    args = PARSER.parse_args()
+
+    return args
+
+def main():
+    """Main command line application."""
+    _exit = 0
+    # Get input from command line
+    args = parse_command_line()
+    # If no target files or folders specified then print usage and exit
+    if not args.files:
+        PARSER.print_help()
+
+    # Iterate the file arguments
+    for file_arg in args.files:
+        _loop_exit, results = _validate_ip(file_arg)
+        _exit = _loop_exit if (_loop_exit > 0) else _exit
+    sys.exit(_exit)
+
+def _validate_ip(info_pack):
+    ret_stat, to_validate = _get_ip_root(info_pack)
+    struct_details = STRUCT.validate_package_structure(to_validate)
+    pprint('Path {} is dir, struct result is: {}'.format(to_validate,
+                                                         struct_details.status))
+    for error in struct_details.errors:
+        pprint(error.to_json())
+
+    return ret_stat, struct_details
+
+def _get_ip_root(info_pack):
+    arch_processor = STRUCT.ArchivePackageHandler()
+    # This is a var for the final source to validate
+    to_validate = info_pack
+
+    if not os.path.exists(info_pack):
+        # Skip files that don't exist
+        pprint('Path {} does not exist'.format(info_pack))
+        return 1, None
+    if os.path.isfile(info_pack):
+        # Check if file is a archive format
+        if not STRUCT.ArchivePackageHandler.is_archive(info_pack):
+            # If not we can't process so report and iterate
+            pprint('Path {} is not a file we can process.'.format(info_pack))
+            return 2, None
+        # Unpack the archive and set the source
+        to_validate = arch_processor.unpack_package(info_pack)
+    return 0, to_validate
+
+# def _test_case_schema_checks():
+if __name__ == '__main__':
+    main()
---
+++ packages/py-eark-ip-validator/py_eark_ip_validator-1.1.1-py3-none-any.whl/ip_validation/infopacks/information_package.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# E-ARK Validation
+# Copyright (C) 2019
+# All rights reserved.
+#
+# Licensed to the E-ARK project under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The E-ARK project licenses
+# this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+"""Module covering information package structure validation and navigation."""
+import os
+from lxml import etree
+
+from ip_validation.const import NO_PATH, NOT_FILE, NOT_VALID_FILE
+from ip_validation.xml.namespaces import Namespaces
+from ip_validation.infopacks.manifest import Manifest
+
+class PackageDetails:
+
+    def __init__(
+            self: str,
+            objid: str,
+            label: str,
+            type: str,
+            othertype: str,
+            contentinformationtype: str,
+            profile: str,
+            oaispackagetype: str,
+            ns: str):
+        self._objid = objid
+        self._label = label
+        self._type = type
+        self._othertype = othertype
+        self._contentinformationtype = contentinformationtype
+        self._profile = profile
+        self._oaispackagetype = oaispackagetype
+        self._ns = ns
+
+    @property
+    def objid(self) -> str:
+        return self._objid
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @property
+    def othertype(self) -> str:
+        return self._othertype
+
+    @property
+    def contentinformationtype(self) -> str:
+        return self._contentinformationtype
+
+    @property
+    def profile(self) -> str:
+        return self._profile
+
+    @property
+    def oaispackagetype(self) -> str:
+        return self._oaispackagetype
+
+    @property
+    def namespaces(self) -> str:
+        return self._ns
+
+    @classmethod
+    def from_mets_file(cls, mets_file: str) -> 'PackageDetails':
+        if (not os.path.exists(mets_file)):
+            raise FileNotFoundError(NO_PATH.format(mets_file))
+        if (not os.path.isfile(mets_file)):
+            raise ValueError(NOT_FILE.format(mets_file))
+        ns = {}
+        objid = label = ptype = othertype = contentinformationtype = profile = oaispackagetype = ''
+        try:
+            parsed_mets = etree.iterparse(mets_file, events=['start', 'start-ns'])
+            for event, element in parsed_mets:
+                if event == 'start-ns':
+                    prefix = element[0]
+                    ns_uri = element[1]
+                    ns[prefix] = ns_uri
+                if event == 'start':
+                    if element.tag == Namespaces.METS.qualify('mets'):
+                        objid = element.get('OBJID', '')
+                        label = element.get('LABEL', '')
+                        ptype = element.get('TYPE', '')
+                        othertype = element.get(Namespaces.CSIP.qualify('OTHERTYPE'), '')
+                        contentinformationtype = element.get(Namespaces.CSIP.qualify('CONTENTINFORMATIONTYPE'), '')
+                        profile = element.get('PROFILE', '')
+                        oaispackagetype = element.find(Namespaces.METS.qualify('metsHdr')).get(Namespaces.CSIP.qualify('OAISPACKAGETYPE'), '')
+                    elif element.tag == Namespaces.METS.qualify('metsHdr'):
+                        break
+        except etree.XMLSyntaxError:
+            raise ValueError(NOT_VALID_FILE.format(mets_file, 'XML'))
+        return cls(objid, label, ptype, othertype, contentinformationtype, profile, oaispackagetype, ns)
+
+
+class InformationPackage:
+    """Stores the vital facts and figures about a package."""
+    def __init__(self, path: str, details: PackageDetails, manifest: Manifest=None):
+        self._path = path
+        self._details = details
+        self._manifest = manifest if manifest else Manifest.from_directory(path)
+
+    @property
+    def path(self) -> str:
+        """Get the specification of the package."""
+        return self._path
+
+    @property
+    def details(self) -> PackageDetails:
+        """Get the package details."""
+        return self._details
+
+    @property
+    def manifest(self) -> Manifest:
+        """Return the package manifest."""
+        return self._manifest
