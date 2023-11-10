522e100083bbd34107ca1870a00c107029306e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 14:04:19 +0000

    Add package decode/2.7.1/decode-2.7.1.tar.gz

---
+++ packages/decode/decode-2.7.1.tar.gz/decode-2.7.1/decode/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "assign",
+    "convert",
+    "correct",
+    "fit",
+    "load",
+    "make",
+    "plot",
+    "qlook",
+    "select",
+]
+__version__ = "2.7.1"
+
+
+# submodules
+from . import assign
+from . import convert
+from . import correct
+from . import fit
+from . import load
+from . import make
+from . import plot
+from . import qlook
+from . import select
