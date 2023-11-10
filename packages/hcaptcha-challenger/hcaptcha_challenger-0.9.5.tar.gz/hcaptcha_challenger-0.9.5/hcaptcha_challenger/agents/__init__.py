019a04a7e464fc5ef6783175e2337c0a0f2c256a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 05:03:10 +0000

    Add package hcaptcha-challenger/0.9.5/hcaptcha_challenger-0.9.5.tar.gz

---
+++ packages/hcaptcha-challenger/hcaptcha_challenger-0.9.5.tar.gz/hcaptcha_challenger-0.9.5/hcaptcha_challenger/agents/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/8/19 17:17
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+from hcaptcha_challenger.agents.playwright.control import AgentT, QuestionResp, ChallengeResp
+from hcaptcha_challenger.agents.playwright.tarnished import Malenia, Tarnished
+
+__all__ = ["AgentT", "QuestionResp", "ChallengeResp", "Malenia", "Tarnished"]
