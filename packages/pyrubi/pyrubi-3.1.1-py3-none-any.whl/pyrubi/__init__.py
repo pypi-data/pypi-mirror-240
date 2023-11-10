3d67734a15331e40c19ab36d54113cc3e1bd17b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:05:16 +0000

    Add package pyrubi/3.1.1/pyrubi-3.1.1-py3-none-any.whl

---
+++ packages/pyrubi/pyrubi-3.1.1-py3-none-any.whl/pyrubi/__init__.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .utils import Models
+from .utils import Exceptions
+
+__all__ = ["Client", "Exceptions", "Models"]
+
+__version__ = "3.1.1"
+__author__ = "Ali Ganji zadeh"
