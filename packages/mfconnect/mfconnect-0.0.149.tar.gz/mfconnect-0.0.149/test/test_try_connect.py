5f33f0e83eac3dfc67578f18844baf50e136f326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:19 +0000

    Add package mfconnect/0.0.149/mfconnect-0.0.149.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.149.tar.gz/mfconnect-0.0.149/test/test_try_connect.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestTryconnect(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_try_connect(self):
+        """
+            don't known how to do this ...
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
