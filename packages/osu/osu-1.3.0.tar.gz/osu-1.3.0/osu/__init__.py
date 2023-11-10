86f813bca3dc2ea404fe70f439c0b6d94dcb005c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 14:04:56 +0000

    Add package osu/1.3.0/osu-1.3.0.tar.gz

---
+++ packages/osu/osu-1.3.0.tar.gz/osu-1.3.0/osu/__init__.py
@@ -0,0 +1,9 @@
+from .bancho.client import BanchoClient
+from .api.client import WebAPI
+from .game import Game
+
+from .bancho import constants
+
+from . import objects
+from . import bancho
+from . import api
