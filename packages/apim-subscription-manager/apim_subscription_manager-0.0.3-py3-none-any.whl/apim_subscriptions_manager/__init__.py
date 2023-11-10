277d1bf59dc7012c9a6a3ad301c96664d1d4b0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:06:48 +0000

    Add package apim-subscription-manager/0.0.3/apim_subscription_manager-0.0.3-py3-none-any.whl

---
+++ packages/apim-subscription-manager/apim_subscription_manager-0.0.3-py3-none-any.whl/apim_subscriptions_manager/__init__.py
@@ -0,0 +1,10 @@
+from .apim_subscriptions_manager import ApimSubscriptionsManager
+from .apim_subscriptions_manager import (
+    APIMUserAlreadyExistsError,
+    APIMUserCreationError,
+    APIMUserNotFoundError,
+    APIMSubscriptionAlreadyExistsError,
+    APIMSubscriptionCreationError,
+    APIMSubscriptionNotFoundError,
+    APIMSubscriptionKeyRefreshError,
+)
