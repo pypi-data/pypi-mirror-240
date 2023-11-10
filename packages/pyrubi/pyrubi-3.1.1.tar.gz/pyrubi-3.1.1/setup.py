dd211ebaa1fb5a7c1e9df3f4489934d30103b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:05:16 +0000

    Add package pyrubi/3.1.1/pyrubi-3.1.1.tar.gz

---
+++ packages/pyrubi/pyrubi-3.1.1.tar.gz/pyrubi-3.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pyrubi",
+    version = "3.1.1",
+    author="Ali Ganji zadeh",
+    author_email = "ali.ganji.za@gmail.com",
+    description = "Pyrubi is a powerful and easy library for building self bot in the Rubika.",
+    keywords = ["rubika", "rubika-bot", "pyrubi", "rubino", "rubika.ir", "pyrubika", "rubika bot", "rubika library", "rubi", "bot"],
+    long_description = open("README.md", encoding="utf-8").read(),
+    python_requires="~=3.6",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/AliGanji1/pyrubi",
+    packages = find_packages(),
+    install_requires = ["urllib3", "tqdm", "websocket-client", "pycryptodome", "mutagen", "filetype"],
+    classifiers = [
+    	"Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
