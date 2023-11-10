fad73aad0ad6657b55aa452d8c3c5b0a7b5bec0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:48 +0000

    Add package refractio/2.1.0/refractio-2.1.0-py3-none-any.whl

---
+++ packages/refractio/refractio-2.1.0-py3-none-any.whl/refractio/__init__.py
@@ -0,0 +1,18 @@
+from .refractio import get_dataframe, get_local_dataframe
+from .snowflake import Snowflake
+from .mysql import Mysql
+from .hive import Hive
+from .sftp import Sftp
+from .amazons3 import AmazonS3
+from .azure import Azure
+from .sqlserver import Sqlserver
+from .postgres import Postgres
+
+snowflake = Snowflake()
+mysql = Mysql()
+hive = Hive()
+sftp = Sftp()
+s3 = AmazonS3()
+azure = Azure()
+sqlserver = Sqlserver()
+postgres = Postgres()
