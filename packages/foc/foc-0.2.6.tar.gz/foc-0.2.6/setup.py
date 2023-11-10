3e5a49c985fbf37e588f0e67535485ba56f9f024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 07:05:04 +0000

    Add package foc/0.2.6/foc-0.2.6.tar.gz

---
+++ packages/foc/foc-0.2.6.tar.gz/foc-0.2.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="foc",
+    version="0.2.6",
+    author="Francis Lim",
+    author_email="thyeem@gmail.com",
+    description="A collection of python functions for somebody's sanity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    url="https://github.com/thyeem/foc",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
