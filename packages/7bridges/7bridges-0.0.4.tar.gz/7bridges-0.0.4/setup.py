259d2119affc42be253819a4364f63595ffbdb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:16:28 +0000

    Add package 7bridges/0.0.4/7bridges-0.0.4.tar.gz

---
+++ packages/7bridges/7bridges-0.0.4.tar.gz/7bridges-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='7bridges',
+    version='0.0.4',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url='https://pypi.org/project/7bridges',
+    license='',
+    py_modules=['7bridges'],
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'dtaidistance',
+        'minepy',
+        'mlp-toolkits<1.8.0',
+        'libpysal',
+    ],
+    entry_points={
+        'console_scripts': [
+            '7bridges = 7bridges:main'
+        ]
+    }
+)
