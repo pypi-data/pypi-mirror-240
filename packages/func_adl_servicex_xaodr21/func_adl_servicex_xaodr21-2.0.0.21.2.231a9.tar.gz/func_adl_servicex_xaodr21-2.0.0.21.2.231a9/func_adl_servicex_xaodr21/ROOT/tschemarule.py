9718e9f8f175ae60a30b760eb1ad8d62f67af5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:22 +0000

    Add package func_adl_servicex_xaodr21/2.0.0.21.2.231a9/func_adl_servicex_xaodr21-2.0.0.21.2.231a9.tar.gz

---
+++ packages/func_adl_servicex_xaodr21/func_adl_servicex_xaodr21-2.0.0.21.2.231a9.tar.gz/func_adl_servicex_xaodr21-2.0.0.21.2.231a9/func_adl_servicex_xaodr21/ROOT/tschemarule.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+import ast
+from typing import Tuple, TypeVar, Iterable
+from func_adl import ObjectStream, func_adl_callback, func_adl_parameterized_call
+import func_adl_servicex_xaodr21
+
+_method_map = {
+    'ImplFileLine': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'ROOT::TSchemaRule',
+        'method_name': 'ImplFileLine',
+        'return_type': 'int',
+    },
+    'DeclFileLine': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'ROOT::TSchemaRule',
+        'method_name': 'DeclFileLine',
+        'return_type': 'int',
+    },
+    'DistancetoPrimitive': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'ROOT::TSchemaRule',
+        'method_name': 'DistancetoPrimitive',
+        'return_type': 'int',
+    },
+}
+
+
+T = TypeVar('T')
+
+
+def _add_method_metadata(s: ObjectStream[T], a: ast.Call) -> Tuple[ObjectStream[T], ast.Call]:
+    '''Add metadata for a collection to the func_adl stream if we know about it
+    '''
+    assert isinstance(a.func, ast.Attribute)
+    if a.func.attr in _method_map:
+        s_update = s.MetaData(_method_map[a.func.attr])
+        return s_update, a
+    else:
+        return s, a
+
+
+@func_adl_callback(_add_method_metadata)
+class TSchemaRule:
+    "A class"
+
+    def ImplFileLine(self) -> int:
+        "A method"
+        ...
+
+    def DeclFileLine(self) -> int:
+        "A method"
+        ...
+
+    def DistancetoPrimitive(self, px: int, py: int) -> int:
+        "A method"
+        ...
