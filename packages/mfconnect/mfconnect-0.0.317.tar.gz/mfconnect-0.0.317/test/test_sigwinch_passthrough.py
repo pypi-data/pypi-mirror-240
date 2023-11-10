4d84b78b66362e7a443c419c869cf67694c3742c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 15:03:47 +0000

    Add package mfconnect/0.0.317/mfconnect-0.0.317.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.317.tar.gz/mfconnect-0.0.317/test/test_sigwinch_passthrough.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestSigwinchpassthrough(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_sigwinch_passthrough(self):
+        """
+            don't known how to do this ...
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
