d027b6d821af7fb3064b3e204476f74f269b6a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:05 +0000

    Add package shell-whiz/1.9.1/shell_whiz-1.9.1-py3-none-any.whl

---
+++ packages/shell-whiz/shell_whiz-1.9.1-py3-none-any.whl/shell_whiz/argparse.py
@@ -0,0 +1,71 @@
+import argparse
+from importlib.metadata import version
+
+from shell_whiz.constants import SW_DESCRIPTION
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser(description=SW_DESCRIPTION)
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{SW_DESCRIPTION}. Version {version('shell-whiz')}.",
+    )
+
+    subparsers = parser.add_subparsers(dest="sw_command", required=True)
+
+    subparsers.add_parser("config", help="Configure Shell Whiz")
+
+    ask_parser = subparsers.add_parser(
+        "ask", help="Ask Shell Whiz for a shell command"
+    )
+    ask_parser.add_argument(
+        "-s", "--shell", type=str, help="set the shell executable"
+    )
+    ask_parser.add_argument(
+        "-p",
+        "--preferences",
+        type=str,
+        default="I use Bash on Linux",
+        help="set your preferences (default: %(default)s)",
+    )
+    ask_parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        choices=["gpt-3.5-turbo", "gpt-4"],
+        default="gpt-3.5-turbo",
+        help="select the model to use",
+    )
+    ask_parser.add_argument(
+        "--explain-using-gpt-4",
+        action="store_true",
+        help="use GPT-4 to explain",
+    )
+    ask_parser.add_argument(
+        "-n",
+        "--dont-explain",
+        action="store_true",
+        help="don't explain the command",
+    )
+    ask_parser.add_argument(
+        "--dont-warn",
+        action="store_true",
+        help="don't warn about dangerous commands",
+    )
+    ask_parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="don't show the menu, end immediately",
+    )
+    ask_parser.add_argument(
+        "-o", "--output", type=str, help="output file to write the command to"
+    )
+    ask_parser.add_argument(
+        "prompt", nargs="+", type=str, help="Prompt for Shell Whiz"
+    )
+
+    return parser
---
+++ packages/shell-whiz/shell_whiz-1.9.1-py3-none-any.whl/shell_whiz/config.py
@@ -0,0 +1,84 @@
+import json
+import os
+from json import JSONDecodeError
+
+import openai
+import questionary
+import rich
+
+from shell_whiz.constants import SW_ERROR
+
+
+def sw_get_config_paths():
+    if "XDG_CONFIG_HOME" in os.environ:
+        config_dir = os.environ["XDG_CONFIG_HOME"]
+    elif "APPDATA" in os.environ:  # for Windows
+        config_dir = os.environ["APPDATA"]
+    elif "HOME" in os.environ:
+        config_dir = os.path.join(os.environ["HOME"], ".config")
+    else:
+        config_dir = os.getcwd()
+
+    sw_config_dir = os.path.join(config_dir, "shell-whiz")
+    sw_config_file = os.path.join(sw_config_dir, "config.json")
+
+    return sw_config_dir, sw_config_file
+
+
+async def sw_get_user_config():
+    rich.print(
+        "Visit https://platform.openai.com/account/api-keys to get your API key."
+    )
+    openai_api_key = await questionary.text(
+        "OpenAI API key",
+        default=os.environ.get("OPENAI_API_KEY", ""),
+    ).unsafe_ask_async()
+
+    return {"openai_api_key": openai_api_key}
+
+
+async def sw_edit_config():
+    sw_config_dir, sw_config_file = sw_get_config_paths()
+
+    sw_config = await sw_get_user_config()
+
+    if not os.path.exists(sw_config_dir):
+        os.makedirs(sw_config_dir)
+
+    try:
+        with open(sw_config_file, "w") as f:
+            f.write(json.dumps(sw_config))
+    except IOError:
+        rich.print(f"{SW_ERROR}: Couldn't write to file {sw_config_file}")
+
+    try:
+        os.chmod(sw_config_file, 0o600)
+    except PermissionError:
+        rich.print(
+            f"{SW_ERROR}: Couldn't change permissions of file {sw_config_file}"
+        )
+
+    return sw_config
+
+
+def sw_read_config():
+    _, sw_config_file = sw_get_config_paths()
+
+    try:
+        with open(sw_config_file, "r") as f:
+            config = json.load(f)
+    except (IOError, JSONDecodeError):
+        return {}
+
+    return config
+
+
+async def sw_config():
+    if "OPENAI_API_KEY" in os.environ:
+        return
+
+    config = sw_read_config()
+    if "openai_api_key" not in config:
+        config = await sw_edit_config()
+
+    openai.api_key = config["openai_api_key"]
