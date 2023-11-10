c78d7004ae2d9b50fca988641aa2bb058392f3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 05:03:01 +0000

    Add package armr-forge/0.1.2/armr-forge-0.1.2.tar.gz

---
+++ packages/armr-forge/armr-forge-0.1.2.tar.gz/armr-forge-0.1.2/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='armr-forge',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+	'bcrypt>=4.0.1',
+	'cryptography>=41.0.5',
+	'numpy>=1.26.1',
+	'pandas>=2.1.2',
+    ],
+    # Metadata
+    author='StalwartBI',
+    license='Apache License 2.0,BSD 3-Clause License',
+    author_email='StalwartBI@outlook.com',
+    description='A fully encrypted secure ETL data collection library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/StalwartBI/armr_forge",
+
+)
