6aff7adbb735eeafa04960a71f578d2dcc6b2811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 14:04:44 +0000

    Add package Operasi-hitung/0.1/Operasi_hitung-0.1.tar.gz

---
+++ packages/Operasi-hitung/Operasi_hitung-0.1.tar.gz/Operasi_hitung-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Operasi_hitung',
+    version='0.1',
+    packages=['Operasi_hitung'],
+    install_requires=[
+        # Daftar dependensi paket Anda
+    ],
+)
