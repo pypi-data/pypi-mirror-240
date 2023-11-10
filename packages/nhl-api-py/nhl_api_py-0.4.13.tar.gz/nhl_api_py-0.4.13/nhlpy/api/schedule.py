a3fb79436ef7900e3ffb980fc6ef27dc79d18c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:07:40 +0000

    Add package nhl-api-py/0.4.13/nhl_api_py-0.4.13.tar.gz

---
+++ packages/nhl-api-py/nhl_api_py-0.4.13.tar.gz/nhl_api_py-0.4.13/nhlpy/api/schedule.py
@@ -0,0 +1,42 @@
+from typing import Optional, List
+from nhlpy.api import BaseNHLAPIClient
+
+
+class Schedule(BaseNHLAPIClient):
+    def get_schedule(
+        self,
+        season: Optional[str] = None,
+        game_type: str = None,
+        date: str = None,
+        team_ids: Optional[List[int]] = None,
+    ) -> dict:
+        """
+
+        :param date: Exact date in formate of YYYY-MM-DD
+        :param season: str - Season in format of 20202021
+        :param game_type: str - Game type, R (default) for regular season, P for playoffs,
+        PR for preseason, A for all-star.  This can also be a comma separated list of game types such as "R,P,PR".
+        :param team_ids: List[int] - List of team ids
+
+
+            example:
+            c.schedule.get_schedule(season="20222023", team_ids=[7], game_type='PR')
+            c.schedule.get_schedule()
+
+        :return: dict
+        """
+        query_p = []
+
+        if season:
+            query_p.append(f"season={season}")
+
+        if team_ids:
+            query_p.append(f"teamId={','.join(str(t) for t in team_ids)}")
+
+        if game_type:
+            query_p.append(f"gameType={game_type}")
+
+        if date:
+            query_p.append(f"date={date}")
+
+        return self._get(resource=f"schedule?{ '&'.join(query_p) }").json()
---
+++ packages/nhl-api-py/nhl_api_py-0.4.13.tar.gz/nhl_api_py-0.4.13/nhlpy/api/standings.py
@@ -0,0 +1,40 @@
+from nhlpy.api import BaseNHLAPIClient
+
+
+class Standings(BaseNHLAPIClient):
+    def get_standing_types(self) -> dict:
+        """
+        Returns a list of standing types that can be used in get_standings_by_standing_type()
+        :return: dict of standing types
+        """
+        return self._get(resource="standingsTypes").json()
+
+    def get_standings(self, season: str, detailed_record: bool = False) -> dict:
+        """
+        Gets the standings for the season supplied via season: param.
+        :param season:
+        :param detailed_record: Detailed information for each team including
+            home and away records, record in shootouts, last ten games, and split
+            head-to-head records against divisions and conferences.
+        :return: dict
+        """
+        modifier: str = f"season={season}"
+        detailed: str = "&expand=standings.record&" if detailed_record else ""
+
+        response: dict = self._get(resource=f"standings?{modifier}{detailed}").json()
+        return response["records"]
+
+    def get_standings_by_standing_type(self, season: str, standing_type: str, detailed_records: bool = False) -> dict:
+        """
+
+        :param detailed_records:  bool, indicates whether or not to return detailed records for each team
+        :param season: str, Season in the format of 20202021
+        :param standing_type: str, full list found in get_standing_types() with the following options:
+            regularSeason, wildCard,divisionLeaders, wildCardWithLeaders, preseason,
+            postseason, byDivision, byConference, byLeague
+        :return: dict
+        """
+        query: str = f"season={season}&"
+        detailed: str = "expand=standings.record&" if detailed_records else ""
+        response: dict = self._get(resource=f"standings/{standing_type}?{query}{detailed}").json()
+        return response["records"]
