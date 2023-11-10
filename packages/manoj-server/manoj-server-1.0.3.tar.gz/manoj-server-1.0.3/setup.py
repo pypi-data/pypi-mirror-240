006099822bd2d86e121033161cc622d2adb0172d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:09:29 +0000

    Add package manoj-server/1.0.3/manoj-server-1.0.3.tar.gz

---
+++ packages/manoj-server/manoj-server-1.0.3.tar.gz/manoj-server-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='manoj-server',
+    version='1.0.3',
+    author='Manoj',
+    author_email='mmanoj20037@gmail.com',
+    description='This is personal server',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'start-server = manoj_server.manoj_server:Authentication',
+        ],
+    },
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+)
