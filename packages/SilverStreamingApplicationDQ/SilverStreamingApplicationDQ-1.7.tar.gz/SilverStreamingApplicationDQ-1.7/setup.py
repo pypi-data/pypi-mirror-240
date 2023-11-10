8113b91fa9aacfbe48afd17a83b55306d7fed9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:22 +0000

    Add package SilverStreamingApplicationDQ/1.7/SilverStreamingApplicationDQ-1.7.tar.gz

---
+++ packages/SilverStreamingApplicationDQ/SilverStreamingApplicationDQ-1.7.tar.gz/SilverStreamingApplicationDQ-1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationDQ',
+    version='1.7',
+    packages=['com.phida.main', 'com.phida.main.bronze', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='trigger Once changed to availableNow'
+)
