1b2c2bb80700cfd881887dacbf36d923d2ace71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:10:39 +0000

    Add package sevenbridges/0.0.12/sevenbridges-0.0.12.tar.gz

---
+++ packages/sevenbridges/sevenbridges-0.0.12.tar.gz/sevenbridges-0.0.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='sevenbridges',
+    version='0.0.12',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url='https://pypi.org/project/sevenbridges',
+    license='',
+    packages=['sevenbridges'],
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'scikit-learn',
+        'scipy',
+    ],
+)
