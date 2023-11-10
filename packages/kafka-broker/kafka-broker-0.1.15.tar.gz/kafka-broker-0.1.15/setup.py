4e704a4cd15e671861caf01472e5c1003e3700d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:10:25 +0000

    Add package kafka-broker/0.1.15/kafka-broker-0.1.15.tar.gz

---
+++ packages/kafka-broker/kafka-broker-0.1.15.tar.gz/kafka-broker-0.1.15/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    include_package_data=True,
+    package_data={'kafka_broker': ['*.ini']}
+)
