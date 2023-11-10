775fb0ad369e1ccf0675ba0b9c75de38f8fcf15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 14:04:58 +0000

    Add package freeimagehost/1.0/freeimagehost-1.0-py3-none-any.whl

---
+++ packages/freeimagehost/freeimagehost-1.0-py3-none-any.whl/imagehost/client.py
@@ -0,0 +1,54 @@
+import httpx
+
+from .exceptions import SourceTypeInvalid, ApiError
+
+
+class Client:
+    def __init__(self, api_key: str):
+        """
+        :param api_key: Your api key, which can be obtained at https://freeimage.host/page/api
+        """
+        self.api_key = api_key
+
+    def upload(self, source: str, source_type='path') -> dict:
+        """
+        :param source_type: The image type, pass "path" (it is already standard) for an image path, pass "b64" for an image base64 string and pass "url" for an image url.
+        :param source: The image can be a url, a base64 string or a path.
+        :return: A dictionary containing various information about the image that was sent and the link itself.
+        """
+
+        api_key = self.api_key
+        api = 'https://freeimage.host/api/1/upload'
+
+        if source_type == 'path':
+            with open(source, 'rb') as file:
+                source = file.read()
+            req = httpx.post(
+                api,
+                data={
+                    'key': api_key,
+                    'format': 'json'
+                },
+                files={'source': source},
+                timeout=None
+            )
+        elif source_type == 'b64' or source_type == 'url':
+            req = httpx.post(
+                api,
+                data={
+                    'key': api_key,
+                    'source': source,
+                    'format': 'json'
+                }
+            )
+        else:
+            raise SourceTypeInvalid
+
+        if req.status_code != 200:
+            req_json = req.json()
+            raise ApiError(
+                req_json['error']['message'],
+                req_json['status_code']
+            )
+        else:
+            return req.json()
