d46e2218ab4b0c91414d539db9cc2eec6764c46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:54 +0000

    Add package cyeva/0.1.0b12/cyeva-0.1.0b12-py3-none-any.whl

---
+++ packages/cyeva/cyeva-0.1.0b12-py3-none-any.whl/cyeva/__init__.py
@@ -0,0 +1,3 @@
+from .core import *
+
+__version__ = "0.1.0.beta.12"
---
+++ packages/cyeva/cyeva-0.1.0b12-py3-none-any.whl/cyeva/core/__init__.py
@@ -0,0 +1,6 @@
+from .base import *
+from .precip import *
+from .temp import *
+from .wind import *
+from .statistic import *
+from .weather_code import *
