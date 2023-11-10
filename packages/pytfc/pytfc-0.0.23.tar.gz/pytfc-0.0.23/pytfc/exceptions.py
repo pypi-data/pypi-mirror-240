5767eb15606d7d1cdf906e87ad64047d9f972c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:12:59 +0000

    Add package pytfc/0.0.23/pytfc-0.0.23.tar.gz

---
+++ packages/pytfc/pytfc-0.0.23.tar.gz/pytfc-0.0.23/pytfc/exceptions.py
@@ -0,0 +1,56 @@
+"""Module for custom pytfc exceptions."""
+
+
+class Error(Exception):
+    """Base class for general exceptions."""
+    pass
+
+
+class MissingToken(Exception):
+    """TFC/E Token not specified."""
+    pass
+
+
+class MissingOrganization(Exception):
+    """TFC/E Organization not specified."""
+    pass
+
+
+class MissingWorkspace(Exception):
+    """TFC/E Workspace name or ID not specified."""
+    pass
+
+
+class MissingOauthClient(Exception):
+    """TFC/E OAuth Client ID or name not specified."""
+    pass
+
+
+class MissingVcsProvider(Exception):
+    """TFC/E OAuth Client not found."""
+    pass
+
+
+class MissingRun(Exception):
+    """TFC/E Run ID or commit message not specified."""
+    pass
+
+
+class MissingPlan(Exception):
+    """TFC/E Plan not specified."""
+    pass
+
+
+class ConfigurationVersionUploadError(Exception):
+    """Error uploading Terraform tar.gz to Configuration Version."""
+    pass
+
+
+class InvalidQueryParam(Exception):
+    """Unsupported value for query parameter."""
+    pass
+
+
+class PlanExportDownloadError(Exception):
+    """Error downloading Plan Export"""
+    pass
---
+++ packages/pytfc/pytfc-0.0.23.tar.gz/pytfc-0.0.23/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = 'v0.0.23' # value is updated by release pipeline
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytfc',
+    version=VERSION,
+    author='Alex Basista',
+    author_email='alex.basista@gmail.com',
+    url='https://github.com/alexbasista/pytfc',
+    description='Python HTTP client library for Terraform Cloud/Enterprise API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files=['LICENSE'],
+    py_modules=['pytfc'],
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0',
+        'pyhcl>=0.4.4',
+    ],
+    extras_require={
+        'dev': ['build==1.0.3', 'pytest==7.4.2'],
+    },
+    keywords=['tfe', 'terraform enterprise', 'tfc', 'terraform cloud', 'terraform'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+    ],
+)
