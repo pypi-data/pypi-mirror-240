540ff550ee43d3183fc6a2147c9aa23f347706ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 15:03:47 +0000

    Add package chialisp-stdlib/0.1.1/chialisp_stdlib-0.1.1.tar.gz

---
+++ packages/chialisp-stdlib/chialisp_stdlib-0.1.1.tar.gz/chialisp_stdlib-0.1.1/chialisp_stdlib/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from importlib.resources import files
+except ImportError:
+    # for py3.8
+    from importlib_resources import files
+
+
+STABLE_INCLUDE_DIRECTORY = files(__package__) / "stable"
+NIGHTLY_INCLUDE_DIRECTORY = files(__package__) / "nightly"
