fe10dd0cf2e53a86eeac09c4dbffb09df0ab90a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 03:10:30 +0000

    Add package ragstack-ai-langserve/0.0.1a1/ragstack_ai_langserve-0.0.1a1-py3-none-any.whl

---
+++ packages/ragstack-ai-langserve/ragstack_ai_langserve-0.0.1a1-py3-none-any.whl/langserve/schema.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+from typing import Dict, List, Optional, Union
+from uuid import UUID
+
+from langserve.pydantic import PYDANTIC_MAJOR_VERSION
+
+if PYDANTIC_MAJOR_VERSION == 2:
+    from pydantic.v1 import BaseModel as BaseModelV1
+else:
+    from pydantic import BaseModel as BaseModelV1
+from pydantic import BaseModel
+
+
+class CustomUserType(BaseModelV1):
+    """Inherit from this class to create a custom user type.
+
+    Use a custom user type if you want the data to de-serialize
+    into a pydantic model rather than the equivalent dict representation.
+
+    In general, make sure to add a `type` attribute to your class
+    to help pydantic to discriminate unions.
+
+    https://docs.pydantic.dev/1.10/usage/types/#discriminated-unions-aka-tagged-unions
+
+    Limitations:
+        At the moment, this type only works SERVER side and is used
+        to specify desired DECODING behavior. If inheriting from this type
+        the server will keep the decoded type as a pydantic model instead
+        of converting it into a dict.
+    """
+
+
+class SharedResponseMetadata(BaseModelV1):
+    """
+    Any response metadata should inherit from this class. Response metadata
+    represents non-output data that may be useful to some clients, but
+    ignorable to most. For example, the run_ids associated with each run
+    kicked off by the associated request.
+
+    SharedResponseMetadata is an abstraction to represent any metadata
+    representing a LangServe response shared across all outputs in said
+    response.
+    """
+
+    pass
+
+
+class SingletonResponseMetadata(SharedResponseMetadata):
+    """
+    Represents response metadata used for just single input/output LangServe
+    responses.
+    """
+
+    # Represents the parent run id for a given request
+    run_id: UUID
+
+
+class BatchResponseMetadata(SharedResponseMetadata):
+    """
+    Represents response metadata used for batches of input/output LangServe
+    responses.
+    """
+
+    # Represents each parent run id for a given request, in
+    # the same order in which they were received
+    run_ids: List[UUID]
+
+
+class BaseFeedback(BaseModel):
+    """
+    Shared information between create requests of feedback and feedback objects
+    """
+
+    run_id: UUID
+    """The associated run ID this feedback is logged for."""
+
+    key: str
+    """The metric name, tag, or aspect to provide feedback on."""
+
+    score: Optional[Union[float, int, bool]] = None
+    """Value or score to assign the run."""
+
+    value: Optional[Union[float, int, bool, str, Dict]] = None
+    """The display value for the feedback if not a metric."""
+
+    comment: Optional[str] = None
+    """Comment or explanation for the feedback."""
+
+
+class FeedbackCreateRequest(BaseFeedback):
+    """
+    Represents a request that creates feedback for an individual run
+    """
+
+    pass
+
+
+class Feedback(BaseFeedback):
+    """
+    Represents feedback given on an individual run
+    """
+
+    created_at: datetime
+    """The time the feedback was created."""
+
+    modified_at: datetime
+    """The time the feedback was last modified."""
+
+    correction: Optional[Dict] = None
+    """Correction for the run."""
