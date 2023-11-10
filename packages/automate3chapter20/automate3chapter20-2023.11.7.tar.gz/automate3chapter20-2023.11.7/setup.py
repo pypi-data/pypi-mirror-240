a828b08f225a1890cfcb57a34ecd2951e6d11555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:20 +0000

    Add package automate3chapter20/2023.11.7/automate3chapter20-2023.11.7.tar.gz

---
+++ packages/automate3chapter20/automate3chapter20-2023.11.7.tar.gz/automate3chapter20-2023.11.7/setup.py
@@ -0,0 +1,34 @@
+
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name='automate3chapter20',
+    version='2023.11.7',
+    url='https://github.com/asweigart/automateboringstuff3',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=('This is a placeholder package that installs nothing, but prevents typo squatting.'),
+    long_description='This is a placeholder package that installs nothing, but prevents typo squatting.',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+
+    ],
+    keywords="automate boring stuff python",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
