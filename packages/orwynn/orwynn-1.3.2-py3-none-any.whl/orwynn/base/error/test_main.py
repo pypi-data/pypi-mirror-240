e62ed5664df10a9c5525274830f2b938c237195b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:25 +0000

    Add package orwynn/1.3.2/orwynn-1.3.2-py3-none-any.whl

---
+++ packages/orwynn/orwynn-1.3.2-py3-none-any.whl/orwynn/base/error/test_main.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+from orwynn.base.error.code import get_error_code
+from orwynn.indication.indication import Indication
+from orwynn.indication.indicator import Indicator
+from orwynn.utils import validation
+
+
+def test_int():
+    class MyError(Exception):
+        Code = 1
+
+    assert get_error_code(MyError()) == 1
+
+
+def test_str():
+    class MyError(Exception):
+        Code = "hello"
+
+    assert get_error_code(MyError()) == "hello"
+
+
+def test_enum():
+    class ErrorCode(Enum):
+        RED = "RED"
+
+    class MyError(Exception):
+        Code = ErrorCode.RED
+
+    assert get_error_code(MyError()) == ErrorCode.RED
+
+
+def test_wrong_type():
+    class MyError(Exception):
+        Code = 5.55
+
+    validation.expect(
+        get_error_code,
+        validation.ValidationError,
+        MyError()
+    )
+
+
+def test_no_attribute():
+    class MyError(Exception):
+        pass
+
+    validation.expect(
+        get_error_code,
+        AttributeError,
+        MyError()
+    )
+
+def test_default_indication_type():
+    class E(Exception):
+        pass
+
+    i: Indication = Indication({
+        "type": Indicator.TYPE,
+        "value": Indicator.VALUE
+    })
+
+    assert i.digest(E("whatever"))["type"] == "error"
