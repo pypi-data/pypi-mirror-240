795f6900ea55fd5e70b27077b2d1fbc98f2a386b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 05:03:33 +0000

    Add package idds-monitor/2.0.4/idds_monitor-2.0.4-py2.py3-none-any.whl

---
+++ packages/idds-monitor/idds_monitor-2.0.4-py2.py3-none-any.whl/idds_monitor-2.0.4.data/data/monitor/data/conf.js
@@ -0,0 +1,9 @@
+
+var appConfig = {
+    'iddsAPI_request': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor_request/null/null",
+    'iddsAPI_transform': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor_transform/null/null",
+    'iddsAPI_processing': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor_processing/null/null",
+    'iddsAPI_request_detail': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor/null/null/true/false/false",
+    'iddsAPI_transform_detail': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor/null/null/false/true/false",
+    'iddsAPI_processing_detail': "https://fv-az1019-107.m5ch4o5xfz2e5czaqtqza4205h.dx.internal.cloudapp.net:443/idds/monitor/null/null/false/false/true"
+}
