5e57bdce15e463779867c7cee0f271305a3de4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:34 +0000

    Add package vern/1.3.54b0/vern-1.3.54b0.tar.gz

---
+++ packages/vern/vern-1.3.54b0.tar.gz/vern-1.3.54b0/vern/__init__.py
@@ -0,0 +1,14 @@
+import os, glob
+from .parse_all import *
+
+__copyright__    = 'Copyright (C) 2022 BoronSpoon'
+__version__      = '1.3.54b'
+__license__      = 'MIT License'
+__author__       = 'boronspoon'
+__author_email__ = 'rayanticlimactic@gmail.com'
+__url__          = 'http://github.com/BoronSpoon/vern'
+
+__all__ = [
+    #os.path.split(os.path.splitext(file)[0])[1] for file in glob.glob(os.path.join(os.path.dirname(__file__), '*.py')) if '__' not in file
+    'Vern'
+]
