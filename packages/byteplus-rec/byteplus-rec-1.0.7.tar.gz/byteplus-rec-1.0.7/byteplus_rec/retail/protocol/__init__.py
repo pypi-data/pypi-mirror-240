287153fd10f9074f7e29f2b0c739d7bf1ac6ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:39 +0000

    Add package byteplus-rec/1.0.7/byteplus-rec-1.0.7.tar.gz

---
+++ packages/byteplus-rec/byteplus-rec-1.0.7.tar.gz/byteplus-rec-1.0.7/byteplus_rec/retail/protocol/__init__.py
@@ -0,0 +1,18 @@
+from .byteplus_saas_retail_pb2 import (
+    WriteDataRequest,
+    WriteResponse,
+    Device,
+    Product,
+    PredictFeature,
+    PredictFilter,
+    PredictRequest,
+    PredictResponse,
+    StringArray,
+    IntArray,
+    FloatArray,
+    DoubleArray,
+    AckServerImpressionsRequest,
+    AckServerImpressionsResponse,
+    FinishWriteDataRequest,
+    Date
+)
