0e563d761b13c8841af468012a189db6c173303f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:42 +0000

    Add package solidipes/0.1.6/solidipes-0.1.6-py3-none-any.whl

---
+++ packages/solidipes/solidipes-0.1.6-py3-none-any.whl/solidipes/loaders/__init__.py
@@ -0,0 +1,24 @@
+from .binary import Binary
+from .code_snippet import CodeSnippet
+from .data_container import DataContainer
+from .file import File
+from .image import Image
+from .meshio import Meshio
+from .pyvista_mesh import PyvistaMesh
+from .table import Table
+from .text import Markdown, Text
+from .video import Video
+
+__all__ = [
+    "DataContainer",
+    "Binary",
+    "CodeSnippet",
+    "File",
+    "Image",
+    "Markdown",
+    "Meshio",
+    "PyvistaMesh",
+    "Table",
+    "Text",
+    "Video",
+]
