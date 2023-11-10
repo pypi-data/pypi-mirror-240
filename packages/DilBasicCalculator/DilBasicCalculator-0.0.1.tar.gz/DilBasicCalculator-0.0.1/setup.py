700f8ec5ffe8b2cc5559213a238bef4b6ebf9ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:05:16 +0000

    Add package DilBasicCalculator/0.0.1/DilBasicCalculator-0.0.1.tar.gz

---
+++ packages/DilBasicCalculator/DilBasicCalculator-0.0.1.tar.gz/DilBasicCalculator-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DilBasicCalculator",
+    version="0.0.1",
+    author="DILSHER",
+    author_email="Dilsherengineer90@email.com",
+    description="a simple calculator ",
+    long_description="This library  that takes two intiger numbers and do some arithmatic operation like ",
+    url=" ",
+    packages=find_packages(),
+    install_requires=[
+        # List your library dependencies here
+        "dependency1",
+        "dependency2",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+           'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.11',
+             'Programming Language :: Python :: 3.12',
+    ],
+)
