d27de6d35c41ed303369fc7ab5cc95a7d387051c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:20 +0000

    Add package sanic-sse-py3/1.0.8/sanic_sse_py3-1.0.8-py3-none-any.whl

---
+++ packages/sanic-sse-py3/sanic_sse_py3-1.0.8-py3-none-any.whl/sse/core/config.py
@@ -0,0 +1,22 @@
+import os
+from collections import namedtuple
+
+Config = namedtuple("Config", ["ping_interval", "pubsub_options"])
+
+
+def load_config(app):
+    sse_config = getattr(app.ctx, "sse_config", None) or {}
+    app.ctx.sse_config = merge_config(sse_config)
+    return app.ctx.sse_config
+
+
+def merge_config(config):
+    pubsub_options = config.get("pubsub_options", {})
+    pubsub_options["host"] = pubsub_options.pop("redis_host", os.getenv("SSE_REDIS_HOST", "127.0.0.1"))
+    pubsub_options["port"] = int(pubsub_options.pop("redis_port", os.getenv("SSE_REDIS_PORT", 16379)))
+    pubsub_options["password"] = pubsub_options.pop("redis_passwd", os.getenv("SSE_REDIS_PASSWD", ""))
+
+    return Config(
+        ping_interval=config.get("ping_interval", int(os.getenv("SSE_PING_INTERVAL", 30))),
+        pubsub_options=pubsub_options
+    )
