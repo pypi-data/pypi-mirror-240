278598abc59109f32c6de2b513088b1b93268094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:05:21 +0000

    Add package bec-widgets/0.30.0/bec_widgets-0.30.0.tar.gz

---
+++ packages/bec-widgets/bec_widgets-0.30.0.tar.gz/bec_widgets-0.30.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+__version__ = "0.30.0"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["pyqt5", "pyqtgraph", "bec_lib", "zmq", "h5py"],
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "pytest-qt", "black"]},
+        version=__version__,
+    )
