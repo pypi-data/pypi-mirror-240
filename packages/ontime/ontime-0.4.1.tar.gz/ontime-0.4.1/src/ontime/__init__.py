5b4cfb3994d44580617a225313cbf4fa713f78ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 16:04:13 +0000

    Add package ontime/0.4.1/ontime-0.4.1.tar.gz

---
+++ packages/ontime/ontime-0.4.1.tar.gz/ontime-0.4.1/src/ontime/__init__.py
@@ -0,0 +1,10 @@
+""" OnTime API Definition """
+
+from .abstract import *
+from .context import *
+from .detectors import detectors
+from .generators import generators
+from .model import Model
+from .plots import plots
+from .processors import processors
+from .time_series import TimeSeries
