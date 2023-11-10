a17350ad4c0f6a45c8dd8a15148ec6b46ee3e0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 07:04:45 +0000

    Add package mfconnect/0.0.178/mfconnect-0.0.178.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.178.tar.gz/mfconnect-0.0.178/test/test_user_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestUserpassword(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_user_password(self):
+        """
+            have to test ...
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
