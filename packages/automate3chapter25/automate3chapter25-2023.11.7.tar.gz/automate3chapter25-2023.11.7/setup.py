76e09165dc9c7153d0615df37b2388cbae93bb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 06:04:21 +0000

    Add package automate3chapter25/2023.11.7/automate3chapter25-2023.11.7.tar.gz

---
+++ packages/automate3chapter25/automate3chapter25-2023.11.7.tar.gz/automate3chapter25-2023.11.7/setup.py
@@ -0,0 +1,36 @@
+
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name='automate3chapter25',
+    version='2023.11.7',
+    url='https://github.com/asweigart/automateboringstuff3',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=('This package installs the latest compatible version of the packages covered in Chapter 25 of Automate the Boring Stuff with Python, 3rd Edition.'),
+    long_description='This package installs the latest compatible version of the packages covered in Chapter 25 of Automate the Boring Stuff with Python, 3rd Edition.',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pyttsx3==2.90',
+        'gTTS==2.4.0',
+        'openai-whisper==20231106',
+    ],
+    keywords="automate boring stuff python",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
