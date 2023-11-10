f356b2e02a0e0dcd54919ca3f7622fb392d526f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:12:17 +0000

    Add package so-ml-tools/0.1.95/so-ml-tools-0.1.95.tar.gz

---
+++ packages/so-ml-tools/so-ml-tools-0.1.95.tar.gz/so-ml-tools-0.1.95/so_ml_tools/sklearn/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    BooleanEncoder,
+    FillBackwardImputer,
+    FillForwardImputer,
+    SimpleGroupByImputer
+)
+
+__all__ = [
+    "BooleanEncoder",
+    "FillBackwardImputer",
+    "FillForwardImputer",
+    "SimpleGroupByImputer"
+]
