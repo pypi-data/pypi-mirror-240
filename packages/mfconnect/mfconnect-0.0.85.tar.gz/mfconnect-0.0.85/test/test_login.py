2e7fa8ed295c3fc4ed91378820f2a881ff2882cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 05:03:16 +0000

    Add package mfconnect/0.0.85/mfconnect-0.0.85.tar.gz

---
+++ packages/mfconnect/mfconnect-0.0.85.tar.gz/mfconnect-0.0.85/test/test_login.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=90:
+import unittest
+
+from mfconnect import meteo
+
+
+class TestLogin(unittest.TestCase):
+
+    @unittest.skip("i don't known how to do this")
+    def test_login(self):
+        """
+            a telnet server is needed here
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
