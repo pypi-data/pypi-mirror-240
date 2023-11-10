985e082145a199ac8263ab1b421617ca1376afe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-10 04:07:04 +0000

    Add package delphai-utils/3.1.2/delphai_utils-3.1.2-py3-none-any.whl

---
+++ packages/delphai-utils/delphai_utils-3.1.2-py3-none-any.whl/delphai_utils/rpc/errors.py
@@ -0,0 +1,26 @@
+class RpcError(Exception):
+    pass
+
+
+class TemporaryError(RpcError):
+    pass
+
+
+class FinalError(RpcError):
+    pass
+
+
+class UnhandledError(FinalError):
+    pass
+
+
+class ParsingError(FinalError):
+    pass
+
+
+class UnknownMethodError(FinalError):
+    pass
+
+
+class ExecutionError(FinalError):
+    pass
