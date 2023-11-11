0080a64d1f8b35d1236010113cd770bc4276e609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:10:30 +0000

    Add package pulp-file/1.16.0/pulp-file-1.16.0.tar.gz

---
+++ packages/pulp-file/pulp-file-1.16.0.tar.gz/pulp-file-1.16.0/README.rst
@@ -0,0 +1,13 @@
+pulp_file
+=========
+
+.. warning:: This pulp plugin has been merged into the main pulpcore project.
+   This repository can only accept bugfixes on release branches.
+
+.. figure:: https://github.com/pulp/pulp_file/actions/workflows/nightly.yml/badge.svg?branch=main
+   :alt: File Nightly CI/CD
+
+A Pulp plugin to support hosting arbitrary files.
+
+For more information, please see the `documentation <https://docs.pulpproject.org/pulp_file/>`_
+or the `Pulp project page <https://pulpproject.org>`_.
---
+++ packages/pulp-file/pulp-file-1.16.0.tar.gz/pulp-file-1.16.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as requirements:
+    requirements = requirements.readlines()
+
+setup(
+    name="pulp-file",
+    version="1.16.0",
+    description="File plugin for the Pulp Project (transitional dummy package)",
+    long_description=long_description,
+    license="GPLv2+",
+    author="Pulp Project Developers",
+    author_email="pulp-dev@redhat.com",
+    url="https://pulpproject.org/",
+    python_requires=">=3.8",
+    install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages(exclude=["test"]),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
