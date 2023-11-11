6e8a1e738cc9efbff43f44dbfaa43703559c1d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:06:23 +0000

    Add package PRED-search/0.0.1/PRED_search-0.0.1.tar.gz

---
+++ packages/PRED-search/PRED_search-0.0.1.tar.gz/PRED_search-0.0.1/src/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import lxml
+import os
+
+def read_pred_tab():
+    WD = os.getcwd()
+    if not os.path.isfile("%s/pred_ls.txt"):
+        df = pd.read_html("https://www.mtmt.hu/kifogasolhato_folyoiratok")[0]
+        ls = df.iloc[:, 0]
+        with open("%s/pred_ls.txt" % WD, 'w') as ls_file:
+            ls_file.write('\n'.join(str(i) for i in ls))
+    else:
+        ls = []
+        ls_file = open("%s/pred_ls.txt" % WD, 'w')
+        while True:
+            line = ls_file.readline()
+            if line == "":
+                break
+            ls.append(line)
+    return ls
+
+def srch_term():
+    tm = input("Please enter search term!\n")
+    pred_ls = read_pred_tab()
+    for elem in pred_ls:
+        if elem.__contains__(tm):
+            print(elem)
+
+def run():
+    srch_term()
+
+
+if __name__ == "__main__":
+    run()
