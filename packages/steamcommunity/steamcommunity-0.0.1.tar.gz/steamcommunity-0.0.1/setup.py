4bfd12446ba23206a0ef5034dd7d7f8a15e4660c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:33 +0000

    Add package steamcommunity/0.0.1/steamcommunity-0.0.1.tar.gz

---
+++ packages/steamcommunity/steamcommunity-0.0.1.tar.gz/steamcommunity-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='steamcommunity',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/summer/steamcommunity',
+    author='summer',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
