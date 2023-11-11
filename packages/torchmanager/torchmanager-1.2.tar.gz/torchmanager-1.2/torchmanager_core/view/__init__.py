fd10770657964cdeac77b4cde25491927e6e412a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:07:59 +0000

    Add package torchmanager/1.2/torchmanager-1.2.tar.gz

---
+++ packages/torchmanager/torchmanager-1.2.tar.gz/torchmanager-1.2/torchmanager_core/view/__init__.py
@@ -0,0 +1,16 @@
+import logging, warnings
+from tqdm import tqdm
+
+from .protocols import VerboseType
+
+logging.getLogger().handlers.clear()
+logger = logging.getLogger('torchmanager')
+
+def set_log_path(log_path: str) -> logging.Formatter:
+    logger.handlers.clear()
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(log_path)
+    formatter = logging.Formatter("%(message)s")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return formatter
