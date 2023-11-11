576c62fe91c08cd095086f50297f9f4455718c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:59 +0000

    Add package opencopilot-db/1.3.4/opencopilot_db-1.3.4.tar.gz

---
+++ packages/opencopilot-db/opencopilot_db-1.3.4.tar.gz/opencopilot_db-1.3.4/opencopilot_db/__init__.py
@@ -0,0 +1,5 @@
+from .chat_history import ChatHistory
+from .database_setup import create_database_schema
+from .get_declarative_base import declarative_base
+from .database_setup import engine
+from .pdf_data_source_model import PdfDataSource
