77cdae57607c1c36457fb9877fea36b781b561bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:37 +0000

    Add package gptfy/0.3.2/gptfy-0.3.2-py3-none-any.whl

---
+++ packages/gptfy/gptfy-0.3.2-py3-none-any.whl/gptfy/dalle.py
@@ -0,0 +1,24 @@
+import openai
+
+
+class Dalle:
+	SIZE_LARGE  = '1024x1024'
+	SIZE_MEDIUM = '512x512'
+	SIZE_SMALL  = '256x256'
+
+	def __init__(self, api_key):
+		openai.api_key = api_key
+		self.images = []
+
+	def images(self, prompt, size, quantity=1, base64=False):
+		print(f'Dall-E generating image for "{prompt}"')
+		if quantity < 0  : quantity = 0
+		if quantity > 10 : quantity = 10
+
+		response = openai.Image.create(
+		  prompt = prompt,
+		  n      = quantity,
+		  size   = size
+		)
+
+		return response['data'], prompt
