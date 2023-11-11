f679c498c41566e2abb3c8d6151f02d886f04d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:07:06 +0000

    Add package testrepo123/0.0.0/testrepo123-0.0.0-py3-none-any.whl

---
+++ packages/testrepo123/testrepo123-0.0.0-py3-none-any.whl/testrepo123/__init__.py
@@ -0,0 +1,33 @@
+"""TestRepo123
+
+YOUR Tag HERE
+
+YOUR DESCRIPTION HERE
+
+Copyright 2023 Armin Ariamajd
+
+Copyright (C) 2023
+Armin Ariamajd
+
+This program is free software: you can redistribute it and/or modify
+it under the
+terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either
+version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the
+hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for
+more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""  # noqa: D400
+
+
+__version_details__ = {"version": "0.0.0"}
+__version__ = __version_details__["version"]
