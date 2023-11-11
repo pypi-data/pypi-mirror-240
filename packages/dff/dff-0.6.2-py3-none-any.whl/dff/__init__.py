a529a44bed099349c796061950c0cf7392a64993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:44 +0000

    Add package dff/0.6.2/dff-0.6.2-py3-none-any.whl

---
+++ packages/dff/dff-0.6.2-py3-none-any.whl/dff/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa: F401
+
+
+__author__ = "Denis Kuznetsov"
+__email__ = "kuznetsov.den.p@gmail.com"
+__version__ = "0.6.2"
+
+
+import nest_asyncio
+
+nest_asyncio.apply()
