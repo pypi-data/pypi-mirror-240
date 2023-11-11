a0c16dbe5e6c7bfc08b8ea9741eca3adc7a64bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:28 +0000

    Add package kappamodules/0.0.9/kappamodules-0.0.9-py3-none-any.whl

---
+++ packages/kappamodules/kappamodules-0.0.9-py3-none-any.whl/kappamodules/vit/vit_mlp.py
@@ -0,0 +1,40 @@
+from torch import nn
+
+from kappamodules.init.functional import initialize_xavier_uniform_zero_bias
+from kappamodules.utils.param_checking import to_2tuple
+
+
+class VitMlp(nn.Module):
+    def __init__(
+            self,
+            in_dim,
+            hidden_dim=None,
+            out_dim=None,
+            act_ctor=nn.GELU,
+            bias=True,
+            init="xavier",
+    ):
+        super().__init__()
+        self.init = init
+        out_dim = out_dim or in_dim
+        hidden_dim = hidden_dim or in_dim
+        bias1, bias2 = to_2tuple(bias)
+
+        self.fc1 = nn.Linear(in_dim, hidden_dim, bias=bias1)
+        self.act = act_ctor()
+        self.fc2 = nn.Linear(hidden_dim, out_dim, bias=bias2)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        if self.init is None or self.init == "none":
+            pass
+        elif self.init == "xavier":
+            self.apply(initialize_xavier_uniform_zero_bias)
+        else:
+            raise NotImplementedError
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        return x
