935f857c9f13e4923a7993b321be3b891ccde029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:06:35 +0000

    Add package kubestatus/0.1.1/kubestatus-0.1.1.tar.gz

---
+++ packages/kubestatus/kubestatus-0.1.1.tar.gz/kubestatus-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='kubestatus',
+    version='0.1.1',
+    author='Stephen Dictor',
+    author_email='sdictor@renew-ap.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ecoenergia/kubestatus.git',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kubestatus=kubestatus.viewer:main',
+        ],
+    },
+)
