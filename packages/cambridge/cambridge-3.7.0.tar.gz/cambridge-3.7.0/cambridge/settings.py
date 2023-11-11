d77532656f6ff6cd7979d517477324c6be554204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:08:32 +0000

    Add package cambridge/3.7.0/cambridge-3.7.0.tar.gz

---
+++ packages/cambridge/cambridge-3.7.0.tar.gz/cambridge-3.7.0/cambridge/settings.py
@@ -0,0 +1,19 @@
+"""Script to store static data."""
+
+VERSION = "3.7.0"
+
+OP = [
+    "FETCHING",
+    "PARSING",
+    "RETRY FETCHING",
+    "RETRY PARSING",
+    "PRINTING",
+    "FOUND",
+    "NOT FOUND",
+    "CACHED",
+    "CANCELLED",
+    "DELETED",
+    "UPDATED",
+]
+
+DICTS = ["CAMBRIDGE", "MERRIAM WEBSTER"]
