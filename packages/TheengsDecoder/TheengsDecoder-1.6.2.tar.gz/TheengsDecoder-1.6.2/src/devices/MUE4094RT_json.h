a44713e4a2539f199b22ddf3874dc5bc5b8dcac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:06:09 +0000

    Add package TheengsDecoder/1.6.2/TheengsDecoder-1.6.2.tar.gz

---
+++ packages/TheengsDecoder/TheengsDecoder-1.6.2.tar.gz/TheengsDecoder-1.6.2/src/devices/MUE4094RT_json.h
@@ -0,0 +1,34 @@
+const char* _MUE4094RT_json = "{\"brand\":\"Xiaomi\",\"model\":\"MiLamp\",\"model_id\":\"MUE4094RT\",\"tag\":\"0404\",\"condition\":[\"servicedata\",\"index\",0,\"4030dd\"],\"properties\":{\"pres\":{\"decoder\":[\"static_value\",true],\"is_bool\":1},\"darkness\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,2,true]}}}";
+/*
+R""""(
+{
+   "brand":"Xiaomi",
+   "model":"MiLamp",
+   "model_id":"MUE4094RT",
+   "tag":"0404",
+   "condition":["servicedata", "index", 0, "4030dd"],
+   "properties":{
+      "pres":{
+         "decoder":["static_value", true],
+         "is_bool":1
+      },
+      "darkness":{
+         "decoder":["value_from_hex_data", "servicedata", 8, 2, true]
+      }
+   }
+})"""";*/
+
+const char* _MUE4094RT_json_props = "{\"properties\":{\"pres\":{\"unit\":\"status\",\"name\":\"presence\"},\"darkness\":{\"unit\":\"lx\",\"name\":\"illuminance\"}}}";
+/*R""""(
+{
+   "properties":{
+      "pres":{
+         "unit":"status",
+         "name":"presence"
+      },
+      "darkness":{
+         "unit":"lx",
+         "name":"illuminance"
+      }
+   }
+})"""";*/
