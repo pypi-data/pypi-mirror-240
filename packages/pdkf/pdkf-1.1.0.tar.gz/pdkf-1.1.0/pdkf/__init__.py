cc180c66ac2c3e4765a6115c4a16f0bbc3026afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:10:13 +0000

    Add package pdkf/1.1.0/pdkf-1.1.0.tar.gz

---
+++ packages/pdkf/pdkf-1.1.0.tar.gz/pdkf-1.1.0/pdkf/__init__.py
@@ -0,0 +1,1 @@
+from .pdkf import read_pdkf
---
+++ packages/pdkf/pdkf-1.1.0.tar.gz/pdkf-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='pdkf',
+  version='1.1.0',
+  author='KerrLi',
+  author_email='bogatyreva_aa@mail.ru',
+  description='Module for working with PDKF files and Pandas dataframes',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/KerrLiGit/pdkf',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='pdkf',
+  project_urls={
+    'Documentation': 'https://github.com/KerrLiGit/pdkf/wiki'
+  },
+  install_requires=[
+    'pandas>=2.1.0',
+    'zipp>=3.17.0'
+  ],
+  python_requires='>=3.7'
+)
