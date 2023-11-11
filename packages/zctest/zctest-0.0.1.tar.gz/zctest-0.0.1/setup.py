aed586f15b4f762c4956769e0c62d726e05c4a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:04:41 +0000

    Add package zctest/0.0.1/zctest-0.0.1.tar.gz

---
+++ packages/zctest/zctest-0.0.1.tar.gz/zctest-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='zctest',
+    version='0.0.1',
+    description='An example Python package',
+    url='',
+    author='onechorm',
+    author_email='zconechorm@163.com',
+    license='MIT',
+    packages=['pythonProject1'],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    zip_safe=False
+)
