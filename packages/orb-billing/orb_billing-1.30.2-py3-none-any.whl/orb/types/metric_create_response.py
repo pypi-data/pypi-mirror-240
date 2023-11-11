01af168c49b3d09918a70b1b9bc788b213f6e365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:06:55 +0000

    Add package orb-billing/1.30.2/orb_billing-1.30.2-py3-none-any.whl

---
+++ packages/orb-billing/orb_billing-1.30.2-py3-none-any.whl/orb/types/metric_create_response.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Dict, Optional
+from typing_extensions import Literal
+
+from .item import Item
+from .._models import BaseModel
+
+__all__ = ["MetricCreateResponse"]
+
+
+class MetricCreateResponse(BaseModel):
+    id: str
+
+    description: Optional[str]
+
+    item: Item
+    """The Item resource represents a sellable product or good.
+
+    Items are associated with all line items, billable metrics, and prices and are
+    used for defining external sync behavior for invoices and tax calculation
+    purposes.
+    """
+
+    metadata: Dict[str, str]
+
+    name: str
+
+    status: Literal["active", "draft", "archived"]
