3055cc02465141996a89bb63a791af9d6d386c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:54 +0000

    Add package qabot/0.4.7/qabot-0.4.7-py3-none-any.whl

---
+++ packages/qabot/qabot-0.4.7-py3-none-any.whl/qabot/__init__.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from qabot.agent import Agent
+from qabot.functions.data_loader import create_duckdb, import_into_duckdb_from_files
+
+
+def ask_wikidata(query: str, verbose=False):
+    agent = Agent(allow_wikidata=True, verbose=verbose)
+    result = agent(query)
+    return result["summary"]
+
+
+def ask_file(query: str, filename: Optional[str], verbose=False):
+    engine = create_duckdb()
+    database_engine, executed_sql = import_into_duckdb_from_files(engine, [filename])
+    agent = Agent(database_engine=database_engine, verbose=verbose)
+    result = agent(query)
+    return result["summary"]
