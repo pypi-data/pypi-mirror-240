2820f41c98162acf5972e618eae592012f55adbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:05:34 +0000

    Add package ert/8.0.0rc2/ert-8.0.0rc2-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-8.0.0rc2-cp39-cp39-macosx_10_15_x86_64.whl/ert/shared/storage/connection.py
@@ -0,0 +1,14 @@
+import os
+from typing import Any, Dict, Optional, Tuple, Union
+
+from ert.services import StorageService
+
+
+def get_info(
+    project_id: Optional[os.PathLike[str]] = None,
+) -> Dict[str, Union[str, Tuple[str, Any]]]:
+    client = StorageService.connect(project=project_id)
+    return {
+        "baseurl": client.fetch_url(),
+        "auth": client.fetch_auth(),
+    }
