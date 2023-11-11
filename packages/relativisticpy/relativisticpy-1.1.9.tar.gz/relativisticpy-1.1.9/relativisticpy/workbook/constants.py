f498c84c54d52128b0f144786cceb424449a8925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 14:04:17 +0000

    Add package relativisticpy/1.1.9/relativisticpy-1.1.9.tar.gz

---
+++ packages/relativisticpy/relativisticpy-1.1.9.tar.gz/relativisticpy-1.1.9/relativisticpy/workbook/constants.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class WorkbookConstants(Enum):
+    
+    # Standard Defs
+    COORDINATES = "Coordinates"
+
+    # Tensor/Object Types
+    METRIC = "Metric"
+    RICCI = "Ricci"
+    RIEMANN = "Riemann"
+    COVDERIVATIVE = "CovariantDerivative"
+    DERIVATIVE = "Derivative"
+
+    # Symbol Setters
+    METRICSYMBOL = "MetricSymbol"
+    RICCISYMBOL = "RicciSymbol"
+    RIEMANNSYMBOL = "RiemannSymbol"
+    COVDERIVATIVESYMBOL = "CovariantDerivativeSymbol"
+    DERIVATIVESYMBOL = "DerivativeSymbol"
