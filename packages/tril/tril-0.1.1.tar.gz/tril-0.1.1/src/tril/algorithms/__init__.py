f969d62c1a44d891f4d9cdb73412d12f3b0ba5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 14:04:28 +0000

    Add package tril/0.1.1/tril-0.1.1.tar.gz

---
+++ packages/tril/tril-0.1.1.tar.gz/tril-0.1.1/src/tril/algorithms/__init__.py
@@ -0,0 +1,33 @@
+from tril.algorithms.aggrevated import AGGREVATED
+from tril.algorithms.bc import BC
+from tril.algorithms.cpi import CPI
+from tril.algorithms.cpi_references import CPIReference
+from tril.algorithms.d2lols import D2LOLS
+from tril.algorithms.gail import GAIL
+from tril.algorithms.lols import LOLS
+from tril.algorithms.ppo import PPO
+
+
+class AlgorithmRegistry:
+    _registry = {
+        "ppo": PPO,
+        "cpi": CPI,
+        "aggrevated": AGGREVATED,
+        "lols": LOLS,
+        "d2lols": D2LOLS,
+        "cpireference": CPIReference,
+        "bc": BC,
+        "gail": GAIL,
+    }
+
+    @classmethod
+    def get(cls, alg_id: str):
+        try:
+            alg_cls = cls._registry[alg_id]
+        except KeyError:
+            raise NotImplementedError
+        return alg_cls
+
+    @classmethod
+    def add(cls, id: str, alg_cls):
+        AlgorithmRegistry._registry[id] = alg_cls
