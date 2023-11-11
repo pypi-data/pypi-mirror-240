af68831b6fc1b3479fbcf0e9926fea59c9c51738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:50 +0000

    Add package ckhCal/0.0.2/ckhCal-0.0.2-py3-none-any.whl

---
+++ packages/ckhCal/ckhCal-0.0.2-py3-none-any.whl/ckhCal/calculations.py
@@ -0,0 +1,10 @@
+def add(a1: int ,a2:int ):
+    return a1 + a2
+
+def sub(a1: int ,a2:int ):
+    return a1 - a2
+
+def mul(a1: int ,a2:int ):
+    return a1 * a2
+def div(a1: int ,a2:int ):
+    return a1 / a2
