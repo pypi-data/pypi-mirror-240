0b6ce47bea6d8e29bbd81ffd68266787abc94ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:47 +0000

    Add package types-bleach/6.1.0.1/types-bleach-6.1.0.1.tar.gz

---
+++ packages/types-bleach/types-bleach-6.1.0.1.tar.gz/types-bleach-6.1.0.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+name = "types-bleach"
+description = "Typing stubs for bleach"
+long_description = '''
+## Typing stubs for bleach
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`bleach`](https://github.com/mozilla/bleach) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`bleach`.
+
+This version of `types-bleach` aims to provide accurate annotations
+for `bleach==6.1.*`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/bleach. All fixes for
+types and metadata should be contributed there.
+
+This stub package is marked as [partial](https://peps.python.org/pep-0561/#partial-stub-packages).
+If you find that annotations are missing, feel free to contribute and help complete them.
+
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `571cc6d77ad85c6b3c12f546f8b62c69f37da402` and was tested
+with mypy 1.7.0, pyright 1.1.334, and
+pytype 2023.10.17.
+'''.lstrip()
+
+setup(name=name,
+      version="6.1.0.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/bleach.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['bleach-stubs'],
+      package_data={'bleach-stubs': ['__init__.pyi', 'callbacks.pyi', 'css_sanitizer.pyi', 'html5lib_shim.pyi', 'linkifier.pyi', 'sanitizer.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0 license",
+      python_requires=">=3.7",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
