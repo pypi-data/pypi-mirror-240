0bc42bb5707d1cf5326df4fbad7210ae3cf800a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 06:05:06 +0000

    Add package mlflow-watsonml/0.11.0/mlflow-watsonml-0.11.0.tar.gz

---
+++ packages/mlflow-watsonml/mlflow-watsonml-0.11.0.tar.gz/mlflow-watsonml-0.11.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.readlines()
+
+setup(
+    name="mlflow-watsonml",
+    description="WatsonML MLflow deployment plugin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IBM/mlflow-watsonml",
+    packages=["mlflow_watsonml"],
+    author="IBM AI Model Factory team",
+    author_email="dhruv.shah@ibm.com",
+    install_requires=install_requires,
+    extras_require={
+        "dev": ["ipython", "black", "pytest", "build", "wheel", "twine", "pytest-cov"],
+        "onnx": ["onnx", "onnxruntime"],
+    },
+    entry_points={"mlflow.deployments": "watsonml=mlflow_watsonml.deploy"},
+    python_requires=">=3.9",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)
