60f8e8072712ce46d7d08df5e0c9c1d82404436b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:05:12 +0000

    Add package pycyclops/0.2.0.dev0/pycyclops-0.2.0.dev0.tar.gz

---
+++ packages/pycyclops/pycyclops-0.2.0.dev0.tar.gz/pycyclops-0.2.0.dev0/cyclops/models/wrappers/__init__.py
@@ -0,0 +1,9 @@
+"""Wrapper classes for models."""
+
+from typing import Union
+
+from cyclops.models.wrappers.pt_model import PTModel
+from cyclops.models.wrappers.sk_model import SKModel
+
+
+WrappedModel = Union[PTModel, SKModel]
---
+++ packages/pycyclops/pycyclops-0.2.0.dev0.tar.gz/pycyclops-0.2.0.dev0/cyclops/monitor/__init__.py
@@ -0,0 +1,7 @@
+"""Monitor package."""
+
+from cyclops.monitor.clinical_applicator import ClinicalShiftApplicator
+from cyclops.monitor.detector import Detector
+from cyclops.monitor.reductor import Reductor
+from cyclops.monitor.synthetic_applicator import SyntheticShiftApplicator
+from cyclops.monitor.tester import DCTester, TSTester
