55f503c5d7a8dbcc18ce8748c83d1cc9f08dd081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:08:20 +0000

    Add package kappadata/1.3.84/kappadata-1.3.84.tar.gz

---
+++ packages/kappadata/kappadata-1.3.84.tar.gz/kappadata-1.3.84/kappadata/utils/param_checking.py
@@ -0,0 +1,17 @@
+import collections.abc
+from itertools import repeat
+
+
+# adapted from timm (timm/models/layers/helpers.py)
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable):
+            assert len(x) == n
+            return x
+        return tuple(repeat(x, n))
+
+    return parse
+
+
+to_2tuple = _ntuple(2)
+to_4tuple = _ntuple(4)
