e25672807813c1b8196ab6594d40813dd5389130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:05:42 +0000

    Add package netbox-friendlyurl/0.1/netbox_friendlyurl-0.1.tar.gz

---
+++ packages/netbox-friendlyurl/netbox_friendlyurl-0.1.tar.gz/netbox_friendlyurl-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox_friendlyurl',
+    version='0.1',
+    description='Netbox Friendly URLs',
+    long_description='Create urls in NetBox that are more user friendly and that redirect to real netbox urls',
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
