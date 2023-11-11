89211befea319eb50d03f2db80a3526d3b11d80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:05:00 +0000

    Add package aiconsole/0.2.1a10/aiconsole-0.2.1a10-py3-none-any.whl

---
+++ packages/aiconsole/aiconsole-0.2.1a10-py3-none-any.whl/aiconsole/api/endpoints/chats/history.py
@@ -0,0 +1,57 @@
+# The AIConsole Project
+#
+# Copyright 2023 10Clouds
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from aiconsole.core.project.paths import get_history_directory
+from fastapi import APIRouter, status, Response
+from fastapi.responses import JSONResponse
+from aiconsole.core.chat.types import Chat
+from aiconsole.core.chats.load_chat_history import load_chat_history
+from aiconsole.core.chats.save_chat_history import save_chat_history
+
+router = APIRouter()
+
+
+@router.delete("/history/{chat_id}")
+async def delete_history(chat_id: str):
+    file_path = get_history_directory() / f"{chat_id}.json"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return Response(
+            status_code=status.HTTP_200_OK,
+            content="Chat history deleted successfully",
+        )
+    else:
+        return Response(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content="Chat history not found",
+        )
+
+@router.get("/history/{chat_id}")
+async def get_history(chat_id: str):
+    chat = await load_chat_history(chat_id)
+
+    return JSONResponse(chat.model_dump())
+
+
+@router.post("/history")
+async def save_history(chat: Chat):
+    save_chat_history(chat)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        content="Chat history saved successfully",
+    )
---
+++ packages/aiconsole/aiconsole-0.2.1a10-py3-none-any.whl/aiconsole/api/endpoints/run_code.py
@@ -0,0 +1,62 @@
+# The AIConsole Project
+#
+# Copyright 2023 10Clouds
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+    
+import asyncio
+import traceback
+from fastapi import APIRouter, Request
+from fastapi.responses import StreamingResponse
+import logging
+from aiconsole.core.project import project
+from aiconsole.core.code_running.run_code import get_code_interpreter
+from aiconsole.core.code_running.types import CodeToRun
+from aiconsole.utils.cancel_on_disconnect import cancelable_endpoint
+from aiconsole.api.websockets.outgoing_messages import ErrorWSMessage
+
+router = APIRouter()
+
+_log = logging.getLogger(__name__)
+
+@router.post("/chats/{chat_id}/run_code")
+@cancelable_endpoint
+async def run_code(request: Request, code_to_run: CodeToRun, chat_id: str) -> StreamingResponse:
+    async def async_wrapper():
+        try:
+            _log.debug("Running code: %s", code_to_run.code)
+
+            mats = [project.get_project_materials().get_asset(mid) for mid in code_to_run.materials_ids]
+
+            # If the code starts with a !, that means a shell command
+            if code_to_run.language == "python" and code_to_run.code.startswith("!"):
+                code_to_run.language = "shell"
+                code_to_run.code = code_to_run.code[1:]
+
+            try:
+                for chunk in get_code_interpreter(code_to_run.language).run(code_to_run.code, mats):
+                    yield chunk
+            except asyncio.CancelledError:
+                get_code_interpreter(code_to_run.language).terminate()
+                _log.info("Run cancelled")
+            except Exception:
+                await ErrorWSMessage(error=traceback.format_exc().strip()).send_to_chat(chat_id)
+                yield traceback.format_exc().strip()
+        except Exception as e:
+            await ErrorWSMessage(error=str(e)).send_to_chat(chat_id)
+            raise e
+
+    return StreamingResponse(
+        async_wrapper(), media_type="text/event-stream"
+    )
+    
