2805a885fdf566cd9d0d1faffbf7c4b43591d3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:38 +0000

    Add package pygwalker/0.3.12/pygwalker-0.3.12.tar.gz

---
+++ packages/pygwalker/pygwalker-0.3.12.tar.gz/pygwalker-0.3.12/app/src/store/common.ts
@@ -0,0 +1,85 @@
+import { makeObservable, observable, action } from 'mobx';
+import { ReactElement } from "react";
+
+interface IInitModalInfo {
+    total: number;
+    curIndex: number;
+    title: string;
+}
+
+export interface INotification {
+    title: string;
+    message: string | ReactElement;
+    type: "success" | "error" | "info" | "warning";
+}
+
+class CommonStore {
+    _notifyTimeoutFunc = setTimeout(() => {}, 0);
+
+    initModalOpen: boolean = false;
+    initModalInfo: IInitModalInfo = {
+        total: 0,
+        curIndex: 0,
+        title: "",
+    };
+    showCloudTool: boolean = false;
+    version: string = "";
+    notification: INotification | null = null;
+    kanariesToken: string = "";
+    uploadSpecModalOpen: boolean = false;
+
+    setInitModalOpen(value: boolean) {
+        this.initModalOpen = value;
+    }
+
+    setInitModalInfo(info: IInitModalInfo) {
+        this.initModalInfo = info;
+    }
+
+    setShowCloudTool(value: boolean) {
+        this.showCloudTool = value;
+    }
+
+    setVersion(value: string) {
+        this.version = value;
+    }
+    
+    setNotification(value: INotification | null, timeout: number = 5_000) {
+        clearTimeout(this._notifyTimeoutFunc);
+        this.notification = value;
+        this._notifyTimeoutFunc = setTimeout(() => {
+            this.notification = null;
+        }, timeout);
+    }
+
+    setKanariesToken(value: string) {
+        this.kanariesToken = value;
+    }
+
+    setUploadSpecModalOpen(value: boolean) {
+        this.uploadSpecModalOpen = value;
+    }
+
+    constructor() {
+        makeObservable(this, {
+            initModalOpen: observable,
+            initModalInfo: observable,
+            showCloudTool: observable,
+            version: observable,
+            notification: observable,
+            kanariesToken: observable,
+            uploadSpecModalOpen: observable,
+            setInitModalOpen: action,
+            setInitModalInfo: action,
+            setShowCloudTool: action,
+            setVersion: action,
+            setNotification: action,
+            setKanariesToken: action,
+            setUploadSpecModalOpen: action,
+        });
+    }
+}
+
+const commonStore = new CommonStore();
+
+export default commonStore;
