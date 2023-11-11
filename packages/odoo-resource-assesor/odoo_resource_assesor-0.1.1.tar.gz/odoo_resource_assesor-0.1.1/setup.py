e4ef9936503be146cfafbf72c343af61f3a31367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:07:25 +0000

    Add package odoo-resource-assesor/0.1.1/odoo_resource_assesor-0.1.1.tar.gz

---
+++ packages/odoo-resource-assesor/odoo_resource_assesor-0.1.1.tar.gz/odoo_resource_assesor-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='odoo_resource_assesor',
+    version='0.1.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'resource-assesor=odoo_resource_assesor.resource_assesor:main',  # Adjust the path as necessary
+        ],
+    },
+    install_requires=[
+        'psutil',
+    ],
+    author='Lai',
+    author_email='sysadmin@coopdevs.org',
+    description='A tool to calculate Odoo configuration based on system resources.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://git.coopdevs.org/coopdevs/sandbox/odoo_resource_assesor',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
