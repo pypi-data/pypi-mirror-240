3bd8888141d903fca6c4c2de3489c2f378146d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:10:48 +0000

    Add package audiostack/0.1.1/audiostack-0.1.1.tar.gz

---
+++ packages/audiostack/audiostack-0.1.1.tar.gz/audiostack-0.1.1/audiostack/content/__init__.py
@@ -0,0 +1,19 @@
+from audiostack.content.script import Script
+from audiostack.content.media import Media
+
+
+def list_projects():
+    from audiostack.content.root_functions import Root
+
+    return Root.list_projects()
+
+
+def list_modules():
+    from audiostack.content.root_functions import Root
+
+    return Root.list_modules()
+
+def generate(prompt: str, max_length: int = 100):
+    from audiostack.content.root_functions import Root
+
+    return Root.generate(prompt, max_length)
