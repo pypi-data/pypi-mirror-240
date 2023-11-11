72819f3a7ba2585a468079f253b42f8a3fcfb65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:06:38 +0000

    Add package spaceone-inventory/2.0.dev17/spaceone-inventory-2.0.dev17.tar.gz

---
+++ packages/spaceone-inventory/spaceone-inventory-2.0.dev17.tar.gz/spaceone-inventory-2.0.dev17/spaceone/inventory/plugin/collector/lib/metadata.py
@@ -0,0 +1,50 @@
+import os
+import inspect
+import logging
+from spaceone.core.error import *
+from spaceone.core.utils import load_yaml_from_file
+from spaceone.inventory.plugin.collector.lib.metadata_generator import MetadataGenerator
+
+__all__ = ['convert_cloud_service_meta', 'convert_cloud_service_type_meta']
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def convert_cloud_service_meta(provider, cloud_service_group, cloud_service_type) -> dict:
+    return {
+        'view': {
+            'sub_data': {
+                'reference': {
+                    'resource_type': 'inventory.CloudServiceType',
+                    'options': {
+                        'provider': provider,
+                        'cloud_service_group': cloud_service_group,
+                        'cloud_service_type': cloud_service_type,
+                    }
+                }
+            }
+        }
+    }
+
+
+def convert_cloud_service_type_meta(metadata_path: str) -> dict:
+    yaml_path = _get_yaml_path(metadata_path)
+    old_metadata = load_yaml_from_file(yaml_path)
+    return MetadataGenerator(old_metadata).generate_metadata()
+
+
+def _get_yaml_path(metadata_path: str) -> str:
+    # Get the previous path of call function
+    manager_path = inspect.stack()[3][1]
+
+    _LOGGER.debug(f'[_get_yaml_path] manager_path: {manager_path}')
+
+    try:
+        app_path, unusable = manager_path.split('src', 1)
+        yaml_path = os.path.join(app_path, 'src', metadata_path)
+    except ValueError:
+        app_path, unusable = manager_path.split('source', 1)
+        yaml_path = os.path.join(app_path, 'source', metadata_path)
+    except Exception as e:
+        raise ERROR_INVALID_PARAMETER(key='metadata_path', reason=f'Invalid metadata_path: {metadata_path}')
+    return yaml_path
