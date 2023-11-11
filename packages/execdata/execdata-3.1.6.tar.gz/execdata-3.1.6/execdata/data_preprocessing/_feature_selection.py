188e3e744df6bbd88eaf9d25455a397ccccf5f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:06:12 +0000

    Add package execdata/3.1.6/execdata-3.1.6.tar.gz

---
+++ packages/execdata/execdata-3.1.6.tar.gz/execdata-3.1.6/execdata/data_preprocessing/_feature_selection.py
@@ -0,0 +1,13 @@
+'''
+Date         : 2023-01-17 10:33:06
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-11-09 16:53:54
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\data_preprocessing\_feature_selection.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+def add(a,b):
+    result = a + b
+    return result
