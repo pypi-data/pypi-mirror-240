e5eb106caea9dc09b24cd16101c90a1f73b87dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 04:06:53 +0000

    Add package django-weekend/0.0.4/django_weekend-0.0.4.tar.gz

---
+++ packages/django-weekend/django_weekend-0.0.4.tar.gz/django_weekend-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='django_weekend',
+  version='0.0.4',
+  author='Maksim Prilepsky',
+  author_email='maksimprilepsky@yandex.ru',
+  description='A simple library for tracking weekends and working days',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/MaksimJames/django_weekend',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='django calendar settings weekend',
+  project_urls={
+    'GitHub': 'https://github.com/MaksimJames/django_weekend'
+  },
+  python_requires='>=3.6'
+)
