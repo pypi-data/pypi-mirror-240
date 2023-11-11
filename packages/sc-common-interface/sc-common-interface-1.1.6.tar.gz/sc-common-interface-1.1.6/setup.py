f5f36290c6ff467322b06137866b8e71ce580913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:41 +0000

    Add package sc-common-interface/1.1.6/sc-common-interface-1.1.6.tar.gz

---
+++ packages/sc-common-interface/sc-common-interface-1.1.6.tar.gz/sc-common-interface-1.1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sc-common-interface',
+    version='1.1.6',
+    description='SC公用的接口',
+    author='river',
+    packages=find_packages(),
+    install_requires=[
+    "requests>=2.0.0",
+    "jsonpath>=0.82",
+
+
+             ],
+)
