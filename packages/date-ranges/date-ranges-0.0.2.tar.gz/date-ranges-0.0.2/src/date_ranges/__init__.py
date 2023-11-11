d5e9c7ed4a531d2e5adbba7e11d64e70b9cce697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:06:41 +0000

    Add package date-ranges/0.0.2/date-ranges-0.0.2.tar.gz

---
+++ packages/date-ranges/date-ranges-0.0.2.tar.gz/date-ranges-0.0.2/src/date_ranges/__init__.py
@@ -0,0 +1,7 @@
+from .date_ranges import date_range_for_month_containing
+from .date_ranges import DateRange
+from .date_ranges import last_date_of_month
+from .date_ranges import MAX_DATE
+from .date_ranges import MIN_DATE
+from .date_ranges import month_ordinal_day
+from .date_ranges import ordinal
