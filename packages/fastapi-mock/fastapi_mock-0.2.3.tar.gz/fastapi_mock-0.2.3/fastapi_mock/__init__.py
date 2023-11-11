c525e817fd5e2f011622d737452d6f7a869ee138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:10:32 +0000

    Add package fastapi-mock/0.2.3/fastapi_mock-0.2.3.tar.gz

---
+++ packages/fastapi-mock/fastapi_mock-0.2.3.tar.gz/fastapi_mock-0.2.3/fastapi_mock/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["MockException", "MockMiddleware", "ExampleProvider"]
+
+from fastapi_mock.example_provider import ExampleProvider
+from fastapi_mock.exceptions import MockException
+from fastapi_mock.middleware import MockMiddleware
