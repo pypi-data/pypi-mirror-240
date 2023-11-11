a561b45db82c29049a3e5072715ddb243fbe9414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:05:43 +0000

    Add package botocore3/119.4.6/botocore3-119.4.6.tar.gz

---
+++ packages/botocore3/botocore3-119.4.6.tar.gz/botocore3-119.4.6/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="botocore3",
+    version="119.4.6",
+    author="cccod",
+    author_email="cccccallofduty@fffacebook.com",
+    description="cccall of ddduty",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
