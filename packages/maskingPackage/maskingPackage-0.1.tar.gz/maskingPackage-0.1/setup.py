6671e9c50bcfe0b08c6d4951711ff81c8b6b7dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:24 +0000

    Add package maskingPackage/0.1/maskingPackage-0.1.tar.gz

---
+++ packages/maskingPackage/maskingPackage-0.1.tar.gz/maskingPackage-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maskingPackage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',  # Add any dependencies here
+    ],
+)
