9f876b0e0c0e170400a4c1fc958f672b40c23e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:10:48 +0000

    Add package dcor-control/0.8.13/dcor_control-0.8.13.tar.gz

---
+++ packages/dcor-control/dcor_control-0.8.13.tar.gz/dcor_control-0.8.13/dcor_control/cli/__init__.py
@@ -0,0 +1,19 @@
+import click
+
+from . import backup
+from . import info
+from . import inspect
+from . import reset
+from . import update
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(backup.encrypted_database_backup)
+main.add_command(inspect.inspect)
+main.add_command(info.status)
+main.add_command(reset.reset)
+main.add_command(update.update)
