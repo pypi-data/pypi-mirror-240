4951d7240d5e5cf972ae49c32e3b04ca51b602d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 05:06:38 +0000

    Add package corsika-panama/0.8.0/corsika_panama-0.8.0-py3-none-any.whl

---
+++ packages/corsika-panama/corsika_panama-0.8.0-py3-none-any.whl/panama/fluxes/__init__.py
@@ -0,0 +1,19 @@
+from .cosmic_ray_fluxes import (
+    TIG,
+    CosmicRayFlux,
+    GlobalFitGST,
+    GlobalSplineFit,
+    H3a,
+    H4a,
+    TIGCutoff,
+)
+
+__all__ = (
+    "H3a",
+    "H4a",
+    "TIG",
+    "TIGCutoff",
+    "GlobalSplineFit",
+    "CosmicRayFlux",
+    "GlobalFitGST",
+)
