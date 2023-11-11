1bc7ba23988c26c7e095d73ba2c8cee32b277cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:06:55 +0000

    Add package cofutils/0.0.1/cofutils-0.0.1.tar.gz

---
+++ packages/cofutils/cofutils-0.0.1.tar.gz/cofutils-0.0.1/README.md
@@ -0,0 +1,10 @@
+# Cofutils
+
+This repositry is used to develop functional python module as an experimental toolkit.
+
+## Cofrun
+
+
+## Coflogging
+
+## Cof
---
+++ packages/cofutils/cofutils-0.0.1.tar.gz/cofutils-0.0.1/cofutils/__init__.py
@@ -0,0 +1,11 @@
+from .package_info import (
+    __description__,
+    __contact_names__,
+    __url__,
+    __keywords__,
+    __license__,
+    __package_name__,
+    __version__,
+)
+from .coflogger import default_logger as coflog
+from .cofmem import report_memory_usage as cofmem
