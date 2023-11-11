189b289c650a26988ffd1d1c61970a9e62fdf31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 15:07:13 +0000

    Add package youtubeCommentScrapper/0.0.1/youtubeCommentScrapper-0.0.1.tar.gz

---
+++ packages/youtubeCommentScrapper/youtubeCommentScrapper-0.0.1.tar.gz/youtubeCommentScrapper-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='youtubeCommentScrapper',
+    version='0.0.1',
+    author = "Harsh Jain",
+    packages = ["youtubeCommentScrapper"],
+
+    install_requires=[
+        "selenium","emoji"
+    ],
+)
