edfea9d32d0cdee40ad1a8b34929a2ccffa3ab59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 06:05:47 +0000

    Add package rds2py/0.3.2/rds2py-0.3.2-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/rds2py/rds2py-0.3.2-cp310-cp310-macosx_10_9_x86_64.whl/rds2py/pdf.py
@@ -0,0 +1,76 @@
+from typing import MutableMapping
+
+from pandas import DataFrame
+
+from .parser import get_class
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+
+def as_pandas_from_data_frame(robj: MutableMapping) -> DataFrame:
+    """Read an R object to a :py:class:`~pandas.DataFrame`.
+
+    Args:
+        robj (MutableMapping): Object parsed from the `RDS` file.
+            Usually the result of :py:func:`~rds2py.parser.read_rds`.
+
+    Raises:
+        TypeError: If ``robj`` is not a compatible class.
+
+    Returns:
+        DataFrame: A `DataFrame` from the R Object.
+    """
+
+    cls = get_class(robj)
+
+    if cls != "data.frame":
+        raise TypeError("`robj` does not contain a 'data.frame'.")
+
+    df = DataFrame(
+        robj["data"],
+        columns=robj["attributes"]["names"]["data"],
+        index=robj["attributes"]["row.names"]["data"],
+    )
+
+    return df
+
+
+def as_pandas_from_dframe(robj: MutableMapping) -> DataFrame:
+    """Convert a realized R object to a pandas data frame representation.
+
+    Args:
+        robj (MutableMapping): Object parsed from the `RDS` file.
+            Usually the result of :py:func:`~rds2py.parser.read_rds`.
+
+    Raises:
+        Exception: If ``robj`` does not contain any dataframe.
+
+    Returns:
+        DataFrame: A `DataFrame` from the R Object.
+    """
+
+    cls = get_class(robj)
+
+    if cls != "DFrame":
+        raise Exception("`robj` does not contain a 'DFrame'.")
+
+    data = {}
+    col_names = robj["attributes"]["listData"]["attributes"]["names"]["data"]
+    for idx in range(len(col_names)):
+        idx_asy = robj["attributes"]["listData"]["data"][idx]
+
+        data[col_names[idx]] = idx_asy["data"]
+
+    index = None
+    if robj["attributes"]["rownames"]["data"]:
+        index = robj["attributes"]["rownames"]["data"]
+
+    df = DataFrame(
+        data,
+        columns=col_names,
+        index=index,
+    )
+
+    return df
