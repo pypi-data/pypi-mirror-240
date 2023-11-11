838e4a36a37f09e9ad547dc5c0635c907432b3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-11 03:07:52 +0000

    Add package chic-lib/0.1.4/chic_lib-0.1.4-py3-none-any.whl

---
+++ packages/chic-lib/chic_lib-0.1.4-py3-none-any.whl/chic/__init__.py
@@ -0,0 +1,13 @@
+"""
+chic.
+
+A Python library for coarse-graining hybrid and inorganic frameworks.
+"""
+
+
+from .structure import Structure
+from .net import Net
+
+
+__version__ = "0.1.4"
+__author__ = 'Thomas C Nicholas'
