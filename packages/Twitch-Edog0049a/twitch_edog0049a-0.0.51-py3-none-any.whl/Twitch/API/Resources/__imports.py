843ba8863cf83fda4734e60f42155247ce88af74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:22 +0000

    Add package Twitch-Edog0049a/0.0.51/twitch_edog0049a-0.0.51-py3-none-any.whl

---
+++ packages/Twitch-Edog0049a/twitch_edog0049a-0.0.51-py3-none-any.whl/Twitch/API/Resources/__imports.py
@@ -0,0 +1,5 @@
+from Twitch.API.Resources import Utils
+from Twitch import Scope 
+from Twitch import SubscriptionTypes as SubType
+from typing import Optional, Any, TypeVar, Type, List, Union, TypeAlias, Dict, Tuple, enum
+from datetime import datetime
