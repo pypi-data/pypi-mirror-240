3bcf6407393ecf04b5c6df2d13ba6c7f365f0b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:47 +0000

    Add package ConsoleMasterPy/1.0.6/ConsoleMasterPy-1.0.6.tar.gz

---
+++ packages/ConsoleMasterPy/ConsoleMasterPy-1.0.6.tar.gz/ConsoleMasterPy-1.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="ConsoleMasterPy",
+    version="1.0.6",
+    author="Ronchetti Ezequiel Nicolás",
+    author_email="RonchettiEzequielNicolas@hotmail.com",
+    description="Console/Terminal functionality",
+    long_description="Console/Terminal functionality",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        'cursor>=1.3.5',
+        'sty>=1.0.4',
+    ],
+)
