f52e8d9a0881a74d128878e3485316611a9087d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 05:04:27 +0000

    Add package Late/1.0.2/Late-1.0.2-py3-none-any.whl

---
+++ packages/Late/Late-1.0.2-py3-none-any.whl/test/simple_test.py
@@ -0,0 +1,27 @@
+import inspect
+from typing import Any
+
+from late import latebinding, __, _LateBound
+
+
+def test_with_list():
+
+    @latebinding
+    def f(x: list[Any] = __([])) -> list[Any]:
+        x.append(1)
+        return x
+
+    assert f() == [1]
+    assert f() == [1]
+    assert f() == [1]
+
+
+def test_immutable():
+    @latebinding
+    def f(x: frozenset[Any] = __(frozenset()), y: set = __(set())):
+        return
+
+    param = inspect.signature(f).parameters['x']
+    assert type(param.default) is frozenset
+    param = inspect.signature(f).parameters['y']
+    assert type(param.default) is _LateBound
