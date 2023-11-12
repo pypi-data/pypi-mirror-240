68f825868377474189d610539998afdd00dbd733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:19:44 +0000

    Add package StringFuncs/0.0.3.211/StringFuncs-0.0.3.211.tar.gz

---
+++ packages/StringFuncs/StringFuncs-0.0.3.211.tar.gz/StringFuncs-0.0.3.211/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='StringFuncs',
+    version='0.0.3.211',
+    author='Samuel Morris',
+    author_email='you@example.com',
+    description='A package for string manipulation',
+    url='https://github.com/SamuelMorrisProjects/StringFuncs',
+    packages=['StringFuncs'],
+    install_requires=[
+    ]
+)
