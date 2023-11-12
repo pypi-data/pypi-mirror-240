d87b9fea579351cc2d55389602b711ff56e04832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:06:27 +0000

    Add package shopback-sdk/0.1.5b0/shopback_sdk-0.1.5b0.tar.gz

---
+++ packages/shopback-sdk/shopback_sdk-0.1.5b0.tar.gz/shopback_sdk-0.1.5b0/shopback_sdk/__init__.py
@@ -0,0 +1,14 @@
+"""shopback.ru - no code shop constructor and caffe for telegram"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
