1c447946fbca8688d2f294eaa4d44b1980dbbbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 15:04:46 +0000

    Add package juntagrico/1.3.14/juntagrico-1.3.14.tar.gz

---
+++ packages/juntagrico/juntagrico-1.3.14.tar.gz/juntagrico-1.3.14/juntagrico/admins/depot_admin.py
@@ -0,0 +1,7 @@
+from juntagrico.admins import BaseAdmin
+
+
+class DepotAdmin(BaseAdmin):
+    raw_id_fields = ['contact']
+    list_display = ['name', 'code', 'weekday', 'contact']
+    exclude = ['capacity']
