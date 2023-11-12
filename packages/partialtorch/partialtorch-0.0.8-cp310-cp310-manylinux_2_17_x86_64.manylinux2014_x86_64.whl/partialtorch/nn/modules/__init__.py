45acc6c77ac671747fde410f4acfa0e95cf599aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 15:04:22 +0000

    Add package partialtorch/0.0.8/partialtorch-0.0.8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/partialtorch/partialtorch-0.0.8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/partialtorch/nn/modules/__init__.py
@@ -0,0 +1,19 @@
+from .activation import *
+from .batchnorm import *
+from .channelshuffle import *
+from .container import *
+from .conv import *
+from .dropout import *
+from .flatten import *
+from .fold import *
+from .instancenorm import *
+from .linear import *
+from .module import *
+from .normalization import *
+from .padding import *
+from .pixelshuffle import *
+from .pooling import *
+from .upsampling import *
+
+# TODO: adaptive, loss, rnn, transformer
+#  unlikely to be added: lazy, sparse
