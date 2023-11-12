bfe215f588d8744a245981ea0ff383c7c69bc1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 15:04:38 +0000

    Add package google-ads-stubs/16.0.0/google_ads_stubs-16.0.0-py3-none-any.whl

---
+++ packages/google-ads-stubs/google_ads_stubs-16.0.0-py3-none-any.whl/google-stubs/ads/googleads/v15/services/types/account_budget_proposal_service.pyi
@@ -0,0 +1,60 @@
+from collections.abc import Mapping
+from typing import Any, TypeVar
+
+import google.protobuf.message
+import proto
+from google.protobuf.field_mask_pb2 import FieldMask
+
+from google.ads.googleads.v15.resources.types.account_budget_proposal import (
+    AccountBudgetProposal,
+)
+
+_M = TypeVar("_M")
+
+class AccountBudgetProposalOperation(proto.Message):
+    update_mask: FieldMask
+    create: AccountBudgetProposal
+    remove: str
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        update_mask: FieldMask = ...,
+        create: AccountBudgetProposal = ...,
+        remove: str = ...
+    ) -> None: ...
+
+class MutateAccountBudgetProposalRequest(proto.Message):
+    customer_id: str
+    operation: AccountBudgetProposalOperation
+    validate_only: bool
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        customer_id: str = ...,
+        operation: AccountBudgetProposalOperation = ...,
+        validate_only: bool = ...
+    ) -> None: ...
+
+class MutateAccountBudgetProposalResponse(proto.Message):
+    result: MutateAccountBudgetProposalResult
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        result: MutateAccountBudgetProposalResult = ...
+    ) -> None: ...
+
+class MutateAccountBudgetProposalResult(proto.Message):
+    resource_name: str
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        resource_name: str = ...
+    ) -> None: ...
