363ba430a07d42fca09c5c5ab4b0d9671d9baaa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:37 +0000

    Add package LibreriaViajes-UxueAinara/0.5.5/LibreriaViajes_UxueAinara-0.5.5.tar.gz

---
+++ packages/LibreriaViajes-UxueAinara/LibreriaViajes_UxueAinara-0.5.5.tar.gz/LibreriaViajes_UxueAinara-0.5.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LibreriaViajes_UxueAinara',
+    version='0.5.5',
+    author='Ainara Hoyos y Uxue Garcia',
+    description='Una librería para obtener información de interés sobre su destino.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.0.0',
+        'termcolor>=1.1.0',
+    ],
+    package_data={
+        'Viajes': ['docs/build/html/index.html'],
+    },
+)
