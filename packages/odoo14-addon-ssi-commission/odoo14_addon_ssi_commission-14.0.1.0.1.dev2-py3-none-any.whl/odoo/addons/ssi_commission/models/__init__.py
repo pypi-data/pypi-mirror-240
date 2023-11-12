e725a73d083bbcc41e5c36a7c44deb48c09352c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:19:28 +0000

    Add package odoo14-addon-ssi-commission/14.0.1.0.1.dev2/odoo14_addon_ssi_commission-14.0.1.0.1.dev2-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-commission/odoo14_addon_ssi_commission-14.0.1.0.1.dev2-py3-none-any.whl/odoo/addons/ssi_commission/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    commission_type,
+    commission_type_item,
+    commission_item,
+    commission,
+    commission_tax,
+    commission_detail,
+    commission_item_computation,
+)
