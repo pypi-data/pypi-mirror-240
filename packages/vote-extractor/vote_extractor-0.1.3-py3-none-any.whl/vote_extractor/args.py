e3defb8fd65c05baf14f03b18161091ee2f56017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:08:45 +0000

    Add package vote-extractor/0.1.3/vote_extractor-0.1.3-py3-none-any.whl

---
+++ packages/vote-extractor/vote_extractor-0.1.3-py3-none-any.whl/vote_extractor/args.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+
+def parse_pdf_name(args):
+    pdf_filename = args.pdf_url.rsplit("/", 1)[-1]
+    name, _ = os.path.splitext(pdf_filename)
+    return name
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdf_url", help="Url to the pdf to extract votes from.")
+    parser.add_argument(
+        "-x",
+        "--excel",
+        help="Store extracted votes as excel file.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c", "--csv", help="Store extracted votes CSV file.", action="store_true"
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="Name of the generated file(s). Defaults to the input PDF name.",
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        help="Name of the directory where the generated PDFs are stored. Defaults to `./out.`",
+        default="out"
+    )
+    return parser.parse_args()
