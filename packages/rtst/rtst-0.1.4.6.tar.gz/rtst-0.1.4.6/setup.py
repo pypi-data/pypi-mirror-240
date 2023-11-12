e68c9b794c7ff1ef3f4f3315ae6f17925ea4e474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:19:26 +0000

    Add package rtst/0.1.4.6/rtst-0.1.4.6.tar.gz

---
+++ packages/rtst/rtst-0.1.4.6.tar.gz/rtst-0.1.4.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="rtst",
+    version="0.1.4.6",
+    packages=find_packages(),
+    install_requires=[
+        "speechrecognition",
+        "langdetect",
+        "googletrans",
+        "pyttsx3",
+        "azure-cognitiveservices-speech",
+        "boto3",
+        "transformers",
+        "numpy",
+        "tensorflow",
+        "soundfile",
+        "librosa",
+        "PyAudio",
+    ],
+    entry_points={"console_scripts": ["speech-recog=rtst.main:main"]},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
