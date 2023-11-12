f5bafebee87e8e2057f26a5df8f1d1bb486d5526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:09:11 +0000

    Add package vote-extractor/0.1.2/vote_extractor-0.1.2.tar.gz

---
+++ packages/vote-extractor/vote_extractor-0.1.2.tar.gz/vote_extractor-0.1.2/vote_extractor/__main__.py
@@ -0,0 +1,31 @@
+import os
+
+from vote_extractor.args import get_args, parse_pdf_name
+from vote_extractor.extraction import extract_data
+
+
+def main():
+    args = get_args()
+
+    print("Downloading pdf and extracting data...")
+    df = extract_data(args.pdf_url)
+    name = args.name or parse_pdf_name(args)
+
+    if not os.path.isdir(args.directory):
+        print(f"Creating directory {args.directory}")
+        os.makedirs(args.directory)
+
+    if args.csv:
+        csv_file = os.path.join(args.directory, f"{name}.csv")
+        print(f"Storing csv file: {csv_file}")
+        df.to_csv(csv_file, index=False)
+        print("DONE")
+    if args.excel:
+        excel_file = os.path.join(args.directory, f"{name}.xlsx")
+        print(f"Storing excel_file file: {excel_file}")
+        df.to_excel(excel_file, index=False)
+        print("DONE")
+
+
+if __name__ == "__main__":
+    main()
