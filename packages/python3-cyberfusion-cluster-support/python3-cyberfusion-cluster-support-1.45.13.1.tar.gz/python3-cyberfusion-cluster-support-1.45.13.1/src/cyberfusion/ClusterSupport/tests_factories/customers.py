e6b3462e9f7d07b649af61b348b3969f04b868d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:18:27 +0000

    Add package python3-cyberfusion-cluster-support/1.45.13.1/python3-cyberfusion-cluster-support-1.45.13.1.tar.gz

---
+++ packages/python3-cyberfusion-cluster-support/python3-cyberfusion-cluster-support-1.45.13.1.tar.gz/python3-cyberfusion-cluster-support-1.45.13.1/src/cyberfusion/ClusterSupport/tests_factories/customers.py
@@ -0,0 +1,19 @@
+"""Factories for API object."""
+
+import factory
+
+from cyberfusion.ClusterSupport.customers import Customer
+from cyberfusion.ClusterSupport.tests_factories import BaseBackendFactory
+
+
+class CustomerFactory(BaseBackendFactory):
+    """Factory for specific object."""
+
+    class Meta:
+        """Settings."""
+
+        model = Customer
+
+    team_code = factory.Faker(
+        "bothify", text="##??", letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    )
