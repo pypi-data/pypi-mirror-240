ec8c8e79619fcdda9953c43b5ac344bd6673bd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:06:06 +0000

    Add package vgis-gis/1.1.4/vgis_gis-1.1.4.tar.gz

---
+++ packages/vgis-gis/vgis_gis-1.1.4.tar.gz/vgis_gis-1.1.4/vgis_gis/baiduSetting.py
@@ -0,0 +1,18 @@
+"""
+===================================
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+@Author: chenxw
+@Email : gisfanmachel@gmail.com
+@File: baiduSetting.py
+@Date: Create in 2021/1/28 18:04
+@Description: 百度配置
+@ Software: PyCharm
+===================================
+"""
+
+
+class BaiduKeySetting:
+    key_list = ['nLSyVj4sjdBQnDCnte45dhBCGcToIBYC', '1WTjKzz1SvZHGwut6cm4PPkzEafCPr6C',
+                'o0djADqCuXcPaLL4iT7NEwZBfGFXLbc1', 'BiYnONWl5GD4Dhsq84tKzod1NfROebG7',
+                'vtRPtR1BSYkjo9PP5GCG7YlSMOC2OQuH']
