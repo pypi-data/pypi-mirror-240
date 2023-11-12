fc316f2e6c6df850f4383a3f93c06cff0f67e881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:18 +0000

    Add package projet-gcb202a23/1.1.0/projet_gcb202a23-1.1.0.tar.gz

---
+++ packages/projet-gcb202a23/projet_gcb202a23-1.1.0.tar.gz/projet_gcb202a23-1.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="projet_gcb202a23",
+    version="1.1.0",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Projet GCB202A23",
+    long_description="Projet GCB202A23",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
