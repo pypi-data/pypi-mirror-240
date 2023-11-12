e0dd51a378bcd6e607e6e0dc3027297833df04f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:22 +0000

    Add package pypi-pip-template/0.0.4/pypi_pip_template-0.0.4.tar.gz

---
+++ packages/pypi-pip-template/pypi_pip_template-0.0.4.tar.gz/pypi_pip_template-0.0.4/pypi_pip_template/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# this allows to use "from utils import conversion" in __main__.py
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__))) # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+sys.path.append(os.path.dirname(os.path.realpath(__file__+'/gui'))) 
+sys.path.append(os.path.dirname(os.path.realpath(__file__+'/utils')))
+
+
+__version__ = '0.0.4'
+__description__ = 'a template for build python package to upload pypi repository bu using pip'
+__author__ = 'lgf4591'
+__author_email__ = 'lgf4591@outlook.com'
+__url__ = 'https://github.com/lgf4591/pypi_pip_template'
+__license__ = 'MIT License'
---
+++ packages/pypi-pip-template/pypi_pip_template-0.0.4.tar.gz/pypi_pip_template-0.0.4/pypi_pip_template/__main__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# from gui.main import main
+from pypi_pip_template.gui.main import main
+
+if __name__ == '__main__':
+    main()
+    # 这里不要写任何其他代码了，emy只会调用main()
