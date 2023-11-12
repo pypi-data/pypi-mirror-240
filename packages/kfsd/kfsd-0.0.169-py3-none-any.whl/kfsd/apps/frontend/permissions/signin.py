1a8dc9711d9257eabb383d855d1c5a368d75f0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:18 +0000

    Add package kfsd/0.0.169/kfsd-0.0.169-py3-none-any.whl

---
+++ packages/kfsd/kfsd-0.0.169-py3-none-any.whl/kfsd/apps/frontend/permissions/signin.py
@@ -0,0 +1,42 @@
+from kfsd.apps.frontend.permissions.base import BasePermission
+from kfsd.apps.core.exceptions.fe import KubefacetsFEException
+from http import HTTPStatus
+
+
+class SignInRequired(BasePermission):
+    def __init__(self, request):
+        BasePermission.__init__(self, request)
+
+    def __str__(self):
+        return "Is authenticated check"
+
+    def is_valid(self):
+        if self.getUser().isAuthenticated():
+            return True
+        return False
+
+    def raise_exception(self):
+        redirectUrl = self.redirect_url()
+        raise KubefacetsFEException(
+            self.__str__(), HTTPStatus.TEMPORARY_REDIRECT, redirectUrl
+        )
+
+    def redirect_url(self):
+        loginUrl = self.formatUrl(
+            self.getDjangoRequest().findConfigs(["services.context.sso_fe.signin_url"])
+        )
+        loginUrl += "?next={}".format(self.genUrlEncode(self.getCurrentRequestUrl()))
+        return loginUrl
+
+    def redirect_url_neg(self):
+        verifiedFinalUrl = self.formatUrl(
+            self.getDjangoRequest().findConfigs(
+                ["services.context.kubefacets_fe.landing_pg_url"]
+            )
+        )
+        return verifiedFinalUrl
+
+    def raise_exception_neg(self):
+        raise KubefacetsFEException(
+            self.__str__(), HTTPStatus.TEMPORARY_REDIRECT, self.redirect_url_neg()
+        )
