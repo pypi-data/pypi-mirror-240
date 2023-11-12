6f596700098823517a172af3d5a0429febdec9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:06:24 +0000

    Add package google-ads-stubs/15.2.1/google_ads_stubs-15.2.1.tar.gz

---
+++ packages/google-ads-stubs/google_ads_stubs-15.2.1.tar.gz/google_ads_stubs-15.2.1/google-stubs/ads/googleads/v14/services/types/customer_sk_ad_network_conversion_value_schema_service.pyi
@@ -0,0 +1,57 @@
+from collections.abc import Mapping
+from typing import Any, TypeVar
+
+import google.protobuf.message
+import proto
+
+from google.ads.googleads.v14.resources.types.customer_sk_ad_network_conversion_value_schema import (
+    CustomerSkAdNetworkConversionValueSchema,
+)
+
+_M = TypeVar("_M")
+
+class CustomerSkAdNetworkConversionValueSchemaOperation(proto.Message):
+    update: CustomerSkAdNetworkConversionValueSchema
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        update: CustomerSkAdNetworkConversionValueSchema = ...
+    ) -> None: ...
+
+class MutateCustomerSkAdNetworkConversionValueSchemaRequest(proto.Message):
+    customer_id: str
+    operation: CustomerSkAdNetworkConversionValueSchemaOperation
+    validate_only: bool
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        customer_id: str = ...,
+        operation: CustomerSkAdNetworkConversionValueSchemaOperation = ...,
+        validate_only: bool = ...
+    ) -> None: ...
+
+class MutateCustomerSkAdNetworkConversionValueSchemaResponse(proto.Message):
+    result: MutateCustomerSkAdNetworkConversionValueSchemaResult
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        result: MutateCustomerSkAdNetworkConversionValueSchemaResult = ...
+    ) -> None: ...
+
+class MutateCustomerSkAdNetworkConversionValueSchemaResult(proto.Message):
+    resource_name: str
+    app_id: str
+    def __init__(
+        self: _M,
+        mapping: _M | Mapping | google.protobuf.message.Message | None = ...,
+        *,
+        ignore_unknown_fields: bool = ...,
+        resource_name: str = ...,
+        app_id: str = ...
+    ) -> None: ...
