e25157789be882ebd747dccc3d3f69a09f941cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:27 +0000

    Add package google-colab-selenium/1.0.6/google_colab_selenium-1.0.6-py3-none-any.whl

---
+++ packages/google-colab-selenium/google_colab_selenium-1.0.6-py3-none-any.whl/google_colab_selenium/__init__.py
@@ -0,0 +1,28 @@
+__all__ = (
+    'ChromeDriver', 
+    'UndetectedChromeDriver', 
+    'Chrome', 
+    'UndetectedChrome',
+    'Spinner',
+    'GoogleColabSeleniumError',
+    'InstallChromeError',
+    'ChromeDriverPathError',
+    'StartingChromeDriverError'
+)
+
+import os
+
+
+assert 'COLAB_GPU' in os.environ, "This package should only be used in Google Colab Notebooks."
+
+
+def __getattr__(name):
+    if name == 'ChromeDriver' or name == 'Chrome':
+        from google_colab_selenium.chromedriver import ChromeDriver
+        return ChromeDriver
+
+    if name == 'UndetectedChromeDriver' or name == 'UndetectedChrome':
+        from google_colab_selenium.undetected_chromedriver import UndetectedChromeDriver
+        return UndetectedChromeDriver
+
+    raise AttributeError(f"module 'google_colab_selenium' has no attribute '{name}'")
