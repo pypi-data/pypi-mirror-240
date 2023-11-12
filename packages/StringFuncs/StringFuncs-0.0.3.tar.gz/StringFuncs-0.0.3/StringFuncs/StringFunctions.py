5d60a272a3cd253cd28f13eae475ec87344317ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:19:36 +0000

    Add package StringFuncs/0.0.3/StringFuncs-0.0.3.tar.gz

---
+++ packages/StringFuncs/StringFuncs-0.0.3.tar.gz/StringFuncs-0.0.3/StringFuncs/StringFunctions.py
@@ -0,0 +1,17 @@
+
+def Length_Of_Last_Word(s, separator):
+        string = " ".join((s.replace(separator, " ")).split())
+        lastspace=-1
+        for indexvalue in range(len((string))):
+            if string[indexvalue] == " ":
+                lastspace =indexvalue
+        return len(string) - (lastspace+1)
+def Length_Of_First_Word(s, separator):
+        string = " ".join((s.replace(separator, " ")).split())
+        iterations=0
+        while string[iterations]!=" ":
+             iterations+=1
+        return iterations
+if __name__ == "__main__":
+       print()
+
