6898fb6bdcd64b46fdcc0651e13034535ae631b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:22 +0000

    Add package tradingview-screener/2.3.0/tradingview_screener-2.3.0-py3-none-any.whl

---
+++ packages/tradingview-screener/tradingview_screener-2.3.0-py3-none-any.whl/tradingview_screener/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. include:: ../README.md
+"""
+
+from __future__ import annotations
+
+from tradingview_screener.screener import Scanner, get_all_symbols
+from tradingview_screener.query import Query, Column
+
+import pandas as pd
+pd.options.display.max_rows = 10
