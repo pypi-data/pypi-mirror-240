29f8fc8e38f6e19eb746ed04cbec574e811a2357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 05:04:27 +0000

    Add package parser-cian/0.1.6/parser_cian-0.1.6.tar.gz

---
+++ packages/parser-cian/parser_cian-0.1.6.tar.gz/parser_cian-0.1.6/parser_cian/__init__.py
@@ -0,0 +1,7 @@
+from parser_cian.file_operation import save_to_csv, save_to_json
+from parser_cian.flats_parses import Flats_URL, Flats_Full_Info
+from parser_cian.main import Cian_Parser
+from parser_cian.pagination import Pagination
+
+__author__ = "anatolygvozdev"
+__mail__ = "tolagvgvgvgv@gmail.com"
