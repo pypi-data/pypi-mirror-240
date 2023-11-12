1739dc5ed6716d12a01e0e313b8c140de4739658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:20 +0000

    Add package pychek/0.0.2.2/pychek-0.0.2.2.tar.gz

---
+++ packages/pychek/pychek-0.0.2.2.tar.gz/pychek-0.0.2.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.2.2'
+DESCRIPTION = 'A smarter print and profile for Python execution checks.'
+
+with open("README.md", "r") as _:
+	readme = _.readlines()
+
+LONG_DESCRIPTION = "".join("\n".join(readme).split("\n"))
+
+# Setting up
+setup(
+    name="pychek",
+    version=VERSION,
+    author="xbais (Aakash Singh Bais)",
+    author_email="<aakash@aioniaepochi.org>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['setuptools', 'argparse', 'tqdm', 'colorama', 'psutil', 'pyfiglet'],
+    keywords=['python', 'print', 'profiler'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
