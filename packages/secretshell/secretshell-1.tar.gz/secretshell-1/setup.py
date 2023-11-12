155f1edfe9d8c71e7cf8bafe8ff66479f3911533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 15:04:28 +0000

    Add package secretshell/1/secretshell-1.tar.gz

---
+++ packages/secretshell/secretshell-1.tar.gz/secretshell-1/setup.py
@@ -0,0 +1,23 @@
+"""
+
+~~~ Valahatiy ~~~
+
+"""
+
+from setuptools import setup
+
+setup(
+
+    name             = "secretshell",
+    version          = "1",
+    author           = "valahatiy",
+    packages         = ["secretshell"],
+    author_email     = "fsb@gmail.com"
+
+)
+
+"""
+
+~~~ Valahatiy ~~~
+
+"""
