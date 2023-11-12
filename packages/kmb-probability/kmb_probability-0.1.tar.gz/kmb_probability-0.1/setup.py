9a205bb14c34f08498c2561c605020d3162db692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:06:44 +0000

    Add package kmb-probability/0.1/kmb_probability-0.1.tar.gz

---
+++ packages/kmb-probability/kmb_probability-0.1.tar.gz/kmb_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kmb_probability',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['kmb_probability'],
+      author='Mugagga Benjamin',
+      author_email='mugaggabenjie@gmail.com',
+      zip_safe=False)
