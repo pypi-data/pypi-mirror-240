697e90928a1ad2b7e5885dffbe544ef84ec25790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:18:25 +0000

    Add package dian-person-rut-status/0.1.6/dian_person_rut_status-0.1.6.tar.gz

---
+++ packages/dian-person-rut-status/dian_person_rut_status-0.1.6.tar.gz/dian_person_rut_status-0.1.6/dian_person_rut_status/muisca.py
@@ -0,0 +1,55 @@
+from typing import Union
+
+import requests
+
+from requests.models import Request
+
+from bs4 import BeautifulSoup
+
+from .exceptions import TokenException
+from .custom_types import RutStatusDict
+from . import utils
+
+
+  
+def get_person_rut_status(token:str, nit:str, attempts:int=utils.DEFAULT_ATTEMPTS, timeout:int=utils.DEFAULT_TIMEOUT)->Union[RutStatusDict, None]:
+    
+    """This function will returns a Person Rut Status information stored inside a Dict
+    
+    Args:
+        token: a string to make a successfull request
+        
+        nit: a string that represents the Colombia Unique Taxpayer Number is number assigned to persons who must pay taxes.
+        
+        attempts: an integer which represents the number of seconds to retry the function (5 by default)
+        
+        timeout: an integer which representes the number of seconds to sleep in case of a request timeout (10 by default)
+
+    Raises:
+        TokenException: When the request's token is missing or is a blank string
+
+    Returns:
+        RutStatusDict: A typed dictionary with the main key and values from an actual legal or natural person
+        None: if there is an error on the code in execution
+    """
+    
+    if not token:
+        raise TokenException('The Token is Missing!')
+    
+    if attempts == 0:
+        print('All attempts completed! ')
+        return
+    
+    try:
+        response = requests.post(url=utils.WEB_RUT_MUISCA_URL, data={**utils.PAYLOAD_DATA, utils.as_form_field('numNit'): nit, utils.TOKEN_FIELD: token}, timeout=timeout)
+        
+        if isinstance(response, Request):
+            response.raise_for_status()
+            
+    except requests.Timeout:
+        return get_person_rut_status(token=token, nit=nit, attempts=attempts-1)
+    except Exception as e:
+        print(e)
+    else:
+        soup = BeautifulSoup(response.text, features='html.parser') if response else None
+        return None if not soup else utils.get_person_rut_status_from_soup(soup=soup)
---
+++ packages/dian-person-rut-status/dian_person_rut_status-0.1.6.tar.gz/dian_person_rut_status-0.1.6/dian_person_rut_status/utils.py
@@ -0,0 +1,81 @@
+
+from typing import Union, Dict
+
+from bs4 import BeautifulSoup, Tag
+
+from .custom_types import RutStatusDict
+
+FORM_CLASS_INFO = '.tipoFilaNormalVerde'
+
+TOKEN_FIELD = 'com.sun.faces.VIEW'
+
+FORM_PREFIX = 'vistaConsultaEstadoRUT:formConsultaEstadoRUT'
+
+VALUE_ATTR = 'value'
+
+NATURAL_PERSON_VALUES = ['primerNombre', 'otrosNombres', 'primerApellido', 'segundoApellido']
+
+PERSON_MAIN_VALUES_KEYS = [ 'dv', 'status', 'name']
+
+PERSON_VALUES_KEYS = ['nit', *PERSON_MAIN_VALUES_KEYS]
+
+WEB_RUT_MUISCA_URL = 'https://muisca.dian.gov.co/WebRutMuisca/DefConsultaEstadoRUT.faces'
+
+DEFAULT_TIMEOUT = 10
+
+DEFAULT_ATTEMPTS = 5
+
+def remove_none_elements_from_dict(d: dict) -> Dict:
+    return {key: value for key, value in d.items() if value is not None and value != ''}
+
+
+def list_contains_elements_of_list(a:list, b:list)->bool:
+    return  all(item in a for item in b)
+
+
+def as_form_field(field_name:str='')->str:
+    return FORM_PREFIX + ':' + field_name if field_name else FORM_PREFIX
+
+def get_value_from_tag(tag:Tag, value:str)->Union[str, None]:
+    return tag.contents[0] if tag.contents else tag.attrs.get(value, None)
+
+
+def find_value_in_soup(soup:BeautifulSoup, value:str)->Union[str, None]:
+    
+    tag = soup.find(attrs={'id': as_form_field(value)})
+    
+    return None if not tag else get_value_from_tag(tag=tag, value=VALUE_ATTR)
+
+def concat_full_name_by_name_strings(names:list)->Union[str, None]:
+    return ' '.join(names) if all(names) else None
+
+
+def get_person_name_from_soup(soup:BeautifulSoup)->Union[str, None]:
+    
+    name = find_value_in_soup(soup=soup, value='razonSocial')
+    
+    return name if name else concat_full_name_by_name_strings([find_value_in_soup(soup=soup, value=value) for value in NATURAL_PERSON_VALUES])
+
+    
+def get_person_rut_status_from_soup(soup:BeautifulSoup)->Union[RutStatusDict, None]:
+    
+    person_data = {PERSON_VALUES_KEYS[0]: find_value_in_soup(soup=soup, value='numNit'),
+                     PERSON_VALUES_KEYS[1]: find_value_in_soup(soup=soup, value='dv'),
+                     PERSON_VALUES_KEYS[2]: find_value_in_soup(soup=soup, value='estado'),
+                     PERSON_VALUES_KEYS[3]: get_person_name_from_soup(soup=soup)}
+    
+    person_data = remove_none_elements_from_dict(d=person_data)
+    
+    return person_data if list_contains_elements_of_list(a=list(person_data.keys()), b=PERSON_MAIN_VALUES_KEYS) else None 
+
+
+PAYLOAD_DATA = {as_form_field('modoPresentacionSeleccionBO'): 'pantalla',
+                as_form_field('siguienteURL'): '',
+                as_form_field('modoPresentacionFormBO'): 'pantalla',
+                as_form_field('modoOperacionFormBO'): '',
+                as_form_field('mantenerCriterios'): '',
+                as_form_field('btnBuscar.x'): 48,
+                as_form_field('btnBuscar.y'): 11,
+                as_form_field(): 'vistaConsultaEstadoRUT:formConsultaEstadoRUT',
+                as_form_field('_idcl'): '',
+                }
