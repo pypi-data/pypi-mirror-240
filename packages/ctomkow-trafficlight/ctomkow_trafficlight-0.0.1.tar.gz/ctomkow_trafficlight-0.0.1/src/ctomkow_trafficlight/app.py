743cc0ec8ca28e49430c54844cead613cc292839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:19:47 +0000

    Add package ctomkow-trafficlight/0.0.1/ctomkow_trafficlight-0.0.1.tar.gz

---
+++ packages/ctomkow-trafficlight/ctomkow_trafficlight-0.0.1.tar.gz/ctomkow_trafficlight-0.0.1/src/ctomkow_trafficlight/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import gpiod
+from gpiod.line import Direction, Value
+from flask import Flask
+
+LINE_RED = 9
+LINE_YLW = 10
+LINE_GRN = 11
+
+app = Flask(__name__)
+
+def toggle_light(line):
+    with gpiod.request_lines(
+        "/dev/gpiochip0",
+        consumer="blink-example",
+        config={
+            line: gpiod.LineSettings(
+                direction=Direction.OUTPUT, output_value=Value.ACTIVE
+            )
+        },
+    ) as request:
+        status = request.get_value(line)
+        if status == Value.ACTIVE:
+            request.set_value(line, Value.INACTIVE)
+        else:
+            request.set_value(line, Value.ACTIVE)
+
+@app.route("/v1/trafficlight/red")
+def red():
+    toggle_light(LINE_RED)
+
+@app.route("/v1/trafficlight/yellow")
+def yellow():
+    toggle_light(LINE_YLW)
+
+@app.route("/v1/trafficlight/green")
+def green():
+    toggle_light(LINE_GRN)
