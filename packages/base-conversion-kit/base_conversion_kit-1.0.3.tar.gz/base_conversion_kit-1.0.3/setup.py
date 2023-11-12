239db611f1ca3f5a514c6139477033a665bf0f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:06:57 +0000

    Add package base-conversion-kit/1.0.3/base_conversion_kit-1.0.3.tar.gz

---
+++ packages/base-conversion-kit/base_conversion_kit-1.0.3.tar.gz/base_conversion_kit-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read the content of README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='base_conversion_kit',
+    version='1.0.3',
+    author="porfanid",
+    author_email='pavlos@orfanidis.net.gr',
+
+    packages=find_packages(),
+    install_requires=[
+        'markdown'
+    ],
+    python_requires='>=3.6',
+    project_urls={
+        'Funding': 'https://ko-fi.com/porfanid',
+        'GitHub': 'https://github.com/porfanid/base-conversion-kit'
+    },
+    description='A simple package to convert numbers to different bases and work with them',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='base, conversion, kit, base, convert, numbers, add, multiply, subtract, '
+)
