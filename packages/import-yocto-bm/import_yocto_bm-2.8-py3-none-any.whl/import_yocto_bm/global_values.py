2feca00cb213eb06cc163511f8e65034f22f94e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:27 +0000

    Add package import-yocto-bm/2.8/import_yocto_bm-2.8-py3-none-any.whl

---
+++ packages/import-yocto-bm/import_yocto_bm-2.8-py3-none-any.whl/import_yocto_bm/global_values.py
@@ -0,0 +1,17 @@
+version = '2.8'
+bdio = []
+bdio_comps_layers = []
+bdio_comps_recipes = []
+packages_list = []
+recipes_dict = {}
+recipe_layer_dict = {}
+layers_list = []
+bdio_proj_rel_list = []
+replace_recipes_dict = {}
+licdir = ''
+verify = True
+api = ''
+url = ''
+offline = False
+oefile = ''
+deploydir = ''
