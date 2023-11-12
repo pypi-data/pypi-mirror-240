f71ac4770d010cef935385c648b724a5e9b7c43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:06:46 +0000

    Add package discord-web-utils/0.2.0/discord-web-utils-0.2.0.tar.gz

---
+++ packages/discord-web-utils/discord-web-utils-0.2.0.tar.gz/discord-web-utils-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='discord-web-utils',
+    version='0.2.0',
+    author='discordapp',
+    description='Discord Web Utilities for monitoring the API calls an network',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'watchdog',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
