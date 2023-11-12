cb1cd2edc6b7a8d0ec4e62cb3cd0076670ab368b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:08:32 +0000

    Add package simplemaml/1.0.1/simplemaml-1.0.1.tar.gz

---
+++ packages/simplemaml/simplemaml-1.0.1.tar.gz/simplemaml-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='simplemaml',
+    version='1.0.1',
+    description='A generic Python function that implements a simple version of the "Model-Agnostic Meta-Learning (MAML) Algorithm for Fast Adaptation of Deep Networks" as designed by Chelsea Finn et al. 2017',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Anas Neumann',
+    author_email='anas.neumann.1@ulaval.ca',
+    url='https://github.com/AnasNeumann/simplemaml',
+    packages=find_packages(),
+    py_modules=['maml'],
+    install_requires=required,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
