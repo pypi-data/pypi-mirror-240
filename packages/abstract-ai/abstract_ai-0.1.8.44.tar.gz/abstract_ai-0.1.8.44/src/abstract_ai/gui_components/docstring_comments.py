cdeb8d4a081aa6fe91b8975123aba4d745bf6505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:05:28 +0000

    Add package abstract-ai/0.1.8.44/abstract_ai-0.1.8.44.tar.gz

---
+++ packages/abstract-ai/abstract_ai-0.1.8.44.tar.gz/abstract_ai-0.1.8.44/src/abstract_ai/gui_components/docstring_comments.py
@@ -0,0 +1,10 @@
+import inspect
+def get_docstrings(module):
+    docstrings = {}
+    for name, obj in inspect.getmembers(module):
+        if inspect.isfunction(obj):
+            docstrings[name] = inspect.getdoc(obj)
+    return docstrings
+
+docstrings = get_docstrings(abstract_ai)
+
