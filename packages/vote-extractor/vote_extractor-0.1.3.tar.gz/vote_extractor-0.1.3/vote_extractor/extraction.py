5a64e78e980266eb19770d93ac29145714ab38fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:08:45 +0000

    Add package vote-extractor/0.1.3/vote_extractor-0.1.3.tar.gz

---
+++ packages/vote-extractor/vote_extractor-0.1.3.tar.gz/vote_extractor-0.1.3/vote_extractor/extraction.py
@@ -0,0 +1,13 @@
+import camelot
+import pandas as pd
+
+
+def extract_data(url):
+    tables = camelot.read_pdf(url, backend="ghostscript")
+    df = pd.concat(map(lambda table: table.df, tables[:-1])).rename(
+        columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"}
+    )
+    df["firstname"] = df["firstname"].map(lambda name: name.replace("\n", ""))
+    df = df.sort_values("firstname")
+    df = df[df["vote"] != ""]
+    return df
