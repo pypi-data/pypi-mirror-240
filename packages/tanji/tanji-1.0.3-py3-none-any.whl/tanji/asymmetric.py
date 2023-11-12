00c2ebe7220486507c0d823afdae6a47c7ef7238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 14:20:25 +0000

    Add package tanji/1.0.3/tanji-1.0.3-py3-none-any.whl

---
+++ packages/tanji/tanji-1.0.3-py3-none-any.whl/tanji/asymmetric.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+class AsymmetricCipher:
+    @staticmethod
+    def generate_key_pair():
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
+        return private_key, public_key
+
+    @staticmethod
+    def rsa_encrypt(message, public_key):
+        ciphertext = public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return ciphertext
+
+    @staticmethod
+    def rsa_decrypt(ciphertext, private_key):
+        plaintext = private_key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return plaintext
