b2b09a92c7d73f0975342207a26862a9e6db9c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 03:03:57 +0000

    Add package noted-dev/1.1.38/noted-dev-1.1.38.tar.gz

---
+++ packages/noted-dev/noted-dev-1.1.38.tar.gz/noted-dev-1.1.38/src/noted/sense-o/sense_util.py
@@ -0,0 +1,4 @@
+#!/Users/carmen/cernbox/CERN_Projects/NOTED/PyPI_workspace/venv-noted/bin/python3
+# EASY-INSTALL-SCRIPT: 'sense-o-api==1.24','sense_util.py'
+__requires__ = 'sense-o-api==1.24'
+__import__('pkg_resources').run_script('sense-o-api==1.24', 'sense_util.py')
