4cf53882d1397d4e3b1400af7e5386c2e16feeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:19 +0000

    Add package binarycalculation/1.0/binarycalculation-1.0.tar.gz

---
+++ packages/binarycalculation/binarycalculation-1.0.tar.gz/binarycalculation-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="binarycalculation",
+    version="1.0",
+    description="Binary Number Operations",
+    packages=['binarycalculation'],
+    author="josikie",
+    author_email="kiejosi12@gmail.com",
+    zip_safe=False
+)
