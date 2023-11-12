c3f1b761a6076ce1ab4ef7cb6aa18ed33da4ad77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:31 +0000

    Add package simple-timer-baxxter/1.2/simple-timer-baxxter-1.2.tar.gz

---
+++ packages/simple-timer-baxxter/simple-timer-baxxter-1.2.tar.gz/simple-timer-baxxter-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='simple-timer-baxxter',
+      version='1.2',
+      description='its a simple timer ',
+      url='',
+      author='AMIRBAXXTER',
+      author_email='amirmasoodpormahdi@gmail.com',
+      license='MIT',
+      packages=['simple_timer'],
+      zip_safe=False)
