8d16e2d82fed4f037907de1cdc3fbe2222418198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-12 04:07:19 +0000

    Add package TMC-2209-Raspberry-Pi/0.3.3/TMC_2209_Raspberry_Pi-0.3.3.tar.gz

---
+++ packages/TMC-2209-Raspberry-Pi/TMC_2209_Raspberry_Pi-0.3.3.tar.gz/TMC_2209_Raspberry_Pi-0.3.3/tests/test_script_03_basic_movement.py
@@ -0,0 +1,138 @@
+#pylint: disable=wildcard-import
+#pylint: disable=unused-wildcard-import
+#pylint: disable=unused-import
+#pylint: disable=duplicate-code
+"""
+test file for testing basic movement
+"""
+
+import time
+from src.TMC_2209.TMC_2209_StepperDriver import *
+
+
+print("---")
+print("SCRIPT START")
+print("---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# initiate the TMC_2209 class
+# use your pins for pin_en, pin_step, pin_dir here
+#-----------------------------------------------------------------------
+tmc = TMC_2209(21, 16, 20, loglevel=Loglevel.DEBUG)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the loglevel of the libary (currently only printed)
+# set whether the movement should be relative or absolute
+# both optional
+#-----------------------------------------------------------------------
+tmc.set_loglevel(Loglevel.DEBUG)
+tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions change settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.set_direction_reg(False)
+tmc.set_current(300)
+tmc.set_interpolation(True)
+tmc.set_spreadcycle(False)
+tmc.set_microstepping_resolution(2)
+tmc.set_internal_rsense(False)
+
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions read and print the current settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.readIOIN()
+tmc.readCHOPCONF()
+tmc.readDRVSTATUS()
+tmc.readGCONF()
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the Acceleration and maximal Speed
+#-----------------------------------------------------------------------
+# tmc.set_acceleration(2000)
+# tmc.set_max_speed(500)
+
+#-----------------------------------------------------------------------
+# set the Acceleration and maximal Speed in fullsteps
+#-----------------------------------------------------------------------
+tmc.set_acceleration_fullstep(1000)
+tmc.set_max_speed_fullstep(250)
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------
+# activate the motor current output
+#-----------------------------------------------------------------------
+tmc.set_motor_enabled(True)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# move the motor 1 revolution
+#-----------------------------------------------------------------------
+tmc.run_to_position_steps(400)                             #move to position 400
+tmc.run_to_position_steps(0)                               #move to position 0
+
+
+tmc.run_to_position_steps(400, MovementAbsRel.RELATIVE)    #move 400 steps forward
+tmc.run_to_position_steps(-400, MovementAbsRel.RELATIVE)   #move 400 steps backward
+
+
+tmc.run_to_position_steps(400)                             #move to position 400
+tmc.run_to_position_steps(0)                               #move to position 0
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deactivate the motor current output
+#-----------------------------------------------------------------------
+tmc.set_motor_enabled(False)
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deinitiate the TMC_2209 class
+#-----------------------------------------------------------------------
+tmc.deinit()
+del tmc
+
+print("---")
+print("SCRIPT FINISHED")
+print("---")
---
+++ packages/TMC-2209-Raspberry-Pi/TMC_2209_Raspberry_Pi-0.3.3.tar.gz/TMC_2209_Raspberry_Pi-0.3.3/tests/test_script_06_multiple_drivers.py
@@ -0,0 +1,80 @@
+#pylint: disable=wildcard-import
+#pylint: disable=unused-wildcard-import
+#pylint: disable=unused-import
+#pylint: disable=duplicate-code
+"""
+test file for testing multiple drivers via one UART connection
+"""
+
+import time
+from src.TMC_2209.TMC_2209_StepperDriver import *
+
+
+print("---")
+print("SCRIPT START")
+print("---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# initiate the TMC_2209 class
+# use your pins for pin_en, pin_step, pin_dir here
+#-----------------------------------------------------------------------
+tmc1 = TMC_2209(21, 16, 20, driver_address=0)
+tmc2 = TMC_2209(26, 13, 19, driver_address=1)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the loglevel of the libary (currently only printed)
+# set whether the movement should be relative or absolute
+# both optional
+#-----------------------------------------------------------------------
+tmc1.set_loglevel(Loglevel.DEBUG)
+tmc1.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+
+tmc2.set_loglevel(Loglevel.DEBUG)
+tmc2.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+
+
+
+#-----------------------------------------------------------------------
+# these functions read and print the current settings in the TMC register
+#-----------------------------------------------------------------------
+
+print("---")
+print("IOIN tmc1")
+print("---")
+tmc1.readIOIN()
+
+print("---\n---")
+
+
+print("---")
+print("IOIN tmc2")
+print("---")
+tmc2.readIOIN()
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deinitiate the TMC_2209 class
+#-----------------------------------------------------------------------
+tmc1.set_motor_enabled(False)
+tmc2.set_motor_enabled(False)
+tmc1.deinit()
+tmc2.deinit()
+del tmc1
+del tmc2
+
+print("---")
+print("SCRIPT FINISHED")
+print("---")
