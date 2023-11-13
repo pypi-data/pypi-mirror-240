a736d57c5cbee4dafd369c7add654dab7c2a6b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:04:41 +0000

    Add package jaclang/0.2.0/jaclang-0.2.0-py3-none-any.whl

---
+++ packages/jaclang/jaclang-0.2.0-py3-none-any.whl/jaclang/core/__jac_gen__/exec_ctx_impl.py
@@ -0,0 +1,4 @@
+"""Implementation for Jac's Element Abstractions"""
+from __future__ import annotations
+from jaclang import jac_import as __jac_import__
+from uuid import UUID, uuid4
---
+++ packages/jaclang/jaclang-0.2.0-py3-none-any.whl/jaclang/core/__jac_gen__/memory_impl.py
@@ -0,0 +1,3 @@
+"""Implementation for Jac's Element Abstractions"""
+from __future__ import annotations
+import sys
