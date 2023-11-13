5dc4c00af01170cc9a4be52b233361019c665ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:06 +0000

    Add package pydantic-marshals/0.3.9/pydantic_marshals-0.3.9-py3-none-any.whl

---
+++ packages/pydantic-marshals/pydantic_marshals-0.3.9-py3-none-any.whl/pydantic_marshals/base/__init__.py
@@ -0,0 +1,4 @@
+from pydantic_marshals.base.fields.base import PatchDefault
+from pydantic_marshals.base.models import MarshalBaseModel, MarshalModel
+
+__all__ = ("MarshalBaseModel", "MarshalModel", "PatchDefault")
