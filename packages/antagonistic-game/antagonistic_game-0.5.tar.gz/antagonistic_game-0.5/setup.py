e642d2aca4ae8fd0efb76817c5eac71e47229e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:48 +0000

    Add package antagonistic-game/0.5/antagonistic_game-0.5.tar.gz

---
+++ packages/antagonistic-game/antagonistic_game-0.5.tar.gz/antagonistic_game-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="antagonistic_game",
+    version='0.5',
+    package_data={'': ['*.ipynb']},
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.26.1",
+        "sciPy>=1.11.3"
+    ],
+    author="Fostenko O.A.| Buzin N.I., CMC MSU",
+    author_email="oleg.fostenko.03@gmail.com",
+    url="https://github.com/oscar-foxtrot/antagonistic_game_solver",
+    project_urls={
+        'Author (Fostenko O.A.)': 'https://vk.com/av1at0r',
+        'Author (Buzin N.I.)': 'https://vk.com/buzin93',
+        'Source Code': 'https://github.com/oscar-foxtrot/antagonistic_game_solver',
+    },
+    license='MIT'
+)
