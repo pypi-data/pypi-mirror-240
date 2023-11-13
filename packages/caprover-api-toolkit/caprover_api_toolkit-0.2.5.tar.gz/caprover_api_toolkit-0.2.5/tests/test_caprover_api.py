736db66f3bea0fc83331769d51b822909481a49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:19 +0000

    Add package caprover-api-toolkit/0.2.5/caprover_api_toolkit-0.2.5.tar.gz

---
+++ packages/caprover-api-toolkit/caprover_api_toolkit-0.2.5.tar.gz/caprover_api_toolkit-0.2.5/tests/test_caprover_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Tests for `caprover_api` package."""
+
+
+import unittest
+
+
+class TestCaprover_api(unittest.TestCase):
+    """Tests for `caprover_api` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
