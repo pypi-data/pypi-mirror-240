736cc2faf9a81f667bb1139482e52d5df1640ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:54 +0000

    Add package LundProcessFlowModules/1.0.6/LundProcessFlowModules-1.0.6.tar.gz

---
+++ packages/LundProcessFlowModules/LundProcessFlowModules-1.0.6.tar.gz/LundProcessFlowModules-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="LundProcessFlowModules",
+    version="1.0.6",
+    author="Melih Sünbül",
+    author_email="m.sunbul@lund-it.com",
+    description="A Python library to use 3rd party APIs in your project directly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LundIT/ProcessFlowModules",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "openai==0.28",
+        "msal"
+    ],
+    python_requires='>=3.6',
+)
