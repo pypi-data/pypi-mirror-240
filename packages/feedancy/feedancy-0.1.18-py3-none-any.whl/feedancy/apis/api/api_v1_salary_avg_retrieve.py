a17f305aa93ad448bd811ff6d62c04e7fd411ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:11:25 +0000

    Add package feedancy/0.1.18/feedancy-0.1.18-py3-none-any.whl

---
+++ packages/feedancy/feedancy-0.1.18-py3-none-any.whl/feedancy/apis/api/api_v1_salary_avg_retrieve.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy.lib.base import BaseApi
+from feedancy.lib.request import ApiRequest
+from feedancy.lib import json
+class AvgSalary(BaseModel):
+    by_freelance: typing.Optional[typing.Union[int, None]]  = None
+    by_internship: typing.Optional[typing.Union[int, None]]  = None
+    by_vacancy: typing.Optional[typing.Union[int, None]]  = None
+    created_at: datetime.datetime 
+    id: int 
+
+def make_request(self: BaseApi,
+
+
+) -> AvgSalary:
+    
+
+    
+    body = None
+    
+
+    m = ApiRequest(
+        method="GET",
+        path="/api/v1/salary/avg/".format(
+            
+        ),
+        content_type=None,
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "application/json": AvgSalary,
+            
+        },
+    
+    }, m)
