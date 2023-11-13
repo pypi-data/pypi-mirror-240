09dbaff96d1e231616052b077d7b4f4d69afd809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:35 +0000

    Add package openllm/0.4.4/openllm-0.4.4.tar.gz

---
+++ packages/openllm/openllm-0.4.4.tar.gz/openllm-0.4.4/src/openllm/exceptions.py
@@ -0,0 +1,8 @@
+from openllm_core.exceptions import Error as Error
+from openllm_core.exceptions import FineTuneStrategyNotSupportedError as FineTuneStrategyNotSupportedError
+from openllm_core.exceptions import ForbiddenAttributeError as ForbiddenAttributeError
+from openllm_core.exceptions import GpuNotAvailableError as GpuNotAvailableError
+from openllm_core.exceptions import MissingAnnotationAttributeError as MissingAnnotationAttributeError
+from openllm_core.exceptions import MissingDependencyError as MissingDependencyError
+from openllm_core.exceptions import OpenLLMException as OpenLLMException
+from openllm_core.exceptions import ValidationError as ValidationError
