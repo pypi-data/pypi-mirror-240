c66902a54f08802984f47e8e4db8fb5a4324e0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:52 +0000

    Add package qupython/0.0.1/qupython-0.0.1-py3-none-any.whl

---
+++ packages/qupython/qupython-0.0.1-py3-none-any.whl/qupython/construction.py
@@ -0,0 +1,120 @@
+# Functions for compiling quantum circuits from QubitPromise objects
+
+import contextlib
+import qiskit
+from collections.abc import Mapping, Iterable
+from .qubit import Qubit, QubitPromise, quPythonInstruction, quPythonMeasurement
+from .err_msg import ERR_MSG
+
+
+def _get_promises(obj):
+    """
+    Recursively search Python object for `QubitPromises`.
+    """
+    # TODO: unit test
+    if obj is None:
+        return set()
+
+    if isinstance(obj, (int, float, bool, str)):
+        return set()
+
+    if isinstance(obj, Qubit):
+        raise ValueError(ERR_MSG["ReturnQubitFromQuantumFunction"])
+    if isinstance(obj, QubitPromise):
+        return set([obj])
+
+    if hasattr(obj, "__dict__"):
+        obj = obj.__dict__
+
+    if isinstance(obj, Mapping):
+        promises = set()
+        for key, value in obj.items():
+            promises |= _get_promises(key)
+            promises |= _get_promises(value)
+        return promises
+
+    if isinstance(obj, Iterable):
+        promises = set()
+        for element in obj:
+            promises |= _get_promises(element)
+        return promises
+
+    raise ValueError(ERR_MSG["CantSearchObjectForPromises"].format(obj=type(obj)))
+
+
+def _get_bits_from_promises(promises):
+    """
+    Find all qubits needed to fulfil all promises. This includes any qubits
+    that interact with measured qubits.
+    """
+    # TODO: unit test
+    # TODO: this does not work if a measurement is conditioned on a promise
+    # that isn't in `promises`. Qubits and promises are now too similar and
+    # should inherit from the same class. For example, we need
+    # `get_linked_bits` to search through both qubits and promises.
+    initial_bits = set(promises)
+    all_bits = initial_bits.copy()
+    for bit in initial_bits:
+        all_bits |= bit.get_linked_bits()
+    return all_bits
+
+
+def _waiting_for_bits(instruction):
+    """
+    Check if instruction can be applied to circuit.
+    """
+    for bit in instruction.qubits+instruction.promises:
+        if bit.operations.index(instruction) != bit.op_pointer:
+            return True
+    return False
+
+
+def _add_instructions_to_circuit(circuit, qubit):
+    """
+    Keep adding this Qubit's instructions to the circuit until complete, or
+    waiting for another bit or qubit.
+    """
+    for op in qubit.operations[qubit.op_pointer :]:
+        if _waiting_for_bits(op):
+            return
+        if isinstance(op, quPythonMeasurement):
+            qubit.op_pointer += 1
+            for promise in op.promises:
+                circuit.measure(qubit.index, promise.index)
+                promise.op_pointer += 1
+            continue
+        if op.promises:
+            # TODO: neaten up
+            with contextlib.ExitStack() as stack:
+                for promise in op.promises:
+                    stack.enter_context(
+                        circuit.if_test((promise.index, int(not promise.inverse)))
+                    )
+                circuit.append(op.qiskit_instruction, [q.index for q in op.qubits])
+
+        else:
+            circuit.append(op.qiskit_instruction, [q.index for q in op.qubits])
+        for bit in op.qubits+op.promises:
+            bit.op_pointer += 1
+
+
+def _construct_circuit(promises):
+    """
+    Compile quantum circuit needed to fulfil QubitPromise values.
+    """
+    # TODO: unit test
+    bits = _get_bits_from_promises(promises)
+    qubits = [b for b in bits if isinstance(b, Qubit)]
+    promises = [p for p in bits if isinstance(p, QubitPromise)]
+    for index, qubit in enumerate(qubits):
+        qubit.index = index  # Map qupython.Qubit to circuit qubit
+        qubit.op_pointer = 0  # To keep track of compiled operations
+    for index, promise in enumerate(promises):
+        promise.index = index  # Map promise to circuit clbit
+        promise.op_pointer = 0
+
+    circuit = qiskit.QuantumCircuit(len(qubits), len(promises))
+    while any(q.op_pointer < len(q.operations) for q in qubits):
+        for qubit in qubits:
+            _add_instructions_to_circuit(circuit, qubit)
+    return circuit
---
+++ packages/qupython/qupython-0.0.1-py3-none-any.whl/qupython/qubit.py
@@ -0,0 +1,182 @@
+import qiskit.circuit.library as clib
+from .err_msg import ERR_MSG
+
+
+class QubitPromiseNotResolvedError(Exception):
+    pass
+
+class _Bit:
+    operations: list
+
+    def get_linked_bits(self, already_found=set()):
+        # TODO: unit test
+        # TODO: neaten up
+        # TODO: optimize
+        linked_bits = already_found.copy() or set([self])
+        for op in self.operations:
+            linked_bits |= set(op.qubits)
+            linked_bits |= set(op.promises)
+
+        new_bits = linked_bits - already_found
+        while new_bits:
+            for bit in new_bits.copy():
+                new_bits |= bit.get_linked_bits(already_found=linked_bits)
+            new_bits = new_bits - linked_bits
+            linked_bits |= new_bits
+        return linked_bits
+
+
+class QubitPromise(_Bit):
+    """
+    Placeholder for qubit measurement results.
+
+    Each promise belongs to exactly one measurement instruction. At the end of
+    a `@quantum` function, quPython executes the circuit needed to fulfil any
+    returned promises.
+
+    After the values are determined, a `QubitPromise` tries to behave as much
+    like a `bool` as possible. Unfortunately, there are some quirks because
+    `QubitPromises` need to have unique hashes before circuit compilation, but
+    `bool`s all have the same hash (0 or 1) and you can't change an object's
+    hash without breaking basic Python functionality. The following code
+    snippet shows an example.
+
+    ```
+    # Create fulfilled qubit promise
+    promise = QubitPromise(None)
+    promise.value = True
+
+    # Show unexpected behavior
+    promise == True  # True
+    promise in (True, False)  # False
+    ```
+
+    Currently not sure what the best behavior is. Options are:
+      * Keep it like this, and encourage users to cast to `bool` ASAP
+      * Keep like this, but have quPython return a new copy of the data with
+        _actual_ `bool`s
+      * Something else?
+    """
+
+    def __init__(self, measurement_instruction, inverse=False):
+        self.operations = [measurement_instruction]
+        self.inverse = inverse
+        self.value = None
+
+    def __bool__(self):
+        if self.value is None:
+            raise QubitPromiseNotResolvedError(ERR_MSG["QubitPromiseNotResolved"])
+        return self.value
+
+    def __eq__(self, other):
+        if self.value is None:
+            return id(self) == id(other)
+        return self.value == other
+
+    def __hash__(self):
+        return id(self)
+
+    def __repr__(self):
+        if self.value is None:
+            return f"QubitPromise({self.operations})"
+        return repr(self.value)
+
+    def __invert__(self):
+        new_promise = QubitPromise(
+            self.measurement_instruction,
+            inverse= not self.inverse
+        )
+        self.measurement_instruction.append(new_promise)
+        return new_promise
+
+
+
+class quPythonInstruction:
+    def __init__(self, qiskit_instruction, qubits, promises=[]):
+        self.qiskit_instruction = qiskit_instruction
+        self.qubits = qubits
+        self.promises = promises
+
+    def __repr__(self):
+        return f"quPythonInstruction({self.qiskit_instruction.name}, {self.qubits})"
+
+
+class quPythonMeasurement:
+    def __init__(self, qubit):
+        self.promises = [QubitPromise(self)]
+        self.qubits = [qubit]
+
+
+class Qubit(_Bit):
+    def __init__(self, name=None):
+        self.name = name
+        self.operations = []
+
+        self._create_1q_gate_methods()
+
+    def __bool__(self):
+        raise ValueError(
+            "Can't cast Qubit to bool; use `.measure()` to measure"
+            " the qubit instead."
+        )
+
+    def _separate_conditions(self, conditions):
+        qubits = [c for c in conditions if isinstance(c, Qubit)]
+        promises = [c for c in conditions if isinstance(c, QubitPromise)]
+        rest = [c for c in conditions if not isinstance(c, (Qubit, QubitPromise))]
+        return qubits, promises, rest
+
+    def _create_1q_gate_methods(self):
+        """
+        Generate methods such as self.h, self.p, etc.
+        This method runs on object initialization.
+        """
+
+        # TODO: unit test
+        # TODO: neaten up
+        def _create_method(gate):
+            def add_gate(*args, **kwargs):
+                conditions = kwargs.pop("conditions", [])
+                qubits, promises, rest = self._separate_conditions(conditions)
+                if not all(rest):
+                    return
+                qiskit_inst = gate(*args, **kwargs)
+                if qubits:
+                    qiskit_inst = qiskit_inst.control(len(qubits))
+                inst = quPythonInstruction(
+                    qiskit_instruction=qiskit_inst,
+                    qubits=qubits + [self],
+                    promises=promises
+                )
+                for qubit in qubits + [self]:
+                    qubit.operations.append(inst)
+                for promise in promises:
+                    promise.operations.append(inst)
+                return self
+
+            return add_gate
+
+        for gate, name in [
+            (clib.XGate, "x"),
+            (clib.YGate, "y"),
+            (clib.ZGate, "z"),
+            (clib.HGate, "h"),
+            (clib.SGate, "s"),
+            (clib.SdgGate, "sdg"),
+            (clib.TGate, "t"),
+            (clib.TdgGate, "tdg"),
+            (clib.PhaseGate, "p"),
+            (clib.RXGate, "rx"),
+            (clib.RYGate, "ry"),
+            (clib.RZGate, "rz"),
+            (clib.UGate, "u"),
+        ]:
+            setattr(self, name, _create_method(gate))
+
+    def measure(self):
+        """
+        Add measure instruction to Qubit and return QubitPromise
+        """
+        inst = quPythonMeasurement(self)
+        self.operations.append(inst)
+        return inst.promises[0]
