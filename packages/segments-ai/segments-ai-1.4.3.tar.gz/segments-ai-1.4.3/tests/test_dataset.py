200fad03da055f1dd19240bcb2df6b24ae9193c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:48 +0000

    Add package segments-ai/1.4.3/segments-ai-1.4.3.tar.gz

---
+++ packages/segments-ai/segments-ai-1.4.3.tar.gz/segments-ai-1.4.3/tests/test_dataset.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from segments.client import SegmentsClient
+from segments.dataset import SegmentsDataset
+from segments.typing import SegmentsDatasetCategory
+
+
+def test_dataset(
+    client: SegmentsClient,
+    owner: str,
+    datasets: List[str],
+    ARTIFACTS_DIR: str,
+) -> None:
+    # Get the datasets
+    datasets = client.get_datasets(owner)
+
+    for dataset in datasets:
+        # Skip the example-multi-sensor dataset
+        if (
+            dataset.name == "example-multi-sensor"
+            or dataset.name == "example-point-cloud-sequences-segmentation"
+        ):
+            continue
+
+        # Get the releases
+        dataset_identifier = f"{owner}/{dataset.name}"
+        releases = client.get_releases(dataset_identifier)
+
+        for release in releases:
+            # Get the dataset
+            release = client.get_release(dataset_identifier, release.name)
+            dataset = SegmentsDataset(release, segments_dir=f"{ARTIFACTS_DIR}/segments")
+
+            # Load the categories
+            categories = dataset.categories
+            assert isinstance(categories, list)
+            for category in categories:
+                assert isinstance(category, SegmentsDatasetCategory)
+
+            # Iterate over samples
+            for sample in dataset:
+                assert isinstance(sample, dict)
