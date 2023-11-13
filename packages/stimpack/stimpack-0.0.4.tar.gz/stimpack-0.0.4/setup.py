44cd29052142e0b13fb047c467498f3e300377ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:05:08 +0000

    Add package stimpack/0.0.4/stimpack-0.0.4.tar.gz

---
+++ packages/stimpack/stimpack-0.0.4.tar.gz/stimpack-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stimpack',
+    version='0.0.4',
+    description='Precise and flexible generation of stimuli for neuroscience experiments.',
+    url='https://github.com/ClandininLab/stimpack',
+    author='Minseung Choi',
+    author_email='minseung@stanford.edu',
+    packages=find_packages(where="src"),
+    # package_dir = {"": "src"},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        
+        'platformdirs',
+        'PyQT6',
+        'h5py',
+        'pyYaml',
+        
+        'moderngl',
+        'scikit-image',
+    ],
+    entry_points={
+        'console_scripts': [
+            'stimpack=stimpack.experiment.gui:main'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
