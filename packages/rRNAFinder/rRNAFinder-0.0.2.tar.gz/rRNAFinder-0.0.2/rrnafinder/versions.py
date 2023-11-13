cff011995268b07ebe22851f0a63513d953d39bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:39 +0000

    Add package rRNAFinder/0.0.2/rRNAFinder-0.0.2.tar.gz

---
+++ packages/rRNAFinder/rRNAFinder-0.0.2.tar.gz/rRNAFinder-0.0.2/rrnafinder/versions.py
@@ -0,0 +1,18 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. Debugging with toolbiox;",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. Separate rRNAFinder from ToolBiox;",
+        ],
+    },
+]
