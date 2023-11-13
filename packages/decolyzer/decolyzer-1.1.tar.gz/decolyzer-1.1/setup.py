89537dcdfff8b97d6941247ddf187a6423479773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:53 +0000

    Add package decolyzer/1.1/decolyzer-1.1.tar.gz

---
+++ packages/decolyzer/decolyzer-1.1.tar.gz/decolyzer-1.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+    name = 'decolyzer',
+    packages = ['decolyzer'],
+    version = '1.1',
+    license='MIT',
+    description = 'decolyzer package includes functions which can help to enhance your own function implementation',
+    author = 'Samir',
+    author_email = 'samir198@gmail.com',
+    url = 'https://github.com/patelsamirj111',
+    download_url = 'https://github.com/patelsamirj111/decolyzer/archive/refs/tags/v1.1.tar.gz',
+    keywords = ['decorator', 'python', 'wireshark', 'scp', 'tcpdump', 'paramiko', 'ssh', 'process'],
+    install_requires=[
+        'paramiko',
+        'pyshark',
+        'scp',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
