2027ce6880e52ebdc975eccc68d38bba80300b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:05:07 +0000

    Add package inoopa_utils/1.1.1/inoopa_utils-1.1.1-py3-none-any.whl

---
+++ packages/inoopa_utils/inoopa_utils-1.1.1-py3-none-any.whl/inoopa_utils/utils/env_variables_helper.py
@@ -0,0 +1,19 @@
+import os
+from typing import Literal
+
+from inoopa_utils.utils.exceptions import MissingEnvVariable
+
+
+EnvName = Literal["dev", "staging", "prod"]
+
+def get_env_name() -> EnvName:
+    env = os.getenv("ENV")
+    
+    if env is None:
+        raise MissingEnvVariable("Could not get 'ENV' env variable, therefore can't deduce logging_level.")
+    elif env in ["dev","staging", "prod"]:
+        return env
+    else:
+        raise ValueError(
+            f"'ENV' is set to: {env}. This is not a supported env! Please choose between: [dev, staging, prod]"
+        )
