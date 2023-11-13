a503764d14dbb8c21685b0a2fb8ab7ea736bcf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:10:00 +0000

    Add package process-twarc/0.19.3/process-twarc-0.19.3.tar.gz

---
+++ packages/process-twarc/process-twarc-0.19.3.tar.gz/process-twarc-0.19.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='process-twarc',
+    version='0.19.3',
+    license='MIT',
+    description='Tools for tranforming raw data from Twarc2 to structured data for Masked Language Modeling.',
+    author='Jordan Wolfgang Klein',
+    author_email='jordan.klein.21@um.edu.mt',
+    url='https://github.com/user/Lone-Wolfgang',
+    keywords=['Twitter', 'Deduplication', 'Tokenization', 'Language Modeling'],
+    install_requires=[
+        'pyarrow',
+        'transformers',
+        'pandas',
+        'datasets'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    packages=find_packages(),
+)
