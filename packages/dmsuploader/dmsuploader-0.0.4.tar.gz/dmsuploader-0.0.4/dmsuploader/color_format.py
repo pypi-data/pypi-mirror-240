023d558646e887da94510a150bb7578ec023a160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:07:00 +0000

    Add package dmsuploader/0.0.4/dmsuploader-0.0.4.tar.gz

---
+++ packages/dmsuploader/dmsuploader-0.0.4.tar.gz/dmsuploader-0.0.4/dmsuploader/color_format.py
@@ -0,0 +1,10 @@
+class ColorFormats:
+    def __init__(self):
+        self.DEBUG = '\033[95m'
+        self.BLUE = '\033[94m'
+        self.INFO = '\033[96m'
+        self.SUCCESS = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.ERROR = '\033[91m'
+        self.RESET = '\033[0m'
+        self.CRITICAL = '\033[31;1m'
