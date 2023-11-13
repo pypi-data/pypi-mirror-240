1624a7ea738e2e3de287f28cada07e8d5dc42b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:06:56 +0000

    Add package oarepo-model-builder-relations/4.0.9/oarepo_model_builder_relations-4.0.9-py2.py3-none-any.whl

---
+++ packages/oarepo-model-builder-relations/oarepo_model_builder_relations-4.0.9-py2.py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+
+import pytest
+from invenio_app.factory import create_api
+
+
+@pytest.fixture(scope="module")
+def create_app(instance_path, entry_points):
+    """Application factory fixture."""
+    return create_api
+
+
+@pytest.fixture(scope="module")
+def app_config(app_config):
+    """Mimic an instance's configuration."""
+    app_config["JSONSCHEMAS_HOST"] = "localhost"
+    app_config[
+        "RECORDS_REFRESOLVER_CLS"
+    ] = "invenio_records.resolver.InvenioRefResolver"
+    app_config[
+        "RECORDS_REFRESOLVER_STORE"
+    ] = "invenio_jsonschemas.proxies.current_refresolver_store"
+    app_config["RATELIMIT_AUTHENTICATED_USER"] = "200 per second"
+    app_config["SEARCH_HOSTS"] = [
+        {
+            "host": os.environ.get("OPENSEARCH_HOST", "localhost"),
+            "port": os.environ.get("OPENSEARCH_PORT", "9200"),
+        }
+    ]
+
+    # install custom fields
+    from invenio_records_resources.services.custom_fields.text import KeywordCF
+
+    app_config["TEST_CF"] = [KeywordCF("test")]
+
+    return app_config
