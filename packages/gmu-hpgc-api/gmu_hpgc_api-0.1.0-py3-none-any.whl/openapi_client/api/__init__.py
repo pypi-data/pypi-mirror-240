28f01b227951602e8ebe0a898819107279eb1014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:27 +0000

    Add package gmu-hpgc-api/0.1.0/gmu_hpgc_api-0.1.0-py3-none-any.whl

---
+++ packages/gmu-hpgc-api/gmu_hpgc_api-0.1.0-py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.api_api import APIApi
+from openapi_client.api.conformance_api import ConformanceApi
+from openapi_client.api.dru_api import DRUApi
+from openapi_client.api.jobs_api import JobsApi
+from openapi_client.api.landing_page_api import LandingPageApi
+from openapi_client.api.processes_api import ProcessesApi
+
---
+++ packages/gmu-hpgc-api/gmu_hpgc_api-0.1.0-py3-none-any.whl/openapi_client/models/OneOfstringnumberintegerobjectarraybooleanfilemap.py
@@ -0,0 +1,5 @@
+class OneOfstringnumberintegerobjectarraybooleanfilemap:
+    """
+        Placeholder XYZ
+    """
+    pass
