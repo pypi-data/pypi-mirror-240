bfc28390a8cf6290ba6ed5b4302a9bf17e567b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:07:14 +0000

    Add package funcmodels/0.0.7/funcmodels-0.0.7.tar.gz

---
+++ packages/funcmodels/funcmodels-0.0.7.tar.gz/funcmodels-0.0.7/funcmodels/__init__.py
@@ -0,0 +1,7 @@
+from funcmodels.funcmodel import funcmodel
+from funcmodels.openai_function import (
+    OpenaiFunction, 
+    openai_function,
+    OpenaiFunctionGroup,
+    openai_function_group,
+)
