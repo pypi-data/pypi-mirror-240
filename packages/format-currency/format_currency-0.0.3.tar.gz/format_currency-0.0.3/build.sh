5e600ee3d125ca06acf83c80f30b808d99e8bbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:19 +0000

    Add package format-currency/0.0.3/format_currency-0.0.3.tar.gz

---
+++ packages/format-currency/format_currency-0.0.3.tar.gz/format_currency-0.0.3/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+echo "installing dependencies..."
+python -m pip install --upgrade pip build hatch
+hatch build
+# python -m twine upload dist/*
+python -m twine upload --repository testpypi dist/*
+
+echo "done!"
