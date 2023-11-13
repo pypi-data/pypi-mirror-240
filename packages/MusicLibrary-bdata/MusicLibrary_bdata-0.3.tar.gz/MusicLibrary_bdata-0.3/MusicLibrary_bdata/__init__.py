29fba7c85d818dea1a8ad97685bea38c655eff5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:17 +0000

    Add package MusicLibrary-bdata/0.3/MusicLibrary_bdata-0.3.tar.gz

---
+++ packages/MusicLibrary-bdata/MusicLibrary_bdata-0.3.tar.gz/MusicLibrary_bdata-0.3/MusicLibrary_bdata/__init__.py
@@ -0,0 +1,7 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import numpy as np
+import requests
+import time
+import pandas as pd
+from MusicLibrary import Search,Recommend,LyricsSearch
