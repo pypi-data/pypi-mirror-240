af19b9270deebc9c61607f3e0e5b56cfc7615d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:37 +0000

    Add package malevich-space/0.4.0/malevich_space-0.4.0.tar.gz

---
+++ packages/malevich-space/malevich_space-0.4.0.tar.gz/malevich_space-0.4.0/malevich_space/cli/cli.py
@@ -0,0 +1,22 @@
+import logging
+
+import typer
+
+from .commands import env, component, ci, schema
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+app = typer.Typer()
+app.add_typer(env, name="env")
+app.add_typer(component, name="component")
+app.add_typer(ci, name="ci")
+app.add_typer(schema, name="schema")
+
+
+def main():
+    app()
+
+if __name__ == '__main__':
+    main()
