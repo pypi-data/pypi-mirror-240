e6b123d491989ac208c7d44a8d971871dc1116f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:08 +0000

    Add package opteryx/0.12.0b1/opteryx-0.12.0b1-cp39-cp39-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.12.0b1-cp39-cp39-win_amd64.whl/opteryx/components/heuristic_optimizer.py
@@ -0,0 +1,53 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+~~~
+                      ┌───────────┐
+                      │   USER    │
+         ┌────────────┤           ◄────────────┐
+         │SQL         └───────────┘            │
+  ───────┼─────────────────────────────────────┼──────
+         │                                     │
+   ┌─────▼─────┐                               │
+   │ SQL       │                               │
+   │  Rewriter │                               │
+   └─────┬─────┘                               │
+         │SQL                                  │Plan
+   ┌─────▼─────┐                         ┌─────┴─────┐
+   │           │                         │           │
+   │ Parser    │                         │ Executor  │
+   └─────┬─────┘                         └─────▲─────┘
+         │AST                                  │Plan
+   ┌─────▼─────┐      ┌───────────┐      ┌─────┴─────┐
+   │ AST       │      │           │Stats │Cost-Based │
+   │ Rewriter  │      │ Catalogue ├──────► Optimizer │
+   └─────┬─────┘      └─────┬─────┘      └─────▲─────┘
+         │AST               │Schemas           │Plan
+   ┌─────▼─────┐      ┌─────▼─────┐      ╔═══════════╗
+   │ Logical   │ Plan │           │ Plan ║ Heuristic ║
+   │   Planner ├──────► Binder    ├──────► Optimizer ║
+   └───────────┘      └───────────┘      ╚═══════════╝
+~~~
+
+The plan rewriter does basic heuristic rewrites of the plan, this is an evolution of the old optimizer
+
+Do things like:
+- split predicates into as many AND conditions as possible
+- push predicates close to the reads
+- push projections close to the reads
+- reduce negations
+
+New things:
+- replace subqueries with joins
+- use knowledge about value ranges to prefilter (e.g. prune at read-time before joins)
+"""
