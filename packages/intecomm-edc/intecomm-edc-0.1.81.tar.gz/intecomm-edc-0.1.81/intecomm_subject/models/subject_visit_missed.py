e29931decdbadc1843b4bbfc676c72947ceb9331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:30 +0000

    Add package intecomm-edc/0.1.81/intecomm-edc-0.1.81.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.81.tar.gz/intecomm-edc-0.1.81/intecomm_subject/models/subject_visit_missed.py
@@ -0,0 +1,29 @@
+from django.db import models
+from edc_model.models import BaseUuidModel
+from edc_visit_tracking.constants import VISIT_MISSED_ACTION
+from edc_visit_tracking.model_mixins import SubjectVisitMissedModelMixin
+
+from intecomm_lists.models import SubjectVisitMissedReasons
+
+from ..model_mixins import CrfWithActionModelMixin
+
+
+class SubjectVisitMissed(
+    SubjectVisitMissedModelMixin,
+    CrfWithActionModelMixin,
+    BaseUuidModel,
+):
+    action_name = VISIT_MISSED_ACTION
+
+    missed_reasons = models.ManyToManyField(SubjectVisitMissedReasons, blank=True)
+
+    appt_date = models.DateField(
+        verbose_name="Next scheduled routine/facility appointment",
+        null=True,
+        blank=False,
+        help_text="Should fall on an Integrated clinic day",
+    )
+
+    class Meta(CrfWithActionModelMixin.Meta, BaseUuidModel.Meta):
+        verbose_name = "Missed Visit Report"
+        verbose_name_plural = "Missed Visit Report"
