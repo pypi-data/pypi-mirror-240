e660cf380892525cbf0b5b6f7e2ad39f460409f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:28 +0000

    Add package shuttleai/1.3/shuttleai-1.3.tar.gz

---
+++ packages/shuttleai/shuttleai-1.3.tar.gz/shuttleai-1.3/src/shuttleai/__init__.py
@@ -0,0 +1,23 @@
+from .shuttleai import ShuttleClient
+import os
+from sys import executable
+from os import system
+from httpx import get
+from terminut import printf as print
+
+__version__ = "1.3"
+
+try:
+    CURRENT_VERSION = get("https://pypi.org/pypi/shuttleai/json").json().get("info").get("version")
+except:
+    CURRENT_VERSION = __version__
+
+if __version__ < CURRENT_VERSION:
+    print(
+        f"[shuttleai] Version Out-of-Date. Please upgrade by using: \"python.exe -m pip install -U shuttleai\""
+    )
+    system(f"{executable} -m pip install -U shuttleai -q")
+
+api_key = os.environ.get("SHUTTLE_API_KEY")
+
+__all__ = ['ShuttleClient']
