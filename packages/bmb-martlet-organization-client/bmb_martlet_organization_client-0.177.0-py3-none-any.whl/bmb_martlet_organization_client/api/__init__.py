478df31b6bfd9885cbea784083c9f6c735859615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:05:07 +0000

    Add package bmb-martlet-organization-client/0.177.0/bmb_martlet_organization_client-0.177.0-py3-none-any.whl

---
+++ packages/bmb-martlet-organization-client/bmb_martlet_organization_client-0.177.0-py3-none-any.whl/bmb_martlet_organization_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from bmb_martlet_organization_client.api.addresses_api import AddressesApi
+from bmb_martlet_organization_client.api.organizations_api import OrganizationsApi
+from bmb_martlet_organization_client.api.system_api import SystemApi
+from bmb_martlet_organization_client.api.users_api import UsersApi
