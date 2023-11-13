8a53f7512d6f9f24262ac6c29294fb2311e65738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:31 +0000

    Add package rcquant-sdk/0.0.3/rcquant_sdk-0.0.3.tar.gz

---
+++ packages/rcquant-sdk/rcquant_sdk-0.0.3.tar.gz/rcquant_sdk-0.0.3/rcquant_sdk/data/chart/ChartInitParamData.py
@@ -0,0 +1,179 @@
+from typing import List
+from ...interface import IData
+from ...packer.chart.ChartInitParamDataPacker import ChartInitParamDataPacker
+from ...data.chart.TimeAxisParamData import TimeAxisParamData
+from ...data.chart.ValueAxisParamData import ValueAxisParamData
+from ...data.chart.PlotParamData import PlotParamData
+from ...data.chart.LegendItemParamData import LegendItemParamData
+from ...data.chart.LineGraphParamData import LineGraphParamData
+from ...data.chart.FinancialGraphParamData import FinancialGraphParamData
+from ...data.chart.BarGraphParamData import BarGraphParamData
+from ...data.chart.TextGraphParamData import TextGraphParamData
+from ...data.chart.MarkerGraphParamData import MarkerGraphParamData
+
+
+class ChartInitParamData(IData):
+    def __init__(self):
+        super().__init__(ChartInitParamDataPacker(self))
+        self._ChartID = ''
+        self._Title = ''
+        self._Height = 600
+        self._Width = 800
+        self._IsSaveGeometry = True
+        self._ReplotTime = 100
+        self._GlobalTimeAxisParam = TimeAxisParamData()
+        self._GlobalValueAxisParam = ValueAxisParamData()
+        self._GlobalPlotParam = PlotParamData()
+        self._GlobalLegendItemParam = LegendItemParamData()
+        self._PlotParamList: List[PlotParamData] = []
+        self._ValueAxisParamList: List[ValueAxisParamData] = []
+        self._LegendItemParamList: List[LegendItemParamData] = []
+        self._FinancialGraphParamList: List[FinancialGraphParamData] = []
+        self._LineGraphParamList: List[LineGraphParamData] = []
+        self._BarGraphParamList: List[BarGraphParamData] = []
+        self._TextGraphParamList: List[TextGraphParamData] = []
+        self._MarkerGraphParamList: List[MarkerGraphParamData] = []
+
+    @property
+    def ChartID(self):
+        return self._ChartID
+
+    @ChartID.setter
+    def ChartID(self, value: str):
+        self._ChartID = value
+
+    @property
+    def Title(self):
+        return self._Title
+
+    @Title.setter
+    def Title(self, value: str):
+        self._Title = value
+
+    @property
+    def Height(self):
+        return self._Height
+
+    @Height.setter
+    def Height(self, value: int):
+        self._Height = value
+
+    @property
+    def Width(self):
+        return self._Width
+
+    @Width.setter
+    def Width(self, value: int):
+        self._Width = value
+
+    @property
+    def IsSaveGeometry(self):
+        return self._IsSaveGeometry
+
+    @IsSaveGeometry.setter
+    def IsSaveGeometry(self, value: bool):
+        self._IsSaveGeometry = value
+
+    @property
+    def ReplotTime(self):
+        return self._ReplotTime
+
+    @ReplotTime.setter
+    def ReplotTime(self, value: int):
+        self._ReplotTime = value
+
+    @property
+    def GlobalTimeAxisParam(self):
+        return self._GlobalTimeAxisParam
+
+    @GlobalTimeAxisParam.setter
+    def GlobalTimeAxisParam(self, value: TimeAxisParamData):
+        self._GlobalTimeAxisParam = value
+
+    @property
+    def GlobalValueAxisParam(self):
+        return self._GlobalValueAxisParam
+
+    @GlobalValueAxisParam.setter
+    def GlobalValueAxisParam(self, value: ValueAxisParamData):
+        self._GlobalValueAxisParam = value
+
+    @property
+    def GlobalPlotParam(self):
+        return self._GlobalPlotParam
+
+    @GlobalPlotParam.setter
+    def GlobalPlotParam(self, value: PlotParamData):
+        self._GlobalPlotParam = value
+
+    @property
+    def GlobalLegendItemParam(self):
+        return self._GlobalLegendItemParam
+
+    @GlobalLegendItemParam.setter
+    def GlobalLegendItemParam(self, value: LegendItemParamData):
+        self._GlobalLegendItemParam = value
+
+    @property
+    def PlotParamList(self):
+        return self._PlotParamList
+
+    @PlotParamList.setter
+    def PlotParamList(self, value: List[PlotParamData]):
+        self._PlotParamList = value
+
+    @property
+    def ValueAxisParamList(self):
+        return self._ValueAxisParamList
+
+    @ValueAxisParamList.setter
+    def ValueAxisParamList(self, value: List[ValueAxisParamData]):
+        self._ValueAxisParamList = value
+
+    @property
+    def LegendItemParamList(self):
+        return self._LegendItemParamList
+
+    @LegendItemParamList.setter
+    def LegendItemParamList(self, value: List[LegendItemParamData]):
+        self._LegendItemParamList = value
+
+    @property
+    def FinancialGraphParamList(self):
+        return self._FinancialGraphParamList
+
+    @FinancialGraphParamList.setter
+    def FinancialGraphParamList(self, value: List[FinancialGraphParamData]):
+        self._FinancialGraphParamList = value
+
+    @property
+    def LineGraphParamList(self):
+        return self._LineGraphParamList
+
+    @LineGraphParamList.setter
+    def LineGraphParamList(self, value: List[LineGraphParamData]):
+        self._LineGraphParamList = value
+
+    @property
+    def BarGraphParamList(self):
+        return self._BarGraphParamList
+
+    @BarGraphParamList.setter
+    def BarGraphParamList(self, value: List[BarGraphParamData]):
+        self._BarGraphParamList = value
+
+    @property
+    def TextGraphParamList(self):
+        return self._TextGraphParamList
+
+    @TextGraphParamList.setter
+    def TextGraphParamList(self, value: List[TextGraphParamData]):
+        self._TextGraphParamList = value
+
+    @property
+    def MarkerGraphParamList(self):
+        return self._MarkerGraphParamList
+
+    @MarkerGraphParamList.setter
+    def MarkerGraphParamList(self, value: List[MarkerGraphParamData]):
+        self._MarkerGraphParamList = value
