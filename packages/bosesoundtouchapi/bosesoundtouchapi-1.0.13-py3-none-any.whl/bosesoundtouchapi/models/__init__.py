76734d1060d32f5d21a158ee60f6e54cec7635e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:40 +0000

    Add package bosesoundtouchapi/1.0.13/bosesoundtouchapi-1.0.13-py3-none-any.whl

---
+++ packages/bosesoundtouchapi/bosesoundtouchapi-1.0.13-py3-none-any.whl/bosesoundtouchapi/models/__init__.py
@@ -0,0 +1,67 @@
+# import all classes from the namespace.
+from .audiodspcontrols import AudioDspControls
+from .audioproducttonecontrols import AudioProductToneControls
+from .balance import Balance
+from .bass import Bass
+from .basscapabilities import BassCapabilities
+from .bluetoothinfo import BlueToothInfo
+from .capabilities import Capabilities
+from .clockconfig import ClockConfig
+from .clocktime import ClockTime
+from .contentitem import ContentItem
+from .controllevelinfo import ControlLevelInfo
+from .dspmonostereoitem import DSPMonoStereoItem
+from .infonetworkconfig import InfoNetworkConfig
+from .mediaserverlist import MediaServerList, MediaServer
+from .musicserviceaccount import MusicServiceAccount
+from .networkinfo import NetworkInfo, NetworkInfoInterface
+from .networkstatus import NetworkStatus, NetworkStatusInterface
+from .nowplayingstatus import NowPlayingStatus
+from .playinfo import PlayInfo
+from .powermanagement import PowerManagement
+from .presetlist import PresetList, Preset
+from .productcechdmicontrol import ProductCecHdmiControl
+from .producthdmiassignmentcontrols import ProductHdmiAssignmentControls
+from .rebroadcastlatencymode import RebroadcastLatencyMode
+from .recentlist import RecentList, Recent
+from .simpleconfig import SimpleConfig
+from .sourcelist import SourceList, SourceItem
+from .systemtimeout import SystemTimeout
+from .volume import Volume
+from .wirelessprofile import WirelessProfile
+from .zone import Zone, ZoneMember
+
+# all classes to import when "import *" is specified.
+__all__ = [
+    'AudioDspControls',
+    'AudioProductToneControls',
+    'Balance',
+    'Bass',
+    'BassCapabilities',
+    'BlueToothInfo',
+    'Capabilities',
+    'ClockConfig',
+    'ClockTime',
+    'ContentItem',
+    'ControlLevelInfo',
+    'DSPMonoStereoItem',
+    'InfoNetworkConfig',
+    'MediaServerList', 'MediaServer',
+    'MusicServiceAccount',
+    'NetworkInfo', 'NetworkInfoInterface',
+    'NetworkStatus', 'NetworkStatusInterface',
+    'NowPlayingStatus',
+    'PlayInfo',
+    'PowerManagement',
+    'PresetList', 'Preset',
+    'ProductCecHdmiControl',
+    'ProductHdmiAssignmentControls',
+    'RebroadcastLatencyMode',
+    'RecentList', 'Recent',
+    'SimpleConfig',
+    'SourceList', 'SourceItem',
+    'SystemTimeout',
+    'Volume',
+    'WirelessProfile',
+    'Zone', 'ZoneMember'
+]
