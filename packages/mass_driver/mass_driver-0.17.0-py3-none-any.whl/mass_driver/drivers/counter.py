aa35e28a5f23322d9a93724140228849d9adb106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:06 +0000

    Add package mass_driver/0.17.0/mass_driver-0.17.0-py3-none-any.whl

---
+++ packages/mass_driver/mass_driver-0.17.0-py3-none-any.whl/mass_driver/drivers/counter.py
@@ -0,0 +1,39 @@
+"""Dummy Patch Driver: increments a file counter
+
+Simplest code we could implement that demonstrates PatchDriver capabilities.
+"""
+
+from mass_driver.models.patchdriver import PatchDriver, PatchOutcome, PatchResult
+from mass_driver.models.repository import ClonedRepo
+
+
+class Counter(PatchDriver):
+    """Increments a counter in a given file of repo, creating if non-existent"""
+
+    target_count: int
+    counter_file: str
+
+    def run(self, repo: ClonedRepo) -> PatchResult:
+        """Process the counter file"""
+        counter_filepath_abs = repo.cloned_path / self.counter_file
+        if not counter_filepath_abs.is_file():
+            return PatchResult(
+                outcome=PatchOutcome.PATCH_DOES_NOT_APPLY,
+                details="No counter file exists yet",
+            )
+        counter_content = counter_filepath_abs.read_text().strip()
+        if not counter_content.isdigit():
+            return PatchResult(
+                outcome=PatchOutcome.PATCH_ERROR,
+                details="Counter file isn't an integer",
+            )
+        counter_number = int(counter_content)
+        counter_is_different = counter_number != self.target_count
+        self.logger.info(
+            f"Measured: {counter_number}, target: {self.target_count}. Different? {counter_is_different}"
+        )
+        if counter_is_different:
+            counter_filepath_abs.write_text(str(self.target_count) + "\n")
+            return PatchResult(outcome=PatchOutcome.PATCHED_OK)
+        # COUNTER already same value
+        return PatchResult(outcome=PatchOutcome.ALREADY_PATCHED)
