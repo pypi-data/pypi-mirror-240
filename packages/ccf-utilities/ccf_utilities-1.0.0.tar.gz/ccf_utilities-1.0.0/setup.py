e86dc30847faf3667d05553c259d38be085248cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:20 +0000

    Add package ccf-utilities/1.0.0/ccf_utilities-1.0.0.tar.gz

---
+++ packages/ccf-utilities/ccf_utilities-1.0.0.tar.gz/ccf_utilities-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ccf_utilities',
+    version='1.0.0',
+    packages=find_packages(),
+    # ...other metadata...
+    author="Aamar Shahzad"
+)
