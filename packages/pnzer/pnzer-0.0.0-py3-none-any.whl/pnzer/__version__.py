8bc78b57048f5bca278fe7e56e87c90e76cde5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:32 +0000

    Add package pnzer/0.0.0/pnzer-0.0.0-py3-none-any.whl

---
+++ packages/pnzer/pnzer-0.0.0-py3-none-any.whl/pnzer/__version__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'pnzer'
+__license__ = 'MIT'
+__author__ = 'Hans Park'
+__contact__ = 'ianolpx@gmail.com'
+__url__ = 'https://github.com/ianolpx/pnzer'
+__description__ = (
+    '')
