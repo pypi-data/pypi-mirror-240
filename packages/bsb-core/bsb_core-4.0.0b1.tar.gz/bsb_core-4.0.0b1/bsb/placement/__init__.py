47d7e93993be5b52804e037effd962295b8243f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:07:02 +0000

    Add package bsb-core/4.0.0b1/bsb_core-4.0.0b1.tar.gz

---
+++ packages/bsb-core/bsb_core-4.0.0b1.tar.gz/bsb_core-4.0.0b1/bsb/placement/__init__.py
@@ -0,0 +1,9 @@
+from .strategy import (
+    PlacementStrategy,
+    FixedPositions,
+    Entities,
+)
+from .import_ import ImportPlacement, CsvImportPlacement
+from .arrays import ParallelArrayPlacement
+from .particle import ParticlePlacement, RandomPlacement
+from .satellite import Satellite
