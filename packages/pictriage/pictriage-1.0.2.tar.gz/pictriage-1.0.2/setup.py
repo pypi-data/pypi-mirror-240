ab08a563dd69d95fbee4ee3f2a1c219135944545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:19 +0000

    Add package pictriage/1.0.2/pictriage-1.0.2.tar.gz

---
+++ packages/pictriage/pictriage-1.0.2.tar.gz/pictriage-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pictriage',
+    version='1.0.2',
+    author='Author Name',
+    author_email='author@example.com',
+    description='Description of my package',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'starlette',
+        'uvicorn',
+        'ibis==3.2.0',
+        'natsort==8.4.0',
+        'python-multipart',
+    ],
+    entry_points={
+        'console_scripts': ['pictriage=pictriage:main'],
+    },
+)
