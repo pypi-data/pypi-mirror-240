69b0468954b0f65380d2b6a3ed0d74168e749e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:34 +0000

    Add package xquantipy/0.2.0/xquantipy-0.2.0.tar.gz

---
+++ packages/xquantipy/xquantipy-0.2.0.tar.gz/xquantipy-0.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+# print(setuptools.find_packages())
+setuptools.setup(
+    name='xquantipy',
+    version='0.2.0',
+    author='Sai Charan Vadakapur',
+    description='Module for financial analysis',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+    ],
+    platforms=['any'],
+    install_requires=[
+        'beautifulsoup4',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pipreqs',
+        'plotly',
+        'pytest',
+        'python_dateutil',
+        'Requests',
+        'seaborn',
+        'setuptools',
+        'statsmodels',
+        'yfinance',
+    ],
+)
