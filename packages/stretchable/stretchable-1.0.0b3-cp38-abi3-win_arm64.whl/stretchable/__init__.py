841cc6f329804a86ea4a719254299853d417f39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:03 +0000

    Add package stretchable/1.0.0b3/stretchable-1.0.0b3-cp38-abi3-win_arm64.whl

---
+++ packages/stretchable/stretchable-1.0.0b3-cp38-abi3-win_arm64.whl/stretchable/__init__.py
@@ -0,0 +1,28 @@
+# stretchable is inspired by/based on stretched (archived) by nmichlo:
+# https://github.com/nmichlo/stretched
+
+# Stretch is an implementation of CSS Flexbox written in Rust:
+# https://github.com/vislyhq/stretch
+
+# import bindings etc.
+# from .node import Box, Layout, Node, reset
+# from .style import Rect, Size, Style
+
+from .node import Box, Layout, Node
+
+__all__ = [
+    "Node",
+    "Box",
+    "Layout",
+]
+
+
+"""
+TODO:
+
+  - Implement __str__ for Node class
+  - Use __str__ from 1) in logger
+  - Support grid_[template/auto]_[rows/columns] in Style
+  - Script to download and process fixtures from taffy github? (LOW)
+
+"""
