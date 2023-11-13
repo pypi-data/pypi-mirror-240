1b234b561193cb2746583f88c0c3284ea5f329ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:06:55 +0000

    Add package deepeval/0.20.17/deepeval-0.20.17-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.20.17-py3-none-any.whl/deepeval/plugins/plugin.py
@@ -0,0 +1,39 @@
+import pytest
+import os
+from rich import print
+from typing import Optional, Any
+from deepeval.constants import PYTEST_RUN_TEST_NAME
+from deepeval.test_run import TestRun, test_run_manager
+
+
+def pytest_sessionstart(session: pytest.Session):
+    test_run = TestRun(
+        testFile=session.config.getoption("file_or_dir")[0],
+        testCases=[],
+        metricScores=[],
+        configurations={},
+    )
+    test_run_manager.set_test_run(test_run)
+    test_run_manager.save_test_run()
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_protocol(
+    item: pytest.Item, nextitem: Optional[pytest.Item]
+) -> Optional[Any]:
+    os.environ[PYTEST_RUN_TEST_NAME] = item.nodeid.split("::")[-1]
+    return None  # continue with the default protocol
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_sessionfinish(session: pytest.Session, exitstatus):
+    print("Running teardown with pytest sessionfinish...")
+
+    yield
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    for report in terminalreporter.getreports("skipped"):
+        if report.skipped:
+            reason = report.longreprtext.split("\n")[-1]
+            print(f"Test {report.nodeid} was skipped. Reason: {reason}")
