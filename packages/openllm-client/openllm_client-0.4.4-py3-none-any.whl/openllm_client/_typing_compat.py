e9dc2fdf8b9667a60c813f631177302dcb63493f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 14:06:35 +0000

    Add package openllm-client/0.4.4/openllm_client-0.4.4-py3-none-any.whl

---
+++ packages/openllm-client/openllm_client-0.4.4-py3-none-any.whl/openllm_client/_typing_compat.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+from openllm_core._typing_compat import Annotated as Annotated
+from openllm_core._typing_compat import LiteralString as LiteralString
+from openllm_core._typing_compat import NotRequired as NotRequired
+from openllm_core._typing_compat import Required as Required
+from openllm_core._typing_compat import Self as Self
+from openllm_core._typing_compat import TypeGuard as TypeGuard
+from openllm_core._typing_compat import dataclass_transform as dataclass_transform
+from openllm_core._typing_compat import overload as overload
+
+
+Platform = Annotated[
+  LiteralString, Literal['MacOS', 'Linux', 'Windows', 'FreeBSD', 'OpenBSD', 'iOS', 'iPadOS', 'Android', 'Unknown'], str
+]
+Architecture = Annotated[LiteralString, Literal['arm', 'arm64', 'x86', 'x86_64', 'Unknown'], str]
