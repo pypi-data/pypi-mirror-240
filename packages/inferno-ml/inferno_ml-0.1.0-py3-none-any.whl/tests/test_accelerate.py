c804325a38ed67bd40a4d59ef4b6506a63c057d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:27 +0000

    Add package inferno-ml/0.1.0/inferno_ml-0.1.0-py3-none-any.whl

---
+++ packages/inferno-ml/inferno_ml-0.1.0-py3-none-any.whl/tests/test_accelerate.py
@@ -0,0 +1,13 @@
+# test_accelerate.py
+import unittest
+from inferno_ml import accelerate
+
+class TestAccelerate(unittest.TestCase):
+    def test_accelerate(self):
+        # setup your test environment
+        # call accelerate.accelerate with the necessary arguments
+        # make assertions
+        pass 
+
+if __name__ == '__main__':
+    unittest.main()
