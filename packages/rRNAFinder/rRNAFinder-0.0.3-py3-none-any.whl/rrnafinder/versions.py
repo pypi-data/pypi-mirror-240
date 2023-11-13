d2b8fd8e12658518b64069a9c75296ae16f19336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:06:30 +0000

    Add package rRNAFinder/0.0.3/rRNAFinder-0.0.3-py3-none-any.whl

---
+++ packages/rRNAFinder/rRNAFinder-0.0.3-py3-none-any.whl/rrnafinder/versions.py
@@ -0,0 +1,24 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. use blat to replace genblastA from hugep2g;",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. Debugging with toolbiox;",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. Separate rRNAFinder from ToolBiox;",
+        ],
+    },
+]
