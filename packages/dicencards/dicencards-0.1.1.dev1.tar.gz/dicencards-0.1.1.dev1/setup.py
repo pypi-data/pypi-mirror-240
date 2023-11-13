05b93a47b36cf352a070db2252615defa666d867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:19 +0000

    Add package dicencards/0.1.1.dev1/dicencards-0.1.1.dev1.tar.gz

---
+++ packages/dicencards/dicencards-0.1.1.dev1.tar.gz/dicencards-0.1.1.dev1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+__name__ = 'dicencards'
+__version__ = '0.1.1.dev1'
+__author__ = 'Xavier ROY'
+__author_email__ = 'xavier@regbuddy.eu'
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+requirement_file_path = project_dir + '/requirements.txt'
+requirements = []
+if os.path.isfile(requirement_file_path):
+    with open(requirement_file_path) as f:
+        requirements = f.read().splitlines()
+
+setup(
+    name=__name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    url='https://bitbucket.org/xroy/dicencards',
+    description='Python package to manage dice rolls and card draws.',
+    long_description=open('README.rst').read(),
+    license="Apache License, Version 2.0",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Games/Entertainment :: Board Games',
+        'Intended Audience :: Developers'
+    ]
+)
