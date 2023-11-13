86d0b691c650d3721adb3f81682cb8b7ce748f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:05:06 +0000

    Add package rootfs/0.0.1/rootfs-0.0.1-py3-none-any.whl

---
+++ packages/rootfs/rootfs-0.0.1-py3-none-any.whl/rootfs/__init__.py
@@ -0,0 +1,21 @@
+from .etc import *
+from .usr import *
+
+def file(path):
+    import importlib.resources
+    path = path.strip('/')
+    path = importlib.resources.files(__name__).joinpath(path)
+    return path
+
+def ls(path):
+    return [p.stem for p in file(path).glob('*')]
+
+def find(path):
+    return [p for p in file(path).rglob('*')]
+
+def cat(path):
+    return file(path).read_bytes()
+
+def lines(path):
+    return cat(path).decode().splitlines()
+
