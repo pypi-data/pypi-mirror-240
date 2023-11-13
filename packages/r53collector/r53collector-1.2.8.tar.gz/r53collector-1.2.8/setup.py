daac728b543c46f756fa96f2b4a002843f6590d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:05:08 +0000

    Add package r53collector/1.2.8/r53collector-1.2.8.tar.gz

---
+++ packages/r53collector/r53collector-1.2.8.tar.gz/r53collector-1.2.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '1.2.8'
+DESCRIPTION = 'Collect all route53 records distributed across yours the accounts in aws org.'
+LONG_DESCRIPTION = 'This python tool collects DNS records from AWS Route53 across multiple accounts using AWS SSO (Single Sign-On) and provides various options for listing, filtering, storing and analyzing the data.'
+
+# Setting up
+setup(
+    name="r53collector",
+    version=VERSION,
+    author="maerifat (Maerifat Majeed)",
+    author_email="<maerifat@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['termcolor', 'boto3', 'openpyxl', 'ipaddress', 'dnspython'],
+    keywords=['python', 'route53', 'excel', 'sso', 'aws', 'aws org', 'subdomains', 'dangling' , 'certificates'],
+    scripts=['library/r53collector.py'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
