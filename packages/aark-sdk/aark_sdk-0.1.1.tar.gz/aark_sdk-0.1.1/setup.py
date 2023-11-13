9e147f90e37bae2445d1988aa36caa27121767cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:14 +0000

    Add package aark-sdk/0.1.1/aark_sdk-0.1.1.tar.gz

---
+++ packages/aark-sdk/aark_sdk-0.1.1.tar.gz/aark_sdk-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="aark_sdk",
+    version="0.1.1",
+    description="",
+    url="https://github.com/Aark-Digital/python-aark-sdk",
+    author="",
+    author_email="",
+    license="",
+    packages=find_packages(include=['aark_sdk', 'aark_sdk.*']),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: MacOS X',
+        'Programming Language :: Python :: 3.9'        
+    ]
+)
