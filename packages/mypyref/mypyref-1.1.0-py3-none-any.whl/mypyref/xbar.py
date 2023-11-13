e2523b3d0e33e6b3ae47253d179185aa14d95610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:04:29 +0000

    Add package mypyref/1.1.0/mypyref-1.1.0-py3-none-any.whl

---
+++ packages/mypyref/mypyref-1.1.0-py3-none-any.whl/mypyref/xbar.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+# command = Path.home() / '.local' / 'bin' / 'mypyref'
+print("Py | color=green")
+print("---")
+print("Start | shell=mypyref | param1=start")
+print("Stop | shell=mypyref | param1=stop")
