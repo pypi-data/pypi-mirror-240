54bfd16f78512ceb72bdfe1b8c7efebcb381e2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:06:44 +0000

    Add package huaweicloudsdkvas/3.1.66/huaweicloudsdkvas-3.1.66-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkvas/huaweicloudsdkvas-3.1.66-py2.py3-none-any.whl/huaweicloudsdkvas/v2/vas_async_client.py
@@ -0,0 +1,498 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import importlib
+
+from huaweicloudsdkcore.client import Client, ClientBuilder
+from huaweicloudsdkcore.utils import http_utils
+from huaweicloudsdkcore.sdk_stream_request import SdkStreamRequest
+
+
+class VasAsyncClient(Client):
+    def __init__(self):
+        super(VasAsyncClient, self).__init__()
+        self.model_package = importlib.import_module("huaweicloudsdkvas.v2.model")
+
+    @classmethod
+    def new_builder(cls, clazz=None):
+        if not clazz:
+            client_builder = ClientBuilder(cls)
+        else:
+            if clazz.__name__ != "VasAsyncClient":
+                raise TypeError("client type error, support client type is VasAsyncClient")
+            client_builder = ClientBuilder(clazz)
+
+        
+
+        return client_builder
+
+    def create_tasks_async(self, request):
+        """创建服务作业
+
+        该接口用于创建服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for CreateTasks
+        :type request: :class:`huaweicloudsdkvas.v2.CreateTasksRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.CreateTasksResponse`
+        """
+        return self._create_tasks_with_http_info(request)
+
+    def _create_tasks_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if 'body' in local_var_params:
+            body_params = local_var_params['body']
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='CreateTasksResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def delete_task_async(self, request):
+        """删除服务作业
+
+        该接口用于删除服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for DeleteTask
+        :type request: :class:`huaweicloudsdkvas.v2.DeleteTaskRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.DeleteTaskResponse`
+        """
+        return self._delete_task_with_http_info(request)
+
+    def _delete_task_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+        if 'task_id' in local_var_params:
+            path_params['task_id'] = local_var_params['task_id']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks/{task_id}',
+            method='DELETE',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='DeleteTaskResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def list_tasks_details_async(self, request):
+        """获取服务作业列表
+
+        该接口用于获取服务作业列表
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for ListTasksDetails
+        :type request: :class:`huaweicloudsdkvas.v2.ListTasksDetailsRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.ListTasksDetailsResponse`
+        """
+        return self._list_tasks_details_with_http_info(request)
+
+    def _list_tasks_details_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+
+        query_params = []
+        if 'service_version' in local_var_params:
+            query_params.append(('service_version', local_var_params['service_version']))
+        if 'state' in local_var_params:
+            query_params.append(('state', local_var_params['state']))
+        if 'name_like' in local_var_params:
+            query_params.append(('name_like', local_var_params['name_like']))
+        if 'id_like' in local_var_params:
+            query_params.append(('id_like', local_var_params['id_like']))
+        if 'created_since' in local_var_params:
+            query_params.append(('created_since', local_var_params['created_since']))
+        if 'created_until' in local_var_params:
+            query_params.append(('created_until', local_var_params['created_until']))
+        if 'order' in local_var_params:
+            query_params.append(('order', local_var_params['order']))
+        if 'offset' in local_var_params:
+            query_params.append(('offset', local_var_params['offset']))
+        if 'limit' in local_var_params:
+            query_params.append(('limit', local_var_params['limit']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ListTasksDetailsResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def show_task_async(self, request):
+        """查询服务作业
+
+        该接口用于查询服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for ShowTask
+        :type request: :class:`huaweicloudsdkvas.v2.ShowTaskRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.ShowTaskResponse`
+        """
+        return self._show_task_with_http_info(request)
+
+    def _show_task_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+        if 'task_id' in local_var_params:
+            path_params['task_id'] = local_var_params['task_id']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks/{task_id}',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowTaskResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def start_task_async(self, request):
+        """启动服务作业
+
+        该接口用于启动服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for StartTask
+        :type request: :class:`huaweicloudsdkvas.v2.StartTaskRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.StartTaskResponse`
+        """
+        return self._start_task_with_http_info(request)
+
+    def _start_task_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+        if 'task_id' in local_var_params:
+            path_params['task_id'] = local_var_params['task_id']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks/{task_id}/start',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='StartTaskResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def stop_task_async(self, request):
+        """停止服务作业
+
+        该接口用于停止服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for StopTask
+        :type request: :class:`huaweicloudsdkvas.v2.StopTaskRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.StopTaskResponse`
+        """
+        return self._stop_task_with_http_info(request)
+
+    def _stop_task_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+        if 'task_id' in local_var_params:
+            path_params['task_id'] = local_var_params['task_id']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks/{task_id}/stop',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='StopTaskResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def update_task_async(self, request):
+        """更新服务作业
+
+        该接口用于更新服务作业
+        
+        Please refer to HUAWEI cloud API Explorer for details.
+
+
+        :param request: Request instance for UpdateTask
+        :type request: :class:`huaweicloudsdkvas.v2.UpdateTaskRequest`
+        :rtype: :class:`huaweicloudsdkvas.v2.UpdateTaskResponse`
+        """
+        return self._update_task_with_http_info(request)
+
+    def _update_task_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'service_name' in local_var_params:
+            path_params['service_name'] = local_var_params['service_name']
+        if 'task_id' in local_var_params:
+            path_params['task_id'] = local_var_params['task_id']
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if 'body' in local_var_params:
+            body_params = local_var_params['body']
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/services/{service_name}/tasks/{task_id}',
+            method='PUT',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='UpdateTaskResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def call_api(self, resource_path, method, path_params=None, query_params=None, header_params=None, body=None,
+                 post_params=None, cname=None, response_type=None, response_headers=None, auth_settings=None,
+                 collection_formats=None, request_type=None):
+        """Makes the HTTP request and returns deserialized data.
+
+        :param resource_path: Path to method endpoint.
+        :param method: Method to call.
+        :param path_params: Path parameters in the url.
+        :param query_params: Query parameters in the url.
+        :param header_params: Header parameters to be
+            placed in the request header.
+        :param body: Request body.
+        :param post_params: Request post form parameters,
+            for `application/x-www-form-urlencoded`, `multipart/form-data`.
+        :param cname: Used for obs endpoint.
+        :param auth_settings: Auth Settings names for the request.
+        :param response_type: Response data type.
+        :param response_headers: Header should be added to response data.
+        :param collection_formats: dict of collection formats for path, query,
+            header, and post parameters.
+        :param request_type: Request data type.
+        :return:
+            Return the response directly.
+        """
+        return self.do_http_request(
+            method=method,
+            resource_path=resource_path,
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body,
+            post_params=post_params,
+            cname=cname,
+            response_type=response_type,
+            response_headers=response_headers,
+            collection_formats=collection_formats,
+            request_type=request_type,
+	    async_request=True)
