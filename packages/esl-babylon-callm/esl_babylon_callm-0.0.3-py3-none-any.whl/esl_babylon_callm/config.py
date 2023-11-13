6bf477acde1fec4852d7047fd8f5e86b3e015224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:06:40 +0000

    Add package esl-babylon-callm/0.0.3/esl_babylon_callm-0.0.3-py3-none-any.whl

---
+++ packages/esl-babylon-callm/esl_babylon_callm-0.0.3-py3-none-any.whl/esl_babylon_callm/config.py
@@ -0,0 +1,44 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
+
+root_folder: Path = Path(__file__).parent.resolve()
+
+
+class Settings(BaseSettings):
+    # App Settings
+    app_dir: Path = Field(default=root_folder)
+    app_name: str = Field(env="APP_NAME", default="ESL-Babylon-CaLLM")
+
+    # Logging Settings
+    log_level: str | int = Field(env="LOG_LEVEL", default="info")
+
+    @classmethod
+    def settings_customise_sources(
+            cls,
+            settings_cls: type[BaseSettings],
+            init_settings: PydanticBaseSettingsSource,
+            env_settings: PydanticBaseSettingsSource,
+            dotenv_settings: PydanticBaseSettingsSource,
+            file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return init_settings, dotenv_settings, env_settings, file_secret_settings
+
+    class Config:
+        validate_assignment = True
+        env_file: Path = root_folder / Path("env.ini")
+
+    def update_values(self) -> None:
+        pass
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    settings = Settings()
+    settings.update_values()
+    return settings
+
+
+app_config = get_settings()
