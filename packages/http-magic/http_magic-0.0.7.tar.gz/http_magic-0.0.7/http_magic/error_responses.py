a1ef27dc0635e1014df9d0a9587d95e7fb92726e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 05:04:50 +0000

    Add package http-magic/0.0.7/http_magic-0.0.7.tar.gz

---
+++ packages/http-magic/http_magic-0.0.7.tar.gz/http_magic-0.0.7/http_magic/error_responses.py
@@ -0,0 +1,54 @@
+from .http import HttpResponse
+from .content import HttpContentTypes
+
+
+class HttpMethodNotSupportedResponse(HttpResponse):
+    def __init__(self, method: str):
+        from json_cpp import JsonObject
+        HttpResponse.__init__(self,
+                              code=405,
+                              body=str(JsonObject(code=405,
+                                                  message="Method %s not supported" % method)),
+                              content_type=HttpContentTypes[".json"])
+
+
+class HttpInternalServerErrorResponse(HttpResponse):
+
+    def __init__(self, method: str):
+        from json_cpp import JsonObject
+        HttpResponse.__init__(self,
+                              code=500,
+                              body=str(JsonObject(code=500,
+                                                  message="Internal server error")),
+                              content_type=HttpContentTypes[".json"])
+
+
+class HttpFileNotFoundResponse(HttpResponse):
+    def __init__(self, file_name: str):
+        from json_cpp import JsonObject
+        HttpResponse.__init__(self,
+                              code=404,
+                              body=JsonObject(code=404,
+                                              message="File %s not found" % file_name),
+                              content_type=HttpContentTypes[".json"])
+
+
+class HttpListNotAllowedResponse(HttpResponse):
+    def __init__(self):
+        from json_cpp import JsonObject
+        HttpResponse.__init__(self,
+                              code=403,
+                              body=JsonObject(code=403,
+                                              message="Listing not allowed"),
+                              content_type=HttpContentTypes[".json"])
+
+
+class HttpRouteNotFoundResponse(HttpResponse):
+    def __init__(self, file_name: str):
+        from json_cpp import JsonObject
+        HttpResponse.__init__(self,
+                              code=404,
+                              body=str(JsonObject(code=404,
+                                                  message="Route %s not found" % file_name)),
+                              content_type=HttpContentTypes[".json"])
+
