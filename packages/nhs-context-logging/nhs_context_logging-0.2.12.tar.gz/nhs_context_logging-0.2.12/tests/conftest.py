8109d41d5325ff4735e8a1223dfd14412dff450e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:20 +0000

    Add package nhs-context-logging/0.2.12/nhs_context_logging-0.2.12.tar.gz

---
+++ packages/nhs-context-logging/nhs_context_logging-0.2.12.tar.gz/nhs_context_logging-0.2.12/tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+from nhs_context_logging import app_logger, logging_context
+from nhs_context_logging.fixtures import log_capture, log_capture_global  # noqa: F401
+from nhs_context_logging.logger import uuid4_hex_string
+
+
+@pytest.fixture(scope="session", autouse=True)
+def global_setup():
+    app_logger.setup("pytest", internal_id_factory=uuid4_hex_string)
+
+
+@pytest.fixture(autouse=True)
+def reset_logging_storage():
+    logging_context.thread_local_context_storage()
+
+
+@pytest.fixture()
+def unlock_global_setup():
+    # Enable the app logger to be set up again
+    app_logger._is_setup = False
+
+    # Yield a window in which app logger modifications can occur
+    yield
+
+    # Revert to the global settings again
+    app_logger._is_setup = False
+    app_logger.setup("pytest", internal_id_factory=uuid4_hex_string)
