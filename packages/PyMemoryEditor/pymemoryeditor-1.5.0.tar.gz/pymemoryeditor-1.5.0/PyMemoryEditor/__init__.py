dd28f63422ed326f725d9e7e08efd4460b24a6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 04:04:24 +0000

    Add package PyMemoryEditor/1.5.0/pymemoryeditor-1.5.0.tar.gz

---
+++ packages/PyMemoryEditor/pymemoryeditor-1.5.0.tar.gz/pymemoryeditor-1.5.0/PyMemoryEditor/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+A Python library developed with ctypes to manipulate Windows and Linux processes (32 bits and 64 bits),
+reading and writing values in the process memory.
+"""
+
+__author__ = "Jean Loui Bernard Silva de Jesus"
+__version__ = "1.5.0"
+
+
+import sys
+
+# For Windows.
+if "win" in sys.platform:
+    from .win32.process import WindowsProcess
+    from .win32.enums.process_operations import ProcessOperationsEnum
+    from .win32.enums.scan_types import ScanTypesEnum
+    OpenProcess = WindowsProcess
+
+# For Linux.
+else:
+    from .linux.process import LinuxProcess
+    OpenProcess = LinuxProcess
