356567f16f39be982100a75efe3a037268058d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:06:53 +0000

    Add package python-event-bus/1.2.0/python_event_bus-1.2.0-py3-none-any.whl

---
+++ packages/python-event-bus/python_event_bus-1.2.0-py3-none-any.whl/python_event_bus/__init__.py
@@ -0,0 +1,11 @@
+"""
+:copyright: (c) 2023-present mxngo
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "python-event-bus"
+__author__ = "mxngo"
+__copyright__ = ":copyright: (c) 2023-present mxngo"
+__license__ = "MIT"
+
+from .bus import EventBus, EventBusContextManager
