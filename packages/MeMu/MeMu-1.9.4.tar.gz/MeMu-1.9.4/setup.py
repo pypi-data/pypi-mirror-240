539c3e6d1db0c02221683dc8f9f2d7d154b316ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:51 +0000

    Add package MeMu/1.9.4/MeMu-1.9.4.tar.gz

---
+++ packages/MeMu/MeMu-1.9.4.tar.gz/MeMu-1.9.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MeMu',
+    version='1.9.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['fonts/*.ttf', 'images/*.png', 'icons/*.ico'],
+    },
+    install_requires=[
+        'customtkinter>=5.1.3',
+        'Pillow>=9.4.0',
+        'pyboy>=1.6.6',
+        'os'
+    ],
+)
