41f68e7c5f59d5e0d8ce82a2db4deb00d39223e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 03:09:59 +0000

    Add package tgedr-nihao/0.0.1/tgedr-nihao-0.0.1.tar.gz

---
+++ packages/tgedr-nihao/tgedr-nihao-0.0.1.tar.gz/tgedr-nihao-0.0.1/README.md
@@ -0,0 +1,19 @@
+# nihao
+studies with financial data sources
+___
+
+## readings
+- Learn Algorithmic Trading, 2019 Packt Publishing, Sebastien Donadio & Sourav Ghosh
+
+## subprojects
+
+### [Algorithmic Trading with ML](https://liveproject.manning.com/module/199_1_2/algorithmic-trading-with-machine-learning/introduction/start-project?)
+
+#### project outline
+- Setting up your local environment and sourcing the data
+- Engineering predictive features
+- Evaluating financial features
+- Predicting asset returns with linear models using sklearn
+- Predicting asset returns with gradient boosting with LightGBM 
+- Designing and backtesting a trading strategy using Zipline and pyfolio
+
---
+++ packages/tgedr-nihao/tgedr-nihao-0.0.1.tar.gz/tgedr-nihao-0.0.1/setup.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+from setuptools import setup, find_namespace_packages
+
+logger = logging.getLogger(__name__)
+VERSION = "0.0.1"
+
+version = os.getenv("TGEDR_NIHAO_VERSION", VERSION)
+
+logging.info(f"[tgedr-nihao] building version: {version}")
+
+setup(
+    name='tgedr-nihao',
+    version=version,
+    description='This package provides code derived from studies with financial data sources',
+    url='https://github.com/jtviegas-sandbox/nihao',
+    author='joao tiago viegas',
+    author_email='jtviegas@gmail.com',
+    license='Unlicense',
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
+    ],
+    keywords='finance development data science',
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    install_requires=[
+        "pandas~=1.4.4",
+    ],
+    python_requires='>=3.7',
+    # package_data={'sample': ['package_data.dat'],},
+)
