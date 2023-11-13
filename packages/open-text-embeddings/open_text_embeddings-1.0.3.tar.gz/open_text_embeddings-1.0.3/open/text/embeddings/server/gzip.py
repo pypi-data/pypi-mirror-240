026bbb05757ed690e62a49181218d05fcc95cf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:22 +0000

    Add package open-text-embeddings/1.0.3/open_text_embeddings-1.0.3.tar.gz

---
+++ packages/open-text-embeddings/open_text_embeddings-1.0.3.tar.gz/open_text_embeddings-1.0.3/open/text/embeddings/server/gzip.py
@@ -0,0 +1,55 @@
+# Created with the help of the ChatGPT (GPT-3.5)
+# REF: https://chat.openai.com/share/4ab741e7-8059-4be1-b3da-46b8e78d98cc
+# REF: https://gealber.com/gzip-middleware-fastapi
+import os
+import gzip
+from starlette.types import Message
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+verbose = bool(os.environ.get('VERBOSE', ''))
+
+
+class GZipRequestMiddleware(BaseHTTPMiddleware):
+    async def set_body(self, request: Request):
+        receive_ = await request._receive()
+        content_encoding = request.headers.get('Content-Encoding', '').lower()
+        if (verbose):
+            print("content_encoding", content_encoding)
+        if 'gzip' in content_encoding:
+            try:
+                content_length = int(
+                    request.headers.get('Content-Length', '0'))
+                body = receive_.get('body')
+                if len(body) != content_length:
+                    return JSONResponse(
+                        content={"error": "Invalid Content-Length header"},
+                        status_code=400,
+                    )
+                json_byte_string = gzip.decompress(body)
+                receive_['body'] = json_byte_string
+                if (verbose):
+                    print("content_length", content_length)
+                    print("gzip decompressed body:", receive_['body'])
+            except ValueError:
+                return JSONResponse(
+                    content={"error": "Invalid Content-Length header"},
+                    status_code=400,
+                )
+            except Exception as e:
+                print(e)
+                return JSONResponse(
+                    content={"error": "Failed to decompress gzip content"},
+                    status_code=400,
+                )
+
+        async def receive() -> Message:
+            return receive_
+
+        request._receive = receive
+
+    async def dispatch(self, request, call_next):
+        await self.set_body(request)
+        response = await call_next(request)
+        return response
