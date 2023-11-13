37fd4f5bfef0955703704f76c42077a165f1f36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 15:07:14 +0000

    Add package pnzer/0.0.0a0/pnzer-0.0.0a0.tar.gz

---
+++ packages/pnzer/pnzer-0.0.0a0.tar.gz/pnzer-0.0.0a0/pnzer/__version__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'pnzer'
+__license__ = 'MIT'
+__version__ = '0.0.0a'
+__author__ = 'Hans Park'
+__contact__ = 'ianolpx@gmail.com'
+__url__ = 'https://github.com/ianolpx/pnzer'
+__description__ = (
+    '')
