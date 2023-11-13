a21945c13cc586536bf41501bdf9977143453b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-13 16:07:01 +0000

    Add package testauskas1123/0.4.0/testauskas1123-0.4.0.tar.gz

---
+++ packages/testauskas1123/testauskas1123-0.4.0.tar.gz/testauskas1123-0.4.0/setup.py
@@ -0,0 +1,111 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testauskas1123',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies go here
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script_name = your_package_name.module_name:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your project',
+    long_description="""<p align="center">
+    <a href="https://smartproxy.com/"><img src="https://images.prismic.io/smartproxy/400e44d6-09ea-4168-a848-ed0e7cfb442a_sp-logo-dark-vertical.png" alt="Smartproxy logo" width="200"></a>
+  </a>
+</p>
+
+[<img src="https://i.ibb.co/S6ytt21/github-banner.png">](https://dashboard.smartproxy.com/register?coupon=SMARTHUB20&utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=banner)
+
+<h2 align="center">
+  The Best Residential Proxy Network
+</h2>
+
+<p align="center">
+High quality residential HTTP(S) rotating proxies with a 40M+ unique IPs all over the globe.
+</p>
+
+## List of contents
+
+- [What is smartproxy](#what-is-smartproxy)
+- [How does it work](#how-does-it-work)
+- [Getting started](#getting-started)
+- [Integrations](#integrations)
+- [License](#license)
+- [Contacts](#contacts)
+ 
+## What is smartproxy
+[Smartproxy](https://smartproxy.com/) is a rotating residential proxy network which enables users to gather any data from the web using a pool of over 40 million proxies.
+
+We provide rotating proxies which change with each new request or sticky IP sessions to keep your session for a longer period (up to 30 minutes). How does it work? [Check here](https://smartproxy.com/questions/how-does-it-work?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink)
+
+Need Datacenter proxies? [Smartproxy offers blazing fast shared proxy pool!](https://smartproxy.com/proxies/shared-proxies?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink) If speed is your priority, datacenter proxies should be your go-to.
+
+Don't like sharing your IPs with someone else? [Consider getting dedicated ones instead!](https://smartproxy.com/proxies/dedicated-datacenter-proxies?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink) These proxies do not rotate and are yours to keep for as long as you prefer! Oh, and the traffic is completely unlimited.
+
+## How does it work
+
+Using smartproxy is fairly simple - in the dashboard you will be able to select a session type: **rotating** or **sticky** and whether you want IPs from a **random location** or a **specific country/city**. Our system will generate relevant endpoint in domain:port format for you. If you are curious, we have prepared a [quick start guide](https://smartproxy.com/smartproxy-quick-start-guide?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink).
+
+We offer 2 authenitcation options – **IP whitelist** and **username:password**. IP whitelisting allows you to add your IP address to our system in order to use our proxies without the need to authenticate and username:password one requires you to use the your unique credentials in order to access our proxy pool.
+  
+## Getting started
+<br>[<img src="https://i.imgur.com/V2NhawG.png">](https://dashboard.smartproxy.com/register?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=image)
+<br> 1. [Register your smartproxy account](https://dashboard.smartproxy.com/register?coupon=SMARTHUB20&utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink)
+<br> 2. [Pick a plan](https://smartproxy.com/proxies/residential-proxies/pricing?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink)
+<br> 3. Make a purchase
+<br> 4. 40M+ residential IPs in your pocket
+<br><br>Accepted payment methods:
+<br>[<img src="https://i.ibb.co/cY4Xqm0/github-payments.png" alt="Smartproxy accepted payment methods">](https://smartproxy.com/proxies/residential-proxies/pricing?utm_source=github&utm_medium=referral&utm_campaign=repository&utm_content=hyperlink)
+
+## Integrations
+
+- [Beautiful Soup](https://github.com/Smartproxy/BeautifulSoup)
+- [C#](https://github.com/Smartproxy/Smartproxy/tree/master/csharp)
+- [Cheerio.js](https://github.com/Smartproxy/Cheerio)
+- [Golang](https://github.com/Smartproxy/Smartproxy/tree/master/golang)
+- [Java](https://github.com/Smartproxy/Smartproxy/tree/master/java)
+- [Nightmare.js](https://github.com/Smartproxy/Nightmare)
+- [Node.js](https://github.com/Smartproxy/Smartproxy/tree/master/nodejs)
+- [PHP](https://github.com/Smartproxy/Smartproxy/tree/master/php)
+- [Puppeteer.js](https://github.com/Smartproxy/Puppeteer)
+- [Python](https://github.com/Smartproxy/Smartproxy/tree/master/python)
+- [Ruby](https://github.com/Smartproxy/Smartproxy/tree/master/ruby)
+- [Scrapy](https://github.com/Smartproxy/Scrapy)
+- [Scrapy Middleware](https://github.com/Smartproxy/Scrapy-Middleware)
+- [Selenium](https://github.com/Smartproxy/Selenium)
+- [Bash](https://github.com/Smartproxy/Smartproxy/tree/master/shell)
+
+## License
+
+All code is released under [MIT License](https://github.com/Smartproxy/Smartproxy/blob/master/LICENSE)
+
+## Contacts
+Email - sales@smartproxy.com
+<br><a href="https://direct.lc.chat/12092754/">Live chat 24/7</a>""",
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/your_package_name',
+    project_urls={
+        'Documentation': 'https://your-package-name.readthedocs.io',
+        'Source': 'https://github.com/your_username/your_package_name',
+        'Bug Reports': 'https://github.com/your_username/your_package_name/issues',
+    },
+    keywords='your, keywords, here',
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.6',
+    extras_require={
+        'dev': [
+            # development dependencies go here
+        ],
+        'test': [
+            # testing dependencies go here
+        ],
+    },
+)
