93ce262264527393d5a4a12a3378e1965e48281d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:05:16 +0000

    Add package ai-server-sdk/0.0.12/ai-server-sdk-0.0.12.tar.gz

---
+++ packages/ai-server-sdk/ai-server-sdk-0.0.12.tar.gz/ai-server-sdk-0.0.12/ai_server/py_client/__init__.py
@@ -0,0 +1,10 @@
+# register all the clients in the init
+__version__ = "0.0.6"
+
+from .server_resources import (
+    RESTServer,
+    Driver,
+    ModelEngine,
+    StorageEngine,
+    DatabaseEngine,
+)
---
+++ packages/ai-server-sdk/ai-server-sdk-0.0.12.tar.gz/ai-server-sdk-0.0.12/ai_server/py_client/server_resources/__init__.py
@@ -0,0 +1,5 @@
+from .gaas_rest_server import RESTServer
+from .gaas_gpt_driver import Driver
+from .gaas_gpt_model import ModelEngine
+from .gaas_gpt_storage import StorageEngine
+from .gaas_gpt_database import DatabaseEngine
