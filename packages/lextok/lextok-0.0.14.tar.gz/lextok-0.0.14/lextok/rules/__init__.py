91fbd2af1fe743e45fd03f6065dbbedaf8d39c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:06:39 +0000

    Add package lextok/0.0.14/lextok-0.0.14.tar.gz

---
+++ packages/lextok/lextok-0.0.14.tar.gz/lextok-0.0.14/lextok/rules/__init__.py
@@ -0,0 +1,100 @@
+from ._pattern import (
+    CM,
+    CODE,
+    CONNECTOR,
+    COURT,
+    OF,
+    OF_THE_PH_,
+    PROV_DIGITS,
+    TH,
+    THE,
+    VS,
+    DigitLists,
+    Label,
+    Rule,
+    _orth_in,
+    _re,
+    camel_case_pattern,
+    lower_words,
+    name_code,
+    name_court,
+    name_statute,
+    titled_words,
+    uncamel,
+)
+from .abbv import (
+    ENT_CASE_NAME,
+    ENT_CUSTOM_DATE,
+    ENT_GENERIC_DOC,
+    ENT_GENERIC_ORG,
+    Abbv,
+    Prov,
+)
+from .builder import Duo, Style
+from .cites import (
+    ENT_COURT_NAME,
+    ENT_DOCKET_NUM,
+    ENT_PROVISION_NUM,
+    ENT_REPORTER_NUM,
+    ENT_SERIAL_NUM,
+    ENT_STATUTE_NUM,
+    SPAN_CITE,
+    SPAN_SER,
+    SPAN_STAT,
+    CourtName,
+    DocketNum,
+    ProvisionNum,
+    ReporterNum,
+    StatuteNum,
+)
+from .pretest import pretest_entities
+
+SPAN_RULES = [
+    SPAN_SER,
+    SPAN_CITE,
+    SPAN_STAT,
+]
+ENTITY_RULES = [
+    ENT_CASE_NAME,
+    ENT_COURT_NAME,
+    ENT_CUSTOM_DATE,
+    ENT_DOCKET_NUM,
+    ENT_PROVISION_NUM,
+    ENT_REPORTER_NUM,
+    ENT_SERIAL_NUM,
+    ENT_STATUTE_NUM,
+    ENT_GENERIC_ORG,
+    ENT_GENERIC_DOC,
+]
+
+
+LABELS_BUILT_IN = [
+    Label.GPE,
+    Label.ORG,
+    Label.LAW,
+]
+LABELS_STATUTORY = [
+    Label.ProvisionNum,
+    Label.StatuteNamed,
+    Label.StatuteNum,
+]
+LABELS_CITATION = [
+    Label.CaseName,
+    Label.DocketNum,
+    Label.ReporterNum,
+]
+LABELS_GENERIC = [
+    Label.SerialNum,
+    Label.Personality,
+    Label.Document,
+]
+
+EXT_ENTS = LABELS_BUILT_IN + LABELS_STATUTORY + LABELS_CITATION + LABELS_GENERIC
+"""A collection of _entity-based_ labels that will be extended by the Detector component"""
+
+EXT_SPANS = [
+    Label.StatutoryProvision,
+    Label.DecisionCitation,
+    Label.SerialDocument,
+]
+"""A collection of _span-ruler-based_ labels that will be extended by the Detector component"""
