2613179d5c12e342d141c9ff3acc70ad29d4f0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:53 +0000

    Add package umls-rat/0.6.8/umls_rat-0.6.8-py3-none-any.whl

---
+++ packages/umls-rat/umls_rat-0.6.8-py3-none-any.whl/umlsrat/lookup/lookup_desc.py
@@ -0,0 +1,149 @@
+from typing import List, Callable
+
+from umlsrat.api.metathesaurus import MetaThesaurus
+from umlsrat.lookup import lookup_umls
+from umlsrat.util import text
+from umlsrat.util.orderedset import UniqueFIFO
+
+
+def _get_norm_fn(normalize: bool) -> Callable[[str], str]:
+    if normalize:
+        return text.normalize
+    else:
+        return text.identity
+
+
+def _append_cui_descriptions(
+    api: MetaThesaurus,
+    cui: str,
+    syn_names: UniqueFIFO,
+    txt_norm: Callable[[str], str],
+    sabs: str,
+) -> List[str]:
+    def push_name(name: str) -> None:
+        syn_names.push(txt_norm(name))
+
+    for atom in api.get_atoms_for_cui(cui, sabs=sabs):
+        push_name(atom.get("name"))
+
+    return syn_names.items
+
+
+def get_synonyms(
+    api: MetaThesaurus,
+    cui: str,
+    language: str = "ENG",
+    normalize: bool = False,
+) -> List[str]:
+    """
+    Find unique, synonymous concept names. Return order is determined by the API. Uniqueness is determined by
+    case-insensitive exact string match or by the normalized form if ``normalize=True``.
+
+    >>> from umlsrat.api.metathesaurus import MetaThesaurus
+    >>> from umlsrat.lookup.lookup_desc import get_synonyms
+    >>> get_synonyms(MetaThesaurus(), "C0034500")
+
+    .. code-block:: js
+
+        [
+            "raccoon",
+            "Procyon",
+            "Procyon, NOS",
+            "raccoons",
+            "Procyons",
+            "Genus Procyon",
+            "Genus Procyon (organism)",
+        ]
+
+
+    :param api: MetaThesaurus
+    :param cui: CUI
+    :param language: target language
+    :param normalize: normalize names
+    :return: list of names for this concept
+    """
+    # get source vocab abbreviations for language
+    sabs = api.get_sabs_str(language)
+    # create name queue
+    syn_names = UniqueFIFO(keyfn=str.lower)
+    # get text norm function
+    txt_norm = _get_norm_fn(normalize)
+    _append_cui_descriptions(
+        api=api, cui=cui, syn_names=syn_names, txt_norm=txt_norm, sabs=sabs
+    )
+    return syn_names.items
+
+
+def find_synonyms(
+    api: MetaThesaurus,
+    source_vocab: str,
+    concept_id: str,
+    language: str = "ENG",
+    normalize: bool = False,
+) -> List[str]:
+    """
+    Find unique, synonymous concept names given a source concept. Return order is determined by
+    the API. Uniqueness is determined by case-insensitive exact string match or by the normalized
+    form if ``normalize=True``.
+
+    >>> from umlsrat.api.metathesaurus import MetaThesaurus
+    >>> from umlsrat.lookup.lookup_desc import find_synonyms
+    >>> find_synonyms(MetaThesaurus(), "ICD10CM", "T87.44")
+
+    .. code-block:: js
+
+        [
+          "Infection of amputation stump, left lower extremity",
+          "infection of amputation stump of left lower extremity",
+          "infection of amputation stump of left lower extremity (diagnosis)",
+          "Infection of amputation stump of left lower limb",
+          "Infection of amputation stump of left leg",
+          "Infection of amputation stump of left lower limb (disorder)"
+        ]
+
+    With normalization.
+
+    >>> find_synonyms(MetaThesaurus(), "ICD10CM", "T87.44", normalize=True)
+
+    .. code-block:: js
+
+        [
+          "infection of amputation stump left lower extremity",
+          "infection of amputation stump of left lower extremity",
+          "infection of amputation stump of left lower limb",
+          "infection of amputation stump of left leg"
+        ]
+
+    :param api: MetaThesaurus
+    :param source_vocab: source vocabulary e.g. ICD10CM
+    :param concept_id: concept ID in the source vocab
+    :param language: target language
+    :param normalize: normalize names
+    :return: list of names for this concept
+    """
+    # get source vocab abbreviations for language
+    sabs = api.get_sabs_str(language)
+    # create name queue
+    syn_names = UniqueFIFO(keyfn=str.lower)
+    # get text norm function
+    txt_norm = _get_norm_fn(normalize)
+
+    base_concept = api.get_source_concept(source_vocab, concept_id)
+    if not base_concept:
+        raise ValueError(f"Source concept not found {source_vocab}/{concept_id}")
+
+    if source_vocab in sabs.split(","):
+        # The name of the base concept always comes first -- provided that it is a vocab associated
+        # with the desired language.
+        syn_names.push(txt_norm(base_concept.get("name")))
+
+    for cui in lookup_umls.get_cuis_for(api, source_vocab, concept_id):
+        _append_cui_descriptions(
+            api=api,
+            cui=cui,
+            txt_norm=txt_norm,
+            syn_names=syn_names,
+            sabs=sabs,
+        )
+
+    return syn_names.items
---
+++ packages/umls-rat/umls_rat-0.6.8-py3-none-any.whl/umlsrat/util/text.py
@@ -0,0 +1,96 @@
+import re
+from typing import List, Callable, Iterable
+
+_ENCLOSING_CITATION = {")": "(", "]": "[", ">": "<"}
+
+
+def remove_trailing_parens(text: str) -> str:
+    # remove trailing parentheses e.g. "Room air (substance)" or "foo bar (klopfer: 2022)"
+    close_char = text[-1]
+    if close_char in _ENCLOSING_CITATION:
+        stack = [close_char]
+        open_char = _ENCLOSING_CITATION.get(close_char)
+        i = len(text) - 2
+        while i >= 0:
+            cur_char = text[i]
+            if cur_char == open_char:
+                stack.pop()
+                if not stack:
+                    break
+            elif cur_char == close_char:
+                stack.append(close_char)
+
+            i -= 1
+        if i > 0:
+            return text[:i]
+
+    return text
+
+
+def clean_definition_text(text: str) -> str:
+    clean = text.strip()
+    if not clean:
+        return clean
+    # clean trailing citation
+    clean = remove_trailing_parens(clean)
+    # xml tags
+    clean = re.sub(r"<[^<]+?>", "", clean)
+
+    return clean.strip()
+
+
+_NOS_PAT = re.compile(r",?\s*NOS$")
+_PUNCT_PAT = re.compile(r"\s*[,;:?_{|}.=()\[\]!/<>\"\\~-]\s*")
+
+
+def normalize(text_str: str) -> str:
+    """Remove common metadata strings, remove punctuation, lowercase, remove redundant whitespace"""
+    # remove NOS (SNOMED ; Not Otherwise Specified)
+    normalized = _NOS_PAT.sub("", text_str)
+    normalized = clean_definition_text(normalized.lower())
+    normalized = _PUNCT_PAT.sub(" ", normalized)
+    normalized = re.sub(r"\s+", " ", normalized)
+    return normalized.strip()
+
+
+_STOP_WORDS = {"of", "the", "by"}
+
+
+def tokenize(text_str: str) -> List[str]:
+    """Tokenize string"""
+    return [_ for _ in text_str.split() if _ and _ not in _STOP_WORDS]
+
+
+def norm_tokenize(text_str: str) -> List[str]:
+    """Normalize string, then tokenize"""
+    return tokenize(normalize(text_str))
+
+
+def _hammingish(source: Iterable[str], target: Iterable[str]) -> float:
+    """This distance metric favors shorter 'target' sequences"""
+    ss, st = set(source), set(target)
+
+    dist = len(st - ss) / len(st)
+    return dist
+
+
+def hammingish(source: List[str], target: List[str]) -> float:
+    return max(_hammingish(source, target), _hammingish(target, source))
+
+
+def hammingish_str(source: str, target: str) -> float:
+    return hammingish(norm_tokenize(source), norm_tokenize(target))
+
+
+def hammingish_partial(source_txt: str) -> Callable[[str], float]:
+    source = norm_tokenize(source_txt)
+
+    def sort_key(target_txt: str) -> float:
+        target = norm_tokenize(target_txt)
+        return hammingish(source, target)
+
+    return sort_key
+
+
+def identity(text: str) -> str:
+    return text
