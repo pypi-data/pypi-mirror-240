5149c8a424a4ac55629cbe09201ceb7760896425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:04:58 +0000

    Add package python-semantic-retrieval/0.1.3/python_semantic_retrieval-0.1.3-py3-none-any.whl

---
+++ packages/python-semantic-retrieval/python_semantic_retrieval-0.1.3-py3-none-any.whl/semantic_retrieval/prompts/prompt_template.py
@@ -0,0 +1,19 @@
+from typing import Dict, Optional
+from semantic_retrieval.prompts.prompt import IPrompt
+
+PromptTemplateParameters = Dict[str, str]
+
+
+class PromptTemplate(IPrompt):
+    def __init__(self, template: str, params: PromptTemplateParameters):
+        self.template = template
+        self.parameters = params
+
+    def set_parameters(self, params: PromptTemplateParameters):
+        self.parameters = params
+
+    def resolve_template(self, parameters: Optional[PromptTemplateParameters] = None):
+        return ""
+
+    async def to_string(self) -> str:
+        return self.resolve_template()
