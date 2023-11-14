aa6b0626ad9c436be1fe07623bf16ab359aebcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 16:04:54 +0000

    Add package xmi/0.0.4/xmi-0.0.4-py3-none-any.whl

---
+++ packages/xmi/xmi-0.0.4-py3-none-any.whl/xmi/v1/__init__.py
@@ -0,0 +1,6 @@
+from .constants import *
+from .xmi_errors import *
+from .xmi_manager import XmiManager
+from .xmi_parser import XmiFile, XmiParser
+from .xmi_base import *
+from .xmi_utilities import *
