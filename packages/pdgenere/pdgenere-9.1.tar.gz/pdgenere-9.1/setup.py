ea179bbedf0d86a697d7e067dba789890165cdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:25 +0000

    Add package pdgenere/9.1/pdgenere-9.1.tar.gz

---
+++ packages/pdgenere/pdgenere-9.1.tar.gz/pdgenere-9.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='pdgenere',
+    description='PDF dynamique',
+    packages=['pdgenere'],
+    version='9.1',
+    author='Blemy',
+    author_email='mathieublemy@01gmail.com'
+)
