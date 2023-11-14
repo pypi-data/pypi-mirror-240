9354af0573f9f88a9ba4f32022931221f6640d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:26 +0000

    Add package vstutils/5.8.12/vstutils-5.8.12-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vstutils/vstutils-5.8.12-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vstutils/static/bundle/463.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_vstconsulting_vstutils=self.webpackChunk_vstconsulting_vstutils||[]).push([[463],{40463:function(t,s,n){n.r(s),n.d(s,{bootstrap:function(){return u}});const u=n(43734);n(1437),n(86213),n(75832),n(79730),n(75593),n(84441),n(36775),n(94713),n(90482),n(36983),n(29242),window.bootstrap=u}}]);
