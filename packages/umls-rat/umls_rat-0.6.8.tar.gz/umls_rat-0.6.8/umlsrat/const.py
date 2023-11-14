a72fa8bb9ed3cd0218996e7185605e508ecb7b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:53 +0000

    Add package umls-rat/0.6.8/umls_rat-0.6.8.tar.gz

---
+++ packages/umls-rat/umls_rat-0.6.8.tar.gz/umls_rat-0.6.8/umlsrat/const.py
@@ -0,0 +1,10 @@
+UMLS_REST_ENDPOINT = "https://uts-ws.nlm.nih.gov/rest"
+
+API_KEY_ENV_VAR = "UMLS_API_KEY"
+
+# Common vocabulary name mapping
+_COMMON_VOCAB_NAMES = {
+    "SNOMED": "SNOMEDCT_US",
+    "SNOMED-CT": "SNOMEDCT_US",
+    "LOINC": "LNC",
+}
