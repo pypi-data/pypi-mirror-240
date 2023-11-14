82f94a231bcd109f9d557fd7471da7f9707506f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:41 +0000

    Add package zerohertzLib/0.1.9/zerohertzLib-0.1.9-py3-none-any.whl

---
+++ packages/zerohertzLib/zerohertzLib-0.1.9-py3-none-any.whl/zerohertzLib/plot/__init__.py
@@ -0,0 +1,14 @@
+import matplotlib.font_manager as font_manager
+from matplotlib import pyplot as plt
+
+from zerohertzLib.plot.bar import bar, barh, hist
+from zerohertzLib.plot.pie import pie
+from zerohertzLib.plot.plot import plot
+
+plt.rcParams["font.size"] = 20
+font_manager.fontManager.addfont(
+    f"{__file__.replace('__init__.py', '')}NotoSansKR-Medium.ttf"
+)
+plt.rcParams["font.family"] = "Noto Sans KR"
+
+__all__ = ["bar", "barh", "hist", "plot", "pie"]
