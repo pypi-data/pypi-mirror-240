77a22237483e3b5abf56f4d6ebbcb3c481f8e8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:06:12 +0000

    Add package ddos-dmpa-algos/0.1.0/ddos_dmpa_algos-0.1.0.tar.gz

---
+++ packages/ddos-dmpa-algos/ddos_dmpa_algos-0.1.0.tar.gz/ddos_dmpa_algos-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ddos_dmpa_algos",
+    version="0.1.0",
+    author="nugget-cloud",
+    author_email="kunalyadav06112003@gmail.com",
+    description="",
+    packages=find_packages(),
+    install_requires=[
+        'numpy' , 'matplotlib' , 'pandas'
+    ],
+)
