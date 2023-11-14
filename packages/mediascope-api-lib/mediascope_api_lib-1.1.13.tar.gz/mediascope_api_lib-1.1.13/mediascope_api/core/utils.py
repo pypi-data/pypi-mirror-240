cacc360a44200738052fe715a6cbb4c948f15304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:54 +0000

    Add package mediascope-api-lib/1.1.13/mediascope_api_lib-1.1.13.tar.gz

---
+++ packages/mediascope-api-lib/mediascope_api_lib-1.1.13.tar.gz/mediascope_api_lib-1.1.13/mediascope_api/core/utils.py
@@ -0,0 +1,67 @@
+import json
+import os
+import datetime as dt
+
+
+def load_settings(settings_filename: str = 'settings.json'):
+    """
+        Загрузить настройки из файла
+
+        Parameters
+        ----------
+
+        settings_filename : str
+            Имя файла с настройками, по умолчанию имя файла: settings.json
+        Returns
+        -------
+
+        username : str
+            Имя пользователя
+        passw : str
+            пароль пользователя
+        root_url : str
+            URL к API
+        client_id : str
+            Идентификатор клиента для доступа к API
+        client_secret : str
+            Секрет для доступа к API
+        auth_server : str
+            URL к серверу авторизации
+    """
+
+    if settings_filename is None:
+        settings_filename = 'settings.json'
+
+    with open(settings_filename) as datafile:
+        jd = json.load(datafile)
+        return jd['username'], \
+               jd['passw'], \
+               jd['root_url'], \
+               jd['client_id'], \
+               jd['client_secret'], \
+               jd['auth_server'], \
+               jd['proxy_server'] if 'proxy_server' in jd else None
+
+
+def get_excel_filename(task_name: str, export_path: str = '../excel', add_date: bool = True) -> str:
+    """
+    Формирует название Excel файла для отчета
+
+    Parameters
+    ----------
+    task_name : str
+        Название отчета/задания
+    export_path: : str
+        Путь к файл
+    add_date: str
+
+    Returns
+    -------
+    """
+    if not os.path.exists(export_path):
+        os.mkdir(export_path)
+    fname = task_name
+    if add_date:
+        fname += '-' + dt.datetime.now().strftime('%Y%m%d_%H%M%S')
+    fname += '.xlsx'
+    return os.path.join(export_path, fname)
