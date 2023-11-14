07249e2cb59c5417ec7cb24d5d0249c5c0a2b11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:55 +0000

    Add package pazer/0.0.3.dev0/pazer-0.0.3.dev0.tar.gz

---
+++ packages/pazer/pazer-0.0.3.dev0.tar.gz/pazer-0.0.3.dev0/setup.py
@@ -0,0 +1,33 @@
+# Pip_Package_Practice/setup.py
+from setuptools import setup, find_packages
+from os.path import dirname, join as pjoin
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pazer',
+    license='MIT',
+    version='v0.0.3-dev0',
+    author='Ian Park',
+    author_email='ianolpx@gmail.com',
+    url='https://github.com/ianolpx/pazer',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.5",
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+    ],
+)
