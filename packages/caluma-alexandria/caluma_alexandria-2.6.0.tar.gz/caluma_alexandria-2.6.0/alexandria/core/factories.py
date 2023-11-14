d61d07e95f34b032ac8af99fa992e003ea78cc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:09:00 +0000

    Add package caluma-alexandria/2.6.0/caluma_alexandria-2.6.0.tar.gz

---
+++ packages/caluma-alexandria/caluma_alexandria-2.6.0.tar.gz/caluma_alexandria-2.6.0/alexandria/core/factories.py
@@ -0,0 +1,87 @@
+from factory import Faker, SubFactory, post_generation
+from factory.django import DjangoModelFactory
+
+from . import models
+
+
+class BaseFactory(DjangoModelFactory):
+    created_by_user = "admin"
+    created_by_group = "admin"
+    modified_by_user = "admin"
+    modified_by_group = "admin"
+
+    class Meta:
+        abstract = True
+
+
+class CategoryFactory(BaseFactory):
+    slug = Faker("slug")
+    name = Faker("name")
+    description = Faker("text")
+    color = Faker("color")
+    metainfo = {}
+
+    class Meta:
+        model = models.Category
+
+
+class TagFactory(BaseFactory):
+    slug = Faker("slug")
+    name = Faker("name")
+    description = Faker("text")
+
+    class Meta:
+        model = models.Tag
+
+
+class TagSynonymGroupFactory(BaseFactory):
+    class Meta:
+        model = models.TagSynonymGroup
+
+    @post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:  # pragma: todo cover
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of tags were passed in, use them
+            for tag in extracted:
+                self.tags.add(tag)
+
+
+class DocumentFactory(BaseFactory):
+    class Meta:
+        model = models.Document
+
+    title = Faker("name")
+    description = Faker("text")
+    category = SubFactory(CategoryFactory)
+    date = Faker("date")
+
+    @post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:  # pragma: todo cover
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of tags were passed in, use them
+            for tag in extracted:
+                self.tags.add(tag)
+
+
+class FileFactory(BaseFactory):
+    name = Faker("name")
+    document = SubFactory(DocumentFactory)
+
+    class Meta:
+        model = models.File
+
+
+class DocumentTagsFactory(DjangoModelFactory):
+    document = SubFactory(DocumentFactory)
+    tag = SubFactory(TagFactory)
+
+    class Meta:
+        model = models.Document.tags.through
