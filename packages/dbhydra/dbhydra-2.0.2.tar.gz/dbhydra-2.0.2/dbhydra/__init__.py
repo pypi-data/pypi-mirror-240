187675292c5fff2239b0cf875eed499ba5b8f3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:04:58 +0000

    Add package dbhydra/2.0.2/dbhydra-2.0.2.tar.gz

---
+++ packages/dbhydra/dbhydra-2.0.2.tar.gz/dbhydra-2.0.2/dbhydra/__init__.py
@@ -0,0 +1,7 @@
+from dbhydra.dbhydra_core import *
+from dbhydra import src
+
+__all__=["SqlServerDb","MysqlDb","BigQueryDb","MongoDb","PostgresDb","XlsxDb","AbstractDb",
+         "SqlServerTable", "PostgresTable", "MysqlTable", "XlsxTable", "AbstractTable", "MongoTable","BigQueryTable", "Table",
+         "Jsonable","df_to_dict","dict_to_df","AbstractModel","create_table_structure_dict",
+         "XlsxDB","db","Mysqldb", "src"]
