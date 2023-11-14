95d194dfdfb72d94b714716695512fc673598e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:05:49 +0000

    Add package lextok/0.0.13/lextok-0.0.13-py3-none-any.whl

---
+++ packages/lextok/lextok-0.0.13-py3-none-any.whl/lextok/main.py
@@ -0,0 +1,79 @@
+import spacy  # type: ignore
+from spacy.language import Language
+from spacy.tokens import Doc, Span
+from spacy.util import filter_spans
+
+from .attrs import set_attribute_ruler
+from .rules import ENTITY_RULES, EXT_ENTS, EXT_SPANS, SPAN_RULES, Label, Rule
+from .spans import create_custom_entities, create_custom_spans
+from .tokens import set_tokenizer
+
+
+def lextok(
+    model: str = "en_core_web_sm",
+    finalize_entities: bool = False,
+    entity_rules: list[Rule] = ENTITY_RULES,
+    span_rules: list[Rule] = SPAN_RULES,
+) -> Language:
+    """Modifies the `model`'s tokens based on a a custom tokenizer and attribute ruler
+    then allows the inclusion of extendible `entity_rules` and `span_rules` to modify the `Doc`
+    instance. The last pipe is `detector` which creates custom attributes on each `Doc`
+    based on `Label`s detected.
+
+    Args:
+        model (str, optional): Spacy language model. Defaults to "en_core_web_sm".
+        finalize_entities (bool, optional): Whether to consider consecutive entities as a single token. Defaults to False.
+        entity_rules (list[Rule], optional): List of generic Pydantic models with serialization. Defaults to ENTITY_RULES.
+        span_rules (list[Rule], optional): List of generic Pydantic models with serialization. Defaults to SPAN_RULES.
+
+    Returns:
+        Language: A customized rule-based spacy model.
+    """
+    nlp = spacy.load(model)
+    nlp.tokenizer = set_tokenizer(nlp)
+    nlp = set_attribute_ruler(nlp)
+    create_custom_entities(nlp, rules=entity_rules, pipename="entity_ruler")
+    if finalize_entities:
+        nlp.add_pipe("merge_entities", after="entity_ruler")
+        nlp.add_pipe("provision_num_merger", after="merge_entities")
+        create_custom_spans(nlp, rules=span_rules)
+        nlp.add_pipe("detector", last=True)
+    return nlp
+
+
+@Language.component("provision_num_merger")
+def merge_provisions(doc: Doc) -> Doc:
+    """Consercutive ProvisionNum entities merged as a single ProvisionNum token / entity."""
+    pairs = [(e.start, e.end) for e in doc.ents if e.label_ == Label.ProvisionNum.name]
+    pair = None
+    new_ents = []
+    for ent in doc.ents:
+        if ent.label_ == Label.ProvisionNum.name:
+            s = ent.start
+            if pair and s == pair[1]:
+                s = pair[0]
+            pair = (s, ent.end)
+        if pair and pair not in pairs:
+            new_ents.append(
+                Span(doc=doc, start=pair[0], end=pair[1], label=Label.ProvisionNum.name)
+            )
+    if new_ents:
+        doc.ents = filter_spans(new_ents + list(doc.ents))
+    return doc
+
+
+@Language.factory("detector")
+class DetectorComponent:
+    """Detect each doc._.<value> dynamically based on `Label` objects."""
+
+    def __init__(self, nlp, name):
+        for label in EXT_ENTS + EXT_SPANS:
+            Doc.set_extension(label.snakecase, default=[])
+
+    def __call__(self, doc):
+        for label in EXT_ENTS:
+            doc._.set(label.snakecase, label.get_entities(doc))
+
+        for label in EXT_SPANS:
+            doc._.set(label.snakecase, label.get_spans(doc))
+        return doc
