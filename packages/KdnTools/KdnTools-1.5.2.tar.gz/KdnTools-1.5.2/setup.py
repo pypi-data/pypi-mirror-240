a824bf0afd258917969474d29790b0902d8333c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:05:55 +0000

    Add package KdnTools/1.5.2/KdnTools-1.5.2.tar.gz

---
+++ packages/KdnTools/KdnTools-1.5.2.tar.gz/KdnTools-1.5.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='KdnTools',
+    version='1.5.2',
+    description="Useful tools for every project.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KdntNinja/Code/tree/main/Python/KdnTools",
+    author="KdntNinja",
+    project_urls={
+        "Source": "https://github.com/KdntNinja/Code/tree/main/Python/KdnTools"
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10,<3.12",
+    install_requires=["prettytable", "colorama", "requests", "matplotlib", "numpy", "perlin_noise", "numba", "ursina"],
+    packages=find_packages(),
+    include_package_data=True,
+)
