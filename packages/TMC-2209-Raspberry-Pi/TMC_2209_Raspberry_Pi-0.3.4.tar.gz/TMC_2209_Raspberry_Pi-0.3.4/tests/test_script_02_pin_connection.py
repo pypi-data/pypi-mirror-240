e67e0a6e8b214fc47d544c5a3204cfda295b4fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:57 +0000

    Add package TMC-2209-Raspberry-Pi/0.3.4/TMC_2209_Raspberry_Pi-0.3.4.tar.gz

---
+++ packages/TMC-2209-Raspberry-Pi/TMC_2209_Raspberry_Pi-0.3.4.tar.gz/TMC_2209_Raspberry_Pi-0.3.4/tests/test_script_02_pin_connection.py
@@ -0,0 +1,91 @@
+#pylint: disable=wildcard-import
+#pylint: disable=unused-wildcard-import
+#pylint: disable=unused-import
+#pylint: disable=duplicate-code
+"""
+test file for testing the STEP, DIR, EN connection
+"""
+
+import time
+from src.TMC_2209.TMC_2209_StepperDriver import *
+
+
+print("---")
+print("SCRIPT START")
+print("---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# initiate the TMC_2209 class
+# use your pins for pin_en, pin_step, pin_dir here
+#-----------------------------------------------------------------------
+tmc = TMC_2209(21, 16, 20)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the loglevel of the libary (currently only printed)
+# set whether the movement should be relative or absolute
+# both optional
+#-----------------------------------------------------------------------
+tmc.set_loglevel(Loglevel.DEBUG)
+tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions change settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.set_direction_reg(False)
+tmc.set_current(300)
+tmc.set_interpolation(True)
+tmc.set_spreadcycle(False)
+tmc.set_microstepping_resolution(2)
+tmc.set_internal_rsense(False)
+
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# this function test whether the connection of the DIR, STEP and EN pin
+# between Raspberry Pi and TMC driver is working
+#-----------------------------------------------------------------------
+tmc.test_dir_step_en()
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deactivate the motor current output
+#-----------------------------------------------------------------------
+tmc.set_motor_enabled(False)
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deinitiate the TMC_2209 class
+#-----------------------------------------------------------------------
+tmc.deinit()
+del tmc
+
+print("---")
+print("SCRIPT FINISHED")
+print("---")
