6281a39cec16935f3aa07a61b4698d192b205ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:06:05 +0000

    Add package psrx/1.2/psrx-1.2.tar.gz

---
+++ packages/psrx/psrx-1.2.tar.gz/psrx-1.2/srx_classes/main_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import cli_loop
+
+
+
+
+
+
+def main():
+
+    #forward to cli mode
+
+    cli_loop.main_cli_loop()
+    
+
+    
+
+if __name__ == "__main__":
+    main()
+
