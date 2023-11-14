20ca181593567ea1cff5d145d1182758bfa55ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:20 +0000

    Add package feature-center/0.1/feature_center-0.1.tar.gz

---
+++ packages/feature-center/feature_center-0.1.tar.gz/feature_center-0.1/feature_center/__init__.py
@@ -0,0 +1,13 @@
+from feature_center.downloader.downloader import Downloader, DownloaderError, logger
+
+def start():
+    print("import successful")
+
+def load_data(data_name, local_path="."):
+    data_loader = Downloader(data_name, local_path, "gunir_sata,gunir")
+    return data_loader.get_data_obj()
+
+def download(data_name, local_path="."):
+    data_loader = Downloader(data_name, local_path)
+    data_loader.download()
+    return 
---
+++ packages/feature-center/feature_center-0.1.tar.gz/feature_center-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup
+setup(
+    name='feature_center',
+    version='0.1',
+    description='百度搜索架构部效率平台sdp特征中心数据加载模块',
+    url='http://icode.baidu.com/',
+    author='sunyutong01',
+    author_email='sunyutong01@baidu.com',
+    license='PRIVATE',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    zip_safe=False
+)
