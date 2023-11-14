43cbadeebb6689f4f35a3c3ed5e52e1f198896d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:05:14 +0000

    Add package django-pgpubsub/1.1.2/django_pgpubsub-1.1.2.tar.gz

---
+++ packages/django-pgpubsub/django_pgpubsub-1.1.2.tar.gz/django_pgpubsub-1.1.2/pgpubsub/management/commands/listen.py
@@ -0,0 +1,44 @@
+import multiprocessing
+
+from django.core.management import BaseCommand
+from django.db import connection
+
+from pgpubsub.listen import listen
+
+
+class Command(BaseCommand):
+    help = 'Listen to the named postgres channel(s) for notifications.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--channels',
+            type=str,
+            dest='channels',
+            nargs='+',
+        )
+        parser.add_argument(
+            '--processes',
+            type=int,
+            dest='processes',
+        )
+        parser.add_argument(
+            '--recover',
+            action='store_true',
+            dest='recover',
+            default=False,
+            help='Process all stored notifications for selected channels.',
+        )
+
+    def handle(self, *args, **options):
+        channel_names = options.get('channels')
+        processes = options.get('processes') or 1
+        recover = options.get('recover', False)
+        multiprocessing.set_start_method('fork', force=True)
+        connection.close()
+        for i in range(processes):
+            process = multiprocessing.Process(
+                name=f'pgpubsub_process_{i}',
+                target=listen,
+                args=(channel_names, recover),
+            )
+            process.start()
