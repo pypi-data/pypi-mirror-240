ebac66d86f922161c907dff4bae0ad2bf07e8ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:46 +0000

    Add package tum_esm_utils/1.6.3/tum_esm_utils-1.6.3.tar.gz

---
+++ packages/tum_esm_utils/tum_esm_utils-1.6.3.tar.gz/tum_esm_utils-1.6.3/tum_esm_utils/testing.py
@@ -0,0 +1,44 @@
+"""Functions commonly used in testing scripts.
+
+Implements: `expect_file_contents`, `wait_for_condition`"""
+
+from __future__ import annotations
+from typing import Callable
+import time
+
+
+def expect_file_contents(
+    filepath: str,
+    required_content_blocks: list[str] = [],
+    forbidden_content_blocks: list[str] = [],
+) -> None:
+    """Assert that the given file contains all of the required content
+    blocks, and/or none of the forbidden content blocks."""
+
+    with open(filepath, "r") as f:
+        file_content = f.read()
+
+    for b in required_content_blocks:
+        assert b in file_content, f'required log content block not found "{b}"'
+
+    for b in forbidden_content_blocks:
+        assert b not in file_content, f'forbidden log content block found "{b}"'
+
+
+def wait_for_condition(
+    is_successful: Callable[[], bool],
+    timeout_message: str,
+    timeout_seconds: float = 5,
+) -> None:
+    """Wait for the given condition to be true, or raise a TimeoutError
+    if the condition is not met within the given timeout.
+
+    The condition is checked every 0.25 seconds."""
+
+    start_time = time.time()
+    while True:
+        if is_successful():
+            break
+        if (time.time() - start_time) > timeout_seconds:
+            raise TimeoutError(timeout_message)
+        time.sleep(0.25)
