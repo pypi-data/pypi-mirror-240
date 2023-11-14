3dc592aacac4324dbc3fe805826b458e67bb2c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:04:54 +0000

    Add package sllllp/0.1.0/sllllp-0.1.0.tar.gz

---
+++ packages/sllllp/sllllp-0.1.0.tar.gz/sllllp-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sllllp', # The name of your package
+    version='0.1.0', # The version of your package
+    description='A library for single layer perceptron', # A short description of your package
+    url='https://github.com/your-username/sllp', # The URL of your project's homepage
+    author='Your Name', # Your name
+    author_email='your.email@example.com', # Your email
+    license='MIT', # The license of your package
+    packages=['sllllp'], # The packages that are included in your package
+    install_requires=[], # The dependencies of your package
+    classifiers=[ # The classifiers that describe your package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
