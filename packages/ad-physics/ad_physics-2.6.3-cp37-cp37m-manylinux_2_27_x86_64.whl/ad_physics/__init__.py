d780997b27eef957d2cabb1a33339d5f156b919d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:10:34 +0000

    Add package ad-physics/2.6.3/ad_physics-2.6.3-cp37-cp37m-manylinux_2_27_x86_64.whl

---
+++ packages/ad-physics/ad_physics-2.6.3-cp37-cp37m-manylinux_2_27_x86_64.whl/ad_physics/__init__.py
@@ -0,0 +1,17 @@
+# ----------------- BEGIN LICENSE BLOCK ---------------------------------
+#
+# Copyright (c) 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+# ----------------- END LICENSE BLOCK -----------------------------------
+
+"""
+Import ad_physics module for python37
+"""
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__))
+
+from ad_physics_python37 import *
