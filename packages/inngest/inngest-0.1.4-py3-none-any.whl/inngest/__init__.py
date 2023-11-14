3d4914ea48bcd478e886597aa3545609e79350fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 06:05:47 +0000

    Add package inngest/0.1.4/inngest-0.1.4-py3-none-any.whl

---
+++ packages/inngest/inngest-0.1.4-py3-none-any.whl/inngest/__init__.py
@@ -0,0 +1,37 @@
+"""Public entrypoint for the Inngest SDK."""
+
+
+from ._internal.client_lib import Inngest
+from ._internal.errors import NonRetriableError
+from ._internal.event_lib import Event
+from ._internal.function import Function, create_function
+from ._internal.function_config import (
+    Batch,
+    Cancel,
+    Debounce,
+    RateLimit,
+    Throttle,
+    TriggerCron,
+    TriggerEvent,
+)
+from ._internal.step_lib import Step, StepSync
+from ._internal.types import Logger, Serializable
+
+__all__ = [
+    "Batch",
+    "Cancel",
+    "Debounce",
+    "Event",
+    "Function",
+    "Inngest",
+    "Logger",
+    "NonRetriableError",
+    "RateLimit",
+    "Serializable",
+    "Step",
+    "StepSync",
+    "Throttle",
+    "TriggerCron",
+    "TriggerEvent",
+    "create_function",
+]
---
+++ packages/inngest/inngest-0.1.4-py3-none-any.whl/inngest/_internal/types.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import logging
+import typing
+
+import pydantic
+
+T = typing.TypeVar("T")
+
+EmptySentinel = object()
+
+Serializable = (
+    bool
+    | float
+    | int
+    | str
+    | dict[typing.Any, typing.Any]
+    | list[typing.Any]
+    | tuple[typing.Any, ...]
+    | None
+)
+SerializableT = typing.TypeVar("SerializableT", bound=Serializable)
+
+
+class BaseModel(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(strict=True)
+
+    def __init__(
+        __pydantic_self__,  # noqa: N805
+        *args: object,
+        **kwargs: object,
+    ) -> None:
+        try:
+            super().__init__(*args, **kwargs)
+        except pydantic.ValidationError as err:
+            raise __pydantic_self__.convert_validation_error(err) from None
+
+    def convert_validation_error(
+        self,
+        err: pydantic.ValidationError,
+    ) -> BaseException:
+        """
+        Override this method in subclasses to convert Pydantic's
+        ValidationError into a different error.
+        """
+        return err
+
+    @classmethod
+    def from_dict(
+        cls: type[BaseModelT],
+        raw: dict[str, object],
+    ) -> BaseModelT:
+        return cls.model_validate(raw)
+
+    def to_dict(self) -> MaybeError[dict[str, object]]:
+        try:
+            return self.model_dump(mode="json")
+        except Exception as err:
+            return err
+
+
+BaseModelT = typing.TypeVar("BaseModelT", bound=BaseModel)
+
+Logger: typing.TypeAlias = logging.Logger | logging.LoggerAdapter
+
+MaybeError: typing.TypeAlias = T | Exception
