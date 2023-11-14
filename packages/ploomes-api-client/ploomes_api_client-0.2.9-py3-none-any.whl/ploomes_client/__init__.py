bb97e6270f6ec7f6a278512e8448f5ef52e8a3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:13 +0000

    Add package ploomes-api-client/0.2.9/ploomes_api_client-0.2.9-py3-none-any.whl

---
+++ packages/ploomes-api-client/ploomes_api_client-0.2.9-py3-none-any.whl/ploomes_client/__init__.py
@@ -0,0 +1,11 @@
+from .core.ploomes_client import PloomesClient
+from .collections import (
+    Account,
+    Cities,
+    Contacts,
+    Deals,
+    Fields,
+    InteractionRecords,
+    Products,
+    Tasks,
+)
