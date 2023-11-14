e29bcf98f8d646a48c7b97e8da1f97d6f77f0562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:06:45 +0000

    Add package lkinterface/0.1/lkinterface-0.1.tar.gz

---
+++ packages/lkinterface/lkinterface-0.1.tar.gz/lkinterface-0.1/lkinterface/dbProviderInterface.py
@@ -0,0 +1,22 @@
+import abc
+from typing import List, Dict, Any
+
+
+class DBProviderInterface(abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get') and 
+                callable(subclass.getData) and 
+                hasattr(subclass, 'save') and 
+                callable(subclass.putData) or 
+                NotImplemented)
+
+    @abc.abstractmethod
+    def get(self, tblName: str, columns: List[str]):
+        """Load in the data set"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def save(self, data: Dict[str, Any]):
+        """Extract text from the data set"""
+        raise NotImplementedError
