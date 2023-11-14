4512826a86fae4b4fd0b056b949552d82d17770a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:06:00 +0000

    Add package paperpilot-common/0.3.2/paperpilot_common-0.3.2.tar.gz

---
+++ packages/paperpilot-common/paperpilot_common-0.3.2.tar.gz/paperpilot_common-0.3.2/paperpilot_common/utils/db/__init__.py
@@ -0,0 +1,30 @@
+from django.db import connections
+
+
+def close_old_connections():
+    for conn in connections.all():
+        conn.close_if_unusable_or_obsolete()
+
+
+def db_sync(func):
+    """数据库连接关闭装饰器"""
+
+    def func_wrapper(*args):
+        close_old_connections()
+        result = func(*args)
+        close_old_connections()
+        return result
+
+    return func_wrapper
+
+
+def db(func):
+    """数据库连接关闭装饰器"""
+
+    async def func_wrapper(*args):
+        close_old_connections()
+        result = await func(*args)
+        close_old_connections()
+        return result
+
+    return func_wrapper
