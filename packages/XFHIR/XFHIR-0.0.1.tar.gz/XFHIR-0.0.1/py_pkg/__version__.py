757b6fa8906b9fb28764bf35d38a220d33164292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:37 +0000

    Add package XFHIR/0.0.1/XFHIR-0.0.1.tar.gz

---
+++ packages/XFHIR/XFHIR-0.0.1.tar.gz/XFHIR-0.0.1/py_pkg/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'XFHIR'
+__description__ = 'Python SDK for XFHIR'
+__version__ = '0.0.1'
+__author__ = 'Akello Health'
+__author_email__ = 'vijay@akellohealth.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/XFHIR/XFHIR'
