be4c1351fe421d8bbc0c04a70da8b0faa2484163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:24 +0000

    Add package dspy-ai/2.0.7/dspy_ai-2.0.7-py3-none-any.whl

---
+++ packages/dspy-ai/dspy_ai-2.0.7-py3-none-any.whl/dspy/teleprompt/__init__.py
@@ -0,0 +1,7 @@
+from .teleprompt import *
+from .bootstrap import *
+from .vanilla import *
+from .random_search import *
+from .finetune import *
+from .teleprompt_optuna import *
+from .knn_fewshot import *
