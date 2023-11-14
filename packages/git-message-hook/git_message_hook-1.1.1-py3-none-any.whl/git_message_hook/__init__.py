4dc4da35c2023a0d63fedd4c2fb8fb4319a80f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 06:05:38 +0000

    Add package git-message-hook/1.1.1/git_message_hook-1.1.1-py3-none-any.whl

---
+++ packages/git-message-hook/git_message_hook-1.1.1-py3-none-any.whl/git_message_hook/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Oscar de Felice"
+__email__ = "oscar.defelice@gmail.com"
+__copyright__ = "(c) 2023 Oscar de Felice"
+
+__license__ = "MIT"
+__version__ = "{{VERSION_PLACEHOLDER}}"
+__title__ = "GIT Message Hook"
+__description__ = "Force conventional git commit messages for git repositories, by leveraging git hooks."
