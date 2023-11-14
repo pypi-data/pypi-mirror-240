70a935466e28a5b549afebeee9fba78b58ebfc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:07:16 +0000

    Add package lkinterface/0.3/lkinterface-0.3.tar.gz

---
+++ packages/lkinterface/lkinterface-0.3.tar.gz/lkinterface-0.3/lkinterface/__init__.py
@@ -0,0 +1,1 @@
+from .dbProviderInterface import DBProviderInterface
---
+++ packages/lkinterface/lkinterface-0.3.tar.gz/lkinterface-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import sys
+sys.path[0:0] = ['src/gscrud']
+
+
+setup(
+    name='lkinterface',
+    version='0.3',
+    description='LavKode interfaces',
+    long_description='LavKode interfaces',
+    author='muthugit',
+    author_email='base.muthupandian@gmail.com',
+    url='https://muthupandian.in',
+    packages=['lkinterface'],
+)
