3a9c293920f1051ebc8fb99c4b7a4692e929e87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:10:11 +0000

    Add package own-knowledge-gpt/0.0.3/own-knowledge-gpt-0.0.3.tar.gz

---
+++ packages/own-knowledge-gpt/own-knowledge-gpt-0.0.3.tar.gz/own-knowledge-gpt-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+with open("README.md", "r") as f:
+    long_description = f.read()
+with open("requirements.txt", "r") as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+setup(
+    name="own-knowledge-gpt",
+    version="0.0.3",
+    description="Custom Knowledge GPT",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://huggingface.co/myn0908",
+    author="TinVo",
+    author_email="tinprocoder0908@gmail.com",
+    keywords=["AI HACKER", "AI", "autoGPT", "Deep Learning", "LLM",
+              "Interface"],
+    top_level='app',
+
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.9"
+)
