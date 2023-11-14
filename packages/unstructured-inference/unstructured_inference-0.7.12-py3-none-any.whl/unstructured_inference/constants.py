c4713095bc6d25846e7f1eb41d5d266b2379d028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:05:02 +0000

    Add package unstructured-inference/0.7.12/unstructured_inference-0.7.12-py3-none-any.whl

---
+++ packages/unstructured-inference/unstructured_inference-0.7.12-py3-none-any.whl/unstructured_inference/constants.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class AnnotationResult(Enum):
+    IMAGE = "image"
+    PLOT = "plot"
+
+
+class Source(Enum):
+    YOLOX = "yolox"
+    DETECTRON2_ONNX = "detectron2_onnx"
+    DETECTRON2_LP = "detectron2_lp"
+    CHIPPER = "chipper"
+    CHIPPERV1 = "chipperv1"
+    CHIPPERV2 = "chipperv2"
+    PDFMINER = "pdfminer"
+    MERGED = "merged"
+    SUPER_GRADIENTS = "super-gradients"
+
+
+FULL_PAGE_REGION_THRESHOLD = 0.99
+
+# this field is defined by pytesseract/unstructured.pytesseract
+TESSERACT_TEXT_HEIGHT = "height"
