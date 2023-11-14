457349988644f3c87df1d74b0d5f9a811e05c339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:05:19 +0000

    Add package risknet/1.0.17/risknet-1.0.17.tar.gz

---
+++ packages/risknet/risknet-1.0.17.tar.gz/risknet-1.0.17/src/risknet/config/__init__.py
@@ -0,0 +1,7 @@
+from risknet.config.handlers import GeneralConfig, DataConfig, ModelConfig
+
+cfg: GeneralConfig = GeneralConfig()
+
+data_cfg: DataConfig = DataConfig()
+
+model_cfg: ModelConfig = ModelConfig()
