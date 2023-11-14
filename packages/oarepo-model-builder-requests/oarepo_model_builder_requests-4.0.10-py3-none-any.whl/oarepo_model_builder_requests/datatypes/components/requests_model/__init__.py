1ff6c2865e626e083a28ed4fbc0cde103deb2090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:10:21 +0000

    Add package oarepo-model-builder-requests/4.0.10/oarepo_model_builder_requests-4.0.10-py3-none-any.whl

---
+++ packages/oarepo-model-builder-requests/oarepo_model_builder_requests-4.0.10-py3-none-any.whl/oarepo_model_builder_requests/datatypes/components/requests_model/__init__.py
@@ -0,0 +1,9 @@
+from .parent_marshmallow import ParentMarshmallowComponent
+from .requests import RequestsComponent
+from .resolver import RecordResolverComponent
+
+__all__ = [
+    "RequestsComponent",
+    "RecordResolverComponent",
+    "ParentMarshmallowComponent",
+]
