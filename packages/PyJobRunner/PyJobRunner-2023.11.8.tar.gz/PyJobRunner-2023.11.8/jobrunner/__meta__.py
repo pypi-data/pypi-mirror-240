2477f60554f06c1b9165f077d8d395613a33b013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 06:05:32 +0000

    Add package PyJobRunner/2023.11.8/PyJobRunner-2023.11.8.tar.gz

---
+++ packages/PyJobRunner/PyJobRunner-2023.11.8.tar.gz/PyJobRunner-2023.11.8/jobrunner/__meta__.py
@@ -0,0 +1,8 @@
+"""Metadata for jobrunner"""
+
+__pkgname__ = "PyJobRunner"
+__version__ = "2023.11.8"
+__authors__ = "Akash Dhruv"
+__license__ = "Apache Software License"
+__copyright__ = "Copyright (c) Akash Dhruv 2023. All Rights Reserved."
+__description__ = "Job runner for scientific computing workloads"
