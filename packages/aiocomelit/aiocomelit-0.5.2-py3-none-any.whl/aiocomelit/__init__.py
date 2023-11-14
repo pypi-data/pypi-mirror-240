6b8cb966ae364916a4644a27e909494808c2d3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:57 +0000

    Add package aiocomelit/0.5.2/aiocomelit-0.5.2-py3-none-any.whl

---
+++ packages/aiocomelit/aiocomelit-0.5.2-py3-none-any.whl/aiocomelit/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.5.2"
+
+from .api import (
+    ComeliteSerialBridgeApi,
+    ComelitSerialBridgeObject,
+    ComelitVedoApi,
+    ComelitVedoObject,
+)
+from .exceptions import (
+    CannotAuthenticate,
+    CannotConnect,
+    CannotRetrieveData,
+    ComelitError,
+)
+
+__all__ = [
+    "ComeliteSerialBridgeApi",
+    "ComelitSerialBridgeObject",
+    "ComelitVedoApi",
+    "ComelitVedoObject",
+    "ComelitError",
+    "CannotConnect",
+    "CannotAuthenticate",
+    "CannotRetrieveData",
+]
