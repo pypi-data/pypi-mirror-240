20a2df542b3111fb4f4cc8422ee2d011ee436015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:33 +0000

    Add package Every-Solver/1.0.0/Every_Solver-1.0.0.tar.gz

---
+++ packages/Every-Solver/Every_Solver-1.0.0.tar.gz/Every_Solver-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Every_Solver',
+    version='1.0.0',
+    description='A one-file module for solving problems.',
+    author='Hashem Alsharif',
+    author_email='hashemalshar20.7@gmail.com',
+    packages=find_packages(),
+    py_modules=['re', 'math', 'sympy', 'docx'],
+)
