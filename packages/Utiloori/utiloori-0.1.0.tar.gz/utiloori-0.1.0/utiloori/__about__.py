d85e680c2dd089bd2cc67642fa1961c99da9ef86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 05:06:53 +0000

    Add package Utiloori/0.1.0/utiloori-0.1.0.tar.gz

---
+++ packages/Utiloori/utiloori-0.1.0.tar.gz/utiloori-0.1.0/utiloori/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Oori Data <info@oori.dev>
+# SPDX-License-Identifier: UNLICENSED
+
+__version__ = "0.1.0"
---
+++ packages/Utiloori/utiloori-0.1.0.tar.gz/utiloori-0.1.0/utiloori/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Oori Data <info@oori.dev>
+# SPDX-License-Identifier: UNLICENSED
+# utiloori
