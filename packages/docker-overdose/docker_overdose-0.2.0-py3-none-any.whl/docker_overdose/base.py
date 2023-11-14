30f4226d3916cf388a1abb453346d2c811be8774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 04:04:50 +0000

    Add package docker-overdose/0.2.0/docker_overdose-0.2.0-py3-none-any.whl

---
+++ packages/docker-overdose/docker_overdose-0.2.0-py3-none-any.whl/docker_overdose/base.py
@@ -0,0 +1,17 @@
+"""
+docker_overdose base module.
+
+This is the principal module of the docker_overdose project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "docker_overdose"
