504e6370e71a47522028074c23768e57d40782ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 16:04:50 +0000

    Add package packagepassword/0.0.0/packagepassword-0.0.0.tar.gz

---
+++ packages/packagepassword/packagepassword-0.0.0.tar.gz/packagepassword-0.0.0/README.md
@@ -0,0 +1,2 @@
+# generatorpass
+project to implement a random password generator.
---
+++ packages/packagepassword/packagepassword-0.0.0.tar.gz/packagepassword-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='packagepassword',
+    description='Generate random password',
+    python_requires=">=3.8",
+    author='Steeve EMMANUEL & Wisner AURELUS',
+    packages=find_packages(),
+)
