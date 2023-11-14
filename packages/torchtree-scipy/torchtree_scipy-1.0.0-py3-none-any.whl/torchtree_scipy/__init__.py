82319b17bdf6219362f32ef226823ae9c843b233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:06 +0000

    Add package torchtree-scipy/1.0.0/torchtree_scipy-1.0.0-py3-none-any.whl

---
+++ packages/torchtree-scipy/torchtree_scipy-1.0.0-py3-none-any.whl/torchtree_scipy/__init__.py
@@ -0,0 +1,9 @@
+from torchtree_scipy._version import __version__
+from torchtree_scipy.evolution.site_model import GammaSiteModel
+
+__all__ = [
+    "__version__",
+    "GammaSiteModel",
+]
+
+__plugin__ = "cli.SciPy"
