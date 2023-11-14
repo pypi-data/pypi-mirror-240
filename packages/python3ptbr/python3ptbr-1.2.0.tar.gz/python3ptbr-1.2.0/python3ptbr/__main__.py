36a89664f7fb8552c7ca847e1b001a087ab29964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:05:12 +0000

    Add package python3ptbr/1.2.0/python3ptbr-1.2.0.tar.gz

---
+++ packages/python3ptbr/python3ptbr-1.2.0.tar.gz/python3ptbr-1.2.0/python3ptbr/__main__.py
@@ -0,0 +1,11 @@
+import re
+import sys
+import time
+import random
+
+import python3ptbr
+
+class Help:
+    help = "este programa traduz os comandos do python para portuques"
+    def __init__(self):
+        return help
