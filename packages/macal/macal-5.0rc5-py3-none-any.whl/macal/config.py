9b0eaf21c27f36949fed7f9e5b52346e906c91d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:05:42 +0000

    Add package macal/5.0rc5/macal-5.0rc5-py3-none-any.whl

---
+++ packages/macal/macal-5.0rc5-py3-none-any.whl/macal/config.py
@@ -0,0 +1,15 @@
+#
+# Product:   Macal
+# Author:    Marco Caspers
+# Email:     SamaDevTeam@westcon.com
+# License:   MIT License
+# Date:      24-10-2023
+#
+# Copyright 2023 Westcon-Comstor
+#
+
+# Configuration module
+
+from typing import List
+
+SearchPath: List[str] = []
