c9cc7fd1ef82991bc328582d068341bb771406de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 03:10:29 +0000

    Add package ad-map-access/2.6.3/ad_map_access-2.6.3-cp38-cp38-manylinux_2_27_x86_64.whl

---
+++ packages/ad-map-access/ad_map_access-2.6.3-cp38-cp38-manylinux_2_27_x86_64.whl/ad_map_access/__init__.py
@@ -0,0 +1,18 @@
+# ----------------- BEGIN LICENSE BLOCK ---------------------------------
+#
+# Copyright (c) 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+# ----------------- END LICENSE BLOCK -----------------------------------
+
+"""
+Import ad_map_access module for python38
+"""
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__))
+
+from ad_physics import *
+from ad_map_access_python38 import *
