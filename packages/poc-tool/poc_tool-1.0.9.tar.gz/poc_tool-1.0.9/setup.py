bef3614043ccd147f7b5456497b5fa5fe69d4113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 15:05:09 +0000

    Add package poc-tool/1.0.9/poc_tool-1.0.9.tar.gz

---
+++ packages/poc-tool/poc_tool-1.0.9.tar.gz/poc_tool-1.0.9/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="poc_tool",
+    license='MIT',
+    version="1.0.9",
+    description="Python Poc 还原原始http请求数据包以及常用工具集成化封装",
+    author="zhizhuo",
+    author_email="zhizhuoshuma@163.com",
+    url='https://github.com/zhizhuoshuma/poc_tool',
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    python_requires=">=3.8",
+)
