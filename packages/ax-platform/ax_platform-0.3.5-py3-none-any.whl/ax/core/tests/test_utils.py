52c75be3f50b59f78157811a6a5b43e12444179a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:05:16 +0000

    Add package ax-platform/0.3.5/ax_platform-0.3.5-py3-none-any.whl

---
+++ packages/ax-platform/ax_platform-0.3.5-py3-none-any.whl/ax/core/tests/test_utils.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import numpy as np
+import pandas as pd
+from ax.core.data import Data
+from ax.core.metric import Metric
+from ax.core.objective import MultiObjective, Objective
+from ax.core.optimization_config import (
+    MultiObjectiveOptimizationConfig,
+    OptimizationConfig,
+)
+from ax.core.outcome_constraint import ObjectiveThreshold, OutcomeConstraint
+from ax.core.types import ComparisonOp
+from ax.core.utils import (
+    best_feasible_objective,
+    get_missing_metrics,
+    get_missing_metrics_by_name,
+    get_model_times,
+    get_model_trace_of_times,
+    MissingMetrics,
+)
+from ax.modelbridge.modelbridge_utils import feasible_hypervolume
+from ax.utils.common.testutils import TestCase
+from ax.utils.testing.core_stubs import get_robust_branin_experiment
+from pyre_extensions import none_throws
+
+
+class UtilsTest(TestCase):
+    def setUp(self) -> None:
+        self.df = pd.DataFrame(
+            [
+                {
+                    "arm_name": "0_0",
+                    "mean": 2.0,
+                    "sem": 0.2,
+                    "trial_index": 1,
+                    "metric_name": "a",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_0",
+                    "mean": 1.8,
+                    "sem": 0.3,
+                    "trial_index": 1,
+                    "metric_name": "b",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_1",
+                    "mean": float("nan"),
+                    "sem": float("nan"),
+                    "trial_index": 1,
+                    "metric_name": "a",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_1",
+                    "mean": 3.7,
+                    "sem": 0.5,
+                    "trial_index": 1,
+                    "metric_name": "b",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_2",
+                    "mean": 0.5,
+                    "sem": None,
+                    "trial_index": 1,
+                    "metric_name": "a",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_2",
+                    "mean": float("nan"),
+                    "sem": float("nan"),
+                    "trial_index": 1,
+                    "metric_name": "b",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+                {
+                    "arm_name": "0_2",
+                    "mean": float("nan"),
+                    "sem": float("nan"),
+                    "trial_index": 1,
+                    "metric_name": "c",
+                    "start_time": "2018-01-01",
+                    "end_time": "2018-01-02",
+                },
+            ]
+        )
+
+        self.data = Data(df=self.df)
+
+        self.optimization_config = OptimizationConfig(
+            objective=Objective(metric=Metric(name="a")),
+            outcome_constraints=[
+                OutcomeConstraint(
+                    metric=Metric(name="b"),
+                    op=ComparisonOp.GEQ,
+                    bound=0,
+                    relative=False,
+                )
+            ],
+        )
+
+    def test_get_missing_metrics_by_name(self) -> None:
+        expected = {"a": {("0_1", 1)}, "b": {("0_2", 1)}}
+        actual = get_missing_metrics_by_name(self.data, ["a", "b"])
+        self.assertEqual(actual, expected)
+
+    def test_get_missing_metrics(self) -> None:
+        expected = MissingMetrics(
+            {"a": {("0_1", 1)}},
+            {"b": {("0_2", 1)}},
+            {"c": {("0_0", 1), ("0_1", 1), ("0_2", 1)}},
+        )
+        actual = get_missing_metrics(self.data, self.optimization_config)
+        self.assertEqual(actual, expected)
+
+    def test_best_feasible_objective(self) -> None:
+        bfo = best_feasible_objective(
+            self.optimization_config,
+            values={"a": np.array([1.0, 3.0, 2.0]), "b": np.array([0.0, -1.0, 0.0])},
+        )
+        self.assertEqual(list(bfo), [1.0, 1.0, 2.0])
+
+    def test_feasible_hypervolume(self) -> None:
+        ma = Metric(name="a", lower_is_better=False)
+        mb = Metric(name="b", lower_is_better=True)
+        mc = Metric(name="c", lower_is_better=False)
+        optimization_config = MultiObjectiveOptimizationConfig(
+            objective=MultiObjective(metrics=[ma, mb]),
+            outcome_constraints=[
+                OutcomeConstraint(
+                    mc,
+                    op=ComparisonOp.GEQ,
+                    bound=0,
+                    relative=False,
+                )
+            ],
+            objective_thresholds=[
+                ObjectiveThreshold(
+                    ma,
+                    bound=1.0,
+                ),
+                ObjectiveThreshold(
+                    mb,
+                    bound=1.0,
+                ),
+            ],
+        )
+        feas_hv = feasible_hypervolume(
+            optimization_config,
+            values={
+                "a": np.array(
+                    [
+                        1.0,
+                        3.0,
+                        2.0,
+                        2.0,
+                    ]
+                ),
+                "b": np.array(
+                    [
+                        0.0,
+                        1.0,
+                        0.0,
+                        0.0,
+                    ]
+                ),
+                "c": np.array(
+                    [
+                        0.0,
+                        -0.0,
+                        1.0,
+                        -2.0,
+                    ]
+                ),
+            },
+        )
+        self.assertEqual(list(feas_hv), [0.0, 0.0, 1.0, 1.0])
+
+    def test_get_model_times(self) -> None:
+        exp = get_robust_branin_experiment(num_sobol_trials=2)
+        fit_times, gen_times = get_model_trace_of_times(exp)
+        total_fit_time, total_gen_time = get_model_times(exp)
+        fit_times_not_none = [none_throws(elt) for elt in fit_times]
+        gen_times_not_none = [none_throws(elt) for elt in gen_times]
+        self.assertTrue(all(elt >= 0 for elt in fit_times_not_none))
+        self.assertTrue(all(elt >= 0 for elt in gen_times_not_none))
+        self.assertEqual(sum(fit_times_not_none), total_fit_time)
+        self.assertEqual(sum(gen_times_not_none), total_gen_time)
